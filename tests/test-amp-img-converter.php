0c965604ac472bcb5c0b2ef72904882e69c753d3
Mohammad Jangda <batmoo@gmail.com>
2015-09-30 04:05:59 -0400

    Add tests for multiple images in a string

--- tests/test-amp-img-converter.php
+++ tests/test-amp-img-converter.php
@@ -40,5 +40,39 @@ class AMP_Img_Converter_Test extends WP_UnitTestCase {
 		$converted = $this->converter->convert( $content );
 		$this->assertEquals( $expected, $converted );
 	}
+
+	function test_multiple_same_image() {
+		$content = '
+<img src="http://placehold.it/350x150" />
+<img src="http://placehold.it/350x150" />
+<img src="http://placehold.it/350x150" />
+<img src="http://placehold.it/350x150" />
+			';
+		$expected = '
+<amp-img src="http://placehold.it/350x150"></amp-img>
+<amp-img src="http://placehold.it/350x150"></amp-img>
+<amp-img src="http://placehold.it/350x150"></amp-img>
+<amp-img src="http://placehold.it/350x150"></amp-img>
+			';
+		$converted = $this->converter->convert( $content );
+		$this->assertEquals( $expected, $converted );
+	}
+
+	function test_multiple_different_images() {
+		$content = '
+<img src="http://placehold.it/350x150" />
+<img src="http://placehold.it/360x160" />
+<img src="http://placehold.it/370x170" />
+<img src="http://placehold.it/380x180" />
+			';
+		$expected = '
+<amp-img src="http://placehold.it/350x150"></amp-img>
+<amp-img src="http://placehold.it/360x160"></amp-img>
+<amp-img src="http://placehold.it/370x170"></amp-img>
+<amp-img src="http://placehold.it/380x180"></amp-img>
+			';
+		$converted = $this->converter->convert( $content );
+		$this->assertEquals( $expected, $converted );
+	}
 }
 
