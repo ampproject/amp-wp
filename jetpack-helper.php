3760abe160acb20ca1a50854a4e8fff4a4455d1a
Jeremy Herve <jeremy@tagada.hu>
2015-10-08 14:58:31 +0000

    Jetpack: remove Related Posts' placeholder.
    Related Posts are not outputted on AMP pages.
    No use to display a "Related" headline at the end of the posts.

--- jetpack-helper.php
+++ jetpack-helper.php
@@ -3,6 +3,7 @@
 // Jetpack bits.
 
 add_action( 'pre_amp_render', 'amp_jetpack_disable_photon' );
+add_action( 'pre_amp_render', 'amp_jetpack_disable_related_posts' );
 
 /**
  * Disables Photon for all images.
@@ -13,4 +14,17 @@ add_action( 'pre_amp_render', 'amp_jetpack_disable_photon' );
  **/
 function amp_jetpack_disable_photon() {
 	add_filter( 'jetpack_photon_skip_image', '__return_true' );
+}
+
+/**
+ * Remove the Related Posts placeholder and headline that gets hooked into the_content
+ *
+ * That placeholder is useless since we can't ouput, and don't want to output Related Posts in AMP.
+ *
+ **/
+function amp_jetpack_disable_related_posts() {
+	if ( class_exists( 'Jetpack_RelatedPosts' ) ) {
+		$jprp = Jetpack_RelatedPosts::init();
+		remove_filter( 'the_content', array( $jprp, 'filter_add_target_to_dom' ), 40 );
+	}
 }
