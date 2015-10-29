5092e0d02a6b635be187ba2008ebb8bafb3b6414
Mohammad Jangda <batmoo@gmail.com>
2015-10-28 22:50:09 -0400

    Refactor conversion methods
    Use just one generic one instead of ones for each converter

--- class-amp-content.php
+++ class-amp-content.php
@@ -24,10 +24,21 @@ class AMP_Content {
 
 		// Convert HTML to AMP
 		// see https://github.com/ampproject/amphtml/blob/master/spec/amp-html-format.md#html-tags)
-		$content = $this->convert_images( $content );
-		$content = $this->convert_videos( $content );
-		$content = $this->convert_audios( $content );
-		$content = $this->convert_iframes( $content );
+		$content = $this->convert( new AMP_Img_Converter( $content ), array(
+			'layout' => 'responsive',
+		) );
+
+		$content = $this->convert( new AMP_Video_Converter( $content ), array(
+			'layout' => 'responsive',
+		) );
+
+		$content = $this->convert( new AMP_Audio_Converter( $content ), array(
+			'layout' => 'responsive',
+		) );
+
+		$content = $this->convert( new AMP_Iframe_Converter( $content ), array(
+			'layout' => 'responsive',
+		) );
 
 		return $content;
 	}
@@ -40,43 +51,9 @@ class AMP_Content {
 		return $this->scripts;
 	}
 
-	private function convert_images( $content ) {
-		$converter = new AMP_Img_Converter( $content );
-		$converted = $converter->convert( array(
-			'layout' => 'responsive',
-		) );
+	private function convert( $converter, $attributes ) {
+		$converted = $converter->convert( $attributes );
 		$this->add_scripts( $converter->get_scripts() );
-
-		return $converted;
-	}
-
-	private function convert_videos( $content ) {
-		$converter = new AMP_Video_Converter( $content );
-		$converted = $converter->convert( array(
-			'layout' => 'responsive',
-		) );
-		$this->add_scripts( $converter->get_scripts() );
-
-		return $converted;
-	}
-
-	private function convert_audios( $content ) {
-		$converter = new AMP_Audio_Converter( $content );
-		$converted = $converter->convert( array(
-			'layout' => 'responsive',
-		) );
-		$this->add_scripts( $converter->get_scripts() );
-
-		return $converted;
-	}
-
-	private function convert_iframes( $content ) {
-		$converter = new AMP_Iframe_Converter( $content );
-		$converted = $converter->convert( array(
-			'layout' => 'responsive',
-		) );
-		$this->add_scripts( $converter->get_scripts() );
-
 		return $converted;
 	}
 }
