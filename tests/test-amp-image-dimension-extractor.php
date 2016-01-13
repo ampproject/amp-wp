d0f1bea749a1f023daabf8fe69b991c3aeec0ac8
Mohammad Jangda <batmoo@gmail.com>
2016-01-12 19:42:13 -0500

    Add tests for extract_from_attachment_metadata

--- tests/test-amp-image-dimension-extractor.php
+++ tests/test-amp-image-dimension-extractor.php
@@ -34,3 +34,51 @@ class AMP_Image_Dimension_Extractor__From_Filename__Test extends WP_UnitTestCase
 		$this->assertEquals( $expected, $dimensions );
 	}
 }
+
+class AMP_Image_Dimension_Extractor__From_Metadata__Test extends WP_UnitTestCase {
+	private $_attachment_id = null;
+
+	function setUp() {
+		parent::setUp();
+
+		$file_path = dirname( __FILE__ ) . '/assets/wordpress-logo.png';
+		$this->_attachment_id = $this->factory->attachment->create_upload_object( $file_path );
+	}
+
+	function test__invalid_attachment() {
+		$source = 'https://example.com/path/to/file.jpg';
+		$expected = false;
+
+		$dimensions = AMP_Image_Dimension_Extractor::extract_from_attachment_metadata(  $source );
+
+		$this->assertEquals( $expected, $dimensions );
+	}
+
+	function test__valid_attachment_missing_metadata() {
+		$source = wp_get_attachment_url( $this->_attachment_id );
+		$expected = false;
+
+		delete_post_meta( $this->_attachment_id, '_wp_attachment_metadata' );
+		$dimensions = AMP_Image_Dimension_Extractor::extract_from_attachment_metadata( $source );
+
+		$this->assertEquals( $expected, $dimensions );
+	}
+
+	function test__valid_attachment_and_dimensions_with_query() {
+		$source = wp_get_attachment_url( $this->_attachment_id ) . '?rand=1';
+		$expected = array( 498, 113 );
+
+		$dimensions = AMP_Image_Dimension_Extractor::extract_from_attachment_metadata( $source );
+
+		$this->assertEquals( $expected, $dimensions );
+	}
+
+	function test__valid_attachment_and_dimensions() {
+		$source = wp_get_attachment_url( $this->_attachment_id );
+		$expected = array( 498, 113 );
+
+		$dimensions = AMP_Image_Dimension_Extractor::extract_from_attachment_metadata( $source );
+
+		$this->assertEquals( $expected, $dimensions );
+	}
+}
