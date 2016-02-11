700dbf76de6ea5ce4af56bccd5211021096c5185
DrewAPicture <drew@10up.com>
2016-02-11 13:57:44 -0700

    First run: AMP template editor in the Customizer.
    Introduces `AMP_Template_Customizer` class, which adds a new panel, section, settings, and controls for manipulating AMP templates in the Customizer.
    
    Also adds a JS file to assets for Customizer postMessage handling (WIP), an amp-backend-actions.php file for loading the Customizer stuff and temporarily outputting an access link for testing purposes.
    
    Finally, modifies the AMP loader to also work for Customizer previews.

--- amp.php
+++ amp.php
@@ -35,6 +35,7 @@ function amp_init() {
 	}
 
 	define( 'AMP_QUERY_VAR', apply_filters( 'amp_query_var', 'amp' ) );
+	amp_add_backend_actions();
 
 	do_action( 'amp_init' );
 
@@ -61,7 +62,11 @@ function amp_force_query_var_value( $query_vars ) {
 }
 
 function amp_maybe_add_actions() {
+<<<<<<< f252d9d056a7f99e76c5a7c43627a8e2e5343976
 	if ( ! is_singular() || is_feed() ) {
+=======
+	if ( ! is_singular() || ! is_customize_preview() ) {
+>>>>>>> First run: AMP template editor in the Customizer.
 		return;
 	}
 
@@ -94,6 +99,15 @@ function amp_load_classes() {
 
 function amp_add_frontend_actions() {
 	require_once( AMP__DIR__ . '/includes/amp-frontend-actions.php' );
+}
+
+/**
+ * Loads the backend actions callbacks.
+ *
+ * @since 0.3
+ */
+function amp_add_backend_actions() {
+	require_once( AMP__DIR__ . '/includes/admin/amp-backend-actions.php' );
 }
 
 function amp_add_post_template_actions() {
---
+++ includes/admin/class-amp-customizer.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * AMP class that implements a template style editor in the Customizer.
+ */
+class AMP_Template_Customizer {
+
+	/**
+	 * Customizer instance.
+	 *
+	 * @access public
+	 * @var WP_Customize_Manager $wp_customizer
+	 */
+	public $wp_customizer;
+
+	/**
+	 * Constructor.
+	 *
+	 * @access public
+	 *
+	 * @param WP_Customize_Manager $wp_customize Customizer instance.
+	 */
+	public function __construct( $wp_customize ) {
+		$this->wp_customize = $wp_customize;
+
+		$this->register_panel();
+		$this->register_sections();
+		$this->register_settings();
+		$this->register_controls();
+		$this->enqueue_scripts();
+	}
+
+	/**
+	 * Registers the AMP Template panel.
+	 *
+	 * @access public
+	 */
+	public function register_panel() {
+		// AMP Templates.
+		$this->wp_customize->add_panel( 'amp_template_editor', array(
+			'type'   => 'amp',
+			'title' => __( 'AMP Templates', 'amp' )
+		) );
+	}
+
+	/**
+	 * Registers the AMP Template panel sections.
+	 *
+	 * @access public
+	 */
+	public function register_sections() {
+
+		// Navigation Bar.
+		$this->wp_customize->add_section( 'amp_navbar_section', array(
+			'title' => __( 'AMP Navigation Bar', 'amp' ),
+			'panel' => 'amp_template_editor',
+		) );
+	}
+
+	/**
+	 * Registers settings for customizing AMP templates.
+	 *
+	 * @access public
+	 */
+	public function register_settings() {
+		// Background Color setting (Navbar).
+		$this->wp_customize->add_setting( 'amp_navbar_background', array(
+			'default'           => '#0a89c0',
+			'sanitize_callback' => 'sanitize_hex_color',
+			'transport'         => 'postMessage'
+		) );
+	}
+
+	/**
+	 * Registers controls for customizing AMP templates.
+	 *
+	 * @access public
+	 */
+	public function register_controls() {
+		// Background Color control (Navbar).
+		$this->wp_customize->add_control( new WP_Customize_Color_Control( $this->wp_customize, 'amp_navbar_background', array(
+			'label'       => __( 'Background Color', 'amp' ),
+			'description' => __( 'This color is used by AMP to format the background of the navigation bar on pages generated for AMP.', 'amp' ),
+			'section'     => 'amp_navbar_section'
+		) ) );
+	}
+
+	/**
+	 * Enqueues scripts used in the Customizer.
+	 *
+	 * @access public
+	 */
+	public function enqueue_scripts() {
+		if ( is_customize_preview() ) {
+			$url = AMP__URL__ . '/assets/js/amp-customizer.js';
+			log_it( $url );
+			wp_enqueue_script( 'amp-customizer', $url, array( 'customize-controls' ) );
+		}
+	}
+}
