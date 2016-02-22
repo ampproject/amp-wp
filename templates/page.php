1921110b222153d04dc419903d3c8d79319654dc
Aaron Brazell <Aaron.brazell@10up.com>
2016-02-22 17:59:51 +0000

    Remove boilerplate tags

--- templates/page.php
+++ templates/page.php
@@ -5,7 +5,6 @@
 	<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,minimal-ui">
 	<link href="https://fonts.googleapis.com/css?family=Merriweather:400,400italic,700,700italic|Open+Sans:400,700,400italic,700italic" rel="stylesheet" type="text/css">
 	<?php do_action( 'amp_post_template_head', $this ); ?>
-	<style>body {opacity: 0}</style><noscript><style>body {opacity: 1}</style></noscript>
 
 	<style amp-custom>
 		<?php $this->load_parts( array( 'style' ) ); ?>
