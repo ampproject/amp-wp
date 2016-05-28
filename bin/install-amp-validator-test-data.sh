f6f0ed2148ef5e18df539451456854402d2e46ba
Chris Smith <chris@teamzen.io>
2016-05-27 21:30:19 -0600

    add our shell script to download and install our test data.  This places the wptest.xml in our plugin folder which we need if we are going to run our validator script later.

---
+++ bin/install-amp-validator-test-data.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#
+# WP-AMP Validator Tests Script
+#
+# Note: This script assumes you have the following installed:
+#  - wp-cli
+#  - amp-validator (npm install -g node-gyp && npm install -g amp-validator)
+#
+#####################################################################################
+
+# Check if our test data exists. If it already does not, we will get it from github.
+if ! [ -f "wptest.xml" ]
+then
+
+	# Get WP Test data.
+	curl -OL https://raw.githubusercontent.com/manovotny/wptest/master/wptest.xml
+
+fi
+
+printf "Do want to install the Test data or have you already installed it? 'Y' or 'N': "
+read INSTALL
+
+if [ 'Y' = "$INSTALL" ] ; then
+
+    wp plugin is-installed wordpress-importer
+    INSTALLED=$?
+    echo $[INSTALLED]
+
+    if [ $[INSTALLED] ] ; then
+
+        printf "Installing and Activating the WordPress importer plugin to handle our data import"
+        wp plugin install wordpress-importer --activate
+    fi
+
+    wp import wptest.xml --authors=create
+
+fi
