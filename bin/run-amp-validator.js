9a9e57ac74a187ca73551874574849457391f59e
Chris Smith <chris@teamzen.io>
2016-05-29 20:43:15 -0600

    add timeout to the exec() function so that we space our validations far enough apart and don't overload the server/node is syncronous calls. Remove unnecessary packages in favor of the amphtml validator version for now.

--- bin/run-amp-validator.js
+++ bin/run-amp-validator.js
@@ -5,11 +5,23 @@
  * from the plugin root dir on your server run 'node bin/run-amp-validator.js' to run
  */
 
-// var util = require('util'),
-var exec = require('child_process').exec;
-function puts(error, stdout, stderr) {
-    console.log(stdout);
+'use strict';
 
+function puts(error, stdout, stderr) {
+
+    if ( error ) {
+        console.log("Error:\r");
+        console.log(error);
+        return;
+    }
+
+    if ( stderr ) {
+        console.log("stderr:\r");
+        console.log(stderr);
+        return;
+    }
+    console.log( stdout );
+    //
     // var result = JSON.parse(stdout);
     //
     // for ( var key in result ) {
@@ -18,83 +30,79 @@ function puts(error, stdout, stderr) {
     //
     //     if ( ! result.success ) {
     //
-    //         console.log(result.errors);
+    //         console.log( "Errors for :"+key+"\n"+result.errors );
     //
     //     } else {
     //
-    //         console.log("Success: "+result.success);
+    //         console.log( "Successfully Validated: "+key );
     //
     //     }
     //
     // }
 }
 
-exec('amp-validator http://auto-amp.dev/2013/01/01/pingbacks-an-trackbacks/amp/' , puts);
-
 /**
  * This parses our XML to gather the links to our posts so we can test them.
- * TODO: When run on all URLs we are getting resource errors from Node.  Need to investigate and possibly break the validation functions into smaller chunks and prevent concurrence.
+ * TODO: Better output and percent completed?
  *
  * We also might be able to utilize the BETA Node API listed here: https://github.com/ampproject/amphtml/tree/master/validator
+ * could not get it to recognize the amp-validator require and ran into url.StartWith not a function errors.
  *
  * TODO: ask the user for their WP base URL
  */
 
-// var amp = require('amp-validator');
-//
-// var returnJSONResults = function(baseName, queryName) {
-//     var XMLPath = "wptest.xml";
-//     var rawJSON = loadXMLDoc(XMLPath);
-//     function loadXMLDoc(filePath) {
-//         var fs = require('fs');
-//         var xml2js = require('xml2js');
-//         var json;
-//         try {
-//             var fileData = fs.readFileSync(filePath, 'ascii');
-//
-//             var parser = new xml2js.Parser();
-//             parser.parseString(fileData.substring(0, fileData.length), function (err, result) {
-//
-//                 var wptestBaseURL = result.rss.channel[0]['wp:base_site_url'],
-//                     items = result.rss.channel[0].item;
-//                 console.log(items.length);
-//
-//                 // console.log(channel[0]);
-//                 // console.log( wptestBaseURL );
-//                 // console.log( items );
-//                 var postCount = 0;
-//                 var postURLs = '';
-//                 for (var i=0 , len = items.length; i< len; i++ ) {
-//                     var item = items[i],
-//                         localBaseURL = 'http://auto-amp.dev',
-//                         postType = item['wp:post_type'][0],
-//                         postStatus = item['wp:status'][0];
-//
-//                     if ( 'post' === postType && 'publish' === postStatus ) {
-//                         postCount++;
-//                         var postDate = new Date( item['wp:post_date'] ),
-//                             link = item.link[0],
-//                             postMonth = ("0" + ( postDate.getMonth()+1 ) ).slice(-2),
-//                             postDay = ("0" + postDate.getDate()).slice(-2),
-//                             localURL = localBaseURL+"/"+postDate.getFullYear()+"/"+postMonth+"/"+postDay;
-//
-//                         link = link.replace(wptestBaseURL, localURL)+"amp/";
-//                         // console.log( link );
-//
-//                         // var cmd = "amp-validator "+link+" -o json";
-//                         var cmd = "amp-validator "+link;
-//                         // console.log( cmd );
-//                         // amp.validate( link );
-//                         postURLs += " "+link;
-//                         // exec(cmd , puts);
-//                     }
-//                 }
-//                 console.log(postCount);
-//                 // console.log(postURLs);
-//             });
-//
-//             // console.log("File '" + filePath + "/ was successfully read.\n");
-//             // return json;
-//         } catch (ex) {console.log(ex)}
-//     }
-// }();
+function loadXMLDoc(filePath) {
+    var fs = require('fs');
+    var xml2js = require('xml2js');
+    var urls = [];
+    try {
+        var fileData = fs.readFileSync(filePath, 'ascii');
+
+        var parser = new xml2js.Parser();
+        parser.parseString(fileData.substring(0, fileData.length), function (err, result) {
+
+            var wptestBaseURL = result.rss.channel[0]['wp:base_site_url'],
+                items = result.rss.channel[0].item;
+
+            var postCount = 0;
+
+            for (var i=0 , len = items.length; i< len; i++ ) {
+                var item = items[i],
+                    localBaseURL = 'http://auto-amp.dev',
+                    postType = item['wp:post_type'][0],
+                    postStatus = item['wp:status'][0],
+                    postPassword = item['wp:post_password'][0];
+
+                if ( 'post' === postType && 'publish' === postStatus && '' === postPassword ) {
+                    postCount++;
+                    var postDate = new Date( item['wp:post_date'] ),
+                        link = item.link[0],
+                        postMonth = ("0" + ( postDate.getMonth()+1 ) ).slice(-2),
+                        postDay = ("0" + postDate.getDate()).slice(-2),
+                        localURL = localBaseURL+"/"+postDate.getFullYear()+"/"+postMonth+"/"+postDay;
+
+                    link = link.replace(wptestBaseURL, localURL)+"amp/";
+
+                    urls.push( link );
+
+                }
+            }
+            console.log("Hang tight, we are going to test "+postCount+" urls...");
+        });
+
+        return urls;
+    } catch (ex) {console.log(ex)}
+}
+
+var exec = require('child_process').exec;
+
+var XMLPath = "wptest.xml";
+var testURLs = loadXMLDoc(XMLPath);
+
+for (var i = 0, len = testURLs.length; i < len; i++) {
+    var url = testURLs[i];
+    var cmd = 'amp-validator ' + url;
+    setTimeout(function(cmd) {
+        exec(cmd, puts);
+    }, i*2000,cmd);
+}
