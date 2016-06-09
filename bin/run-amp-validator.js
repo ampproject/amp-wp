d0716fe5646eaeea23c5ef69f0ec1e984f3bd62e
Chris Smith <chris@teamzen.io>
2016-06-08 22:06:46 -0600

    replace our amp-html readFromUrl and readFromReadable functions with the much simpler fetch package.  Now we can use the main branch of ampproject/amphtml in our package.json.

--- bin/run-amp-validator.js
+++ bin/run-amp-validator.js
@@ -8,16 +8,13 @@
 'use strict';
 
 const prompt = require('prompt'),
-    url = require('url'),
     Promise = require('bluebird'),
     ampValidator = require('amp-html/validator'),
-    http = require('http');
+    fetch = require('node-fetch');
 
+//exec = require('child-process').exec()
 /**
  * This parses our XML to gather the links to our posts so we can test them.
- *
- * We also might be able to utilize the BETA Node API listed here: https://github.com/ampproject/amphtml/tree/master/validator
- * could not get it to recognize the amp-validator require and ran into url.StartWith not a function errors.
  *
  */
 
@@ -59,6 +56,8 @@ function loadXMLDoc(filePath, localBaseURL) {
 
         });
         //Control URLs for Testing purposes
+        urls = [];
+        urls.push( localBaseURL+'/wp-content/plugins/amp-wp/bin/doesnotexist.html' );
         urls.push( localBaseURL+'/wp-content/plugins/amp-wp/bin/failure.html' );
         urls.push( localBaseURL+'/wp-content/plugins/amp-wp/bin/success.html' );
         return urls;
@@ -79,46 +78,6 @@ var promiseWhile = function(condition, action) {
 
     return resolver.promise;
 };
-
-function readFromReadable(name, readable) {
-    return new Promise(function(resolve, reject) {
-        const chunks = [];
-        readable.setEncoding('utf8');
-        readable.on('data', (chunk) => { chunks.push(chunk); });
-        readable.on('end', () => { resolve(chunks.join('')); });
-        readable.on('error', (error) => {
-            reject(new Error('Could not read from ' + name + ' - ' + error.message));
-        });
-    });
-}
-
-function readFromUrl(url) {
-    return new Promise(function(resolve, reject) {
-        const clientModule = url.startsWith('http://') ? http : https;
-        const req = clientModule.request(url, (response) => {
-            if (response.statusCode !== 200) {
-            // https://nodejs.org/api/http.html says: "[...] However, if
-            // you add a 'response' event handler, then you must consume
-            // the data from the response object, either by calling
-            // response.read() whenever there is a 'readable' event, or by
-            // adding a 'data' handler, or by calling the .resume()
-            // method."
-            response.resume();
-            reject(new Error(
-                'Unable to fetch ' + url + ' - HTTP Status ' +
-                response.statusCode));
-            } else {
-                resolve(response);
-            }
-        });
-        req.on('error', (error) => {  // E.g., DNS resolution errors.
-            reject(
-            new Error('Unable to fetch ' + url + ' - ' + error.message));
-        });
-        req.end();
-    })
-    .then(readFromReadable.bind(null, url));
-}
 
 //This is the data schema to gather the user's local URL and make sure it is the right format.
 var promptSchema = {
@@ -159,22 +118,34 @@ prompt.get(promptSchema, function( err, result) {
     }, function() {
 
         return new Promise( function( resolve, reject ) {
-            readFromUrl( testUrls[i] )
-                .then( function( response ) {
-                    return ourInstance.then( function(validator) {
-                        const result = validator.validateString(response);
-                        ((result.status === 'PASS') ? console.log : console.error)((i+1)+": " + testUrls[i] + " returned: " + result.status);
+            fetch( testUrls[i] )
+                .then( function( res ) {
+                    if ( res.ok ) {
+                        return res.text();
+                    } else {
+                        var response = (i + 1) + ': Unable to fetch ' + testUrls[i] + ' - HTTP Status ' + res.status + ' - ' + res.statusText;
+                        console.log(response);
+                    }
+                }).then(function(body) {
+                    if ( body ) {
+                        return ourInstance.then(function (validator) {
+                            const result = validator.validateString(body);
+                            ((result.status === 'PASS') ? console.log : console.error)((i + 1) + ": " + testUrls[i] + " returned: " + result.status);
 
-                        for (const error of result.errors) {
-                            let msg = 'line ' + error.line + ', col ' + error.col + ': ' + error.message;
-                            if (error.specUrl !== null) {
-                                msg += ' (see ' + error.specUrl + ')';
+                            for (const error of result.errors) {
+                                let msg = 'line ' + error.line + ', col ' + error.col + ': ' + error.message;
+                                if (error.specUrl !== null) {
+                                    msg += ' (see ' + error.specUrl + ')';
+                                }
+                                ((error.severity === 'ERROR') ? console.error : console.warn)(msg);
                             }
-                            ((error.severity === 'ERROR') ? console.error : console.warn)(msg);
-                        }
+                            i++;
+                            resolve();
+                        });
+                    } else {
                         i++;
                         resolve();
-                    });
+                    }
 
                 });
 
