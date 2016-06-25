329518d66e3f49ec813caf196da47390e9fe1297
Chris Smith <chris@teamzen.io>
2016-06-25 13:43:27 -0600

    Remove specifying node version using `node_js` in travis as it did not work
    Remove parsing our XML file in favor of using WP-CLI to gather our post URLs
    
    Add URL npm package so we can Parse URLs

--- bin/run-amp-validator.js
+++ bin/run-amp-validator.js
@@ -11,62 +11,14 @@ const Promise = require('bluebird'),
     ampValidator = require('amp-html/validator'),
     fetch = require('node-fetch'),
     exec = require('child_process').exec,
-    colors = require('colors');
+    colors = require('colors'),
+    url = require('url');
 
 colors.setTheme({
     info: 'green',
     debug: 'blue',
     error: 'red'
 });
-
-/**
- * This parses our XML to gather the links to our posts so we can test them.
- *
- */
-
-function loadXMLDoc(filePath, localBaseURL) {
-    var fs = require('fs');
-    var xml2js = require('xml2js');
-    var urls = [];
-    try {
-        var fileData = fs.readFileSync(filePath, 'ascii');
-
-        var parser = new xml2js.Parser();
-        parser.parseString(fileData.substring(0, fileData.length), function (err, result) {
-
-            var wptestBaseURL = result.rss.channel[0]['wp:base_site_url'],
-                items = result.rss.channel[0].item;
-
-            var postCount = 0;
-
-            for (var i=0 , len = items.length; i< len; i++ ) {
-                var item = items[i],
-                    postType = item['wp:post_type'][0],
-                    postStatus = item['wp:status'][0],
-                    postPassword = item['wp:post_password'][0];
-
-                if ( 'post' === postType && 'publish' === postStatus && '' === postPassword ) {
-                    postCount++;
-                    var postDate = new Date( item['wp:post_date'] ),
-                        link = item.link[0],
-                        postMonth = ("0" + ( postDate.getMonth()+1 ) ).slice(-2),
-                        postDay = ("0" + postDate.getDate()).slice(-2),
-                        localURL = localBaseURL+"/"+postDate.getFullYear()+"/"+postMonth+"/"+postDay;
-
-                    link = link.replace(wptestBaseURL, localURL)+"amp/";
-
-                    urls.push( link );
-
-                }
-            }
-
-        });
-        //Control URLs for Testing purposes
-        urls.push( localBaseURL+'/wp-content/plugins/amp-wp/bin/failure.html' );
-        urls.push( localBaseURL+'/wp-content/plugins/amp-wp/bin/success.html' );
-        return urls;
-    } catch (ex) {console.log(ex)}
-}
 
 var promiseWhile = function(condition, action) {
     var resolver = Promise.defer();
@@ -84,17 +36,28 @@ var promiseWhile = function(condition, action) {
 };
 
 
-//At this point, our user will be prompted for their local install URL
-exec('wp option get siteurl --quiet --skip-plugins=wordpress-importer', (error, stdout, stderr) => {
+//run a WP-CLI command to collect our installs post URLS and test them
+exec('wp post list --post_type=post --posts_per_page=-1 --post_status=publish --post_password="" --format=json --fields=url --quiet --skip-plugins=wordpress-importer', function(error, stdout, stderr) {
     if (error) {
-        console.error(`exec error: ${error}`);
+        console.error('exec error: '+error);
         return;
     }
-    var localUrl = stdout.replace(/(\r\n|\n|\r)/gm,"");
 
-    //Get our XML path and parse the document to gather our post URLs
-    var XMLPath = "wptest.xml",
-        testUrls = loadXMLDoc(XMLPath, localUrl);
+    var testUrls = [];
+    var items = JSON.parse(stdout.trim());
+
+    for (var i=0 , len = items.length; i < len; i++ ) {
+        var item = items[i];
+
+        testUrls.push( item['url']+"amp/" );
+
+    }
+
+    //Control URLs for Testing purposes
+    var localBaseURL = url.parse(testUrls[0]);
+    localBaseURL = localBaseURL.protocol + "//" + localBaseURL.hostname;
+    testUrls.push( localBaseURL+'/wp-content/plugins/amp-wp/bin/failure.html' );
+    testUrls.push( localBaseURL+'/wp-content/plugins/amp-wp/bin/success.html' );
 
     var i = 0,
         len = testUrls.length - 1;
