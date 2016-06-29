f91899822525639b6d5b749258951ca546b721a4
Chris Smith <chris@teamzen.io>
2016-06-28 20:36:39 -0600

    no .htaccess file after doing the WP-CLI command.  Lets add a local config file to bin and then during the install we'll move it to the wp root

--- bin/install-amp-validator-test-data.sh
+++ bin/install-amp-validator-test-data.sh
@@ -20,43 +20,44 @@ fi
 
 if [ "${TRAVIS}" = "true" ]; then
 
+    #we are going to setup a wp install, so start from root not amp-wp
     cd ..
-    #/tmp/wordpress/
-
     mkdir wp
     cd wp/
 
+    #install WP-CLI
     curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-
     chmod +x wp-cli.phar
     sudo mv wp-cli.phar /usr/local/bin/wp
 
+    #Use WP-CLI to download, configure and install WP
     wp core download --version=${WP_VERSION}
-
     wp core config --dbname=wordpress_test --dbuser=root
-
     wp core install --url=http://auto-amp.dev --title=Test --admin_user=admin --admin_password=password --admin_email=test@test.com --skip-email
 
+    # move our WP-CLI config file out of bin into root WP
+    mv ../amp-wp/bin/wp-cli.local.yml ../
+
+    # move our amp-wp plugin from the travis root into our new plugins folder
     mv ../amp-wp wp-content/plugins
 
+    # move to our Travis root
     cd ..
 
+    # change the wp folder name to amp-wp and then move there so our Travis globals point to the right place
     mv wp amp-wp
-
     cd amp-wp
 
-    ls
-
+    #get the necessary plugins ready
     wp plugin activate amp-wp
-
     wp plugin install wordpress-importer --activate
 
+    #import our test data
     wp import wp-content/plugins/amp-wp/tests/assets/wptest.xml --authors=create --quiet
-
     wp import wp-content/plugins/amp-wp/tests/assets/amptest-wordpress.xml --authors=create --quiet
 
+    #set our URL structure
     wp rewrite structure '/%year%/%monthnum%/%day%/%postname%/' --hard
-
     cat .htaccess
 
 else
