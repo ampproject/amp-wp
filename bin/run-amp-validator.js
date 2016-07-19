5d244b0741ce44db1868a7b8a4488e05d0ed7aee
Chris Smith <chris@teamzen.io>
2016-07-19 13:53:34 -0600

    change to dynamic Mocha tests for each url.  Always include the test urls, but set the test for failure.html and 404.html to expect failure so that we know the validator is working the way we expect and not just validating everything

--- bin/run-amp-validator.js
+++ bin/run-amp-validator.js
@@ -14,9 +14,7 @@ const Promise       = require('bluebird'),
     url             = require('url'),
     chai            = require('chai');
 
-var testUrls = [],
-    ourResults = [],
-    ourErrors = [];
+var testUrls = [];
 
 chai.should();
 chai.use(require('chai-things'));
@@ -39,154 +37,46 @@ var promiseFromChildProcess = function(child) {
     });
 };
 
+var validateUrl = function(url){
+    return new Promise( function( resolve, reject ) {
+        fetch(url)
+        .then(function (res) {
 
-/**
- * While Looping for Promises
- * adapted from: http://blog.victorquinn.com/javascript-promise-while-loop
- */
-var promiseWhile = function(condition, action) {
-    var resolver = Promise.defer();
+            if ( res.ok ) {
+                return res.text();
+            } else {
+                var response = new Error ('Unable to fetch ' + res.url + ' - HTTP Status ' + res.status + ' - ' + res.statusText);
+                reject( response );
+            }
 
-    var loop = function() {
-        if (!condition()) return resolver.resolve();
-        return Promise.cast(action())
-            .then(loop)
-            .catch(resolver.reject);
-    };
+        }).then(function (body) {
+            const ourInstance = ampValidator.getInstance();
+            if (body) {
 
-    process.nextTick(loop);
+                ourInstance.then(function (validator) {
 
-    return resolver.promise;
-};
+                    const result = validator.validateString(body);
+                    if (result.status === 'PASS') {
+                        resolve( result.status );
+                    } else {
+                        let msg = url + '\n';
+                        for (const error of result.errors) {
+                            msg += ('     line ' + error.line + ', col ' + error.col + ': ').debug + error.message.error;
+                            if (error.specUrl !== null) {
+                                msg += '\n     (see ' + error.specUrl + ')';
+                            }
+                        }
+                        var errorMessage = new Error ( msg );
+                        reject( errorMessage );
+                    }
+                });
+            } else {
+                var error = new Error('FAIL - no body');
+                reject( error );
+            }
 
-/**
- * promiseWithTimeout
- * adapted from: http://exploringjs.com/es6/ch_promises.html
- * @param ms integer time in milliseconds before timing out our promise with a reject()
- * @param promise string name of promise function to timeout
- */
-var promiseWithTimeout = function (ms, promise) {
-    return new Promise(function(resolve, reject){
-       promise.then(resolve);
-        setTimeout(function(){
-            reject( new Error('Timeout after '+ms+'ms'));
-        }, ms);
-    });
-};
-
-/**
- * Output the errors stored in ourErrors
- * @param   errors  array of error messages
- */
-var outputErrors = function( errors ){
-    console.log('----------------------------------------------------------------------------'.error);
-    console.log('---------------------------------Errors-------------------------------------'.error);
-    console.log('----------------------------------------------------------------------------\n'.error);
-    for (var i = 0, num = errors.length; i < num; i++) {
-        console.log('||||||||||||||||||||||||||||||        ' + (i + 1) + '        ||||||||||||||||||||||||||||||');
-        console.log(errors[i]);
-        console.log('|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n');
-    }
-    console.log('----------------------------------------------------------------------------'.error);
-    console.log('----------------------------------------------------------------------------\n'.error);
-};
-
-/**
- * Process the status over the fetched URL
- * @param   result  object of the fetch result
- */
-var processUrlStatus = function( result ){
-    if ( result.ok ) {
-        return result.text();
-    } else {
-        var response = 'FAIL: '.error + 'Unable to fetch ' + result.url + ' - HTTP Status ' + result.status + ' - ' + result.statusText + '\n';
-        // i++;
-        return Promise.reject(response);
-    }
-};
-
-/**
- * When the validator returns an error, we need to process that message into a helpful error message
- * @param result string result from the Amp Validator that contains our error message
- */
-var processValidationError = function( result ) {
-    let msg = result.status.error + ": " + url + '\n';
-    for (const error of result.errors) {
-        msg += ('     line ' + error.line + ', col ' + error.col + ': ').debug + error.message.error;
-        if (error.specUrl !== null) {
-            msg += '\n     (see ' + error.specUrl + ')';
-        }
-    }
-    ourErrors.push(msg);
-    ourResults.push('FAIL');
-};
-
-/**
- * Run each URL through the validator
- * @param body  string  html of the page from fetch
- */
-var processValidation = function( body ) {
-    const ourInstance = ampValidator.getInstance();
-    if (body) {
-        //We use timeout here because the AMP validator tool needs an internet connection.  If you are testing locally we want this to fail.  Without the timeout mocha was just failing assertion 1, but didn't provide any information as to why it failed.
-        return promiseWithTimeout(2000,
-            ourInstance)
-            .then(function (validator) {
-
-                const result = validator.validateString(body);
-                if (result.status === 'PASS') {
-
-                    ourResults.push('PASS');
-
-                } else {
-
-                    processValidationError( result );
-
-                }
-                return Promise.resolve(result);
-
-            }).catch(function (reason) {
-
-                ourErrors.push(reason);
-                console.error('Error or timeout', reason);
-                return Promise.reject(reason);
-
-            });
-    } else {
-        return Promise.reject('Body was empty');
-    }
-};
-
-/**
- * Validate urls and add results to our results variables
- * @param   urls  array of urls to be tested
- */
-var validateUrls = function( urls ) {
-    var i = 0,
-        len = urls.length - 1;
-    return promiseWhile(function() {
-        return i <= len;
-    }, function() {
-        return new Promise( function( resolve, reject ) {
-            var url = urls[i];
-            fetch( url )
-            .then( function(res){
-
-                i++;
-                return processUrlStatus( res );
-
-            }).then(function( body ) {
-
-                return processValidation( body );
-
-            }).catch(function(error){
-
-                ourErrors.push( error );
-                ourResults.push( 'FAIL' );
-
-            }).then(function(){
-                resolve();
-            });
+        }).catch(function(error){
+            return done(error);
         });
     });
 };
@@ -208,16 +98,12 @@ describe('AMP Validation Suite', function() {
                 testUrls.push( item['url']+"amp/" );
 
             }
-
-            /**
-             * Control URLs for Testing purposes
-             * comment out for production usage
-             */
-            // var localBaseURL = url.parse(testUrls[0]);
-            // localBaseURL = localBaseURL.protocol + "//" + localBaseURL.hostname;
-            // testUrls.push(localBaseURL + '/wp-content/plugins/amp-wp/tests/assets/success.html');
-            // testUrls.push(localBaseURL + '/wp-content/plugins/amp-wp/tests/assets/404.html');
-            // testUrls.push(localBaseURL + '/wp-content/plugins/amp-wp/tests/assets/failure.html');
+            
+            var localBaseURL = url.parse(testUrls[0]);
+            localBaseURL = localBaseURL.protocol + "//" + localBaseURL.hostname;
+            testUrls.push(localBaseURL + '/wp-content/plugins/amp-wp/tests/assets/success.html');
+            testUrls.push(localBaseURL + '/wp-content/plugins/amp-wp/tests/assets/404.html');
+            testUrls.push(localBaseURL + '/wp-content/plugins/amp-wp/tests/assets/failure.html');
 
             if ( 0 === testUrls.length ) {
                 console.error('URLs not retrieved.'.error);
@@ -236,34 +122,42 @@ describe('AMP Validation Suite', function() {
             }, function (err) {
                 console.error('Child Exec rejected: ' + err);
                 process.exit(1);
-        }).then(function() {
-
-            return validateUrls( testUrls );
-
-        }).then(function(){
-
-            if (ourErrors.length > 0) {
-
-                //call our output errors function to print the errors in the console.
-                outputErrors( ourErrors );
-
-            }
-
+        })
+        .then(function() {
+            describe('Dynamic tests for URLs', function(){
+                testUrls.forEach( function( url ) {
+                    if ( '404.html' == url.substr(url.length - 8) || 'failure.html' == url.substr(url.length - 12)) {
+                        it( url + ' should NOT validate', function(done) {
+                            validateUrl( url ).
+                            then(function(urlResult){
+                                urlResult.should.equal('PASS');
+                                done(urlResult);
+                            }).catch(function(error){
+                                done();
+                            });
+                        });
+                    } else {
+                        it( url + ' should validate', function(done) {
+                            validateUrl( url ).
+                            then(function(urlResult){
+                                urlResult.should.equal('PASS');
+                                done();
+                            }).catch(function(error){
+                                done(error);
+                            });
+                        });
+                    }
+                });
+            });
         });
     });
 
-    it('Get All Validation Results', function(){
-        ourResults.length.should.equal(testUrls.length);
+
+    it('Control Test to Make Mocha run before()', function () {
+        // console.log('Mocha should not require this hack IMHO');
     });
 
-    it('All URLs correctly validate', function(){
-        ourResults.length.should.equal(testUrls.length);
-        ourResults.should.all.be.equal('PASS');
-    });
-    it('No Errors found', function(){
-        ourResults.length.should.equal(testUrls.length);
-        ourErrors.length.should.equal(0);
-    });
+
 
 
 
