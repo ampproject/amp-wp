b74325b21c3e17ba35e0799e352e3db362fd6c45
Allan Cole <allan.cole@automattic.com>
2016-08-30 12:14:09 -0400

    Escaping URLs and HTML

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 <footer class="amp-wp-footer" role="contentinfo" itemscope="itemscope" itemtype="http://schema.org/WPFooter">
 	<div>
 		<h2><?php echo esc_html( $this->get( 'blog_name' ) ); ?></h2>
-		<p><?php _e( 'WordPress', 'amp' ); ?></p>
+		<p><?php esc_html_e( 'WordPress', 'amp' ); ?></p>
 		<a href="#top" class="back-to-top"><?php _e( 'Back to top', 'amp' ); ?></a>
 	</div>
 </footer>
--- templates/meta-comments-link.php
+++ templates/meta-comments-link.php
@@ -1,3 +1,3 @@
 	<div class="amp-wp-meta amp-wp-comment-link">
-		<a class="amp-wp-comments-link-button" href="<?php echo get_comment_link() ?>" title="Leave a Comment" itemprop="discussionUrl">Leave a Comment</a>
+		<a class="amp-wp-comments-link-button" href="<?php echo esc_url( get_comment_link() ); ?>" title="<?php esc_attr_e( 'Leave a Comment', 'amp' ); ?>" itemprop="discussionUrl"><?php esc_html_e( 'Leave a Comment', 'amp' ); ?></a>
 	</div>
