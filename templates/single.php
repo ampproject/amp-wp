f0a70c0519c3732bbae17af61c77447c27275efb
Allan Cole <allan.cole@automattic.com>
2016-08-30 12:15:10 -0400

    Adding back language_attributes to <html>

--- templates/single.php
+++ templates/single.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html amp>
+<html amp <?php language_attributes(); ?>>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
