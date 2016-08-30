3620f5031f9fffc027987e4c05b629a8694c8992
Allan Cole <allan.cole@automattic.com>
2016-08-30 16:57:42 -0400

    Improving share links display

--- templates/meta-sharing.php
+++ templates/meta-sharing.php
@@ -9,20 +9,13 @@
 	<?php } else { ?>
 
 	<div class="amp-wp-meta amp-wp-share-links">
-		Share this:
+		<span>Share this: </span>
 		<amp-social-share type="twitter" width="40" height="40"></amp-social-share>
 		<amp-social-share type="facebook" width="40" height="40"></amp-social-share>
 		<amp-social-share type="pinterest" width="40" height="40"></amp-social-share>
 		<amp-social-share type="linkedin" width="40" height="40"></amp-social-share>
 		<amp-social-share type="gplus" width="40" height="40"></amp-social-share>
 		<amp-social-share type="email" width="40" height="40"></amp-social-share>
-
-	<?php /*
-		<a href="http://facebook.com"><span class="screen-reader-text">Facebook</span></a>
-		<a href="http://youtube.com"><span class="screen-reader-text">YouTube</span></a>
-		<a href="http://pinterest.com"><span class="screen-reader-text">Pinterest</span></a>
-		<a href="http://vimeo.com"><span class="screen-reader-text">Vimeo</span></a>
-	*/ ?>
 	</div>
 
 <?php } ?>
