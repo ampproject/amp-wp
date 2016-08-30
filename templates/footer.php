54aa9988b3863ae5914000a0ace5727d355a1ffc
Allan Cole <allan.cole@automattic.com>
2016-08-30 12:08:03 -0400

    Changing default footer credit
    - This will need to connect to the new Footer Content override JetPack
    solution in a future iteration

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 <footer class="amp-wp-footer" role="contentinfo" itemscope="itemscope" itemtype="http://schema.org/WPFooter">
 	<div>
 		<h2><?php echo esc_html( $this->get( 'blog_name' ) ); ?></h2>
-		<p><?php _e( 'Powered by WordPress.com', 'amp' ); ?></p>
+		<p><?php _e( 'WordPress', 'amp' ); ?></p>
 		<a href="#top" class="back-to-top"><?php _e( 'Back to top', 'amp' ); ?></a>
 	</div>
 </footer>
