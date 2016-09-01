8d134ee8734f4c1efa735a8336090b9b36b6e4a5
Allan Cole <allan.cole@automattic.com>
2016-09-01 11:18:54 -0400

    Adding Pubmine (WordAds)

--- templates/footer-wordads.php
+++ templates/footer-wordads.php
@@ -1,8 +1,9 @@
 <div class="adspace">
 	<amp-ad width=300 height=250
-		type="a9"
-		data-aax_size="300x250"
-		data-aax_pubname="test123"
-		data-aax_src="302">
+		type="pubmine"
+		data-adsafe="1"
+		data-section="1"
+		data-siteid="37790885"
+		data-wordads="1">
 	</amp-ad>
 </div>
