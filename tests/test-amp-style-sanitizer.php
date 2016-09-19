28092c4de741f9ee05fd5d8320d9ead4112eedac
Corey McKrill <corey.mckrill@gmail.com>
2016-09-19 11:32:29 -0700

    Preserve inline styles: Use static content in unit tests
    Instead of duplicating the Style sanitizer's methods for generating
    class names and outputting style rules, the expected values are just
    strings. This helps avoid testing issues in the future if any of the
    methods change.
    
    See https://github.com/Automattic/amp-wp/pull/470#pullrequestreview-437437

--- tests/test-amp-style-sanitizer.php
+++ tests/test-amp-style-sanitizer.php
@@ -11,20 +11,39 @@ class AMP_Style_Sanitizer_Test extends WP_UnitTestCase {
 
 			'span_one_style' => array(
 				'<span style="color: #00ff00;">This is green.</span>',
-				'<span style="color: #00ff00;" class="' . $this->generate_class_name( 'color: #00ff00;' ) . '">This is green.</span>',
-				$this->wrap_style( array( "color: #00ff00;" ) ),
+				'<span style="color: #00ff00;" class="amp-wp-inline-style-bb01159393134c225a1df0d44226c3d2">This is green.</span>',
+				"
+/* Inline Styles */
+.amp-wp-inline-style-bb01159393134c225a1df0d44226c3d2 {
+	color: #00ff00;
+}
+
+		",
 			),
 
 			'span_one_style_bad_format' => array(
 				'<span style="color  : #00ff00">This is green.</span>',
-				'<span style="color  : #00ff00" class="' . $this->generate_class_name( 'color: #00ff00;' ) . '">This is green.</span>',
-				$this->wrap_style( array( "color: #00ff00;" ) ),
+				'<span style="color  : #00ff00" class="amp-wp-inline-style-bb01159393134c225a1df0d44226c3d2">This is green.</span>',
+				"
+/* Inline Styles */
+.amp-wp-inline-style-bb01159393134c225a1df0d44226c3d2 {
+	color: #00ff00;
+}
+
+		",
 			),
 
 			'span_two_styles_reversed' => array(
 				'<span style="width: 350px; color: #00ff00;">This is green.</span>',
-				'<span style="width: 350px; color: #00ff00;" class="' . $this->generate_class_name( "color: #00ff00;\n\twidth: 350px;" ) . '">This is green.</span>',
-				$this->wrap_style( array( "color: #00ff00;\n\twidth: 350px;" ) ),
+				'<span style="width: 350px; color: #00ff00;" class="amp-wp-inline-style-a754703dea82f6e9d2782874c77bdb60">This is green.</span>',
+				"
+/* Inline Styles */
+.amp-wp-inline-style-a754703dea82f6e9d2782874c77bdb60 {
+	color: #00ff00;
+	width: 350px;
+}
+
+		",
 			),
 
 			'div_kses_banned_style' => array(
@@ -41,29 +60,19 @@ class AMP_Style_Sanitizer_Test extends WP_UnitTestCase {
 
 			'two_nodes' => array(
 				'<span style="color: #00ff00;"><span style="color: #ff0000;">This is red.</span></span>',
-				'<span style="color: #00ff00;" class="' . $this->generate_class_name( 'color: #00ff00;' ) . '"><span style="color: #ff0000;" class="' . $this->generate_class_name( 'color: #ff0000;' ) . '">This is red.</span></span>',
-				$this->wrap_style( array( "color: #00ff00;", "color: #ff0000;" ) ),
+				'<span style="color: #00ff00;" class="amp-wp-inline-style-bb01159393134c225a1df0d44226c3d2"><span style="color: #ff0000;" class="amp-wp-inline-style-cc68ddc00c4fa7c4e4ea8d4351e25831">This is red.</span></span>',
+				"
+/* Inline Styles */
+.amp-wp-inline-style-bb01159393134c225a1df0d44226c3d2 {
+	color: #00ff00;
+}
+.amp-wp-inline-style-cc68ddc00c4fa7c4e4ea8d4351e25831 {
+	color: #ff0000;
+}
+
+		",
 			),
 		);
-	}
-
-	private function generate_class_name( $string ) {
-		return 'amp-wp-inline-style-' . md5( $string );
-	}
-
-	private function wrap_style( $array ) {
-		ob_start(); ?>
-
-/* Inline Styles */
-<?php foreach ( $array as $style ) : ?>
-.<?php echo $this->generate_class_name( $style ); ?> {
-	<?php echo $style; ?>
-
-}
-<?php endforeach; ?>
-
-		<?php
-		return ob_get_clean();
 	}
 
 	/**
