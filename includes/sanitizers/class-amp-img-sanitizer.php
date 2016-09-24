9a49e219a44f299bc95c86a55cb68798a5df79a3
Michael Turk <turk.michael@gmail.com>
2016-09-24 08:41:23 -0400

    AMP-WP: Bring modified files back into conformance with WordPress coding/formatting standards
    I'd thought PHPStorm was taking care of compliance with WordPress coding/formatting standards after it prompted me to do so and I said yes. Fool me once...

--- includes/sanitizers/class-amp-img-sanitizer.php
+++ includes/sanitizers/class-amp-img-sanitizer.php
@@ -17,90 +17,89 @@ class AMP_Img_Sanitizer extends AMP_Base_Sanitizer {
 	private static $script_slug = 'amp-anim';
 	private static $script_src = 'https://cdn.ampproject.org/v0/amp-anim-0.1.js';
 
-	public function sanitize()
-    {
-        $nodes = $this->dom->getElementsByTagName(self::$tag);
-        $have_dimensions = array();
-        $need_dimensions = array();
+	public function sanitize() {
 
-        $num_nodes = $nodes->length;
+		$nodes = $this->dom->getElementsByTagName( self::$tag );
+		$have_dimensions = array();
+		$need_dimensions = array();
 
-        if (0 === $num_nodes) {
-            return;
-        }
+		$num_nodes = $nodes->length;
 
-        for ($i = $num_nodes - 1; $i >= 0; $i--) {
-            $node = $nodes->item($i);
+		if ( 0 === $num_nodes ) {
+			return;
+		}
 
-            if (!$node->hasAttribute('src')) {
-                $node->parentNode->removeChild($node);
-                continue;
-            }
+		for ( $i = $num_nodes - 1; $i >= 0; $i-- ) {
+			$node = $nodes->item( $i );
 
-            // Determine which images need their dimensions determined/extracted
-            if ( !$node->hasAttribute('width') || ! $node->hasAttribute('height')) {
-                $need_dimensions[ $node->getAttribute('src') ][] = $node;
-            } else {
-                $have_dimensions[ $node->getAttribute('src') ][] = $node;
-            }
-        }
-        $this->determine_dimensions($need_dimensions);
+			if ( ! $node->hasAttribute( 'src' ) ) {
+				$node->parentNode->removeChild( $node );
+				continue;
+			}
 
-        //Calling function twice to save overhead of merging arrays just to send single array to function
-        $this->adjust_and_replace($need_dimensions);
-        $this->adjust_and_replace($have_dimensions);
-    }
+			// Determine which images need their dimensions determined/extracted.
+			if ( ! $node->hasAttribute( 'width' ) || ! $node->hasAttribute( 'height' ) ) {
+				$need_dimensions[ $node->getAttribute( 'src' ) ][] = $node;
+			} else {
+				$have_dimensions[ $node->getAttribute( 'src' ) ][] = $node;
+			}
+		}
+		$this->determine_dimensions( $need_dimensions );
 
-    /**
-     * Figure out width and height attribute values for images that don't have them by
-     * attempting to determine actual dimensions and setting reasonable defaults otherwise.
-     *
-     * @param array $need_dimensions List of Img src url to node mappings corresponding to images that need dimensions
-     */
-    private function determine_dimensions($need_dimensions)
-    {
-        $dimensions_by_url = AMP_Image_Dimension_Extractor::extract( array_keys($need_dimensions) );
+		// Calling function twice to save overhead of merging arrays just to send single array to function.
+		$this->adjust_and_replace( $need_dimensions );
+		$this->adjust_and_replace( $have_dimensions );
+	}
 
-        foreach ($dimensions_by_url as $url => $dimensions) {
-            foreach ($need_dimensions[$url] as $node) {
-                //Provide default dimensions for images whose dimensions we couldn't fetch
-                if (false === $dimensions) {
-                    $width = isset($this->args['content_max_width']) ? $this->args['content_max_width'] : self::FALLBACK_WIDTH;
-                    $height = self::FALLBACK_HEIGHT;
-                    $node->setAttribute('width', $width);
-                    $node->setAttribute('height', $height);
-                    $class = $node->hasAttribute('class') ? $node->getAttribute('class') . ' amp-wp-unknown-size' : 'amp-wp-unknown-size';
-                    $node->setAttribute('class', $class);
-                } else {
-                    $node->setAttribute('width', $dimensions['width']);
-                    $node->setAttribute('height', $dimensions['height']);
-                }
-            }
-        }
-    }
+	/**
+	 * Figure out width and height attribute values for images that don't have them by
+	 * attempting to determine actual dimensions and setting reasonable defaults otherwise.
+	 *
+	 * @param array $need_dimensions List of Img src url to node mappings corresponding to images that need dimensions.
+	 */
+	private function determine_dimensions( $need_dimensions ) {
+		$dimensions_by_url = AMP_Image_Dimension_Extractor::extract( array_keys( $need_dimensions ) );
 
-    /**
-     * Now that all images have width and height attributes, make final tweaks and replace original image nodes
-     *
-     * @param array $node_lists Img DOM nodes (now with width and height attributes)
-     */
-    private function adjust_and_replace($node_lists) {
-        foreach ($node_lists as $node_list) {
-            foreach ($node_list as $node) {
-                $old_attributes = AMP_DOM_Utils::get_node_attributes_as_assoc_array($node);
-                $new_attributes = $this->filter_attributes($old_attributes);
-                $new_attributes = $this->enforce_sizes_attribute($new_attributes);
-                if ($this->is_gif_url($new_attributes['src'])) {
-                    $this->did_convert_elements = true;
-                    $new_tag = 'amp-anim';
-                } else {
-                    $new_tag = 'amp-img';
-                }
-                $new_node = AMP_DOM_Utils::create_node($this->dom, $new_tag, $new_attributes);
-                $node->parentNode->replaceChild($new_node, $node);
-            }
-        }
-    }
+		foreach ( $dimensions_by_url as $url => $dimensions ) {
+			foreach ( $need_dimensions[ $url ] as $node ) {
+				// Provide default dimensions for images whose dimensions we couldn't fetch.
+				if ( false === $dimensions ) {
+					$width = isset( $this->args['content_max_width'] ) ? $this->args['content_max_width'] : self::FALLBACK_WIDTH;
+					$height = self::FALLBACK_HEIGHT;
+					$node->setAttribute( 'width', $width );
+					$node->setAttribute( 'height', $height );
+					$class = $node->hasAttribute( 'class' ) ? $node->getAttribute( 'class' ) . ' amp-wp-unknown-size' : 'amp-wp-unknown-size';
+					$node->setAttribute( 'class', $class );
+				} else {
+					$node->setAttribute( 'width', $dimensions['width'] );
+					$node->setAttribute( 'height', $dimensions['height'] );
+				}
+			}
+		}
+	}
+
+	/**
+	 * Now that all images have width and height attributes, make final tweaks and replace original image nodes
+	 *
+	 * @param array $node_lists Img DOM nodes (now with width and height attributes).
+	 */
+	private function adjust_and_replace( $node_lists ) {
+		foreach ( $node_lists as $node_list ) {
+			foreach ( $node_list as $node ) {
+				$old_attributes = AMP_DOM_Utils::get_node_attributes_as_assoc_array( $node );
+				$new_attributes = $this->filter_attributes( $old_attributes );
+				$new_attributes = $this->enforce_sizes_attribute( $new_attributes );
+				if ( $this->is_gif_url( $new_attributes['src'] ) ) {
+					$this->did_convert_elements = true;
+					$new_tag = 'amp-anim';
+				} else {
+					$new_tag = 'amp-img';
+				}
+				$new_node = AMP_DOM_Utils::create_node( $this->dom, $new_tag, $new_attributes );
+				$node->parentNode->replaceChild( $new_node, $node );
+			}
+		}
+	}
 
 	public function get_scripts() {
 		if ( ! $this->did_convert_elements ) {
@@ -140,6 +139,6 @@ class AMP_Img_Sanitizer extends AMP_Base_Sanitizer {
 	private function is_gif_url( $url ) {
 		$ext = self::$anim_extension;
 		$path = parse_url( $url, PHP_URL_PATH );
-		return $ext === substr( $path, -strlen( $ext ) );
+		return substr( $path, -strlen( $ext ) ) === $ext;
 	}
 }
--- includes/utils/class-amp-image-dimension-extractor.php
+++ includes/utils/class-amp-image-dimension-extractor.php
@@ -2,43 +2,43 @@
 
 class AMP_Image_Dimension_Extractor {
 	static $callbacks_registered = false;
-    const STATUS_FAILED_LAST_ATTEMPT = 'failed';
-    const STATUS_IMAGE_EXTRACTION_FAILED= 'failed';
+	const STATUS_FAILED_LAST_ATTEMPT = 'failed';
+	const STATUS_IMAGE_EXTRACTION_FAILED = 'failed';
 
-    static public function extract( $urls ) {
+	static public function extract( $urls ) {
 		if ( ! self::$callbacks_registered ) {
 			self::register_callbacks();
 		}
 
-		foreach ($urls as &$url) {
-            $url = self::normalize_url($url);
-        }
+		foreach ( $urls as &$url ) {
+			$url = self::normalize_url( $url );
+		}
 
 		$dimensions = apply_filters( 'amp_extract_image_dimensions', $urls );
 
-        if ( has_filter('amp_extract_image_dimensions') ) {
-            return $dimensions;
-        } else {
-            return self::modify_return_value_for_tests_that_disable_extraction_filter($dimensions);
-        }
+		if ( has_filter( 'amp_extract_image_dimensions' ) ) {
+			return $dimensions;
+		} else {
+			return self::modify_return_value_for_tests_that_disable_extraction_filter( $dimensions );
+		}
 	}
 
-    /**
-     * Account for the possibility of the amp_extract_image_dimensions filter being removed (see AMP_Img_Sanitizer_Test)
-     *
-     * Not surprisingly, the functions invoked by the amp_extract_image_dimensions filter modify the value returned by
-     * AMP_Image_Dimension_Extractor::extract. For that reason, the return value has to be massaged before being returned
-     * when the amp_extract_image_dimensions filter is removed via tests.
-     *
-     * @param array $urls A list or urls that would have had dimensions fetched had amp_extract_image_dimensions filter not been removed
-     */
-	private static function modify_return_value_for_tests_that_disable_extraction_filter($urls) {
+	/**
+	 * Account for the possibility of the amp_extract_image_dimensions filter being removed (see AMP_Img_Sanitizer_Test)
+	 *
+	 * Not surprisingly, the functions invoked by the amp_extract_image_dimensions filter modify the value returned by
+	 * AMP_Image_Dimension_Extractor::extract. For that reason, the return value has to be massaged before being returned
+	 * when the amp_extract_image_dimensions filter is removed via tests.
+	 *
+	 * @param array $urls A list or urls that would have had dimensions fetched had amp_extract_image_dimensions filter not been removed
+	 */
+	private static function modify_return_value_for_tests_that_disable_extraction_filter( $urls ) {
 	    $dimensions = array();
-        foreach ( $urls as $url ) {
-            $dimensions[$url] = false;
-        }
-        return $dimensions;
-    }
+		foreach ( $urls as $url ) {
+			$dimensions[ $url ] = false;
+		}
+		return $dimensions;
+	}
 
 	public static function normalize_url( $url ) {
 		if ( empty( $url ) ) {
@@ -76,152 +76,154 @@ class AMP_Image_Dimension_Extractor {
 		do_action( 'amp_extract_image_dimensions_callbacks_registered' );
 	}
 
-    /**
-     * Extract dimensions from downloaded images (or transient/cached dimensions from downloaded images)
-     *
-     * @param array $urls Image urls
-     * @return array Dimensions mapped to image urls, or false if they could not be retrieved
-     */
-	public static function extract_by_downloading_images($urls, $mode = 'concurrent') {
-        $transient_expiration = 30 * DAY_IN_SECONDS;
+	/**
+	 * Extract dimensions from downloaded images (or transient/cached dimensions from downloaded images)
+	 *
+	 * @param array  $urls Image urls.
+	 * @param string $mode Whether image dimensions should be extracted concurrently or synchronously.
+	 * @return array Dimensions mapped to image urls, or false if they could not be retrieved
+	 */
+	public static function extract_by_downloading_images( $urls, $mode = 'concurrent' ) {
+		$transient_expiration = 30 * DAY_IN_SECONDS;
 
-        $urls_to_fetch = array();
-        $all_dimensions = array();
-        $images = array();
+		$urls_to_fetch = array();
+		$all_dimensions = array();
+		$images = array();
 
-        self::determine_which_images_to_fetch($urls, $urls_to_fetch, $all_dimensions);
-        self::fetch_images($urls_to_fetch, $images, $mode);
-        self::process_fetched_images($urls_to_fetch, $images, $all_dimensions, $transient_expiration);
+		self::determine_which_images_to_fetch( $urls, $urls_to_fetch, $all_dimensions );
+		self::fetch_images( $urls_to_fetch, $images, $mode );
+		self::process_fetched_images( $urls_to_fetch, $images, $all_dimensions, $transient_expiration );
 
-        return $all_dimensions;
+		return $all_dimensions;
 	}
 
-    /**
-     * Determine which images to fetch by checking for dimensions in transient/cache.
-     * Creates a short lived transient that acts as a semaphore so that another visitor
-     * doesn't trigger a remote fetch for the same image at the same time.
-     *
-     * @param array $urls Urls of image src
-     * @param array $urls_to_fetch Urls of images to fetch because dimensions are not in transient/cache
-     * @param array $all_dimensions "Master" list of img url to dimension mappings - used here to track which dimensions couldn't be retrieved
-     */
-	private static function determine_which_images_to_fetch(&$urls, &$urls_to_fetch, &$all_dimensions) {
-        foreach ( $urls as $url ) {
-            $url_hash = md5( $url );
-            $transient_name = sprintf( 'amp_img_%s', $url_hash );
-            $dimensions = get_transient( $transient_name );
+	/**
+	 * Determine which images to fetch by checking for dimensions in transient/cache.
+	 * Creates a short lived transient that acts as a semaphore so that another visitor
+	 * doesn't trigger a remote fetch for the same image at the same time.
+	 *
+	 * @param array $urls Urls of image src.
+	 * @param array $urls_to_fetch Urls of images to fetch because dimensions are not in transient/cache.
+	 * @param array $all_dimensions "Master" list of img url to dimension mappings - used here to track which dimensions couldn't be retrieved.
+	 */
+	private static function determine_which_images_to_fetch( &$urls, &$urls_to_fetch, &$all_dimensions ) {
+		foreach ( $urls as $url ) {
+			$url_hash = md5( $url );
+			$transient_name = sprintf( 'amp_img_%s', $url_hash );
+			$dimensions = get_transient( $transient_name );
 
-            if ( is_array( $dimensions ) ) {
-                $all_dimensions[$url] = array(
-                    'width' => $dimensions[0],
-                    'height' => $dimensions[1],
-                );
-            } else {
-                if ( self::STATUS_FAILED_LAST_ATTEMPT === $dimensions ) {
-                    // If we couldn't get dimensions for this image the last time we tried, don't try again until the transient expires
-                    $all_dimensions[$url] = false;
-                    continue;
-                } else {
-                    // Ensure we're the only one trying to extract dimensions for this image at the moment so we don't duplicate requests
-                    $transient_lock_name = sprintf( 'amp_lock_%s', $url_hash );
-                    if ( false !== get_transient( $transient_lock_name ) ) {
-                        $all_dimensions[$url] = false;
-                        continue;
-                    } else {
-                        $urls_to_fetch[$url]['url'] = $url;
-                        $urls_to_fetch[$url]['transient_name'] = $transient_name;
-                        $urls_to_fetch[$url]['transient_lock_name'] = $transient_lock_name;
-                        set_transient($transient_lock_name, 1, MINUTE_IN_SECONDS);
-                    }
-                }
-            }
-        }
-    }
+			if ( is_array( $dimensions ) ) {
+				$all_dimensions[ $url ] = array(
+					'width' => $dimensions[0],
+					'height' => $dimensions[1],
+				);
+			} else {
+				if ( self::STATUS_FAILED_LAST_ATTEMPT === $dimensions ) {
+					// If we couldn't get dimensions for this image the last time we tried, don't try again until the transient expires.
+					$all_dimensions[ $url ] = false;
+					continue;
+				} else {
+					// Ensure we're the only one trying to extract dimensions for this image at the moment so we don't duplicate requests.
+					$transient_lock_name = sprintf( 'amp_lock_%s', $url_hash );
+					if ( false !== get_transient( $transient_lock_name ) ) {
+						$all_dimensions[ $url ] = false;
+						continue;
+					} else {
+						$urls_to_fetch[ $url ]['url'] = $url;
+						$urls_to_fetch[ $url ]['transient_name'] = $transient_name;
+						$urls_to_fetch[ $url ]['transient_lock_name'] = $transient_lock_name;
+						set_transient( $transient_lock_name, 1, MINUTE_IN_SECONDS );
+					}
+				}
+			}
+		}
+	}
 
-    /**
-     * Fetch dimensions of remote images
-     *
-     * @param array $urls_to_fetch Image src urls to fetch
-     * @param array $images Array to populate with results of image/dimension inspection
-     */
-    private static function fetch_images($urls_to_fetch, &$images, $mode) {
-        //User Faster Image when PHP version supports it (it contains a closure that could not be ported to 5.2)
-        if ( $mode === 'synchronous' || strnatcmp( phpversion(), '5.3.0' ) < 0 ) {
-            self::fetch_images_via_fast_image( $urls_to_fetch, $images );
-        } else {
-            self::fetch_images_via_faster_image( $urls_to_fetch, $images );
-        }
-    }
+	/**
+	 * Fetch dimensions of remote images
+	 *
+	 * @param array  $urls_to_fetch Image src urls to fetch.
+	 * @param array  $images Array to populate with results of image/dimension inspection.
+	 * @param string $mode Whether image dimensions should be extracted concurrently or synchronously.
+	 */
+	private static function fetch_images( $urls_to_fetch, &$images, $mode ) {
+		// User Faster Image when PHP version supports it (it contains a closure that could not be ported to 5.2).
+		if ( 'synchronous' === $mode || strnatcmp( phpversion(), '5.3.0' ) < 0 ) {
+			self::fetch_images_via_fast_image( $urls_to_fetch, $images );
+		} else {
+			self::fetch_images_via_faster_image( $urls_to_fetch, $images );
+		}
+	}
 
-    /**
-     * Fetch images via FastImage library
-     *
-     * @param array $urls_to_fetch Image src urls to fetch
-     * @param array $images Array to populate with results of image/dimension inspection
-     */
-    private static function fetch_images_via_fast_image( $urls_to_fetch, &$images ) {
-        require_once( AMP__DIR__ . '/includes/lib/class-fastimage.php' );
-        $image = new FastImage();
-        $urls = array();
-        // array_column doesn't exist in PHP 5.2
-        foreach ( $urls_to_fetch as $key => $value) {
-            $urls[] = $key;
-        }
-        foreach ( $urls as $url ) {
-            $result = $image->load($url);
-            if ( false === $result ) {
-                $images[ $url ]['size'] = self::STATUS_IMAGE_EXTRACTION_FAILED;
-            } else {
-                $size = $image->getSize();
-                $images[ $url ]['size'] = $size;
-            }
-        }
-    }
+	/**
+	 * Fetch images via FastImage library
+	 *
+	 * @param array $urls_to_fetch Image src urls to fetch.
+	 * @param array $images Array to populate with results of image/dimension inspection.
+	 */
+	private static function fetch_images_via_fast_image( $urls_to_fetch, &$images ) {
+		require_once( AMP__DIR__ . '/includes/lib/class-fastimage.php' );
+		$image = new FastImage();
+		$urls = array();
+		// array_column doesn't exist in PHP 5.2.
+		foreach ( $urls_to_fetch as $key => $value ) {
+			$urls[] = $key;
+		}
+		foreach ( $urls as $url ) {
+			$result = $image->load( $url );
+			if ( false === $result ) {
+				$images[ $url ]['size'] = self::STATUS_IMAGE_EXTRACTION_FAILED;
+			} else {
+				$size = $image->getSize();
+				$images[ $url ]['size'] = $size;
+			}
+		}
+	}
 
-    /**
-     * Fetch images via FasterImage library
-     *
-     * @param array $urls_to_fetch Image src urls to fetch
-     * @param array $images Array to populate with results of image/dimension inspection
-     */
-    private static function fetch_images_via_faster_image( $urls_to_fetch, &$images ) {
-        if ( ! class_exists( 'Faster_Image_B52f1a8_Faster_Image' ) ) {
-            require_once( AMP__DIR__ . '/includes/lib/class-faster-image-b52f1a8-faster-image.php' );
-        }
-        $client = new Faster_Image_B52f1a8_Faster_Image();
-        $images = $client->batch( array_column( $urls_to_fetch, 'url' ) );
-    }
+	/**
+	 * Fetch images via FasterImage library
+	 *
+	 * @param array $urls_to_fetch Image src urls to fetch.
+	 * @param array $images Array to populate with results of image/dimension inspection.
+	 */
+	private static function fetch_images_via_faster_image( $urls_to_fetch, &$images ) {
+		if ( ! class_exists( 'Faster_Image_B52f1a8_Faster_Image' ) ) {
+			require_once( AMP__DIR__ . '/includes/lib/class-faster-image-b52f1a8-faster-image.php' );
+		}
+		$client = new Faster_Image_B52f1a8_Faster_Image();
+		$images = $client->batch( array_column( $urls_to_fetch, 'url' ) );
+	}
 
-    /**
-     * Determine success or failure of remote fetch, integrate fetched dimensions into url to dimension mapping,
-     * cache fetched dimensions via transient and release/delete semaphore transient
-     *
-     * @param array $urls_to_fetch List of image urls that were fetched and transient names corresponding to each (for unlocking semaphore, setting "real" transient)
-     * @param array $images Results of remote fetch mapping fetched image url to dimensions
-     * @param array $all_dimensions "Master" map of image url to dimensions to be updated with results of remote fetch
-     * @param int $transient_expiration Duration image dimensions should exist in transient/cache
-     */
-    private static function process_fetched_images($urls_to_fetch, $images, &$all_dimensions, $transient_expiration) {
-        foreach ( $urls_to_fetch as $url_data) {
-            $image_data = $images[$url_data['url']];
-            if ( self::STATUS_IMAGE_EXTRACTION_FAILED === $image_data['size'] ) {
-                $all_dimensions[$url_data['url']] = false;
-                set_transient( $url_data['transient_name'], self::STATUS_FAILED_LAST_ATTEMPT, $transient_expiration );
-            } else {
-                $all_dimensions[$url_data['url']] = array(
-                    'width' => $image_data['size'][0],
-                    'height' => $image_data['size'][1],
-                );
-                set_transient(
-                    $url_data['transient_name'],
-                    array(
-                        $image_data['size'][0],
-                        $image_data['size'][1],
-                    ),
-                    $transient_expiration
-                );
-            }
-            delete_transient( $url_data['transient_lock_name'] );
-        }
-    }
+	/**
+	 * Determine success or failure of remote fetch, integrate fetched dimensions into url to dimension mapping,
+	 * cache fetched dimensions via transient and release/delete semaphore transient
+	 *
+	 * @param array $urls_to_fetch List of image urls that were fetched and transient names corresponding to each (for unlocking semaphore, setting "real" transient).
+	 * @param array $images Results of remote fetch mapping fetched image url to dimensions.
+	 * @param array $all_dimensions "Master" map of image url to dimensions to be updated with results of remote fetch.
+	 * @param int   $transient_expiration Duration image dimensions should exist in transient/cache.
+	 */
+	private static function process_fetched_images( $urls_to_fetch, $images, &$all_dimensions, $transient_expiration ) {
+		foreach ( $urls_to_fetch as $url_data ) {
+			$image_data = $images[ $url_data['url'] ];
+			if ( self::STATUS_IMAGE_EXTRACTION_FAILED === $image_data['size'] ) {
+				$all_dimensions[ $url_data['url'] ] = false;
+				set_transient( $url_data['transient_name'], self::STATUS_FAILED_LAST_ATTEMPT, $transient_expiration );
+			} else {
+				$all_dimensions[ $url_data['url'] ] = array(
+					'width' => $image_data['size'][0],
+					'height' => $image_data['size'][1],
+				);
+				set_transient(
+					$url_data['transient_name'],
+					array(
+						$image_data['size'][0],
+						$image_data['size'][1],
+					),
+					$transient_expiration
+				);
+			}
+			delete_transient( $url_data['transient_lock_name'] );
+		}
+	}
 }
