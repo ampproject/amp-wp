e3ab19e9153764cebff969d6618ee79a4018817e
Mohammad Jangda <batmoo@gmail.com>
2016-09-28 12:31:19 -0400

    Port error checking from #491

--- templates/meta-taxonomy.php
+++ templates/meta-taxonomy.php
@@ -1,4 +1,4 @@
-<?php $categories = get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'amp' ) ); ?>
+<?php $categories = get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'amp' ), '', $this->ID ); ?>
 <?php if ( $categories ) : ?>
 	<div class="amp-wp-meta amp-wp-tax-category">
 		Categories:
@@ -6,8 +6,14 @@
 	</div>
 <?php endif; ?>
 
-<?php $tags = get_the_tag_list( '', _x( ', ', 'Used between list items, there is a space after the comma.', 'amp' ) ); ?>
-<?php if ( $tags ) : ?>
+<?php
+$tags = get_the_tag_list(
+	'',
+	_x( ', ', 'Used between list items, there is a space after the comma.', 'amp' ),
+	'',
+	$this->ID
+); ?>
+<?php if ( $tags && ! is_wp_error( $tags ) ) : ?>
 	<div class="amp-wp-meta amp-wp-tax-tag">
 		Tags:
 		<?php echo $tags; ?>
