53cd5db4ef858a40e2e73491d8833a5748240b82
david-binda <david.binovec@gmail.com>
2016-09-28 12:58:39 +0200

    Adding `! is_wp_error()` check to templates/meta-taxonomy.php
    Since the `get_the_tag_list` function may return WP_Error in some cases, we should not be checking only for a boolean / string in the conditional before using the returned value as a string, but we also should check for WP_Error case.
    
    This commit is adding the conditional to the mix and should prevent Fatal Errors in cases when the code is trying to use an object of WP_Error as a string.

--- templates/meta-taxonomy.php
+++ templates/meta-taxonomy.php
@@ -7,7 +7,7 @@
 <?php endif; ?>
 
 <?php $tags = get_the_tag_list( '', _x( ', ', 'Used between list items, there is a space after the comma.', 'amp' ) ); ?>
-<?php if ( $tags ) : ?>
+<?php if ( $tags && ! is_wp_error( $tags ) ) : ?>
 	<li class="amp-wp-tax-tag">
 		<span class="screen-reader-text">Tags:</span>
 		<?php echo $tags; ?>
