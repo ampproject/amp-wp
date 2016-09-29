2973764f0e6c05d63565a554aeb2eae65646d00a
Mohammad Jangda <batmoo@gmail.com>
2016-09-29 12:04:53 -0400

    Fix incorrect use of translation function
    In comments template

--- templates/meta-comments-link.php
+++ templates/meta-comments-link.php
@@ -5,7 +5,7 @@ $comments_link_url = $this->get( 'comments_link_url' );
 	<?php $comments_link_text = $this->get( 'comments_link_text' ); ?>
 	<div class="amp-wp-meta amp-wp-comments-link">
 		<a href="<?php echo esc_url( $comments_link_url ); ?>">
-			<?php esc_html_e( $comments_link_text ); ?>
+			<?php echo esc_html( $comments_link_text ); ?>
 		</a>
 	</div>
 <?php endif; ?>
