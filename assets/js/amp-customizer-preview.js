8532e25093fa1a155755477a2ecb2bbb923a57b5
Mohammad Jangda <batmoo@gmail.com>
2016-10-05 17:18:36 -0400

    Prevent navigation on AMP Customizer preview
    The Customizer breaks pretty spectacularly when that happens as it's only meant to work for AMP pages.

--- assets/js/amp-customizer-preview.js
+++ assets/js/amp-customizer-preview.js
@@ -1,6 +1,17 @@
 ( function( $ ) {
 	'use strict';
 
+	// Don't allow navigation away from the AMP page in the preview.
+	// The Customizer breaks pretty spectacularly when that happens as it's only meant to work for AMP pages.
+	$( 'a' ).click( function( e ) {
+		var href = this.getAttribute( 'href' );
+		if ( href && href.indexOf( '#' ) === 0 ) {
+			return true;
+		}
+
+		return false;
+	} );
+
 	// Nav bar text color.
 	wp.customize( 'amp_customizer[header_color]', function( value ) {
 		value.bind( function( to ) {
