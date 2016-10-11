c71beee86eee824c41cd0ab2c919185fa00b46a8
Mohammad Jangda <batmoo@gmail.com>
2016-10-11 16:21:56 -0400

    back-compat for sanitize_hex_color
    It was introduced globally in 4.6 so let's add it if it's not defined.

--- amp.php
+++ amp.php
@@ -113,6 +113,7 @@ function amp_add_frontend_actions() {
 
 function amp_add_post_template_actions() {
 	require_once( AMP__DIR__ . '/includes/amp-post-template-actions.php' );
+	require_once( AMP__DIR__ . '/includes/amp-post-template-functions.php' );
 }
 
 function amp_prepare_render() {
---
+++ includes/amp-post-template-functions.php
@@ -0,0 +1,15 @@
+<?php
+
+// Was only available in Customizer > 4.6
+if ( ! function_exists( 'sanitize_hex_color' ) ) {
+	function sanitize_hex_color( $color ) {
+		if ( '' === $color ) {
+			return '';
+		}
+
+		// 3 or 6 hex digits, or the empty string.
+		if ( preg_match('|^#([A-Fa-f0-9]{3}){1,2}$|', $color ) ) {
+			return $color;
+		}
+	}
+}
