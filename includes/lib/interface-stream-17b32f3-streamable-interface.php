a791a7526a8df9c3e72432961b083bd3f6036979
Paul Schreiber <paul.schreiber@espn.com>
2017-03-08 13:18:10 -0500

    revert changes to third-party libraries in includes/lib/

--- includes/lib/interface-stream-17b32f3-streamable-interface.php
+++ includes/lib/interface-stream-17b32f3-streamable-interface.php
@@ -7,33 +7,33 @@
  */
 interface Stream_17b32f3_Streamable_Interface {
 
-	/**
-	 * Append to the stream string
-	 *
-	 * @param $string
-	 */
-	public function write( $string);
+    /**
+     * Append to the stream string
+     *
+     * @param $string
+     */
+    public function write($string);
 
-	/**
-	 * Get Characters from the string
-	 *
-	 * @param $characters
-	 */
-	public function read( $characters);
+    /**
+     * Get Characters from the string
+     *
+     * @param $characters
+     */
+    public function read($characters);
 
-	/**
-	 * Get characters from the string but don't move the pointer
-	 *
-	 * @param $characters
-	 *
-	 * @return mixed
-	 */
-	public function peek( $characters);
+    /**
+     * Get characters from the string but don't move the pointer
+     *
+     * @param $characters
+     *
+     * @return mixed
+     */
+    public function peek($characters);
 
-	/**
-	 * Resets the pointer to the 0 position
-	 * @return mixed
-	 */
-	public function resetPointer();
+    /**
+     * Resets the pointer to the 0 position
+     * @return mixed
+     */
+    public function resetPointer();
 
 }
