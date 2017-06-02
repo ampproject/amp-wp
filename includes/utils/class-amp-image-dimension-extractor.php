bbd16567f63d16eede854af38fb29a6c8e054732
Mohammad Jangda <batmoo@gmail.com>
2017-06-02 14:15:18 -0400

    Replace all instances of parse_url with helper

--- includes/utils/class-amp-image-dimension-extractor.php
+++ includes/utils/class-amp-image-dimension-extractor.php
@@ -37,7 +37,7 @@ class AMP_Image_Dimension_Extractor {
 			return set_url_scheme( $url, 'http' );
 		}
 
-		$parsed = parse_url( $url );
+		$parsed = AMP_WP_Utils::parse_url( $url );
 		if ( ! isset( $parsed['host'] ) ) {
 			$path = '';
 			if ( isset( $parsed['path'] ) ) {
