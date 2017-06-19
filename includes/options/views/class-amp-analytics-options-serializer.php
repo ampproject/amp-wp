854da9c02b32d09220b347688c38dcf36fe87b03
Alberto Medina <albertomedina@google.com>
2017-06-19 10:54:33 -0700

    Fix proper handling of form submission in serializer

--- includes/options/views/class-amp-analytics-options-serializer.php
+++ includes/options/views/class-amp-analytics-options-serializer.php
@@ -6,33 +6,32 @@ class Analytics_Options_Serializer {
 
 		$option_name = 'analytics';
 
-		if ( empty( $_POST['vendor-type'] ) ||
-		     empty( $_POST['config'] ) ) {
-			return;
-		}
+		if ( !( empty( $_POST['vendor-type'] ) ||
+		     empty( $_POST['config'] ) ) ) {
 
-		if ( empty($_POST['id-value'])) {
-			$_POST['id-value'] = md5($_POST['config']);
-		}
+			if ( empty( $_POST['id-value'] ) ) {
+				$_POST['id-value'] = md5( $_POST['config'] );
+			}
 
-		$new_analytics_options = array(
-			$_POST['id-value'],
-			$_POST['vendor-type'],
-			stripslashes($_POST['config'])
-		);
+			$new_analytics_options = array(
+				$_POST['id-value'],
+				$_POST['vendor-type'],
+				stripslashes( $_POST['config'] )
+			);
 
-		$inner_option_name = $_POST['vendor-type'] . '-' . $_POST['id-value'];
-		$analytics_options = get_option($option_name);
-		if ( ! $analytics_options ) {
-			$analytics_options = array();
-		}
+			$inner_option_name = $_POST['vendor-type'] . '-' . $_POST['id-value'];
+			$analytics_options = get_option( $option_name );
+			if ( ! $analytics_options ) {
+				$analytics_options = array();
+			}
 
-		if ( isset($_POST['delete']) ) {
-			unset($analytics_options[$inner_option_name]);
-		} else {
-			$analytics_options[$inner_option_name] = $new_analytics_options;
+			if ( isset( $_POST['delete'] ) ) {
+				unset( $analytics_options[ $inner_option_name ] );
+			} else {
+				$analytics_options[ $inner_option_name ] = $new_analytics_options;
+			}
+			update_option( $option_name, $analytics_options, false );
 		}
-		update_option( $option_name, $analytics_options, false);
 		// [Redirect] Keep the user in the analytics options page
 		// Wrap in is_admin() to enable phpunit tests to exercise this code
 		if ( is_admin() ) {
