7f37177bfe2a3504d16b06f1186bef88aeb09b29
Mohammad Jangda <batmoo@gmail.com>
2017-07-10 17:02:02 -0400

    wp_redirect => wp_safe_redirect

--- includes/options/views/class-amp-options-manager.php
+++ includes/options/views/class-amp-options-manager.php
@@ -3,7 +3,7 @@
 require_once( AMP__DIR__ . '/includes/utils/class-amp-html-utils.php' );
 
 class AMP_Options_Manager {
-	
+
 	public static function save() {
 		// Request must come from user with right capabilities
 		if ( ! current_user_can( 'manage_options' ) ) {
@@ -13,28 +13,28 @@ class AMP_Options_Manager {
 		check_admin_referer( 'analytics-options', 'analytics-options' );
 
 		$status = AMP_Options_Manager::submit( $_POST );
-		
+
 		// Redirect to keep the user in the analytics options page
 		// Wrap in is_admin() to enable phpunit tests to exercise this code
-		wp_redirect( admin_url( 'admin.php?page=amp-analytics-options&valid=' . $status  ) );
+		wp_safe_redirect( admin_url( 'admin.php?page=amp-analytics-options&valid=' . $status ) );
 		exit;
 	}
-	
+
 	public static function submit( $data) {
-		
+
 		$option_name = 'amp-analytics';
-		
+
 		// Validate JSON configuration is valid
 		$is_valid_json = AMP_HTML_Utils::valid_json( stripslashes( $_POST['config'] ) );
-		
+
 		// Check save/delete pre-conditions and proceed if correct
 		if ( ! ( empty( $_POST['vendor-type'] ) || empty( $_POST['config'] ) ) &&
 		     $is_valid_json ) {
-			
+
 			if ( empty( $_POST['id-value'] ) ) {
 				$_POST['id-value'] = md5( $_POST['config'] );
 			}
-			
+
 			// Prepare the data for the new analytics setting
 			$new_analytics_option = array(
 				sanitize_key( $_POST['id-value'] ),
@@ -43,18 +43,18 @@ class AMP_Options_Manager {
 			);
 			// Identifier for analytics option
 			$inner_option_name = sanitize_key($_POST['vendor-type'] . '-' . $_POST['id-value'] );
-			
+
 			// Grab the amp_options from the DB
 			$amp_options = get_option( 'amp-options' );
 			if ( ! $amp_options ) {
 				$amp_options = array();
 			}
-			
+
 			// Grab the amp-analytics options
 			$amp_analytics = isset($amp_options[ $option_name ])
 				? $amp_options[ $option_name ]
 				: array();
-			
+
 			if ( isset( $_POST['delete'] ) ) {
 				unset( $amp_analytics[ $inner_option_name ] );
 			} else {
