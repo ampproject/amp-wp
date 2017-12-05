e169bc0e7fc97fa354ed141d2ddd14d4e80ae427
ThierryA <thierry@getbeans.io>
2017-12-05 09:32:59 +0100

    Code formatting

--- includes/actions/class-amp-actions.php
+++ includes/actions/class-amp-actions.php
@@ -9,6 +9,7 @@ abstract class AMP_Actions {
 	public static function add_analytics_scripts( $template ) {}
 	public static function add_analytics_data( $template ) {}
 	public static function add_canonical_link( $template ) {}
+
 	/**
 	 * Add AMP generator metadata.
 	 *
