2ea134118369a1999108375047ed31a39e245ea2
Kaitlyn <kaitnyl@gmail.com>
2017-12-05 23:10:57 -0400

    Move AMP Customizer into main Customizer to expand usability across all posts and pages.

---
+++ assets/js/amp-customize-controls.js
@@ -0,0 +1,194 @@
+( function( api, $ ) {
+	'use strict';
+
+	/**
+	 * Check if the URL is AMPified.
+	 *
+	 * @param {string} url URL.
+	 * @return {boolean} whether it is an AMP URL.
+	 */
+	function isAmpUrl( url ) {
+		var urlParser = document.createElement( 'a' );
+		urlParser.href = url;
+		if ( /(^|\?|&)amp=1(?=&|$)/.test( urlParser.search ) ) {
+			return true;
+		}
+		return ( /\/amp\/?$/ ).test( urlParser.pathname );
+	}
+
+	/**
+	 * Create an non-AMP version of a URL.
+	 *
+	 * @param {string} url URL.
+	 * @return {string} non-AMPified URL.
+	 */
+	function unampifyUrl( url ) {
+		var urlParser = document.createElement( 'a' );
+		urlParser.href = url;
+		urlParser.pathname = urlParser.pathname.replace( /\/amp\/?$/, '' );
+		urlParser.search = urlParser.search.replace( /(^|\?|&)amp=1/, '' );
+		return urlParser.href;
+	}
+
+	/**
+	 * Create an AMP version of a URL.
+	 *
+	 * @param {string} url URL.
+	 * @return {string} AMPified URL.
+	 */
+	function ampifyUrl( url ) {
+		var urlParser = document.createElement( 'a' );
+		urlParser.href = unampifyUrl( url );
+		if ( urlParser.search.length ) {
+			urlParser.search += '&';
+		}
+		urlParser.search += 'amp=1';
+		return urlParser.href;
+	}
+
+	/**
+	 * Hook up all AMP preview interactions once panel is ready.
+	 *
+	 * @param {wp.customize.Panel} panel The AMP panel.
+	 * @return {void}
+	 */
+	function panelReady( panel ) {
+		var ampToggle = false;
+
+		/**
+		 * Make current URL AMPified if toggle is on.
+		 *
+		 * @param {string} url URL.
+		 * @return {string} AMPified URL.
+		 */
+		function setCurrentAmpUrl( url ) {
+			if ( ! ampToggle && isAmpUrl( url ) ) {
+				return unampifyUrl( url );
+			} else if ( ampToggle && ! isAmpUrl( url ) ) {
+				return ampifyUrl( url );
+			}
+			return url;
+		}
+
+		/**
+		 * Swap to AMP version of URL in preview.
+		 *
+		 * @return {void}
+		 */
+		function toggleAmpView() {
+			api.previewer.previewUrl.set( setCurrentAmpUrl( api.previewer.previewUrl.get() ) );
+		}
+
+		/**
+		 * Inspect iFrame metadata to see if loaded page was successfully AMP'd.
+		 *
+		 * @return {void}
+		 */
+		function isPageAmpified() {
+			var ampEnabled = false,
+				headContent = $( api.previewer.preview.iframe ).contents().find( 'head' ).html();
+
+			if ( headContent.search( '<meta name="generator" content="AMP Plugin' ) >= 0 ) {
+				ampEnabled = true;
+			}
+
+			// Update tooltip display based on above IF it's currently toggled on.
+			if ( ampToggle && ! ampEnabled ) {
+				toggleControl( false, true, true );
+			} else if ( ! ampToggle && ! ampEnabled ) {
+
+				// Enable toggle but don't trigger anything else.
+				$( '.amp-toggle input' ).prop( 'disabled', false );
+			}
+		}
+
+		/**
+		 * Central function to control UI toggle elements.
+		 *
+		 * @param {bool} checked If toggle input should be checked.
+		 * @param {bool} disabled If toggle input should be disabled.
+		 * @param {bool} notification If tooltip notification should appear.
+		 * @return {void}
+		 */
+		function toggleControl( checked, disabled, notification ) {
+			var $input = $( '.amp-toggle input' ),
+				$tooltip = $( '.amp-toggle .tooltip' ),
+				tooltipTimer = 5000;
+
+			$input.prop( 'checked', checked );
+			$input.prop( 'disabled', disabled );
+
+			if ( notification ) {
+				$tooltip.fadeIn();
+				setTimeout( function() {
+					$tooltip.fadeOut();
+				}, tooltipTimer );
+			} else {
+				$tooltip.hide();
+			}
+
+			$input.trigger( 'change' );
+		}
+
+		// AMP panel triggers the input toggle for AMP preview.
+		panel.expanded.bind( function() {
+			var panelOpen = panel.expanded.get();
+			toggleControl( panelOpen, panelOpen, false );
+		} );
+
+		// If AMP panel is open, let it handle enable/disable of the toggle,
+		// otherwise auto enable toggle when mobile device is selected.
+		api.previewedDevice.bind( function( device ) {
+			if ( ! panel.expanded.get() && ! ampToggle ) {
+				toggleControl( 'mobile' === device, false, false );
+			}
+		} );
+
+		// Preview is ready, check if it's AMP'd in case we need to show tooltip.
+		api.previewer.bind( 'ready', isPageAmpified );
+
+		// Persist the presence or lack of the amp=1 param when navigating in the preview,
+		// even if current page is not yet supported.
+		api.previewer.previewUrl.validate = ( function( prevValidate ) {
+			return function( value ) {
+				var val = prevValidate.call( this, value );
+				if ( val ) {
+					val = setCurrentAmpUrl( val );
+				}
+				return val;
+			};
+		} )( api.previewer.previewUrl.validate );
+
+		// Adding checkbox toggle before device selection.
+		$( '.devices-wrapper' ).before( '<label class="amp-toggle">' +
+			'<input type="checkbox">' +
+			'<span class="slider"></span>' +
+		'</label>' );
+
+		// Notification tooltip for AMP toggle.
+		$( '.amp-toggle input' ).before( '<span class="tooltip">' +
+			'This page is not AMP compatible.<br>' +
+			'<a data-post="' + window.ampPost + '">Navigate to an AMP compatible page</a>' +
+		'</span>' );
+
+		$( '.amp-toggle .tooltip a' ).on( 'click', function() {
+			var url = $( this ).data( 'post' );
+			if ( url.length ) {
+				api.previewer.previewUrl.set( setCurrentAmpUrl( url ) );
+				toggleControl( false, false, false );
+			}
+		} );
+
+		// Main control for toggling AMP preview.
+		$( '#customize-footer-actions' ).on( 'change', '.amp-toggle input', function( event ) {
+			ampToggle = $( this ).is( ':checked' );
+			toggleAmpView();
+			event.stopPropagation();
+		} );
+	}
+
+	api.bind( 'ready', function() {
+		api.panel( 'amp_panel', panelReady );
+	} );
+
+} )( wp.customize, jQuery );
--- includes/admin/class-amp-customizer.php
+++ includes/admin/class-amp-customizer.php
@@ -41,80 +41,24 @@ class AMP_Template_Customizer {
 
 		do_action( 'amp_customizer_init', $self );
 
-		// Settings need to be registered for regular customize requests as well (since save is handled there)
 		$self->register_settings();
-
-		// Our custom panels only need to go for AMP Customizer requests though
-		if ( self::is_amp_customizer() ) {
-			if ( empty( $_GET['url'] ) ) { // input var ok
-				$wp_customize->set_preview_url( amp_admin_get_preview_permalink() );
-			}
-
-			$self->_unregister_core_ui();
-			$self->register_ui();
-		} elseif ( is_customize_preview() ) {
-			// Delay preview-specific actions until we're sure we're rendering an AMP page, since it's too early for `is_amp_endpoint()` here.
-			add_action( 'pre_amp_render_post', array( $self, 'init_preview' ) );
-		}
-	}
-
-	/**
-	 * Filters the core components to unhook the nav_menus and widgets panels.
-	 *
-	 * @since 0.4
-	 * @access private
-	 *
-	 * @return array Array of core Customizer components to keep active.
-	 */
-	public static function _unregister_core_panels( $panels ) {
-		if ( self::is_amp_customizer() ) {
-			$panels = array();
-		}
-		return $panels;
-	}
-
-	/**
-	 * Removes all non-AMP sections and panels.
-	 *
-	 * Provides a clean, standalone instance-like experience by removing all non-AMP registered panels and sections.
-	 *
-	 * @since 0.4
-	 * @access private
-	 */
-	private function _unregister_core_ui() {
-		$panels   = $this->wp_customize->panels();
-		$sections = $this->wp_customize->sections();
-
-		foreach ( $panels as $panel_id => $object ) {
-			$this->wp_customize->remove_panel( $panel_id );
-		}
-
-		foreach ( $sections as $section_id => $object ) {
-			$this->wp_customize->remove_section( $section_id );
-		}
-	}
-
-	public function init_preview() {
-		// Preview needs controls registered too for postMessage communication.
-		$this->register_ui();
-
-		add_action( 'amp_post_template_footer', array( $this, 'add_preview_scripts' ) );
+		$self->register_ui();
 	}
 
 	/**
 	 * Sets up the AMP Customizer preview.
 	 */
 	public function register_ui() {
-		add_action( 'customize_controls_enqueue_scripts', array( $this, 'add_customizer_scripts' ) );
-		add_filter( 'customize_previewable_devices', array( $this, 'force_mobile_preview' ) );
-
 		$this->wp_customize->add_panel( self::PANEL_ID, array(
-			'type'  => 'amp',
-			'title' => __( 'AMP', 'amp' ),
+			'type'        => 'amp',
+			'title'       => __( 'AMP', 'amp' ),
 			'description' => sprintf( __( '<a href="%s" target="_blank">The AMP Project</a> is a Google-led initiative that dramatically improves loading speeds on phones and tablets. You can use the Customizer to preview changes to your AMP template before publishing them.', 'amp' ), 'https://ampproject.org' ),
 		) );
 
 		do_action( 'amp_customizer_register_ui', $this->wp_customize );
+
+		add_action( 'customize_controls_enqueue_scripts', array( $this, 'add_customizer_scripts' ) );
+		add_action( 'amp_post_template_footer', array( $this, 'add_customizer_preview_script' ) );
 	}
 
 	/**
@@ -127,41 +71,43 @@ class AMP_Template_Customizer {
 		do_action( 'amp_customizer_register_settings', $this->wp_customize );
 	}
 
+	/**
+	 * Load up AMP scripts needed for Customizer integrations.
+	 *
+	 * @since 0.6
+	 * @access public
+	 */
 	public function add_customizer_scripts() {
-		wp_enqueue_script( 'wp-util' ); // fix `wp.template is not a function`
-		do_action( 'amp_customizer_enqueue_scripts' );
+		wp_enqueue_script(
+			'amp-customizer',
+			amp_get_asset_url( 'js/amp-customize-controls.js' ),
+			array( 'jquery', 'customize-controls' ),
+			$version = false,
+			$footer  = true
+		);
+
+		wp_localize_script( 'amp-customizer', 'ampPost', amp_admin_get_preview_permalink() );
+
+		wp_enqueue_style(
+			'amp-customizer',
+			amp_get_asset_url( 'css/amp-customizer.css' )
+		);
+
+		do_action( 'amp_customizer_enqueue_scripts', $this->wp_customize );
 	}
 
 	/**
-	 * Enqueues scripts and fires the 'wp_footer' action so we can output customizer scripts.
+	 * To view AMP templates within the Customizer, we need to ensure the customize-preview.js is loaded.
 	 *
-	 * This breaks AMP validation in the customizer but is necessary for the live preview.
-	 *
-	 * @since 0.4
+	 * @since 0.6
 	 * @access public
 	 */
-	public function add_preview_scripts() {
-		wp_enqueue_script(
-			'amp-customizer',
-			amp_get_asset_url( 'js/amp-customizer-preview.js' ),
-			array( 'jquery', 'customize-preview', 'wp-util' ),
-			$version = false,
-			$footer = true
-		);
-
-		do_action( 'amp_customizer_enqueue_preview_scripts', $this->wp_customize );
-
-		/** This action is documented in wp-includes/general-template.php */
-		do_action( 'wp_footer' );
-	}
-
-	public function force_mobile_preview( $devices ) {
-		if ( isset( $devices['mobile'] ) ) {
-			$devices['mobile']['default'] = true;
-			unset( $devices['desktop']['default'] );
+	public function add_customizer_preview_script() {
+		if ( is_customize_preview() ) {
+			global $wp_customize;
+			$wp_customize->customize_preview_settings();
+			wp_print_scripts( array( 'customize-preview' ) );
 		}
-
-		return $devices;
 	}
 
 	public static function is_amp_customizer() {
