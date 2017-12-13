550ed6574e9bfebdca4ad5e28077cc0d4684253c
Ryan Kienstra <ryan.kienstra@xwp.co>
2017-12-12 19:49:49 -0600

    Issue 806 : Add media and embeds to test page.
    Use Weston's wp-cli script to create a test post.
    Add media items to the post content.
    And add several embeds.
    Several embeds are still not rendering,
    Even on a standard, non-AMP page.

--- bin/create-embed-test-post.php
+++ bin/create-embed-test-post.php
@@ -18,6 +18,180 @@ $data_entries = array(
 			return sprintf( '[gallery ids="%s"]', implode( ',', $data['ids'] ) );
 		},
 	),
+	array(
+		'heading' => 'Media Image',
+		'content' => amp_image_markup(),
+	),
+	array(
+		'heading' => 'Media Caption',
+		'content' => '[caption width=150]' . amp_image_markup() . 'Example image caption[/caption]',
+	),
+	array(
+		'heading' => 'Media Video',
+		'content' => '[video src=https://videos.files.wordpress.com/DK5mLrbr/video-ca6dc0ab4a_hd.mp4]',
+	),
+	array(
+		'heading' => 'Media Audio',
+		'content' => '[audio src=https://wptavern.com/wp-content/uploads/2017/11/EPISODE-296-Gutenberg-Telemetry-Calypso-and-More-With-Matt-Mullenweg.mp3]',
+	),
+	array(
+		'prepare' => 'amp_test_prepare_video_attachments',
+		'heading' => 'Video Playlist',
+		'content' => function( $data ) {
+			return isset( $data['ids'] ) ? sprintf( '[playlist type="video" ids="%s"]', implode( ',', $data['ids'] ) ) : 'There are no videos, so no playlist is expected';
+		},
+	),
+	array(
+		'prepare' => 'amp_test_prepare_audio_attachments',
+		'heading' => 'Audio Playlist',
+		'content' => function( $data ) {
+			return isset( $data['ids'] ) ? sprintf( '[playlist ids="%s"]', implode( ',', $data['ids'] ) ) : 'There audio files, so no playlist is expected';
+		},
+	),
+	array(
+		'heading' => 'CloudUp Image Embed',
+		'content' => 'https://cloudup.com/iWn3EIpgjev',
+	),
+	array(
+		'heading' => 'CloudUp Video Embed',
+		'content' => 'https://cloudup.com/ioyW8a_Tjme',
+	),
+	array(
+		'heading' => 'CloudUp Gallery Embed',
+		'content' => 'https://cloudup.com/cQNdYtQLO5U',
+	),
+	array(
+		'heading' => 'CollegeHumor Embed',
+		'content' => 'http://www.collegehumor.com/video/40002823/how-to-actually-finish-something-for-once',
+	),
+	array(
+		'heading' => 'DailyMotion Embed',
+		'content' => 'http://www.dailymotion.com/embed/video/x6bacgf',
+	),
+	array(
+		'heading' => 'Facebook Post Embed',
+		'content' => 'https://www.facebook.com/WordPress/posts/10155651799877911',
+	),
+	array(
+		'heading' => 'Facebook Video Embed',
+		'content' => 'https://www.facebook.com/WordPress/videos/10154702401472911/',
+	),
+	array(
+		'heading' => 'Flickr Image Embed',
+		'content' => 'https://www.flickr.com/photos/sylvainmessier/38089894895/in/explore-2017-12-11/',
+	),
+	array(
+		'heading' => 'Flickr Video Embed',
+		'content' => 'https://flic.kr/p/5TPDWa',
+	),
+	array(
+		'heading' => 'Funny Or Die Video Embed',
+		'content' => 'http://FunnyOrDie.com/m/b1dn',
+	),
+	array(
+		'heading' => 'Hulu Embed',
+		'content' => 'https://www.hulu.com/watch/807443',
+	),
+	array(
+		'heading' => 'Imgur Embed',
+		'content' => '<blockquote class="imgur-embed-pub" lang="en" data-id="HNQ2WRt"><a href="//imgur.com/HNQ2WRt">Takeoff</a></blockquote>',
+	),
+	array(
+		'heading' => 'Instagram Embed',
+		'content' => 'https://www.instagram.com/p/bNd86MSFv6/',
+	),
+	array(
+		'heading' => 'Isuu Embed',
+		'content' => 'https://issuu.com/ajcwfu/docs/seatatthetablefinal',
+	),
+	array(
+		'heading' => 'Kickstarter Embed',
+		'content' => 'https://www.kickstarter.com/projects/iananderson/save-froots-magazine',
+	),
+	array(
+		'heading' => 'Meetup Embed',
+		'content' => 'https://www.meetup.com/WordPress-Mexico',
+	),
+	array(
+		'heading' => 'Mixcloud Embed',
+		'content' => 'https://www.mixcloud.com/TheWireMagazine/adventures-in-sound-and-music-hosted-by-derek-walmsley-7-december-2017/',
+	),
+	array(
+		'heading' => 'Photobucket Embed',
+		'content' => 'http://s1284.photobucket.com/user/adonchin/media/20171116_181841_zpsrjuop6u7.jpg.html',
+	),
+	array(
+		'heading' => 'Polldaddy Embed',
+		'content' => 'https://polldaddy.com/poll/7012505/',
+	),
+	array(
+		'heading' => 'Reddit Embed',
+		'content' => 'https://www.reddit.com/r/Android/comments/7jbkub/google_maps_will_soon_tell_you_when_its_time_to/?ref=share&ref_source=link',
+	),
+	array(
+		'heading' => 'Reverb Nation Embed',
+		'content' => 'https://www.reverbnation.com/fernandotorresleiva/song/28755694-breve-amor-new-version',
+	),
+	array(
+		'heading' => 'Scribd Embed',
+		'content' => '[scribd id=124550852 key=key-1hh4hsgsvzz8jdl28w3v mode=scroll]',
+	),
+	array(
+		'heading' => 'SlideShare Embed',
+		'content' => 'https://www.slideshare.net/slideshow/embed_code/key/u6WNbsR5worSzC',
+	),
+	array(
+		'heading' => 'SmugMug Embed',
+		'content' => 'https://stuckincustoms.smugmug.com/Portfolio/i-GnwtS8R/A',
+	),
+	array(
+		'heading' => 'Soundcloud Embed',
+		'content' => 'https://soundcloud.com/jack-villano-villano/mozart-requiem-in-d-minor',
+	),
+	array(
+		'heading' => 'Speaker Deck Embed',
+		'content' => 'https://speakerdeck.com/caitiem20/distributed-sagas-a-protocol-for-coordinating-microservices',
+	),
+	array(
+		'heading' => 'Spotify Embed',
+		'content' => 'https://open.spotify.com/track/2XULDEvijLgHttFgLzzpM5',
+	),
+	array(
+		'heading' => 'Ted Embed',
+		'content' => 'https://www.ted.com/talks/derek_sivers_how_to_start_a_movement',
+	),
+	array(
+		'heading' => 'Tumblr Post Embed',
+		'content' => 'https://embed.tumblr.com/embed/post/CHB7nLkCLl-ODZ7tdPU9SQ/168290317795',
+	),
+	array(
+		'heading' => 'Twitter Embed',
+		'content' => 'https://twitter.com/WordPress/status/936550699336437760',
+	),
+	array(
+		'heading' => 'VideoPress Embed',
+		'content' => 'https://videopress.com/v/kUJmAcSf',
+	),
+	array(
+		'heading' => 'Vimeo Embed',
+		'content' => 'https://vimeo.com/59172123',
+	),
+	array(
+		'heading' => 'Vine Embed',
+		'content' => '[vine url="https://vine.co/v/bEIHZpD2JWz"]',
+	),
+	array(
+		'heading' => 'WordPress Plugin Directory Embed',
+		'content' => 'https://wordpress.org/plugins/amp/',
+	),
+	array(
+		'heading' => 'WordPress TV Embed',
+		'content' => 'https://videopress.com/v/DK5mLrbr',
+	),
+	array(
+		'heading' => 'YouTube Embed',
+		'content' => 'https://www.youtube.com/watch?v=XOY3ZUO6P0k',
+	),
 );
 
 /**
@@ -35,8 +209,61 @@ function amp_test_prepare_image_attachments( $data ) {
 	) );
 	$data['ids'] = wp_list_pluck( $attachments, 'ID' );
 
-	// @todo Add some attachments if count( $data['ids'] ) < 5.
+	$image_count_needed = 5;
+	if ( count( $data['ids'] ) < $image_count_needed ) {
+		$data['ids'] = wp_list_pluck( amp_get_media_items( 'image', $image_count_needed ), 'ID' );
+	}
 	return $data;
+}
+
+/**
+ * Get the markup for an image.
+ *
+ * @return string $image Markup for the image.
+ */
+function amp_image_markup() {
+	$image_posts = amp_get_media_items( 'image', 1 );
+	$image = reset( $image_posts );
+	return isset( $image->ID ) ? wp_get_attachment_image( $image->ID ) : null;
+}
+
+/**
+ * Get video IDs to test a video playlist.
+ *
+ * @param array $data Entry data.
+ * @return array $data Entry data, with video IDs.
+ */
+function amp_test_prepare_video_attachments( $data ) {
+	$data['ids'] = wp_list_pluck( amp_get_media_items( 'video' ), 'ID' );
+	return $data;
+}
+
+/**
+ * Get audio IDs to test an audio playlist.
+ *
+ * @param array $data Entry data.
+ * @return array $data Data, with video IDs.
+ */
+function amp_test_prepare_audio_attachments( $data ) {
+	$data['ids'] = wp_list_pluck( amp_get_media_items( 'audio' ), 'ID' );
+	return $data;
+}
+
+/**
+ * Get media items, using a \WP_Query.
+ *
+ * @param integer $type The post_mime_type of the media item.
+ * @param integer $image_count The number of images for which to query.
+ * @return array $media_items The media items from the query.
+ */
+function amp_get_media_items( $type, $image_count = 10 ) {
+	$query = new \WP_Query( array(
+		'post_type'      => 'attachment',
+		'post_mime_type' => $type,
+		'post_status'    => 'inherit',
+		'posts_per_page' => $image_count,
+	) );
+	return $query->get_posts();
 }
 
 // Run the script.
