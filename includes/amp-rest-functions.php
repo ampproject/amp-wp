b7c5963665dd5e87b0c75fbd7343717b77ba6d75
David Cramer <david@digilab.co.za>
2018-01-23 14:03:25 +0200

    optimize comments endpoint

--- includes/amp-rest-functions.php
+++ includes/amp-rest-functions.php
@@ -29,10 +29,14 @@ function amp_register_endpoints() {
  */
 function amp_get_comments( $request ) {
 
-	$id     = $request->get_param( 'id' );
-	$return = array(
+	$id       = $request->get_param( 'id' );
+	$comments = get_comments( array(
+		'post_ID' => $id,
+		'order'   => 'ASC',
+	) );
+	$return   = array(
 		'items' => array(
-			'comment' => amp_get_comments_recursive( $id, 0 ),
+			'comment' => amp_get_comments_recursive( 0, $comments ),
 		),
 	);
 
@@ -43,25 +47,22 @@ function amp_get_comments( $request ) {
  * Recursively get comments for the Post and Parent
  *
  * @since 0.7
- * @param int $id The post ID.
- * @param int $parent The comment parent.
+ * @param int   $parent The comment parent.
+ * @param array $comments The list of comments.
  *
  * @return array The array of comments.
  */
-function amp_get_comments_recursive( $id, $parent ) {
-
-	$comments = get_comments( array(
-		'post_ID' => $id,
-		'parent'  => $parent,
-		'order'   => 'ASC',
-	) );
+function amp_get_comments_recursive( $parent, $comments ) {
 
 	$return = array();
 	foreach ( $comments as $comment ) {
+		if ( (int) $parent !== (int) $comment->comment_parent ) {
+			continue;
+		}
 		$GLOBALS['comment']    = $comment; // WPCS: override ok.
 		$comment->comment_date = get_comment_date();
 		$comment->comment_time = get_comment_time();
-		$comment->comment      = amp_get_comments_recursive( $id, $comment->comment_ID );
+		$comment->comment      = amp_get_comments_recursive( $comment->comment_ID, $comments );
 		$return[]              = $comment;
 	}
 
