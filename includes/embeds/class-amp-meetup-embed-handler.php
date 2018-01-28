e2cc3e1aeb5d6d1b6ea234adc286a38615ad6a50
Weston Ruter <weston@xwp.co>
2018-01-27 20:10:30 -0800

    Filter embed_oembed_html to capture CSS; move to amp-custom styles

--- includes/embeds/class-amp-meetup-embed-handler.php
+++ includes/embeds/class-amp-meetup-embed-handler.php
@@ -19,53 +19,81 @@ class AMP_MeetUp_Embed_Handler extends AMP_Base_Embed_Handler {
 	const URL_PATTERN = '#https?://(www\.)?meetu(\.ps|p\.com)/.*#i';
 
 	/**
+	 * CSS.
+	 *
+	 * @var string
+	 */
+	protected $captured_css;
+
+	/**
+	 * AMP_MeetUp_Embed_Handler constructor.
+	 *
+	 * @param array $args Args.
+	 */
+	public function __construct( array $args = array() ) {
+		parent::__construct( $args );
+		add_filter( 'amp_custom_styles', array( $this, 'filter_amp_custom_styles' ) );
+		add_action( 'amp_post_template_css', array( $this, 'do_action_amp_post_template_css' ) );
+	}
+
+	/**
 	 * Register embed.
 	 */
 	public function register_embed() {
-		wp_embed_register_handler( 'meetup', self::URL_PATTERN, array( $this, 'oembed' ), -1 );
+		add_filter( 'embed_oembed_html', array( $this, 'filter_embed_oembed_html' ), 10, 2 );
 	}
 
 	/**
 	 * Unregister embed.
 	 */
 	public function unregister_embed() {
-		wp_embed_unregister_handler( 'meetup', -1 );
+		remove_filter( 'embed_oembed_html', array( $this, 'filter_embed_oembed_html' ), 10 );
 	}
 
 	/**
-	 * Embed found with matching URL callback.
+	 * Filter amp_custom_styles.
 	 *
-	 * @param array $matches URL regex matches.
-	 * @param array $attr    Additional parameters.
-	 * @param array $url     URL.
+	 * @param string $css Amend CSS.
+	 * @return string CSS.
+	 */
+	public function filter_amp_custom_styles( $css ) {
+		if ( $this->captured_css ) {
+			$css .= wp_strip_all_tags( $this->captured_css );
+		}
+		return $css;
+	}
+
+	/**
+	 * Add styles for AMP post template.
+	 */
+	public function do_action_amp_post_template_css() {
+		if ( $this->captured_css ) {
+			echo wp_strip_all_tags( $this->captured_css ); // XSS OK.
+		}
+	}
+
+	/**
+	 * Filter oEmbed HTML for SoundCloud to convert to AMP.
+	 *
+	 * @param string $cache Cache for oEmbed.
+	 * @param string $url   Embed URL.
 	 * @return string Embed.
 	 */
-	public function oembed( $matches, $attr, $url ) {
-		return $this->render( array( 'url' => $url ) );
-	}
-
-	/**
-	 * Extract the MeetUp CSS and output in header (otherwise stripped).
-	 * Output the MeetUp oEmbed as usual.
-	 *
-	 * @param array $args parameters used for output.
-	 * @return string Rendered content.
-	 */
-	public function render( $args ) {
-		$args = wp_parse_args( $args, array(
-			'url' => false,
-		) );
-
-		if ( empty( $args['url'] ) ) {
-			return '';
+	public function filter_embed_oembed_html( $cache, $url ) {
+		$parsed_url = wp_parse_url( $url );
+		if ( false === strpos( $parsed_url['host'], 'meetup.com' ) ) {
+			return $cache;
 		}
+		if ( preg_match( ':<style[^>]*>(.+)</style>:s', $cache, $matches ) ) {
+			$this->captured_css = $matches[1];
 
-		$content = wp_oembed_get( $args['url'] );
+			// Eliminate illegal CSS.
+			$this->captured_css = str_replace( '!important', '', $this->captured_css );
 
-		// Strip AMP-illegal style from response.
-		$content = preg_replace( '#<style.+</style>#', '', $content );
-
-		return $content;
+			// Remove CSS from embed.
+			$cache = str_replace( $matches[0], '', $cache );
+		}
+		return $cache;
 	}
 }
 
