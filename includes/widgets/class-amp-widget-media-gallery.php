2aa7ba2e60ca5e02e1de24ec29389d7094586980
Weston Ruter <weston@xwp.co>
2018-01-29 17:36:27 -0800

    Use do_shortcode() instead of calling gallery embed logic directly

--- includes/widgets/class-amp-widget-media-gallery.php
+++ includes/widgets/class-amp-widget-media-gallery.php
@@ -10,6 +10,8 @@ if ( class_exists( 'WP_Widget_Media_Gallery' ) ) {
 	/**
 	 * Class AMP_Widget_Media_Gallery
 	 *
+	 * @todo This subclass can eventually be eliminated once #25435 is merged and the WP_Widget_Media_Gallery::render_media() does do_single_shortcode( 'gallery', $instance ).
+	 * @link https://core.trac.wordpress.org/ticket/25435
 	 * @since 0.7.0
 	 * @package AMP
 	 */
@@ -19,8 +21,7 @@ if ( class_exists( 'WP_Widget_Media_Gallery' ) ) {
 		 * Renders the markup of the widget.
 		 *
 		 * Mainly copied from WP_Widget_Media_Gallery::render_media().
-		 * But instead of calling shortcode_gallery(),
-		 * It uses this plugin's embed handler for galleries.
+		 * But instead of calling shortcode_gallery(), it calls do_shortcode().
 		 *
 		 * @since 0.7.0
 		 *
@@ -45,8 +46,21 @@ if ( class_exists( 'WP_Widget_Media_Gallery' ) ) {
 				$shortcode_atts['orderby'] = 'rand';
 			}
 
-			$handler = new AMP_Gallery_Embed_Handler();
-			echo $handler->shortcode( $shortcode_atts ); // WPCS: XSS ok.
+			/*
+			 * The following calls do_shortcode() in case another plugin overrides the gallery shortcode.
+			 * The AMP_Gallery_Embed_Handler in the plugin is doing this itself, but other plugins may
+			 * do it as well, so this ensures that a plugin has the option to override the gallery behavior
+			 * via registering a different gallery shortcode handler. The shortcode serialization can be
+			 * eliminated once WP Trac #25435 is merged and the Gallery widget uses the proposed do_single_shortcode().
+			 */
+			$shortcode_atts_str = '';
+			if ( is_array( $shortcode_atts['ids'] ) ) {
+				$shortcode_atts['ids'] = join( ',', $shortcode_atts['ids'] );
+			}
+			foreach ( $shortcode_atts as $key => $value ) {
+				$shortcode_atts_str .= sprintf( ' %s="%s"', $key, esc_attr( $value ) );
+			}
+			echo do_shortcode( "[gallery $shortcode_atts_str]" ); // WPCS: XSS ok.
 		}
 
 	}
