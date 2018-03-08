8e6698bbc6f69f9e09731c6a51851ab1fd7a65f7
Weston Ruter <weston@xwp.co>
2018-03-08 12:17:11 -0800

    Add test for link with scheme-less URL having bad URL

--- tests/test-tag-and-attribute-sanitizer.php
+++ tests/test-tag-and-attribute-sanitizer.php
@@ -684,7 +684,11 @@ class AMP_Tag_And_Attribute_Sanitizer_Test extends WP_UnitTestCase {
 				'<a class="foo" href="http://foo bar">value</a>',
 				'<a class="foo" href="">value</a>',
 			),
-			'a_with_mail_host'                                         => array(
+			'a_with_wrong_schemeless_host'                              => array(
+				'<a class="foo" href="//bad domain with a space.com/foo">value</a>',
+				'<a class="foo" href="">value</a>',
+			),
+			'a_with_mail_host'                                          => array(
 				'<a class="foo" href="mail to:foo@bar.com">value</a>',
 				'<a class="foo" href="">value</a>',
 			),
