242d9d5c3b50197acf6de26c5f11d8ad9b5653a0
Weston Ruter <weston@xwp.co>
2018-04-04 14:28:48 -0700

    Ensure amphtml link is properly output in theme support paired mode on non-singular queries
    * Consolidate logic from amp_frontend_add_canonical() into AMP_Theme_Support.
    * Add tests for amp_frontend_add_canonical()

--- includes/amp-frontend-actions.php
+++ includes/amp-frontend-actions.php
@@ -10,23 +10,31 @@ add_action( 'wp_head', 'amp_frontend_add_canonical' );
 /**
  * Add amphtml link to frontend.
  *
+ * @todo This function's name is incorrect. It's not about adding a canonical link but adding the amphtml link.
+ *
  * @since 0.2
  */
 function amp_frontend_add_canonical() {
 
-	// Prevent showing amphtml link if theme supports AMP but paired mode is not available.
-	if ( current_theme_supports( 'amp' ) && ! AMP_Theme_Support::is_paired_available() ) {
-		return;
-	}
-
 	/**
 	 * Filters whether to show the amphtml link on the frontend.
 	 *
+	 * @todo This filter's name is incorrect. It's not about adding a canonical link but adding the amphtml link.
 	 * @since 0.2
 	 */
 	if ( false === apply_filters( 'amp_frontend_show_canonical', true ) ) {
 		return;
 	}
 
-	printf( '<link rel="amphtml" href="%s">', esc_url( amp_get_permalink( get_queried_object_id() ) ) );
+	$amp_url = null;
+	if ( is_singular() ) {
+		$amp_url = amp_get_permalink( get_queried_object_id() );
+	} elseif ( isset( $_SERVER['REQUEST_URI'] ) ) {
+		$host_url = preg_replace( '#(^https?://[^/]+)/.*#', '$1', home_url( '/' ) );
+		$self_url = esc_url_raw( $host_url . wp_unslash( $_SERVER['REQUEST_URI'] ) );
+		$amp_url  = add_query_arg( amp_get_slug(), '', $self_url );
+	}
+	if ( $amp_url ) {
+		printf( '<link rel="amphtml" href="%s">', esc_url( $amp_url ) );
+	}
 }
