d87564d8f32a8c97c20428d74803435e9235b888
Weston Ruter <weston@xwp.co>
2018-08-01 13:43:28 -0700

    Wrap info in details, default open when no entries

--- includes/options/views/class-amp-analytics-options-submenu-page.php
+++ includes/options/views/class-amp-analytics-options-submenu-page.php
@@ -87,16 +87,23 @@ class AMP_Analytics_Options_Submenu_Page {
 
 	/**
 	 * Render title.
+	 *
+	 * @param bool $has_entries Whether there are entries.
 	 */
-	public function render_title() {
+	public function render_title( $has_entries = false ) {
 		?>
 		<div class="wrap">
 			<h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
 			<?php settings_errors(); ?>
-			<p>
-				<?php echo wp_kses_post( __( 'For Google Analytics, please see <a href="https://developers.google.com/analytics/devguides/collection/amp-analytics/" target="_blank">Adding Analytics to your AMP pages</a>; see also the <a href="https://github.com/Automattic/amp-wp/wiki/Analytics" target="_blank">Analytics wiki page</a>. The analytics configuration supplied below must take the form of JSON objects, which begin with a &#8220;{&#8221; and end with a &#8220;}&#8221;. Do not include any HTML tags like &#8220;<code>&lt;amp-analytics&gt;</code>&#8221; or &#8220;<code>&lt;script&gt;</code>&#8221;. A common entry would have the type &#8220;<code>googleanalytics</code>&#8221; and a configuration that looks like the following (where <code>UA-XXXXX-Y</code> is replaced with your own site\'s account number):', 'amp' ) ); ?>
 
-				<pre>{
+			<details <?php echo ! $has_entries ? 'open' : ''; ?>>
+				<summary>
+					<?php esc_html_e( 'Learn about analytics for AMP.', 'amp' ); ?>
+				</summary>
+				<p>
+					<?php echo wp_kses_post( __( 'For Google Analytics, please see <a href="https://developers.google.com/analytics/devguides/collection/amp-analytics/" target="_blank">Adding Analytics to your AMP pages</a>; see also the <a href="https://github.com/Automattic/amp-wp/wiki/Analytics" target="_blank">Analytics wiki page</a>. The analytics configuration supplied below must take the form of JSON objects, which begin with a &#8220;{&#8221; and end with a &#8220;}&#8221;. Do not include any HTML tags like &#8220;<code>&lt;amp-analytics&gt;</code>&#8221; or &#8220;<code>&lt;script&gt;</code>&#8221;. A common entry would have the type &#8220;<code>googleanalytics</code>&#8221; and a configuration that looks like the following (where <code>UA-XXXXX-Y</code> is replaced with your own site\'s account number):', 'amp' ) ); ?>
+
+					<pre>{
 	"vars": {
 		"account": "UA-XXXXX-Y"
 	},
@@ -107,7 +114,8 @@ class AMP_Analytics_Options_Submenu_Page {
 		}
 	}
 }</pre>
-			</p>
+				</p>
+			</details>
 		</div><!-- .wrap -->
 		<?php
 	}
@@ -164,7 +172,7 @@ class AMP_Analytics_Options_Submenu_Page {
 
 		$analytics_entries = AMP_Options_Manager::get_option( 'analytics', array() );
 
-		$this->render_title();
+		$this->render_title( ! empty( $analytics_entries ) );
 
 		// Render entries stored in the DB.
 		foreach ( $analytics_entries as $entry_id => $entry ) {
