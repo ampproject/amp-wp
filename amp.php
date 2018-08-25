a2d21e9b8b15ec198cff7924dffca96d01560d6c
Weston Ruter <weston@xwp.co>
2018-08-25 06:40:34 -0700

    Ensure font stylesheets are requested in CORS mode in both AMP and non-AMP documents

--- amp.php
+++ amp.php
@@ -102,6 +102,9 @@ add_action( 'wp_default_scripts', 'amp_register_default_scripts' );
 // Ensure async and custom-element/custom-template attributes are present on script tags.
 add_filter( 'script_loader_tag', 'amp_filter_script_loader_tag', PHP_INT_MAX, 2 );
 
+// Ensure crossorigin=anonymous is added to font links.
+add_filter( 'style_loader_tag', 'amp_filter_font_style_loader_tag_with_crossorigin_anonymous', 10, 4 );
+
 /**
  * Set up AMP.
  *
