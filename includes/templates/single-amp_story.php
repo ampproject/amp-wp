dc87187c3ca770c43e39f370aeb622f3f7c5d14c
Weston Ruter <weston@xwp.co>
2018-08-29 12:16:04 -0700

    Ensure amp-runtime and Gutenberg block styles are included on story template

--- includes/templates/single-amp_story.php
+++ includes/templates/single-amp_story.php
@@ -13,7 +13,11 @@ the_post();
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
 		<title><?php echo esc_html( wp_get_document_title() ); ?></title>
-		<?php wp_scripts()->do_items( array( 'amp-story' ) ); ?>
+		<?php
+		wp_enqueue_scripts();
+		wp_scripts()->do_items( array( 'amp-runtime' ) ); // @todo Duplicate with AMP_Theme_Support::enqueue_assets().
+		wp_styles()->do_items( array( 'wp-block-library' ) ); // @todo We need to allow a theme to enqueue their own AMP story styles.
+		?>
 		<?php rel_canonical(); ?>
 		<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
 	</head>
