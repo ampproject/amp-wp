2ce2e5e30079062de1d91a081673ba1993b59d1a
Ryan Kienstra <kienstraryan@gmail.com>
2018-09-13 14:28:02 -0500

    Addres PHPCS warning by aligning =
    The build failed, with:
    Equals sign not aligned with surrounding assignments.
    So address this by aligning the =

--- includes/validation/class-amp-invalid-url-post-type.php
+++ includes/validation/class-amp-invalid-url-post-type.php
@@ -664,7 +664,7 @@ class AMP_Invalid_URL_Post_Type {
 						if ( 1 === $count ) {
 							$output[] = sprintf( '<span class="dashicons dashicons-admin-plugins"></span></span><strong>%s</strong><br/>', esc_html__( 'Plugin', 'amp' ) );
 						} else {
-							$output[] = sprintf( '<span class="dashicons dashicons-admin-plugins"></span><strong>%s (%d)</strong><span class="dashicons toggle-sources dashicons-arrow-down"></span>', esc_html__( 'Plugins', 'amp' ), $count );
+							$output[]                   = sprintf( '<span class="dashicons dashicons-admin-plugins"></span><strong>%s (%d)</strong><span class="dashicons toggle-sources dashicons-arrow-down"></span>', esc_html__( 'Plugins', 'amp' ), $count );
 							$sources_container_classes .= ' collapsed';
 						}
 						$output[] = '<div class="' . $sources_container_classes . '">';
@@ -673,13 +673,13 @@ class AMP_Invalid_URL_Post_Type {
 						$output[] = '</div>';
 					}
 					if ( isset( $sources['core'] ) ) {
-						$output[] = '<div class="source">';
-						$count = count( array_unique( $sources['core'] ) );
+						$output[]                  = '<div class="source">';
+						$count                     = count( array_unique( $sources['core'] ) );
 						$sources_container_classes = 'sources-container sources-core';
 						if ( 1 === $count ) {
 							$output[] = sprintf( '<span class="dashicons dashicons-wordpress-alt"></span><strong>%s</strong><br/>', esc_html__( 'Other', 'amp' ) );
 						} else {
-							$output[] = sprintf( '<span class="dashicons dashicons-wordpress-alt"></span><strong>%s (%d)</strong><span class="dashicons toggle-sources dashicons-arrow-down"></span>', esc_html__( 'Other', 'amp' ), $count );
+							$output[]                   = sprintf( '<span class="dashicons dashicons-wordpress-alt"></span><strong>%s (%d)</strong><span class="dashicons toggle-sources dashicons-arrow-down"></span>', esc_html__( 'Other', 'amp' ), $count );
 							$sources_container_classes .= ' collapsed';
 						}
 						$output[] = '<div class="' . $sources_container_classes . '">';
