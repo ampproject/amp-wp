69a9c637f8f4eb18ff0cc24f6b0e665578541c30
Miina Sikk <miina.sikk@gmail.com>
2018-11-26 13:34:30 -0300

    Rework Block Navigation

--- blocks/amp-story/amp-story-page.js
+++ blocks/amp-story/amp-story-page.js
@@ -1,3 +1,5 @@
+/* global ReactDOM */
+
 import uuid from 'uuid/v4';
 import BlockNavigation from './block-navigation';
 import {
@@ -15,6 +17,8 @@ const {
 	InspectorControls,
 	Inserter
 } = wp.editor;
+
+const { Component } = wp.element;
 
 const ALLOWED_BLOCKS = [
 	'amp/amp-story-grid-layer-vertical',
@@ -53,8 +57,6 @@ export default registerBlockType(
 		title: __( 'Page', 'amp' ),
 		category: 'layout',
 		icon: BLOCK_ICONS[ 'amp/amp-story-page' ],
-
-		// @todo Enforce that the amp-story-page can only be a root-level block; Using `parent: []` does not work, and it causes the inserter to be disabled entirely.
 		attributes: {
 			id: {
 				type: 'string',
@@ -80,50 +82,75 @@ export default registerBlockType(
 		 * https://github.com/ampproject/amphtml/blob/87fe1d02f902be97b596b36ec3421592c83d241e/extensions/amp-story/validator-amp-story.protoascii#L146-L171
 		 * */
 
-		edit( props ) {
-			const { setAttributes, attributes, isSelected } = props;
-			const onChangeBackgroundColor = newBackgroundColor => {
-				setAttributes( { backgroundColor: newBackgroundColor } );
-			};
-			let displayNavigation = false;
+		edit: class extends Component {
+			constructor( props ) {
+				// Call parent constructor.
+				super( props );
 
-			// If the page ID is not set, add one.
-			if ( ! attributes.id ) {
-				setAttributes( { id: uuid() } );
+				if ( ! props.attributes.id ) {
+					this.props.setAttributes( { id: uuid() } );
+				}
 			}
 
-			if (
-				( ! getSelectedBlockClientId() && 0 === getBlockIndex( props.clientId ) ) ||
-				( hasSelectedInnerBlock( props.clientId, true ) || isSelected )
-			) {
-				displayNavigation = true;
-			}
-
-			return [
-				<InspectorControls key='controls'>
-					<PanelColorSettings
-						title={ __( 'Background Color Settings', 'amp' ) }
-						initialOpen={ false }
-						colorSettings={ [
-							{
-								value: attributes.backgroundColor,
-								onChange: onChangeBackgroundColor,
-								label: __( 'Background Color', 'amp' )
+			componentDidUpdate() {
+				// If no blocks are selected or if it's the current page, change the view.
+				if ( ! getSelectedBlockClientId() || this.props.clientId === getSelectedBlockClientId() || hasSelectedInnerBlock( this.props.clientId, true ) ) {
+					const editLayout = document.getElementsByClassName( 'edit-post-layout' );
+					if ( editLayout.length ) {
+						const blockNav = document.getElementById( 'amp-root-navigation' );
+						if ( ! blockNav ) {
+							let navWrapper = document.createElement( 'div' );
+							navWrapper.id = 'amp-root-navigation';
+							editLayout[ 0 ].appendChild( navWrapper );
+						}
+						let navList;
+						if ( hasSelectedInnerBlock( this.props.clientId, true ) || this.props.isSelected ) {
+							let className = 'editor-selectors';
+							if ( 0 === getBlockIndex( this.props.clientId ) ) {
+								className += ' amp-story-page-first';
 							}
-						] }
-					/>
-				</InspectorControls>,
-				displayNavigation && (
-					<div key='layerManager' className='editor-selectors'>
-						<Inserter rootClientId={ props.clientId } />
-						<BlockNavigation />
+							navList =
+								<div key='layerManager' className={ className }>
+									<Inserter rootClientId={ this.props.clientId } />
+									<BlockNavigation />
+								</div>;
+						} else {
+							navList =
+								<div key='layerManager' className='editor-selectors'>
+									<BlockNavigation />
+								</div>;
+						}
+						ReactDOM.render( navList, document.getElementById( 'amp-root-navigation' ) );
+					}
+				}
+			}
+
+			render() {
+				const props = this.props;
+				const { setAttributes, attributes } = props;
+				const onChangeBackgroundColor = newBackgroundColor => {
+					setAttributes( { backgroundColor: newBackgroundColor } );
+				};
+
+				return [
+					<InspectorControls key='controls'>
+						<PanelColorSettings
+							title={ __( 'Background Color Settings', 'amp' ) }
+							initialOpen={ false }
+							colorSettings={ [
+								{
+									value: attributes.backgroundColor,
+									onChange: onChangeBackgroundColor,
+									label: __( 'Background Color', 'amp' )
+								}
+							] }
+						/>
+					</InspectorControls>,
+					<div key="contents" className={ maybeIsSelectedParentClass( props.clientId ) } style={{ backgroundColor: attributes.backgroundColor }}>
+						<InnerBlocks template={ TEMPLATE } allowedBlocks={ ALLOWED_BLOCKS } />
 					</div>
-				),
-				// Get the template dynamically.
-				<div key="contents" className={ maybeIsSelectedParentClass( props.clientId ) } style={{ backgroundColor: attributes.backgroundColor }}>
-					<InnerBlocks template={ TEMPLATE } allowedBlocks={ ALLOWED_BLOCKS } />
-				</div>
-			];
+				];
+			}
 		},
 
 		save( { attributes } ) {
