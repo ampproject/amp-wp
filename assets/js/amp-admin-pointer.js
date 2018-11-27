6477e74d01a8b3c773c14dbd2e72589b46b36a1b
Alberto Medina <aa.medina@gmail.com>
2018-11-26 18:08:30 -0800

    Add missing license header files reported by crosslint

--- assets/js/amp-admin-pointer.js
+++ assets/js/amp-admin-pointer.js
@@ -1,4 +1,21 @@
 /**
+ * This file is part of the AMP Plugin for WordPress.
+ *
+ * The AMP Plugin for WordPress is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * The AMP Plugin for WordPress is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
  * Adds an admin pointer that describes new features in 1.0.
  */
 
--- assets/js/amp-customize-controls.js
+++ assets/js/amp-customize-controls.js
@@ -1,3 +1,20 @@
+/**
+ * This file is part of the AMP Plugin for WordPress.
+ *
+ * The AMP Plugin for WordPress is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * The AMP Plugin for WordPress is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with the AMP Plugin for WordPress. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 /* exported ampCustomizeControls */
 /* eslint no-magic-numbers: [ "error", { "ignore": [ 0, 1, 250] } ] */
 
