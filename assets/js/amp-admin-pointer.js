e7919f4922a3412e2f55f6cd950dc6f20c33c672
Alberto Medina <aa.medina@gmail.com>
2018-11-26 18:13:05 -0800

    Fix typo in Licence header

--- assets/js/amp-admin-pointer.js
+++ assets/js/amp-admin-pointer.js
@@ -12,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+ * along with the AMP Plugin for WordPress. If not, see <https://www.gnu.org/licenses/>.
  */
 
 /**
