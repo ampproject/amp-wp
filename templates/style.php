0277678f05cb650a1dcc892d99c15a2b62ce56a4
mostafaabd <tafa777@gmail.com>
2018-11-29 23:02:32 +0200

    Add CSS style to support RTL language in frontend

--- templates/style.php
+++ templates/style.php
@@ -386,3 +386,53 @@ amp-carousel > amp-img > img {
 	position: absolute;
 	right: 16px;
 }
+
+<?php if ( function_exists( 'is_rtl' ) && is_rtl() ) { ?>
+
+body {
+	text-align: right;
+}
+
+blockquote {
+	border-left: 0;
+	border-right: 2px solid <?php echo sanitize_hex_color( $link_color ); ?>;
+}
+
+.amp-wp-header .amp-wp-site-icon {	
+	right: 0;
+	left: 18px;
+}
+
+.amp-wp-article-header .amp-wp-meta:last-of-type {
+	text-align: left;
+}
+
+.amp-wp-article-header .amp-wp-meta:first-of-type {
+	text-align: right;
+}
+
+.amp-wp-byline amp-img {
+	margin-right: 0;
+	margin-left: 6px;
+}
+
+.amp-wp-posted-on {
+	text-align: left;
+}
+
+.amp-wp-article-content ul,
+.amp-wp-article-content ol {
+	margin-left: 0;
+	margin-right: 1em;
+}
+
+.amp-wp-article-content amp-img {
+	margin: 0 auto;
+}
+
+.back-to-top {
+	right: auto;
+	right: 16px;
+}
+
+<?php } ?>
