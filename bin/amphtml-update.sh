2f33454432e1e957d0df971407dc2c40175ccad4
Weston Ruter <westonruter@google.com>
2019-02-08 17:19:24 -0800

    Install amphtml as composer dev-dependency pinned at a release version

--- bin/amphtml-update.sh
+++ bin/amphtml-update.sh
@@ -17,27 +17,14 @@ fi
 sudo apt-get install git python protobuf-compiler python-protobuf
 
 # Create and go to vendor.
-if [[ ! -e $VENDOR_PATH ]]; then
-	mkdir $VENDOR_PATH
+if [[ ! -e $VENDOR_PATH/ampproject/amphtml ]]; then
+	echo "Error: You must do composer install --dev."
+	exit 1
 fi
-cd $VENDOR_PATH
-
-# Clone amphtml repo.
-if [[ ! -e $VENDOR_PATH/amphtml ]]; then
-	git clone https://github.com/ampproject/amphtml amphtml
-else
-	cd $VENDOR_PATH/amphtml/validator
-	git fetch --tags
-fi
-
-# Check out the latest tag.
-cd $VENDOR_PATH/amphtml
-LATEST_TAG=$( git describe --abbrev=0 --tags )
-git checkout $LATEST_TAG
 
 # Copy script to location and go there.
-cp $BIN_PATH/amphtml-update.py $VENDOR_PATH/amphtml/validator
-cd $VENDOR_PATH/amphtml/validator
+cp $BIN_PATH/amphtml-update.py $VENDOR_PATH/ampproject/amphtml/validator
+cd $VENDOR_PATH/ampproject/amphtml/validator
 
 # Run script.
 python amphtml-update.py
