d3973ab2059ab020443985c32d80888d6358090a
Pascal Birchler <pascalb@google.com>
2019-02-20 15:09:15 +0100

    Improve Webpack and Babel config for WP React development

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,92 @@
-/* global require, module, __dirname */
+/* global process, require, module, __dirname */
 
 const path = require( 'path' );
 
+const externals = {
+	// Make localized data importable.
+	'amp-validation-i18n': 'ampValidationI18n',
+	jquery: 'jQuery',
+	lodash: 'lodash',
+	react: 'React',
+	'react-dom': 'ReactDOM'
+};
+
+// Define WordPress dependencies
+const wpDependencies = [
+	'blocks',
+	'components',
+	'compose',
+	'data',
+	'editor',
+	'edit-post',
+	'element',
+	'hooks',
+	'i18n',
+	'api-fetch',
+	'blocks',
+	'components',
+	'compose',
+	'date',
+	'editor',
+	'element',
+	'hooks',
+	'i18n',
+	'utils',
+	'data',
+	'viewport',
+	'core-data',
+	'plugins',
+	'edit-post',
+	'keycodes'
+];
+
+/**
+ * Given a string, returns a new string with dash separators converted to
+ * camel-case equivalent. This is not as aggressive as `_.camelCase` in
+ * converting to uppercase, where Lodash will convert letters following
+ * numbers.
+ *
+ * @param {string} string Input dash-delimited string.
+ *
+ * @return {string} Camel-cased string.
+ */
+function camelCaseDash( string ) {
+	return string.replace(
+		/-([a-z])/,
+		( match, letter ) => letter.toUpperCase()
+	);
+}
+
+wpDependencies.forEach( ( name ) => {
+	externals[ `@wordpress/${ name }` ] = {
+		this: [ 'wp', camelCaseDash( name ) ]
+	};
+} );
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 module.exports = {
+	mode: isProduction ? 'production' : 'development',
 	entry: {
 		'./assets/js/amp-blocks-compiled': './blocks/index.js',
-		'./assets/js/amp-block-editor-toggle-compiled': './assets/src/amp-block-editor-toggle',
-		'./assets/js/amp-validation-detail-toggle-compiled': './assets/src/amp-validation-detail-toggle',
-		'./assets/js/amp-validation-single-error-url-details-compiled': './assets/src/amp-validation-single-error-url-details',
+		'./assets/js/amp-block-editor-toggle-compiled': './assets/src/amp-block-editor-toggle.js',
+		'./assets/js/amp-validation-detail-toggle-compiled': './assets/src/amp-validation-detail-toggle.js',
+		'./assets/js/amp-validation-single-error-url-details-compiled': './assets/src/amp-validation-single-error-url-details.js',
 		'./assets/js/amp-story-blocks-compiled': './blocks/stories.js'
 	},
 	output: {
 		path: path.resolve( __dirname ),
-		filename: '[name].js'
+		filename: '[name].js',
+		library: 'AMP',
+		libraryTarget: 'this',
 	},
-	externals: {
-		// Make localized data importable.
-		'amp-validation-i18n': 'ampValidationI18n'
-	},
-	devtool: 'cheap-eval-source-map',
+	externals,
+	devtool: isProduction ? undefined : 'cheap-eval-source-map',
 	module: {
 		rules: [
 			{
 				test: /\.js$/,
-				exclude: /(node_modules)/,
+				exclude: /node_modules/,
 				use: {
 					loader: 'babel-loader'
 				}
