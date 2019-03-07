06d4f4f936d532299eeed83e346c24e00f37119b
Miina Sikk <miina.sikk@gmail.com>
2019-03-07 23:52:39 +0100

    Use anchor for setting page ID.

--- assets/src/blocks/amp-story/edit.js
+++ assets/src/blocks/amp-story/edit.js
@@ -41,10 +41,6 @@ class EditPage extends Component {
 		// Call parent constructor.
 		super( props );
 
-		if ( ! props.attributes.id ) {
-			this.props.setAttributes( { id: uuid() } );
-		}
-
 		if ( ! props.attributes.anchor ) {
 			this.props.setAttributes( { anchor: uuid() } );
 		}
