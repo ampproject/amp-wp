347a8cc176077f3d116f4e05dd9db5cf305d966b
Ryan Kienstra <kienstraryan@gmail.com>
2019-03-11 20:54:39 -0600

    Add a comment describing the styling being registered
    It's registered outside of the class that
    handles AMP stories,
    so describe what the stylesheet is.

--- includes/amp-helper-functions.php
+++ includes/amp-helper-functions.php
@@ -457,6 +457,7 @@ function amp_register_default_scripts( $wp_scripts ) {
 		);
 	}
 
+	// Register the styling for the /embed endpoint and the Latest Stories block.
 	wp_register_style(
 		AMP_Story_Post_Type::STORY_CARD_CSS_SLUG,
 		amp_get_asset_url( '/css/' . AMP_Story_Post_Type::STORY_CARD_CSS_SLUG . '.css' ),
