19d8e81f55d1f96863e6f4df2d34c36991275988
Pascal Birchler <pascalb@google.com>
2019-03-13 18:30:53 +0100

    Add color settings to text block

--- assets/src/blocks/amp-story-text/edit.js
+++ assets/src/blocks/amp-story-text/edit.js
@@ -1,10 +1,24 @@
 /**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
 import { Fragment } from '@wordpress/element';
 import { PanelBody, SelectControl } from '@wordpress/components';
-import { RichText, InspectorControls, FontSizePicker, withFontSizes } from '@wordpress/editor';
+import { compose } from '@wordpress/compose';
+import {
+	RichText,
+	InspectorControls,
+	FontSizePicker,
+	withFontSizes,
+	withColors,
+	PanelColorSettings,
+	ContrastChecker,
+} from '@wordpress/editor';
 
 /**
  * Internal dependencies
@@ -12,16 +26,25 @@ import { RichText, InspectorControls, FontSizePicker, withFontSizes } from '@wor
 import { FontFamilyPicker } from '../../components';
 import { maybeEnqueueFontStyle } from '../../helpers';
 
-function TextBlock( {
-	attributes,
-	setAttributes,
-	className,
-	fontSize,
-	setFontSize,
-} ) {
-	const { placeholder, content, type, ampFontFamily } = attributes;
+function TextBlock( props ) {
+	const {
+		attributes,
+		setAttributes,
+		className,
+		fontSize,
+		setFontSize,
+		backgroundColor,
+		textColor,
+		setBackgroundColor,
+		setTextColor,
+	} = props;
 
-	const fontSizeClass = fontSize.class || undefined;
+	const {
+		placeholder,
+		content,
+		type,
+		ampFontFamily,
+	} = attributes;
 
 	return (
 		<Fragment>
@@ -50,6 +73,32 @@ function TextBlock( {
 						] }
 					/>
 				</PanelBody>
+				<PanelColorSettings
+					title={ __( 'Color Settings', 'amp' ) }
+					initialOpen={ false }
+					colorSettings={ [
+						{
+							value: backgroundColor.color,
+							onChange: setBackgroundColor,
+							label: __( 'Background Color', 'amp' ),
+						},
+						{
+							value: textColor.color,
+							onChange: setTextColor,
+							label: __( 'Text Color', 'amp' ),
+						},
+					] }
+				>
+					<ContrastChecker
+						{ ...{
+							textColor: textColor.color,
+							backgroundColor: backgroundColor.color,
+							// Todo: Calculate from page background.
+							fallbackBackgroundColor: undefined,
+						} }
+						fontSize={ fontSize.size }
+					/>
+				</PanelColorSettings>
 			</InspectorControls>
 			<RichText
 				identifier="content"
@@ -58,13 +107,24 @@ function TextBlock( {
 				value={ content }
 				onChange={ ( value ) => setAttributes( { content: value } ) }
 				style={ {
+					backgroundColor: backgroundColor.color,
+					color: textColor.color,
 					fontSize: fontSize.size ? fontSize.size + 'px' : undefined,
 				} }
-				className={ `${ className } ${ fontSizeClass }` }
+				className={ classnames( className, {
+					'has-text-color': textColor.color,
+					'has-background': backgroundColor.color,
+					[ backgroundColor.class ]: backgroundColor.class,
+					[ textColor.class ]: textColor.class,
+					[ fontSize.class ]: fontSize.class,
+				} ) }
 				placeholder={ placeholder || __( 'Write text…', 'amp' ) }
 			/>
 		</Fragment>
 	);
 }
 
-export default withFontSizes( 'fontSize' )( TextBlock );
+export default compose(
+	withColors( 'backgroundColor', { textColor: 'color' } ),
+	withFontSizes( 'fontSize' ),
+)( TextBlock );
