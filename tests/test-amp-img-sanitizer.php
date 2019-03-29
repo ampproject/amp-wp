a82a87ffb6cc42f986278992939df9d7b514c74f
Weston Ruter <westonruter@google.com>
2019-03-28 21:14:07 -0700

    Add test for sizes attribute being removed from converted img

--- tests/test-amp-img-sanitizer.php
+++ tests/test-amp-img-sanitizer.php
@@ -214,6 +214,16 @@ class AMP_Img_Sanitizer_Test extends WP_UnitTestCase {
 				'<amp-img src="http://placehold.it/100x100" layout="fixed" width="100" height="100"><noscript><img src="http://placehold.it/100x100" width="100" height="100"></noscript></amp-img>',
 				null,
 			),
+
+			'img_with_sizes_attribute_removed'         => array(
+				'<img width="825" height="510" src="http://placehold.it/825x510" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="" sizes="(max-width: 34.9rem) calc(100vw - 2rem), (max-width: 53rem) calc(8 * (100vw / 12)), (min-width: 53rem) calc(6 * (100vw / 12)), 100vw">',
+				'<amp-img width="825" height="510" src="http://placehold.it/825x510" class="attachment-post-thumbnail size-post-thumbnail wp-post-image amp-wp-enforced-sizes" alt="" layout="intrinsic"><noscript><img width="825" height="510" src="http://placehold.it/825x510" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="" sizes="(max-width: 34.9rem) calc(100vw - 2rem), (max-width: 53rem) calc(8 * (100vw / 12)), (min-width: 53rem) calc(6 * (100vw / 12)), 100vw"></noscript></amp-img>',
+			),
+
+			'amp_img_with_sizes_attribute_retained'    => array(
+				'<amp-img width="825" height="510" src="http://placehold.it/825x510" alt="" layout="intrinsic"></amp-img>',
+				null,
+			),
 		);
 	}
 
