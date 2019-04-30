164df7387a898499abb1e18de00b0e3826f5a0ff
Pascal Birchler <pascalb@google.com>
2019-04-30 15:44:22 +0200

    Extend amp-stories-fonts mock

--- tests/unit/__mocks__/amp-stories-fonts.js
+++ tests/unit/__mocks__/amp-stories-fonts.js
@@ -1,4 +1,22 @@
 module.exports = [
-	{ name: 'Arial' },
-	{ name: 'Roboto' },
+	{
+		name: 'Arial',
+		slug: 'arial',
+	},
+	{
+		name: 'Roboto',
+		slug: 'roboto',
+		handle: 'roboto-font',
+		src: 'https://fonts.googleapis.com/css?family=Roboto%3A400%2C700&subset=latin%2Clatin-ext',
+	},
+	{
+		name: 'Ubuntu',
+		slug: 'ubuntu',
+		src: 'https://fonts.googleapis.com/css?family=Ubuntu%3A400%2C700&subset=latin%2Clatin-ext',
+	},
+	{
+		name: 'Verdana',
+		slug: 'verdana',
+		handle: 'verdana-font',
+	},
 ];
