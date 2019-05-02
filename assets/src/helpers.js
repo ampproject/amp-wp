489bbe6c9faf569155e2b4428494fb21b2f00507
Pascal Birchler <pascalb@google.com>
2019-05-02 22:09:03 +0200

    Revert de81615e3af6ce79cac8dc09208608d758306d3b

--- assets/src/helpers.js
+++ assets/src/helpers.js
@@ -13,7 +13,7 @@ import { count } from '@wordpress/wordcount';
 import { __, _x, sprintf } from '@wordpress/i18n';
 import { select, dispatch } from '@wordpress/data';
 import { createBlock } from '@wordpress/blocks';
-import { getColorClassName, getColorObjectByAttributeValues, RichText } from '@wordpress/block-editor';
+import { getColorClassName, getColorObjectByAttributeValues, getFontSize, RichText } from '@wordpress/block-editor';
 
 /**
  * Internal dependencies
@@ -887,14 +887,14 @@ export const getStylesFromBlockAttributes = ( {
 } ) => {
 	const textClass = getColorClassName( 'color', textColor );
 
-	const { colors } = select( 'core/block-editor' ).getSettings();
+	const { colors, fontSizes } = select( 'core/block-editor' ).getSettings();
 
 	/*
      * Calculate font size using vw to make it responsive.
      *
      * Get the font size in px based on the slug with fallback to customFontSize.
      */
-	const userFontSize = fontSize ? getFontSizeFromSlug( fontSize ) : customFontSize;
+	const userFontSize = fontSize ? getFontSize( fontSizes, fontSize, customFontSize ).size : customFontSize;
 	const fontSizeResponsive = userFontSize && ( ( userFontSize / STORY_PAGE_INNER_WIDTH ) * 100 ).toFixed( 2 ) + 'vw';
 
 	const appliedBackgroundColor = getBackgroundColorWithOpacity( colors, backgroundColor, customBackgroundColor, opacity );
@@ -907,25 +907,6 @@ export const getStylesFromBlockAttributes = ( {
 		height: `${ getPercentageFromPixels( 'y', height ) }%`,
 		textAlign: align,
 	};
-};
-
-/**
- * Get font size from slug.
- *
- * @param {string} slug Font slug.
- * @return {number} Font size in pixels.
- */
-const getFontSizeFromSlug = ( slug ) => {
-	switch ( slug ) {
-		case 'small':
-			return 19.5;
-		case 'large':
-			return 36.5;
-		case 'huge':
-			return 49.5;
-		default:
-			return 16;
-	}
 };
 
 export const getMetaBlockSettings = ( { attribute, placeholder, tagName = 'p', isEditable = false } ) => {
