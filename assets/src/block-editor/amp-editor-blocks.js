385663e50cca960a286e956eb34679fc6116e138
Pascal Birchler <pascalb@google.com>
2019-05-14 16:02:03 -0700

    Don’t wait for domReady to unregister block types

--- assets/src/block-editor/amp-editor-blocks.js
+++ assets/src/block-editor/amp-editor-blocks.js
@@ -4,7 +4,6 @@
 import { addFilter } from '@wordpress/hooks';
 import { getBlockType, unregisterBlockType } from '@wordpress/blocks';
 import { select } from '@wordpress/data';
-import domReady from '@wordpress/dom-ready';
 
 /**
  * Internal dependencies
@@ -12,30 +11,23 @@ import domReady from '@wordpress/dom-ready';
 import { addAMPAttributes, filterBlocksEdit, filterBlocksSave, addAMPExtraProps } from './helpers';
 import './store';
 
-/**
- * Initialize editor integration.
+/*
+ * If there's no theme support, unregister blocks that are only meant for AMP.
+ * The Latest Stories block is meant for AMP and non-AMP, so don't unregister it here.
  */
-domReady( () => {
-	/*
-	 * If there's no theme support, unregister blocks that are only meant for AMP.
-	 * The Latest Stories block is meant for AMP and non-AMP, so don't unregister it here.
-	 */
-	const ampDependentBlocks = [
-		'amp-brid-player',
-		'amp-ima-video',
-		'amp-jwplayer',
-		'amp-mathml',
-		'amp-o2-player',
-		'amp-ooyala-player',
-		'amp-reach-player',
-		'amp-springboard-player',
-		'amp-timeago',
-	];
+const ampDependentBlocks = [
+	'amp-brid-player',
+	'amp-ima-video',
+	'amp-jwplayer',
+	'amp-mathml',
+	'amp-o2-player',
+	'amp-ooyala-player',
+	'amp-reach-player',
+	'amp-springboard-player',
+	'amp-timeago',
+];
 
-	if ( select( 'amp/block-editor' ).isNativeAMP() ) {
-		return;
-	}
-
+if ( ! select( 'amp/block-editor' ).isNativeAMP() ) {
 	for ( const block of ampDependentBlocks ) {
 		const blockName = 'amp/' + block;
 
@@ -43,7 +35,7 @@ domReady( () => {
 			unregisterBlockType( blockName );
 		}
 	}
-} );
+}
 
 addFilter( 'blocks.registerBlockType', 'ampEditorBlocks/addAttributes', addAMPAttributes );
 addFilter( 'blocks.getSaveElement', 'ampEditorBlocks/filterSave', filterBlocksSave );
