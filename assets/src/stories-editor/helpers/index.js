6621a7ae0ce88a4478e935bebbf18b1f72b20c23
Miina Sikk <miina.sikk@gmail.com>
2019-05-15 12:07:53 +0200

    Make getting percentage value from pixels more precise.

--- assets/src/stories-editor/helpers/index.js
+++ assets/src/stories-editor/helpers/index.js
@@ -684,9 +684,9 @@ export const calculateFontSize = ( measurer, expectedHeight, expectedWidth, maxF
  */
 export const getPercentageFromPixels = ( axis, pixelValue ) => {
 	if ( 'x' === axis ) {
-		return Math.round( ( pixelValue / STORY_PAGE_INNER_WIDTH ) * 100 );
+		return Number( ( ( pixelValue / STORY_PAGE_INNER_WIDTH ) * 100 ).toFixed( 2 ) );
 	} else if ( 'y' === axis ) {
-		return Math.round( ( pixelValue / STORY_PAGE_INNER_HEIGHT ) * 100 );
+		return Number( ( ( pixelValue / STORY_PAGE_INNER_HEIGHT ) * 100 ).toFixed( 2 ) );
 	}
 	return 0;
 };
