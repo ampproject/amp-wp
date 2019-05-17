f4e65ca3741c1ba1ed7049816bd0dbaf5d8d575f
Pascal Birchler <pascalb@google.com>
2019-05-17 14:01:49 -0700

    Update templates after #2296

--- includes/admin/class-amp-story-templates.php
+++ includes/admin/class-amp-story-templates.php
@@ -16,7 +16,7 @@ class AMP_Story_Templates {
 	 *
 	 * @var string
 	 */
-	const STORY_TEMPLATES_VERSION = '0.2.4';
+	const STORY_TEMPLATES_VERSION = '0.2.5';
 
 	/**
 	 * Slug for templates' taxonomy.
