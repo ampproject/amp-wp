3e533e1809a8724c66bb9ea8f59f83330bb6beaa
Pascal Birchler <pascalb@google.com>
2019-05-31 14:57:49 +0200

    Bump story template version

--- includes/admin/class-amp-story-templates.php
+++ includes/admin/class-amp-story-templates.php
@@ -16,7 +16,7 @@ class AMP_Story_Templates {
 	 *
 	 * @var string
 	 */
-	const STORY_TEMPLATES_VERSION = '0.2.6';
+	const STORY_TEMPLATES_VERSION = '0.2.7';
 
 	/**
 	 * Slug for templates' taxonomy.
