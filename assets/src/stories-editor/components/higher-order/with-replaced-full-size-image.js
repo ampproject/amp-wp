fb29ca8a227f5825ab4519c13e98aa71e4f5ef81
Weston Ruter <westonruter@google.com>
2019-06-11 15:33:30 -0700

    Add missing modifierName to createHigherOrderComponent

--- assets/src/stories-editor/components/higher-order/with-replaced-full-size-image.js
+++ assets/src/stories-editor/components/higher-order/with-replaced-full-size-image.js
@@ -17,54 +17,57 @@ import { MAX_IMAGE_SIZE_SLUG } from '../../constants';
 
 const BLOCK_EDITOR_STORE = 'core/block-editor';
 
-export default createHigherOrderComponent( ( BlockEdit ) => {
-	return class extends BlockEdit {
-		/**
-		 * The class constructor.
-		 */
-		constructor() {
-			super( ...arguments );
-			this.componentDidUpdate = this.componentDidUpdate.bind( this );
-			this.ampReplaceFullSizeImage = this.ampReplaceFullSizeImage.bind( this );
-		}
-
-		/**
-		 * When the component updates, if it's for the Image block, replace the full size image.
-		 */
-		componentDidUpdate() {
-			const { name } = this.props;
-			if ( 'core/image' === name ) {
-				this.ampReplaceFullSizeImage();
+export default createHigherOrderComponent(
+	( BlockEdit ) => {
+		return class extends BlockEdit {
+			/**
+			 * The class constructor.
+			 */
+			constructor() {
+				super( ...arguments );
+				this.componentDidUpdate = this.componentDidUpdate.bind( this );
+				this.ampReplaceFullSizeImage = this.ampReplaceFullSizeImage.bind( this );
 			}
 
-			if ( super.componentDidUpdate ) {
-				super.componentDidUpdate();
-			}
-		}
+			/**
+			 * When the component updates, if it's for the Image block, replace the full size image.
+			 */
+			componentDidUpdate() {
+				const { name } = this.props;
+				if ( 'core/image' === name ) {
+					this.ampReplaceFullSizeImage();
+				}
 
-		/**
-		 * Replaces the 'full' image size with a custom size, which has a limited height.
-		 *
-		 * This prevents the user from selecting an image in the Image block that's too big.
-		 * For example, 3200 x 4000.
-		 */
-		ampReplaceFullSizeImage() {
-			const initialImageSizes = get( select( BLOCK_EDITOR_STORE ).getSettings( 'imageSizes' ), [ 'imageSizes' ] );
-			if ( ! initialImageSizes ) {
-				return;
+				if ( super.componentDidUpdate ) {
+					super.componentDidUpdate();
+				}
 			}
 
-			// If the AMP Story slug isn't present, add it.
-			const sizesWithoutFullSize = initialImageSizes.filter( ( size ) => 'full' !== size.slug );
-			if ( ! sizesWithoutFullSize.filter( ( size ) => MAX_IMAGE_SIZE_SLUG === size.slug ).length ) {
-				sizesWithoutFullSize.push(
-					{
-						slug: MAX_IMAGE_SIZE_SLUG,
-						name: __( 'AMP Story Max Size', 'amp' ),
-					}
-				);
-				dispatch( BLOCK_EDITOR_STORE ).updateSettings( { imageSizes: sizesWithoutFullSize } );
+			/**
+			 * Replaces the 'full' image size with a custom size, which has a limited height.
+			 *
+			 * This prevents the user from selecting an image in the Image block that's too big.
+			 * For example, 3200 x 4000.
+			 */
+			ampReplaceFullSizeImage() {
+				const initialImageSizes = get( select( BLOCK_EDITOR_STORE ).getSettings( 'imageSizes' ), [ 'imageSizes' ] );
+				if ( ! initialImageSizes ) {
+					return;
+				}
+
+				// If the AMP Story slug isn't present, add it.
+				const sizesWithoutFullSize = initialImageSizes.filter( ( size ) => 'full' !== size.slug );
+				if ( ! sizesWithoutFullSize.filter( ( size ) => MAX_IMAGE_SIZE_SLUG === size.slug ).length ) {
+					sizesWithoutFullSize.push(
+						{
+							slug: MAX_IMAGE_SIZE_SLUG,
+							name: __( 'AMP Story Max Size', 'amp' ),
+						}
+					);
+					dispatch( BLOCK_EDITOR_STORE ).updateSettings( { imageSizes: sizesWithoutFullSize } );
+				}
 			}
-		}
-	};
-} );
+		};
+	},
+	'withReplacedFullSizeImage'
+);
