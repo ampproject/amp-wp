9117c5e2845c721e7bf42398d7d0d2b1b2c4833e
Pascal Birchler <pascalb@google.com>
2019-06-11 20:31:13 +0200

    Use AMP__DIR__ constant in main plugin file
    `__DIR__` is PHP 5.3+ only, and for the time being ideally the main plugin file should be parseable with PHP 5.2 as well.

--- amp.php
+++ amp.php
@@ -145,7 +145,7 @@ if ( count( $_amp_missing_functions ) > 0 ) {
 
 unset( $_amp_required_extensions, $_amp_missing_extensions, $_amp_required_constructs, $_amp_missing_classes, $_amp_missing_functions, $_amp_required_extension, $_amp_construct_type, $_amp_construct, $_amp_constructs );
 
-if ( ! file_exists( __DIR__ . '/vendor/autoload.php' ) || ! file_exists( __DIR__ . '/vendor/sabberworm/php-css-parser' ) || ! file_exists( __DIR__ . '/assets/js/amp-block-editor.js' ) ) {
+if ( ! file_exists( AMP__DIR__ . '/vendor/autoload.php' ) || ! file_exists( AMP__DIR__ . '/vendor/sabberworm/php-css-parser' ) || ! file_exists( AMP__DIR__ . '/assets/js/amp-block-editor.js' ) ) {
 	$_amp_load_errors->add(
 		'build_required',
 		sprintf(
