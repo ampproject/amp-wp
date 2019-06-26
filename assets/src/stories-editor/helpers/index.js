9311245f31428f861679900979bfe289b1149a87
Miina <miina.sikk@gmail.com>
2019-06-26 15:41:48 +0200

    Add caption toggle for video block. (#2646)

--- assets/src/stories-editor/helpers/index.js
+++ assets/src/stories-editor/helpers/index.js
@@ -311,6 +311,11 @@ export const addAMPAttributes = ( settings, name ) => {
 	}
 
 	if ( isVideoBlock ) {
+		addedAttributes.ampShowCaption = {
+			type: 'boolean',
+			default: false,
+		};
+
 		// Required defaults for AMP validity.
 		addedAttributes.autoplay = {
 			...settings.attributes.autoplay,
@@ -1030,21 +1035,28 @@ export const getMetaBlockSettings = ( { attribute, placeholder, tagName = 'p', i
 };
 
 /**
- * Removes a pre-set caption from image block.
+ * Removes a pre-set caption from image and video block.
  *
  * @param {string} clientId Block ID.
  */
-export const maybeRemoveImageCaption = ( clientId ) => {
+export const maybeRemoveMediaCaption = ( clientId ) => {
 	const block = getBlock( clientId );
 
-	if ( ! block || 'core/image' !== block.name ) {
+	const isImage = 'core/image' === block.name;
+	const isVideo = 'core/video' === block.name;
+
+	if ( ! block || ( ! isImage && ! isVideo ) ) {
 		return;
 	}
 
 	const { attributes } = block;
 
-	// If we have an image with pre-set caption we should remove the caption.
-	if ( ! attributes.ampShowImageCaption && attributes.caption && 0 !== attributes.caption.length ) {
+	// If we have an image or video with pre-set caption we should remove the caption.
+	if (
+		( ( ! attributes.ampShowImageCaption && isImage ) || ( ! attributes.ampShowCaption && isVideo ) ) &&
+			attributes.caption &&
+			0 !== attributes.caption.length
+	) {
 		updateBlockAttributes( clientId, { caption: '' } );
 	}
 };
