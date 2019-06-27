bcd64ecae7881b3aa21fc23fd024b8666dd5bfa9
Pascal Birchler <pascalb@google.com>
2019-06-27 16:06:36 +0200

    Restructure and update test setup
    Adds subfolders to `tests/` and updates the e2e config with to bring it in line with what has been added to core.

--- bin/install-wordpress.sh
+++ bin/install-wordpress.sh
@@ -3,31 +3,13 @@
 # Exit if any command fails.
 set -e
 
-# Gutenberg script includes.
+# Include useful functions
 . "$(dirname "$0")/includes.sh"
-
-# Set up environment variables
-. "$(dirname "$0")/bootstrap-env.sh"
 
 # These are the containers and values for the development site.
 CLI='cli'
 CONTAINER='wordpress'
 SITE_TITLE='AMP Dev'
-
-# If we're installing/re-installing the test site, change the containers used.
-if [ "$1" == '--e2e_tests' ]; then
-	CLI="${CLI}_e2e_tests"
-	CONTAINER="${CONTAINER}_e2e_tests"
-	SITE_TITLE='AMP Testing'
-
-	if ! docker ps | grep -q $CONTAINER; then
-		echo -e $(error_message "WordPress e2e tests run in their own Docker container, but that container wasn't found.")
-		echo "Please restart your Docker containers by running 'docker-compose $DOCKER_COMPOSE_FILE_OPTIONS down && docker-compose $DOCKER_COMPOSE_FILE_OPTIONS up -d' or"
-		echo "by running './bin/setup-local-env.sh' again."
-		echo ""
-		exit 1
-	fi
-fi
 
 # Get the host port for the WordPress container.
 HOST_PORT=$(docker-compose $DOCKER_COMPOSE_FILE_OPTIONS port $CONTAINER 80 | awk -F : '{printf $2}')
@@ -62,16 +44,6 @@ if [ "$E2E_ROLE" = "author" ]; then
 	docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm -u 33 $CLI post update 1 --post_author=2 --quiet
 fi
 
-# Make sure the uploads and upgrade folders exist and we have permissions to add files.
-echo -e $(status_message "Ensuring that files can be uploaded...")
-docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm $CONTAINER chmod 767 /var/www/html/wp-content/plugins
-docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm $CONTAINER chmod 767 /var/www/html/wp-config.php
-docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm $CONTAINER chmod 767 /var/www/html/wp-settings.php
-docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm $CONTAINER mkdir -p /var/www/html/wp-content/uploads
-docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm $CONTAINER chmod -v 767 /var/www/html/wp-content/uploads
-docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm $CONTAINER mkdir -p /var/www/html/wp-content/upgrade
-docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm $CONTAINER chmod 767 /var/www/html/wp-content/upgrade
-
 CURRENT_WP_VERSION=$(docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run -T --rm $CLI core version)
 echo -e $(status_message "Current WordPress version: $CURRENT_WP_VERSION...")
 
@@ -91,20 +63,13 @@ if [ "$CURRENT_URL" != "http://localhost:$HOST_PORT" ]; then
 	docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm -u 33 $CLI option update siteurl "http://localhost:$HOST_PORT" --quiet
 fi
 
-# Activate AMP plugin.
-echo -e $(status_message "Activating AMP plugin...")
-docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm -u 33 $CLI plugin activate amp --quiet
-
-if [ "$POPULAR_PLUGINS" == "true" ]; then
-	echo -e $(status_message "Activating popular plugins...")
-	docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm -u 33 $CLI plugin install advanced-custom-fields --activate --quiet
-	docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm -u 33 $CLI plugin install jetpack --activate --quiet
-	docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm -u 33 $CLI plugin install wpforms-lite --activate --quiet
-fi
-
 # Install a dummy favicon to avoid 404 errors.
 echo -e $(status_message "Installing a dummy favicon...")
 docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm $CONTAINER touch /var/www/html/favicon.ico
+
+# Activate AMP plugin.
+echo -e $(status_message "Activating AMP plugin...")
+docker-compose $DOCKER_COMPOSE_FILE_OPTIONS run --rm -u 33 $CLI plugin activate amp --quiet
 
 # Configure site constants.
 echo -e $(status_message "Configuring site constants...")
