bf8ccc04375513a369dcd473a7f9c75d93694443
Pascal Birchler <pascalb@google.com>
2019-07-01 10:49:58 +0200

    Add check for composer installation status

--- bin/local-env/start.sh
+++ bin/local-env/start.sh
@@ -9,10 +9,13 @@ set -e
 # Change to the expected directory
 cd "$(dirname "$0")/../.."
 
-# Check Node and NVM are installed
+# Check whether Node and NVM are installed
 . "$(dirname "$0")/install-node-nvm.sh"
 
-# Check Docker is installed and running
+# Check whether Composer installed
+. "$(dirname "$0")/install-composer.sh"
+
+# Check whether Docker is installed and running
 . "$(dirname "$0")/launch-containers.sh"
 
 # Set up WordPress Development site.
