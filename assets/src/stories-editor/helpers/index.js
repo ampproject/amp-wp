8f1fb37f7e39cd541d9ba75954196fcb5fae5e09
Pascal Birchler <pascalb@google.com>
2019-07-04 14:08:28 +0200

    Change block dimensions when text outgrows current one

--- assets/src/stories-editor/helpers/index.js
+++ assets/src/stories-editor/helpers/index.js
@@ -1153,7 +1153,7 @@ const getBlockInnerTextElement = ( block ) => {
 
 	switch ( name ) {
 		case 'amp/amp-story-text':
-			return document.querySelector( `#block-${ clientId } .block-editor-rich-text__editable.is-amp-fit-text` );
+			return document.querySelector( `#block-${ clientId } .block-editor-rich-text__editable` );
 
 		case 'amp/amp-story-post-title':
 		case 'amp/amp-story-post-author':
@@ -1208,6 +1208,40 @@ export const maybeUpdateFontSize = ( block ) => {
 				const fitFontSize = calculateFontSize( metaBlockElement, height, width, MAX_FONT_SIZE, MIN_FONT_SIZE );
 				if ( fitFontSize && autoFontSize !== fitFontSize ) {
 					updateBlockAttributes( clientId, { autoFontSize: fitFontSize } );
+				}
+			}
+
+			break;
+	}
+};
+
+/**
+ * Updates a block's width and height in case it doesn't use amp-fit-text and the font size has changed.
+ *
+ * @param {Object}  block                         Block object.
+ * @param {string}  block.clientId                Block client ID.
+ * @param {Object}  block.attributes              Block attributes.
+ * @param {number}  block.attributes.width        Block width in pixels.
+ * @param {number}  block.attributes.height       Block height in pixels.
+ * @param {string}  block.attributes.content      Block inner content.
+ * @param {boolean} block.attributes.ampFitText   Whether amp-fit-text should be used or not.
+ * @param {number}  block.attributes.autoFontSize Automatically determined font size for amp-fit-text blocks.
+ */
+export const maybeUpdateBlockDimensions = ( block ) => {
+	const { name, clientId, attributes } = block;
+	const { width, height, ampFitText, content } = attributes;
+
+	if ( ampFitText ) {
+		return;
+	}
+
+	switch ( name ) {
+		case 'amp/amp-story-text':
+			const element = getBlockInnerTextElement( block );
+
+			if ( element && content.length ) {
+				if ( element.offsetHeight > height && element.offsetWidth > width ) {
+					updateBlockAttributes( clientId, { height: element.offsetHeight, width: element.offsetWidth } );
 				}
 			}
 
