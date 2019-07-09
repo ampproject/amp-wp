e2133c787d5cc5a298c1f14b1f0df4b994065eba
Ryan Kienstra <kienstraryan@gmail.com>
2019-07-08 19:43:56 -0500

    Remove extra empty line, improve DocBlock heading
    The heading mentioned the file size,
    where it should have been type.

--- assets/src/common/helpers/index.js
+++ assets/src/common/helpers/index.js
@@ -286,9 +286,8 @@ export const enforceFileType = function( attachment, SelectionError ) {
 	}
 };
 
-
 /**
- * If the attachment has the wrong file type, this displays a notice in the Media Library and disables the 'Select' button.
+ * If the attachment has the wrong file size, this displays a notice in the Media Library and disables the 'Select' button.
  *
  * This is not an arrow function so that it can be called with enforceFileType.call( this, foo, bar ).
  *
