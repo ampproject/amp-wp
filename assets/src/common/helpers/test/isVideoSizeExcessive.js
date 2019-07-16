3ce5e76b6ee8868c814a0a248e3ac0b7e679f8df
Pascal Birchler <pascalb@google.com>
2019-07-16 13:54:54 +0200

    Use parameterized test for isVideoSizeExcessive

--- assets/src/common/helpers/test/isVideoSizeExcessive.js
+++ assets/src/common/helpers/test/isVideoSizeExcessive.js
@@ -1,26 +1,24 @@
 /**
+ * External dependencies
+ */
+import each from 'jest-each';
+
+/**
  * Internal dependencies
  */
 import { isVideoSizeExcessive } from '../';
 
 describe( 'isVideoSizeExcessive', () => {
-	it( 'should return true for a video with 3 MB per second', () => {
-		const videoSize = 12000000 / 4;
-		expect( isVideoSizeExcessive( videoSize ) ).toBe( true );
-	} );
-
-	it( 'should return true for a video with slightly over 1 MB per second', () => {
-		const videoSize = 4000001 / 4;
-		expect( isVideoSizeExcessive( videoSize ) ).toBe( true );
-	} );
-
-	it( 'should return false for a video with exactly 1 MB per second', () => {
-		const videoSize = 4000000 / 4;
-		expect( isVideoSizeExcessive( videoSize ) ).toBe( false );
-	} );
-
-	it( 'should return false for a video of much less than 1 MB per second', () => {
-		const videoSize = 4000000 / 4;
-		expect( isVideoSizeExcessive( videoSize ) ).toBe( false );
-	} );
+	// @todo Use numeric separator once using Node v12.5.0+, see https://v8.dev/features/numeric-separators
+	each( [
+		[ 3000000, true ],
+		[ 1000000.1, true ],
+		[ 1000000, false ],
+		[ 500000, false ],
+	] ).test(
+		'should return excessive video size status',
+		( bytesPerSecond, expected ) => {
+			expect( isVideoSizeExcessive( bytesPerSecond ) ).toBe( expected );
+		}
+	);
 } );
