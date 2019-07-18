731c328db877acb7805423045e64eb4b6d8ed06d
Luis Herranz <luisherranz@gmail.com>
2019-07-18 16:27:22 +0200

    Prettify again, using lint:php:fix instead of phpcbf.

--- includes/class-amp-rest-api.php
+++ includes/class-amp-rest-api.php
@@ -10,63 +10,60 @@
 /**
  * Class AMP_REST_API.
  */
-class AMP_REST_API
-{
-    /**
-     * Init.
-     */
-    public static function init()
-    {
-        if (! class_exists('AMP_REST_API') ) {
-            return;
-        }
+class AMP_REST_API {
 
-        add_action('rest_api_init', [ __CLASS__, 'rest_api_init' ]);
-    }
+	/**
+	 * Init.
+	 */
+	public static function init() {
+		if ( ! class_exists( 'AMP_REST_API' ) ) {
+			return;
+		}
 
-    /**
-     * Register other actions and filters to be used during the REST API initilization.
-     *
-     * @return void
-     */ 
-    public static function rest_api_init()
-    {
-        // Register a rest_prepare_{$post_type} filter for each one of the post types supported
-        // by the AMP plugin.
-        foreach( AMP_Post_Type_Support::get_builtin_supported_post_types() as $post_type ) {
-            $post_type_supported = post_type_supports($post_type, AMP_Post_Type_Support::SLUG);
-        
-            if ($post_type_supported ) {
-                add_filter('rest_prepare_' . $post_type, [ __CLASS__, 'add_content_amp_field' ], 10, 3);
-            }
-        }
-    }
+		add_action( 'rest_api_init', [ __CLASS__, 'rest_api_init' ] );
+	}
 
-    /**
-     * Adds a new field `amp` in the content of the REST API response.
-     *
-     * @param  WP_REST_Response $response Response object.
-     * @param  WP_Post          $post     Post object.
-     * @param  WP_REST_Request  $request  Request object.
-     * @return WP_REST_Response Response object.
-     */
-    public static function add_content_amp_field( $response, $post, $request )
-    {
-        // Check if the query param "_amp" is present.
-        $amp = is_string($request->get_param('_amp'));
-                                
-        // If "_amp" is present and there a content.rendered, output the content.amp field.
-        if ($amp && isset($response->data['content']['rendered'])) {
-            $response->data['content']['amp'] = 'An AMP valid version of content.rendered.';
-            $response->data['amp'] = array(
-                "styles" => "amp-img { border: 5px solid black; } ...",
-                "scripts" => array(
-                    "https://cdn.ampproject.org/v0/amp-install-serviceworker-0.1.js",
-                    "https://cdn.ampproject.org/v0/amp-animation-0.1.js",
-                    "https://cdn.ampproject.org/v0/amp-iframe-0.1.js"
-                )
-            );
-        }
-        return $response;
-    }
+	/**
+	 * Register other actions and filters to be used during the REST API initilization.
+	 *
+	 * @return void
+	 */
+	public static function rest_api_init() {
+		// Register a rest_prepare_{$post_type} filter for each one of the post types supported
+		// by the AMP plugin.
+		foreach ( AMP_Post_Type_Support::get_builtin_supported_post_types() as $post_type ) {
+			$post_type_supported = post_type_supports( $post_type, AMP_Post_Type_Support::SLUG );
+
+			if ( $post_type_supported ) {
+				add_filter( 'rest_prepare_' . $post_type, [ __CLASS__, 'add_content_amp_field' ], 10, 3 );
+			}
+		}
+	}
+
+	/**
+	 * Adds a new field `amp` in the content of the REST API response.
+	 *
+	 * @param  WP_REST_Response $response Response object.
+	 * @param  WP_Post          $post     Post object.
+	 * @param  WP_REST_Request  $request  Request object.
+	 * @return WP_REST_Response Response object.
+	 */
+	public static function add_content_amp_field( $response, $post, $request ) {
+		// Check if the query param "_amp" is present.
+		$amp = is_string( $request->get_param( '_amp' ) );
+
+		// If "_amp" is present and there a content.rendered, output the content.amp field.
+		if ( $amp && isset( $response->data['content']['rendered'] ) ) {
+			$response->data['content']['amp'] = 'An AMP valid version of content.rendered.';
+			$response->data['amp']            = [
+				'styles'  => 'amp-img { border: 5px solid black; } ...',
+				'scripts' => [
+					'https://cdn.ampproject.org/v0/amp-install-serviceworker-0.1.js',
+					'https://cdn.ampproject.org/v0/amp-animation-0.1.js',
+					'https://cdn.ampproject.org/v0/amp-iframe-0.1.js',
+				],
+			];
+		}
+		return $response;
+	}
 }
