52870b71099685b8e82916568702dca312f25ee6
Weston Ruter <westonruter@google.com>
2019-07-18 23:17:38 -0500

    Populate content.amp with markup, scripts, and styles

--- includes/class-amp-rest-api.php
+++ includes/class-amp-rest-api.php
@@ -11,7 +11,6 @@
  */
 class AMP_REST_API {
 
-
 	/**
 	 * Init.
 	 */
@@ -28,9 +27,7 @@ class AMP_REST_API {
 		// Register a rest_prepare_{$post_type} filter for each one of the post types supported
 		// by the AMP plugin.
 		foreach ( AMP_Post_Type_Support::get_eligible_post_types() as $post_type ) {
-			$post_type_supported = post_type_supports( $post_type, AMP_Post_Type_Support::SLUG );
-
-			if ( $post_type_supported ) {
+			if ( post_type_supports( $post_type, AMP_Post_Type_Support::SLUG ) && post_type_supports( $post_type, 'editor' ) ) {
 				add_filter( 'rest_prepare_' . $post_type, [ __CLASS__, 'add_content_amp_field' ], 10, 3 );
 			}
 		}
@@ -45,21 +42,63 @@ class AMP_REST_API {
 	 * @return WP_REST_Response Response object.
 	 */
 	public static function add_content_amp_field( $response, $post, $request ) {
-		// Check if the query param "_amp" is present.
-		$amp = is_string( $request->get_param( '_amp' ) );
-
-		// If "_amp" is present and there a content.rendered, output the content.amp field.
-		if ( $amp && isset( $response->data['content']['rendered'] ) ) {
-			$response->data['content']['amp'] = 'An AMP valid version of content.rendered.';
-			$response->data['amp']            = [
-				'styles'  => 'amp-img { border: 5px solid black; } ...',
-				'scripts' => [
-					'https://cdn.ampproject.org/v0/amp-install-serviceworker-0.1.js',
-					'https://cdn.ampproject.org/v0/amp-animation-0.1.js',
-					'https://cdn.ampproject.org/v0/amp-iframe-0.1.js',
-				],
-			];
+		// Skip if _amp param is not present.
+		// @todo Figure out a better way to selectively include amp content without having to introduce a new query var.
+		if ( false === $request->get_param( '_amp' ) || ! isset( $response->data['content']['rendered'] ) ) {
+			return $response;
 		}
+
+		$sanitizers     = amp_get_content_sanitizers();
+		$embed_handlers = AMP_Theme_Support::register_content_embed_handlers();
+		$sanitizers['AMP_Embed_Sanitizer']['embed_handlers'] = $embed_handlers;
+
+		$return_amp = static function() {
+			return 'amp';
+		};
+
+		add_filter( 'wp_video_shortcode_library', $return_amp );
+		add_filter( 'wp_audio_shortcode_library', $return_amp );
+
+		$data = [
+			'markup'  => '',
+			'styles'  => '',
+			'scripts' => [],
+		];
+
+		/** This filter is documented in wp-includes/post-template.php */
+		$content = apply_filters( 'the_content', $post->post_content );
+
+		$dom     = AMP_DOM_Utils::get_dom_from_content( $content );
+		$results = AMP_Content_Sanitizer::sanitize_document( $dom, $sanitizers, [ 'return_styles' => false ] );
+
+		$data['markup'] = AMP_DOM_Utils::get_content_from_dom( $dom );
+		$data['styles'] = $results['stylesheets'];
+		foreach ( $results['scripts'] as $handle => $src ) {
+			if ( true === $src && wp_script_is( $handle, 'registered' ) ) {
+				$src = wp_scripts()->registered[ $handle ]->src;
+			}
+
+			$async  = true;
+			$script = compact( 'src', 'async' );
+
+			if ( 'amp-mustache' === $handle ) {
+				$script['custom-template'] = $handle;
+			} else {
+				$script['custom-element'] = $handle;
+			}
+
+			$data['scripts'][ $handle ] = $script;
+		}
+
+		// Remove filters that to clean up applying filters for whatever comes next.
+		remove_filter( 'wp_video_shortcode_library', $return_amp );
+		remove_filter( 'wp_audio_shortcode_library', $return_amp );
+		foreach ( $embed_handlers as $embed_handler ) {
+			$embed_handler->unregister_embed();
+		}
+
+		$response->data['content']['amp'] = $data;
+
 		return $response;
 	}
 }
