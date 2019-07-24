cf16c5f834b55f14c95f1b1907045a3213448420
Pascal Birchler <pascalb@google.com>
2019-07-24 16:39:12 +0200

    Copy block insertion utilities from upstream
    The default openGlobalBlockInserter utility from the e2e-test-utils package does not work because the selector there is different from the one in the AMP Stories editor ("Add block" vs. "Add element").

--- tests/e2e/utils/index.js
+++ tests/e2e/utils/index.js
@@ -2,3 +2,6 @@ export { activatePlugin } from './activate-plugin';
 export { deactivatePlugin } from './deactivate-plugin';
 export { activateExperience } from './activate-experience';
 export { deactivateExperience } from './deactivate-experience';
+export { openGlobalBlockInserter } from './open-global-block-inserter';
+export { searchForBlock } from './search-for-block';
+export { insertBlock } from './insert-block';
