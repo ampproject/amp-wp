db7db86e7a2090e43fb389b5e06b69b1b5e3a03f
Pascal Birchler <pascalb@google.com>
2019-07-24 15:35:11 +0200

    Add and reorganize e2e test utils

---
+++ tests/e2e/utils/index.js
@@ -0,0 +1,4 @@
+export { activatePlugin } from './activate-plugin';
+export { deactivatePlugin } from './deactivate-plugin';
+export { activateExperience } from './activate-experience';
+export { deactivateExperience } from './deactivate-experience';
