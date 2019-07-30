e7319d56c5f8c0be4bdcdc6cdb25c6e68f2ce054
Miina Sikk <miina.sikk@gmail.com>
2019-07-30 18:20:18 +0300

    Try fixing tests.

--- tests/e2e/specs/stories-editor/animations.test.js
+++ tests/e2e/specs/stories-editor/animations.test.js
@@ -24,18 +24,18 @@ describe( 'Story Animations', () => {
 	it( 'should save correct animation values', async () => {
 		// Add Author block with animation.
 		await insertBlock( 'Author' );
-		page.waitForSelector( '.components-select-control__input' );
+		await page.waitForSelector( '.components-select-control__input' );
 		await page.select( '.components-select-control__input', 'fly-in-bottom' );
-		page.waitForSelector( 'input[aria-label="Delay (ms)"]' );
+		await page.waitForSelector( 'input[aria-label="Delay (ms)"]' );
 		await page.type( 'input[aria-label="Delay (ms)"]', '15' );
 
 		await saveDraft();
 		await page.reload();
 
-		page.waitForSelector( '.wp-block-amp-amp-story-post-author' );
+		await page.waitForSelector( '.wp-block-amp-amp-story-post-author' );
 
 		await selectBlockByClassName( 'wp-block-amp-amp-story-post-author' );
-		page.waitForSelector( 'input[aria-label="Delay (ms)"]' );
+		await page.waitForSelector( 'input[aria-label="Delay (ms)"]' );
 
 		expect( await page.evaluate( () => {
 			return document.querySelector( 'input[aria-label="Delay (ms)"]' ).value;
@@ -49,31 +49,30 @@ describe( 'Story Animations', () => {
 	it( 'should save correct animation after values', async () => {
 		// Add Author block with animation.
 		await insertBlock( 'Author' );
-		page.waitForSelector( '.components-select-control__input' );
+		await page.waitForSelector( '.components-select-control__input' );
 		await page.select( '.components-select-control__input', 'fly-in-bottom' );
 
 		// Add Date block with animation.
 		await insertBlock( 'Date' );
-		page.waitForSelector( '.components-select-control__input' );
+		await page.waitForSelector( '.components-select-control__input' );
 		await page.select( '.components-select-control__input', 'pulse' );
-		page.waitForSelector( 'label[for="amp-stories-animation-order-picker"]' );
+
+		// Add Author block as Begin After.
+		await page.waitForSelector( 'button[aria-label="Begin immediately"]' );
 		await clickButton( 'Immediately' );
 
-		page.waitForSelector( '.components-animate__appear' );
+		page.waitForSelector( '.components-animate__appear button[aria-checked="false"]' );
 		await page.click( '.components-animate__appear button[aria-checked="false"]' );
 		await saveDraft();
 		await page.reload();
 
-		page.waitForSelector( '.wp-block-amp-amp-story-post-author' );
-
-		await selectBlockByClassName( 'wp-block-amp-amp-story-post-author' );
-		page.waitForSelector( 'input[aria-label="Delay (ms)"]' );
-
+		await page.waitForSelector( '.wp-block-amp-amp-story-post-date' );
 		await selectBlockByClassName( 'wp-block-amp-amp-story-post-date' );
-		page.waitForSelector( '.components-animate__appear' );
+
+		page.waitForSelector( '.components-animate__appear button[aria-label="Begin after: Story Author"]' );
 
 		expect( await page.evaluate( () => {
-			return document.querySelector( 'button[aria-label="Begin after: Story Author"]' ).innerHTML;
+			//return document.querySelector( '.components-animate__appear button[aria-label="Begin after: Story Author"]' ).innerHTML;
 		} ) ).toContain( 'admin' );
 	} );
 
@@ -85,17 +84,17 @@ describe( 'Story Animations', () => {
 		// Add Date block with animation.
 		await insertBlock( 'Date' );
 		await page.select( '.components-select-control__input', 'pulse' );
-		page.waitForSelector( 'label[for="amp-stories-animation-order-picker"]' );
+		await page.waitForSelector( 'label[for="amp-stories-animation-order-picker"]' );
 		await clickButton( 'Immediately' );
 
-		page.waitForSelector( '.components-animate__appear' );
+		await page.waitForSelector( '.components-animate__appear button[aria-checked="false"]' );
 		await page.click( '.components-animate__appear button[aria-checked="false"]' );
 
 		await selectBlockByClassName( 'wp-block-amp-amp-story-post-author' );
 
-		page.waitForSelector( 'label[for="amp-stories-animation-order-picker"]' );
+		await page.waitForSelector( 'label[for="amp-stories-animation-order-picker"]' );
 		await clickButton( 'Immediately' );
-		page.waitForSelector( '.components-animate__appear' );
+		await page.waitForSelector( '.components-animate__appear' );
 
 		expect( await page.evaluate( () => {
 			return document.querySelector( '.components-animate__appear button[aria-checked="false"]' );
