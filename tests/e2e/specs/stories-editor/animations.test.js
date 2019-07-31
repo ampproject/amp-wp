fc6f2913d2e46eca90b9c9151b374de07db2b76d
Miina Sikk <miina.sikk@gmail.com>
2019-07-31 13:14:01 +0300

    Add skipped test for fixing later.

--- tests/e2e/specs/stories-editor/animations.test.js
+++ tests/e2e/specs/stories-editor/animations.test.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { last } from 'lodash';
+
+/**
  * WordPress dependencies
  */
 import { createNewPost, clickButton, saveDraft } from '@wordpress/e2e-test-utils';
@@ -113,5 +118,36 @@ describe( 'Story Animations', () => {
 		expect( await page.evaluate( () => {
 			return document.querySelector( 'button[aria-label="Begin immediately"]' ).innerHTML;
 		} ) ).toContain( 'Immediately' );
+	} );
+
+	// This test is skipped since for some reason the preview tab stays waiting.
+	// @todo Needs fixing.
+	it.skip( 'should save ID to the same element as animation', async () => { // eslint-disable-line jest/no-disabled-tests
+		// Add Author block with animation.
+		await insertBlock( 'Author' );
+		await page.select( animationTypeSelector, 'pulse' );
+		await saveDraft();
+
+		const previewSelector = '.editor-post-preview';
+		await page.waitForSelector( previewSelector );
+
+		let openTabs = await browser.pages();
+		const expectedTabsCount = openTabs.length + 1;
+
+		await page.click( previewSelector );
+		await page.waitForNavigation();
+
+		// Wait for the new tab to open.
+		while ( openTabs.length < expectedTabsCount ) {
+			openTabs = await browser.pages(); // eslint-disable-line
+		}
+
+		const previewPage = last( openTabs );
+		await previewPage.waitForSelector( '.amp-story-block-wrapper' );
+
+		expect( await previewPage.evaluate( () => {
+			const el = document.querySelector( '.amp-story-block-wrapper' );
+			return el.getAttribute( 'id' ).length && el.getAttribute( 'animate-in' ).length;
+		} ) ).toBe( true );
 	} );
 } );
