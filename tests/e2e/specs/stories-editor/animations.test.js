624b75c9b8af3622f41d8f9c87d6ab389432f75c
Miina Sikk <miina.sikk@gmail.com>
2019-08-05 18:15:52 +0300

    Add test with correct animation settings.

--- tests/e2e/specs/stories-editor/animations.test.js
+++ tests/e2e/specs/stories-editor/animations.test.js
@@ -114,4 +114,39 @@ describe( 'Story Animations', () => {
 			return document.querySelector( 'button[aria-label="Begin immediately"]' ).innerHTML;
 		} ) ).toContain( 'Immediately' );
 	} );
+
+	it( 'should save ID to the same element as animation', async () => {
+		// Add Author block with animation.
+		await insertBlock( 'Author' );
+		await page.select( animationTypeSelector, 'pulse' );
+
+		// Add Date block with animation.
+		await insertBlock( 'Date' );
+		await page.waitForSelector( animationTypeSelector );
+		await page.select( animationTypeSelector, 'pulse' );
+
+		// Add Author block as Begin After.
+		await page.waitForSelector( 'button[aria-label="Begin immediately"]' );
+		await clickButton( 'Immediately' );
+
+		await page.waitForSelector( unSelectedAnimationSelector );
+		await page.evaluate( ( selector ) => {
+			document.querySelector( selector ).click();
+		}, unSelectedAnimationSelector );
+		await saveDraft();
+
+		const editorPage = page;
+		const previewPage = await openPreviewPage( editorPage, 'amp-story' );
+		await previewPage.waitForSelector( '.amp-story-block-wrapper' );
+
+		let [ elementHandle ] = await previewPage.$x( '//*[contains(@class,"amp-story-block-wrapper")]/@animate-in' );
+		const animationHandle = await elementHandle.getProperty( 'value' );
+		const animateValue = await animationHandle.jsonValue();
+		expect( animateValue ).toBe( 'pulse' );
+
+		[ elementHandle ] = await previewPage.$x( '//*[contains(@class,"amp-story-block-wrapper")]/@id' );
+		const idHandle = await elementHandle.getProperty( 'value' );
+		const idValue = await idHandle.jsonValue();
+		expect( idValue ).not.toBeUndefined();
+	} );
 } );
