c32c617904a62132e090f24ff83f8ae90c26b927
Ryan Kienstra <kienstraryan@gmail.com>
2019-08-04 19:09:18 -0500

    Add an e2e test for the Block Manager being disabled
    Add a very simple test
    that the text 'Block Manager doesn't appear.

---
+++ tests/e2e/specs/stories-editor/block-manager-disabled.test.js
@@ -0,0 +1,35 @@
+/**
+ * WordPress dependencies
+ */
+import { createNewPost, getAllBlocks, selectBlockByClientId } from '@wordpress/e2e-test-utils';
+
+/**
+ * Internal dependencies
+ */
+import { activateExperience, deactivateExperience } from '../../utils';
+
+describe( 'Block Manager', () => {
+	beforeAll( async () => {
+		await activateExperience( 'stories' );
+	} );
+
+	afterAll( async () => {
+		await deactivateExperience( 'stories' );
+	} );
+
+	it( 'the button should not appear due to styling of display: none', async () => {
+		await createNewPost( { postType: 'amp_story' } );
+
+		// Select the page block.
+		await selectBlockByClientId(
+			( await getAllBlocks() )[ 0 ].clientId
+		);
+
+		// Click the 'More tools & options' button to expand the popover.
+		const buttonSelector = '.components-button.components-icon-button.components-dropdown-menu__toggle';
+		await page.waitForSelector( buttonSelector );
+		await page.click( buttonSelector );
+
+		await expect( page ).not.toMatch( 'Block Manager' );
+	} );
+} );
