d6326b2ef1e1e832a485dee44d5461417adee2e3
Pascal Birchler <pascalb@google.com>
2019-08-07 14:16:37 +0200

    Move block validation CSS to component

--- assets/src/block-validation/components/higher-order/with-validation-error-notice/index.js
+++ assets/src/block-validation/components/higher-order/with-validation-error-notice/index.js
@@ -9,7 +9,8 @@ import { withSelect } from '@wordpress/data';
 /**
  * Internal dependencies
  */
-import { ValidationErrorMessage } from '../';
+import { ValidationErrorMessage } from '../../';
+import './edit.css';
 
 const applyWithSelect = withSelect( ( select, { clientId } ) => {
 	const { getBlockValidationErrors } = select( 'amp/block-validation' );
