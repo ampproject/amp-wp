c42812735e4f2c6721f86ee376cd26964a3cf73a
Miina Sikk <miina.sikk@gmail.com>
2019-08-12 18:07:57 +0300

    Continue implementing the embed logic.

--- assets/src/stories-editor/blocks/amp-story-page-attachment/edit.js
+++ assets/src/stories-editor/blocks/amp-story-page-attachment/edit.js
@@ -3,6 +3,7 @@
  */
 import classnames from 'classnames';
 import PropTypes from 'prop-types';
+import { map } from 'lodash';
 
 /**
  * WordPress dependencies
@@ -11,12 +12,17 @@ import { __ } from '@wordpress/i18n';
 import { Component } from '@wordpress/element';
 import { apiFetch } from '@wordpress/api-fetch';
 import { addQueryArgs } from '@wordpress/url';
+import { decodeEntities } from '@wordpress/html-entities';
+import { Popover, Spinner, withSpokenMessages } from '@wordpress/components';
+import { withInstanceId, compose } from '@wordpress/compose';
 
 class PageAttachmentEdit extends Component {
 	constructor( props ) {
 		super( props );
 		this.state = {
 			suggestions: [],
+			showSuggestions: false,
+			selectedSuggestion: null,
 		};
 	}
 
@@ -31,32 +37,104 @@ class PageAttachmentEdit extends Component {
 
 		return map( posts, ( post ) => ( {
 			id: post.id,
+			title: decodeEntities( post.title ) || __( '(no title)', 'amp' ),
 		} ) );
-	};
+	}
+
+	componentWillUnmount() {
+		delete this.suggestionsRequest;
+	}
+
+	onChange( event ) {
+		const { setAttributes } = this.props;
+		const inputValue = event.target.value;
+		//setAttributes( { postId: inputValue } );
+		this.updateSuggestions( inputValue );
+	}
+
+	updateSuggestions( value ) {
+		// Show the suggestions after typing at least 2 characters
+		// and also for URLs
+		if ( value.length < 2 || /^https?:/.test( value ) ) {
+			this.setState( {
+				showSuggestions: false,
+				selectedSuggestion: null,
+				loading: false,
+			} );
+
+			return;
+		}
+
+		this.setState( {
+			showSuggestions: true,
+			selectedSuggestion: null,
+			loading: true,
+		} );
+
+		const request = this.fetchPostSuggestions( value );
+
+		request.then( ( suggestions ) => {
+			// A fetch Promise doesn't have an abort option. It's mimicked by
+			// comparing the request reference in on the instance, which is
+			// reset or deleted on subsequent requests or unmounting.
+			if ( this.suggestionsRequest !== request ) {
+				return;
+			}
+
+			this.setState( {
+				suggestions,
+				loading: false,
+			} );
+
+			if ( !! suggestions.length ) {
+				this.props.debouncedSpeak( sprintf( _n(
+					'%d result found, use up and down arrow keys to navigate.',
+					'%d results found, use up and down arrow keys to navigate.',
+					suggestions.length
+				), suggestions.length ), 'assertive' );
+			} else {
+				this.props.debouncedSpeak( __( 'No results.' ), 'assertive' );
+			}
+		} ).catch( () => {
+			if ( this.suggestionsRequest === request ) {
+				this.setState( {
+					loading: false,
+				} );
+			}
+		} );
+
+		this.suggestionsRequest = request;
+	}
 
 	render() {
 		const {
 			attributes,
 			setAttributes,
 			isSelected,
+			instanceId,
 		} = this.props;
 
 		const {
 			postId,
 		} = attributes;
 
+		const { showSuggestions, suggestions, selectedSuggestion, loading } = this.state;
+
+		const suggestionsListboxId = `block-editor-url-input-suggestions-${ instanceId }`;
+		const suggestionOptionIdPrefix = `block-editor-url-input-suggestion-${ instanceId }`;
+
 		return (
 			<>
 				<div>test</div>
 				{ isSelected && (
-					<div className={ classnames( 'editor-url-input block-editor-url-input', 'test' ) }>
+					<div className="amp-page-attachment-input">
 						<input
 							id={ 'test' }
 							type="text"
 							aria-label={ __( 'ID' ) }
 							required
 							value={ postId }
-							onChange={ ( value ) => setAttributes( { postId: value } ) }
+							onChange={ this.onChange }
 							onInput={ ( event ) => {
 								event.stopPropagation();
 							} }
@@ -71,6 +149,41 @@ class PageAttachmentEdit extends Component {
 						/>
 
 						{ ( loading ) && <Spinner /> }
+
+						{ showSuggestions && !! suggestions.length &&
+						<Popover
+							position="bottom"
+							noArrow
+							focusOnMount={ false }
+						>
+							<div
+								className={ classnames(
+									'editor-url-input__suggestions',
+									'block-editor-url-input__suggestions',
+								) }
+								id={ suggestionsListboxId }
+								ref={ this.autocompleteRef }
+								role="listbox"
+							>
+								{ suggestions.map( ( suggestion, index ) => (
+									<button
+										key={ suggestion.id }
+										role="option"
+										tabIndex="-1"
+										id={ `${ suggestionOptionIdPrefix }-${ index }` }
+										ref={ this.bindSuggestionNode( index ) }
+										className={ classnames( 'editor-url-input__suggestion block-editor-url-input__suggestion', {
+											'is-selected': index === selectedSuggestion,
+										} ) }
+										onClick={ () => this.handleOnClick( suggestion ) }
+										aria-selected={ index === selectedSuggestion }
+									>
+										{ suggestion.title }
+									</button>
+								) ) }
+							</div>
+						</Popover>
+						}
 					</div>
 				) }
 			</>
@@ -84,4 +197,7 @@ PageAttachmentEdit.propTypes = {
 	} ).isRequired,
 };
 
-export default PageAttachmentEdit;
+export default compose(
+	withSpokenMessages,
+	withInstanceId,
+)( PageAttachmentEdit );
