614c009a8b217f2ef6f8cba20fc97a3c72b9100f
Pascal Birchler <pascalb@google.com>
2019-08-13 12:12:26 +0200

    Fix selector

--- tests/e2e/specs/stories-editor/stories-admin-pointer.test.js
+++ tests/e2e/specs/stories-editor/stories-admin-pointer.test.js
@@ -7,6 +7,6 @@ describe( 'AMP Stories Admin Pointer', () => {
 	it( 'should be displayed', async () => {
 		await visitAdminPage( '/' );
 
-		await expect( page ).toMatchElement( '.wp-admin-pointer', { text: 'You can now enable Stories' } );
+		await expect( page ).toMatchElement( '.wp-amp-pointer', { text: 'You can now enable Stories' } );
 	} );
 } );
