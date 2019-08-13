a259b304a9c6b225fd5ddf8caa3d318e328630a7
Miina Sikk <miina.sikk@gmail.com>
2019-08-13 18:14:18 +0300

    Remove duplicate import.

--- assets/src/stories-editor/blocks/amp-story-cta/index.js
+++ assets/src/stories-editor/blocks/amp-story-cta/index.js
@@ -11,7 +11,6 @@ import edit from './edit';
 import save from './save';
 import deprecated from './deprecated';
 import blockIcon from '../../../../images/stories-editor/call-to-action.svg';
-import { createBlock, getBlockAttributes } from '@wordpress/blocks';
 
 const schema = {
 	url: {
