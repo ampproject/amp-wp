1f1cd0eae4a6f1d6dfb6927dcb3cda66421cb1fa
Pascal Birchler <pascalb@google.com>
2019-08-13 20:07:37 +0200

    Fix poster image extraction (#3021)
    * Use image/jpeg mime type for extracted poster image
    * Reject promise on `MediaError`

--- assets/src/stories-editor/helpers/index.js
+++ assets/src/stories-editor/helpers/index.js
@@ -1553,8 +1553,6 @@ export const getUniqueId = () => {
 /**
  * Returns an image of the first frame of a given video.
  *
- * @todo Perhaps allow specifying wanted image type.
- *
  * @param {string} src Video src URL.
  * @return {Promise<string>} The extracted image in base64-encoded format.
  */
@@ -1562,10 +1560,13 @@ export const getFirstFrameOfVideo = ( src ) => {
 	const video = document.createElement( 'video' );
 	video.muted = true;
 	video.crossOrigin = 'anonymous';
+	video.preload = 'metadata';
+	video.currentTime = 0.5; // Needed to seek forward.
 
-	return new Promise( ( resolve ) => {
-		video.src = src;
-		video.addEventListener( 'loadeddata', () => {
+	return new Promise( ( resolve, reject ) => {
+		video.addEventListener( 'error', reject );
+
+		video.addEventListener( 'canplay', () => {
 			const canvas = document.createElement( 'canvas' );
 			canvas.width = video.videoWidth;
 			canvas.height = video.videoHeight;
@@ -1573,8 +1574,10 @@ export const getFirstFrameOfVideo = ( src ) => {
 			const ctx = canvas.getContext( '2d' );
 			ctx.drawImage( video, 0, 0, canvas.width, canvas.height );
 
-			canvas.toBlob( resolve );
+			canvas.toBlob( resolve, 'image/jpeg' );
 		} );
+
+		video.src = src;
 	} );
 };
 
