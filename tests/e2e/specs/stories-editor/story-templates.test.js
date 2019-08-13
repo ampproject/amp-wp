23eb566db180f61e472ca6de04f1f9056fc1ab19
Miina Sikk <miina.sikk@gmail.com>
2019-08-13 17:53:13 +0300

    Add tests for ensuring that templates have expected content within preview.

--- tests/e2e/specs/stories-editor/story-templates.test.js
+++ tests/e2e/specs/stories-editor/story-templates.test.js
@@ -92,5 +92,44 @@ describe( 'Story Templates', () => {
 			const defaultStyledQuote = await page.$$eval( '.amp-page-active .wp-block-quote.is-style-white', ( elements ) => elements.length );
 			expect( defaultStyledQuote ).toStrictEqual( 0 );
 		} );
+
+		it( 'should contain expected content in the template preview', async () => {
+			await createNewPost( { postType: 'amp_story' } );
+
+			await openTemplateInserter();
+
+			// Wait until the templates are loaded and blocks accessible.
+			await page.waitForSelector( '.block-editor-block-preview .wp-block' );
+
+			const templateContents = await page.evaluate( () => {
+				const contents = [];
+				const previews = document.querySelectorAll( '.block-editor-block-preview .block-editor-inner-blocks.has-overlay' );
+				previews.forEach( function( preview, index ) {
+					contents[ index ] = preview.innerHTML;
+				} );
+				return contents;
+			} );
+
+			// Travel Tip template.
+			expect( templateContents[ 0 ] ).toContain( 'This template is great for sharing tips' );
+			// Quote template.
+			expect( templateContents[ 1 ] ).toContain( '<p>Everyone soon or late comes round Rome</p>' );
+			// Travel CTA template.
+			expect( templateContents[ 2 ] ).toContain( '<strong>Show packing tips</strong>' );
+			// Title Page.
+			expect( templateContents[ 3 ] ).toContain( 'wp-block-amp-amp-story-post-date has-text-color' );
+			// Vertical.
+			expect( templateContents[ 4 ] ).toContain( 'Journey into the past' );
+			// Fandom Title.
+			expect( templateContents[ 5 ] ).toContain( 'SPOILERS ALERT' );
+			// Fandom CTA.
+			expect( templateContents[ 6 ] ).toContain( '<strong>S</strong>tart Quiz' );
+			// Fandom Fact.
+			expect( templateContents[ 7 ] ).toContain( 'Robb Start made Jon his heir<br data-rich-text-line-break=\"true\">(in books)' );
+			// Fandom Fact Text.
+			expect( templateContents[ 8 ] ).toContain( 'One of the biggest things missing from the show is the fact that before his death, Robb Start legitimizes Jon Snow as a Stark and makes him his heir.' );
+			// Fandom Intro
+			expect( templateContents[ 9 ] ).toContain( 'got-logo.png' );
+		} );
 	} );
 } );
