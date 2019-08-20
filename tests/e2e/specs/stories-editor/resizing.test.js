5b4fa2da6026981d369871802769a47f58b51d87
Miina Sikk <miina.sikk@gmail.com>
2019-08-20 12:35:18 +0300

    Add test for resizing to min width and height.

--- tests/e2e/specs/stories-editor/resizing.test.js
+++ tests/e2e/specs/stories-editor/resizing.test.js
@@ -22,17 +22,35 @@ describe( 'Resizing', () => {
 	} );
 
 	describe( 'Text block', () => {
-		beforeAll( async () => {
+		beforeEach( async () => {
 			await createNewPost( { postType: 'amp_story' } );
 			// Select the Text block inserted by default.
 			await selectBlockByClassName( 'wp-block-amp-story-text' );
 		} );
 
 		it( 'it should not resize smaller than the set minimum width and height', async () => {
-			const resizableHandle = await page.$( '.wp-block.is-selected .components-resizable-box__handle-bottom' );
-			await page.waitFor( 4000 );
-			await dragAndResize( resizableHandle, { x: 0, y: 50 } );
-			await page.waitFor( 4000 );
+			const textBlockMinHeight = 30;
+			const textBlockMinWidth = 40;
+			let height, width;
+			const resizableHandleBottom = await page.$( '.wp-block.is-selected .components-resizable-box__handle-bottom' );
+			await dragAndResize( resizableHandleBottom, { x: 0, y: -250 } );
+			height = await page.evaluate( () => document.querySelector( '.wp-block.is-selected' ).clientHeight );
+			expect( height ).toStrictEqual( textBlockMinHeight );
+
+			const resizableHandleTop = await page.$( '.wp-block.is-selected .components-resizable-box__handle-top' );
+			await dragAndResize( resizableHandleTop, { x: 0, y: 250 } );
+			height = await page.evaluate( () => document.querySelector( '.wp-block.is-selected' ).clientHeight );
+			expect( height ).toStrictEqual( textBlockMinHeight );
+
+			const resizableHandleLeft = await page.$( '.wp-block.is-selected .components-resizable-box__handle-left' );
+			await dragAndResize( resizableHandleLeft, { x: 300, y: 0 } );
+			width = await page.evaluate( () => document.querySelector( '.wp-block.is-selected' ).clientWidth );
+			expect( width ).toStrictEqual( textBlockMinWidth );
+
+			const resizableHandleRight = await page.$( '.wp-block.is-selected .components-resizable-box__handle-right' );
+			await dragAndResize( resizableHandleRight, { x: -300, y: 0 } );
+			width = await page.evaluate( () => document.querySelector( '.wp-block.is-selected' ).clientWidth );
+			expect( width ).toStrictEqual( textBlockMinWidth );
 		} );
 	} );
 } );
