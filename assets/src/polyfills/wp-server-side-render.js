51424a5d68534b17f254fade794d967dd1feb5de
Weston Ruter <westonruter@google.com>
2019-08-20 13:01:09 -0700

    Build 1.2.1-RC1

--- assets/src/polyfills/wp-server-side-render.js
+++
@@ -1,8 +0,0 @@
-/**
- * WordPress dependencies
- */
-import * as serverSideRender from '@wordpress/server-side-render';
-
-window.wp = window.wp || {};
-
-window.wp.serverSideRender = serverSideRender;
