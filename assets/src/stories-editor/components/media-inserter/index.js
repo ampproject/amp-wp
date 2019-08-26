8d976662da6f888cc46c6fe7bccb0aa2942b0ef7
Jonny Harris <jon@spacedmonkey.co.uk>
2019-08-26 14:08:42 +0100

    Refactor code to use controls array notation. Also now saves the background to the block.

--- assets/src/stories-editor/components/media-inserter/index.js
+++ assets/src/stories-editor/components/media-inserter/index.js
@@ -7,71 +7,95 @@ import PropTypes from 'prop-types';
  * WordPress dependencies
  */
 import { getBlockType, createBlock } from '@wordpress/blocks';
-import { BlockIcon, MediaUpload, MediaUploadCheck } from '@wordpress/block-editor';
+import { BlockIcon } from '@wordpress/block-editor';
 import { withDispatch, withSelect } from '@wordpress/data';
-import { DropdownMenu, MenuGroup, MenuItem } from '@wordpress/components';
-import { Fragment } from '@wordpress/element';
+import { DropdownMenu } from '@wordpress/components';
 import { compose, ifCondition } from '@wordpress/compose';
 import { __, sprintf } from '@wordpress/i18n';
+/**
+ * Internal dependencies
+ */
+import { IMAGE_BACKGROUND_TYPE, VIDEO_BACKGROUND_TYPE } from '../../constants';
 
-const MediaInsider = ( { insertBlock, canInsertBlockType, showInserter } ) => {
-	const instructions = <p>{ __( 'To edit the background image or video, you need permission to upload media.', 'amp' ) }</p>;
-	const mediaId = 0;
+const MediaInsider = ( { insertBlock, updateBlock, canInsertBlockType, showInserter } ) => {
+	const blocks = [
+		'core/video',
+		'core/image',
+	];
+
+	const dropDownOptions = [
+		{
+			title: __( 'Insert background image', 'amp' ),
+			icon: 'format-image',
+			onClick: () => mediaPicker( __( 'Select background image', 'amp' ), 'image', updateBlock ),
+			disabled: ! showInserter,
+		},
+		{
+			title: __( 'Insert background video', 'amp' ),
+			icon: 'media-video',
+			onClick: () => mediaPicker( __( 'Select background video', 'amp' ), 'video', updateBlock ),
+			disabled: ! showInserter,
+		},
+	];
+
+	blocks.map( ( block ) => {
+		if ( ! canInsertBlockType( block ) ) {
+			return null;
+		}
+
+		const blockType = getBlockType( block );
+		const item = {
+			title: sprintf( __( 'Insert %s', 'amp' ), blockType.title ),
+			onClick: () => insertBlock( block ),
+			disabled: ! showInserter,
+			icon: <BlockIcon icon={ blockType.icon } />,
+		};
+		dropDownOptions.unshift( item );
+		return null;
+	} );
+
 	return (
 		<DropdownMenu
 			icon="admin-media"
-			label="Select a media"
-		>
-			{ ( { onClose } ) => (
-				<Fragment>
-					<MenuGroup>
-						<MenuItem
-							icon="image"
-							onClick={ () => insertBlock( 'core/image' )  }
-						>
-							{ __( 'Insert image', 'amp' ) }
-						</MenuItem>
-						<MediaUploadCheck fallback={ instructions }>
-							<MediaUpload
-								onSelect={ ( media ) => console.log( 'selected ' + media.length ) }
-								value={ mediaId }
-								render={ ( { open } ) => (
-								<MenuItem
-									icon="format-image"
-									onClick={ open }
-										>
-										{ __( 'Insert background image', 'amp' ) }
-								</MenuItem>
-							) }
-							/>
-						</MediaUploadCheck>
-						<MediaUploadCheck fallback={ instructions }>
-							<MediaUpload
-								onSelect={ ( media ) => console.log( 'selected ' + media.length ) }
-								allowedTypes={ [ 'video' ] }
-								value={ mediaId }
-								render={ ( { open } ) => (
-									<MenuItem
-										icon="media-video"
-										onClick={ open }
-									>
-										{ __( 'Insert background video', 'amp' ) }
-									</MenuItem>
-								) }
-							/>
-						</MediaUploadCheck>
-
-					</MenuGroup>
-				</Fragment>
-			) }
-		</DropdownMenu>
+			label={ __( 'Select media', 'amp' ) }
+			controls={ dropDownOptions }
+			hasArrowIndicator={ true }
+			toggleProps={
+				{ labelPosition: 'bottom' }
+			}
+		/>
 	);
 };
 
 MediaInsider.propTypes = {
 	insertBlock: PropTypes.func.isRequired,
+	updateBlock: PropTypes.func.isRequired,
 	canInsertBlockType: PropTypes.func.isRequired,
 	showInserter: PropTypes.bool.isRequired,
+};
+
+const mediaPicker = ( dialogTitle, mediaType, updateBlock ) => {
+	// Create the media frame.
+	const fileFrame = wp.media( {
+		title: dialogTitle,
+		button: {
+			text: __( 'Use as background', 'amp' ),
+		},
+		multiple: false,
+		library: {
+			type: mediaType,
+		},
+	} );
+	let attachment;
+
+	// When an image is selected, run a callback.
+	fileFrame.on( 'select', () => {
+		attachment = fileFrame.state().get( 'selection' ).first().toJSON();
+		updateBlock( attachment );
+	} );
+
+	// Finally, open the modal
+	fileFrame.open();
 };
 
 const applyWithSelect = withSelect( ( select ) => {
@@ -104,6 +128,59 @@ const applyWithDispatch = withDispatch( ( dispatch, props, { select } ) => {
 			const insertedBlock = createBlock( name, {} );
 
 			insertBlock( insertedBlock, index, currentPage );
+		},
+		updateBlock: ( media ) => {
+			const clientId = getCurrentPage();
+			const { updateBlockAttributes } = dispatch( 'core/block-editor' );
+
+			if ( ! clientId ) {
+				return;
+			}
+
+			if ( ! media || ! media.url ) {
+				updateBlockAttributes( clientId,
+					{
+						mediaUrl: undefined,
+						mediaId: undefined,
+						mediaType: undefined,
+						mediaAlt: undefined,
+						poster: undefined,
+					}
+				);
+				return;
+			}
+
+			let mediaType;
+
+			// For media selections originated from a file upload.
+			if ( media.media_type ) {
+				if ( media.media_type === VIDEO_BACKGROUND_TYPE ) {
+					mediaType = VIDEO_BACKGROUND_TYPE;
+				} else {
+					mediaType = IMAGE_BACKGROUND_TYPE;
+				}
+			} else {
+				// For media selections originated from existing files in the media library.
+				if (
+					media.type !== IMAGE_BACKGROUND_TYPE &&
+					media.type !== VIDEO_BACKGROUND_TYPE
+				) {
+					return;
+				}
+
+				mediaType = media.type;
+			}
+
+			const mediaAlt = media.alt || media.title;
+			const mediaUrl = media.url;
+			const poster = VIDEO_BACKGROUND_TYPE === mediaType && media.image && media.image.src !== media.icon ? media.image.src : undefined;
+			updateBlockAttributes( clientId, {
+				mediaUrl,
+				mediaId: media.id,
+				mediaType,
+				mediaAlt,
+				poster,
+			} );
 		},
 	};
 } );
