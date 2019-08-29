dd6b80904147f4c2cab3e45711561c1b79e28276
Alain Schlesser <alain.schlesser@gmail.com>
2019-08-29 12:57:32 +0200

    Add logic to remove unwanted attributes on invalid urls

--- includes/sanitizers/class-amp-href-sanitizer.php
+++ includes/sanitizers/class-amp-href-sanitizer.php
@@ -38,8 +38,22 @@ final class AMP_Href_Sanitizer extends AMP_Base_Sanitizer {
 			}
 
 			$url = esc_url( $node->getAttribute( 'href' ) );
+
 			if ( empty( $url ) || false === wp_parse_url( $url ) ) {
 				$node->removeAttribute( 'href' );
+
+				/*
+				 * "The target, download, rel, rev, hreflang, and type attributes must be omitted
+				 * if the href attribute is not present."
+				 * See: https://www.w3.org/TR/2016/REC-html51-20161101/textlevel-semantics.html#the-a-element
+				 */
+				foreach ( [
+					'target', 'download', 'rel', 'rev', 'hreflang', 'type'
+				] as $attribute ) {
+					if ( $node->hasAttribute( $attribute ) ) {
+						$node->removeAttribute( $attribute );
+					}
+				}
 			}
 		}
 	}
