57299ae606300d7c15ee27f2ed681caabe9a6354
Alain Schlesser <alain.schlesser@gmail.com>
2019-08-29 15:43:31 +0200

    Get rid of 1 level of indentation with an early bail

--- includes/sanitizers/class-amp-href-sanitizer.php
+++ includes/sanitizers/class-amp-href-sanitizer.php
@@ -53,18 +53,21 @@ final class AMP_Href_Sanitizer extends AMP_Base_Sanitizer {
 
 			$url = esc_url( $node->getAttribute( 'href' ) );
 
-			if ( empty( $url ) || false === wp_parse_url( $url ) ) {
-				$node->removeAttribute( 'href' );
+			if ( ! empty( $url ) && false !== wp_parse_url( $url ) ) {
+				continue;
+			}
 
-				/*
-				 * "The target, download, rel, rev, hreflang, and type attributes must be omitted
-				 * if the href attribute is not present."
-				 * See: https://www.w3.org/TR/2016/REC-html51-20161101/textlevel-semantics.html#the-a-element
-				 */
-				foreach ( self::$attributes_tied_to_href as $attribute ) {
-					if ( $node->hasAttribute( $attribute ) ) {
-						$node->removeAttribute( $attribute );
-					}
+			// Href URL is invalid, so we remove the entire attribute.
+			$node->removeAttribute( 'href' );
+
+			/*
+			 * "The target, download, rel, rev, hreflang, and type attributes must be omitted
+			 * if the href attribute is not present."
+			 * See: https://www.w3.org/TR/2016/REC-html51-20161101/textlevel-semantics.html#the-a-element
+			 */
+			foreach ( self::$attributes_tied_to_href as $attribute ) {
+				if ( $node->hasAttribute( $attribute ) ) {
+					$node->removeAttribute( $attribute );
 				}
 			}
 		}
