704081544c76747077fcc18819c2436f96fdef17
Jonny Harris <jon@spacedmonkey.co.uk>
2019-09-05 17:07:44 +0100

    Fix e2e tests.

--- tests/e2e/specs/stories-editor/font-picker.js
+++ tests/e2e/specs/stories-editor/font-picker.js
@@ -9,7 +9,7 @@ import { createNewPost, saveDraft } from '@wordpress/e2e-test-utils';
 import { activateExperience, deactivateExperience, selectBlockByClassName, getBlocksOnPage } from '../../utils';
 
 const textBlockClass = 'wp-block-amp-story-text';
-const fontPickerID = 'amp-stories-font-family-picker';
+const fontPickerID = 'autocomplete__input';
 
 describe( 'Font picker in Text Block', () => {
 	beforeAll( async () => {
@@ -30,28 +30,29 @@ describe( 'Font picker in Text Block', () => {
 		await page.keyboard.type( textToWrite );
 		await page.$eval( '.block-editor-block-list__layout .block-editor-block-list__block .wp-block-amp-amp-story-text', ( node ) => node.textContent );
 
-		await page.click( `#${ fontPickerID }` );
+		await page.click( `.${ fontPickerID }` );
 	} );
 
 	it( 'should be able to search for ubuntu font', async () => {
-		await page.keyboard.type( 'Ubuntu' );
+		await page.keyboard.type( 'Arimo' );
 
 		const nodes = await page.$x(
-			'//ul[@id="amp-stories-font-family-picker__listbox"]//li'
+			'//ul[contains(@class,"autocomplete__menu")]//li'
 		);
 		expect( nodes ).toHaveLength( 1 );
-		await expect( page ).toMatchElement( '#ubuntu-font' );
+		await expect( page ).toMatchElement( '#arimo-font' );
 	} );
 
 	it( 'should be able to search for Arial font and get multi results', async () => {
 		await page.keyboard.type( 'pt sans' );
 
 		const nodes = await page.$x(
-			'//ul[@id="amp-stories-font-family-picker__listbox"]//li'
+			'//ul[contains(@class,"autocomplete__menu")]//li'
 		);
-		expect( nodes ).toHaveLength( 2 );
+		expect( nodes ).toHaveLength( 3 );
 		await expect( page ).toMatchElement( '#pt-sans-font' );
 		await expect( page ).toMatchElement( '#pt-sans-narrow-font' );
+		await expect( page ).toMatchElement( '#pt-sans-caption-font' );
 	} );
 
 	it( 'should be able to search for none existing font', async () => {
