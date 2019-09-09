37a3355e548f105b5791289b366c08f18d3b8b3c
Morten Barklund <morten@barklund.dk>
2019-09-09 17:01:45 +0200

    Updated Status component to simple functional comp
    This component serves the purpose of announcing to screen readers, that the search results have changed in the autocomplete. And it needs a small change to occur when the message of "query too short" is displayed, even as the user types more characters. Thus a number of commas were inserted, and this still happens based off query length.
    
    However, I don't think this component works at all, as it only works if it was in the DOM on the very first load of the HTML. And it clearly wasn't for a plurality of reasons. But I'm okay with keeping it for a11y reasons even though it might not actually serve that purpose.

--- assets/src/stories-editor/components/font-family-picker/status.js
+++ assets/src/stories-editor/components/font-family-picker/status.js
@@ -4,78 +4,54 @@
  */
 import PropTypes from 'prop-types';
 
-/**
- * WordPress dependencies
- */
-import { Component } from '@wordpress/element';
+const Status = ( {
+	length,
+	queryLength,
+	minQueryLength,
+	selectedOption,
+	selectedOptionIndex,
+	tQueryTooShort,
+	tNoResults,
+	tSelectedOption,
+	tResults,
+} ) => {
+	const queryTooShort = queryLength < minQueryLength;
+	const noResults = length === 0;
 
-export default class Status extends Component {
-	constructor( props ) {
-		super( props );
-		this.state = {
-			bump: false,
-		};
+	const contentSelectedOption = selectedOption ?
+		tSelectedOption( selectedOption, length, selectedOptionIndex ) :
+		'';
+
+	let content = null;
+	if ( queryTooShort ) {
+		content = tQueryTooShort( minQueryLength );
+	} else if ( noResults ) {
+		content = tNoResults();
+	} else {
+		content = tResults( length, contentSelectedOption );
 	}
 
-	componentWillReceiveProps( { queryLength } ) {
-		const hasChanged = queryLength !== this.props.queryLength;
-		if ( hasChanged ) {
-			this.setState( ( { bump } ) => ( { bump: ! bump } ) );
-		}
-	}
-
-	render() {
-		const {
-			length,
-			queryLength,
-			minQueryLength,
-			selectedOption,
-			selectedOptionIndex,
-			tQueryTooShort,
-			tNoResults,
-			tSelectedOption,
-			tResults,
-		} = this.props;
-		const { bump } = this.state;
-
-		const queryTooShort = queryLength < minQueryLength;
-		const noResults = length === 0;
-
-		const contentSelectedOption = selectedOption ?
-			tSelectedOption( selectedOption, length, selectedOptionIndex ) :
-			'';
-
-		let content = null;
-		if ( queryTooShort ) {
-			content = tQueryTooShort( minQueryLength );
-		} else if ( noResults ) {
-			content = tNoResults();
-		} else {
-			content = tResults( length, contentSelectedOption );
-		}
-
-		return <div
-			aria-atomic="true"
-			aria-live="polite"
-			role="status"
-			style={ {
-				border: '0',
-				clip: 'rect(0 0 0 0)',
-				height: '1px',
-				marginBottom: '-1px',
-				marginRight: '-1px',
-				overflow: 'hidden',
-				padding: '0',
-				position: 'absolute',
-				whiteSpace: 'nowrap',
-				width: '1px',
-			} }
-		>
-			{ content }
-			<span>{ bump ? ',' : ',,' }</span>
-		</div>;
-	}
-}
+	return <div
+		aria-atomic="true"
+		aria-live="polite"
+		role="status"
+		style={ {
+			border: '0',
+			clip: 'rect(0 0 0 0)',
+			height: '1px',
+			marginBottom: '-1px',
+			marginRight: '-1px',
+			overflow: 'hidden',
+			padding: '0',
+			position: 'absolute',
+			whiteSpace: 'nowrap',
+			width: '1px',
+		} }
+	>
+		{ content }
+		{ queryTooShort && ','.repeat( queryLength ) }
+	</div>;
+};
 
 Status.propTypes = {
 	length: PropTypes.number.isRequired,
@@ -88,3 +64,5 @@ Status.propTypes = {
 	tSelectedOption: PropTypes.func.isRequired,
 	tResults: PropTypes.func.isRequired,
 };
+
+export default Status;
