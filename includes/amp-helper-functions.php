8794360b3562cad6b6e6887311320b4edc7c08f0
Alain Schlesser <alain.schlesser@gmail.com>
2019-09-11 17:50:27 +0200

    Add array_colum() pollyfill for PHP < 5.5
    This code is copied from the [`ramsey/array_colum`](https://github.com/ramsey/array_column) package, to avoid adding a dependency for now. The plan is to move to PHP 5.6+ in the near future anyway, so this is just meant to quickly fix the issue until it can be dropped again after the version requirement bump.
    
    All changes to the original code did not change the logic, they only adapted the code style to satisfy PHPCS.

--- includes/amp-helper-functions.php
+++ includes/amp-helper-functions.php
@@ -1240,3 +1240,120 @@ function amp_print_story_auto_ads() {
 
 	echo AMP_HTML_Utils::build_tag( 'amp-story-auto-ads', [], $script_element ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 }
+
+/*
+ * The function below is copied from the ramsey/array_column package.
+ *
+ * Changes were made to code style to pass PHPCS requirements, but logic is unchanged.
+ *
+ * This can be removed once the required PHP version moves to PHP 5.5+.
+ *
+ * @link https://github.com/ramsey/array_column
+ *
+ * @copyright Copyright (c) Ben Ramsey (http://benramsey.com)
+ * @license   http://opensource.org/licenses/MIT MIT
+ */
+if ( ! function_exists( 'array_column' ) ) {
+	/**
+	 * Returns the values from a single column of the input array, identified by
+	 * the $columnKey.
+	 *
+	 * Optionally, you may provide an $indexKey to index the values in the returned
+	 * array by the values from the $indexKey column in the input array.
+	 *
+	 * @param array $input      A multi-dimensional array (record set) from which to pull
+	 *                          a column of values.
+	 * @param mixed $column_key The column of values to return. This value may be the
+	 *                          integer key of the column you wish to retrieve, or it
+	 *                          may be the string key name for an associative array.
+	 * @param mixed $index_key  (Optional.) The column to use as the index/keys for
+	 *                          the returned array. This value may be the integer key
+	 *                          of the column, or it may be the string key name.
+	 * @return array|bool
+	 */
+	function array_column( $input = null, $column_key = null, $index_key = null ) {
+		// Using func_get_args() in order to check for proper number of
+		// parameters and trigger errors exactly as the built-in array_column()
+		// does in PHP 5.5.
+		$argc   = func_num_args();
+		$params = func_get_args();
+
+		if ( $argc < 2 ) {
+			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_trigger_error,WordPress.Security.EscapeOutput.OutputNotEscaped
+			trigger_error( "array_column() expects at least 2 parameters, {$argc} given", E_USER_WARNING );
+			return null;
+		}
+
+		if ( ! is_array( $params[0] ) ) {
+			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_trigger_error,WordPress.Security.EscapeOutput.OutputNotEscaped
+			trigger_error( 'array_column() expects parameter 1 to be array, ' . gettype( $params[0] ) . ' given', E_USER_WARNING );
+			return null;
+		}
+
+		if ( ! is_int( $params[1] )
+			&& ! is_float( $params[1] )
+			&& ! is_string( $params[1] )
+			&& null !== $params[1]
+			&& ! ( is_object( $params[1] ) && method_exists( $params[1], '__toString' ) )
+		) {
+			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_trigger_error
+			trigger_error( 'array_column(): The column key should be either a string or an integer', E_USER_WARNING );
+			return false;
+		}
+
+		if ( isset( $params[2] )
+			&& ! is_int( $params[2] )
+			&& ! is_float( $params[2] )
+			&& ! is_string( $params[2] )
+			&& ! ( is_object( $params[2] ) && method_exists( $params[2], '__toString' ) )
+		) {
+			// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_trigger_error
+			trigger_error( 'array_column(): The index key should be either a string or an integer', E_USER_WARNING );
+			return false;
+		}
+
+		$params_input      = $params[0];
+		$params_column_key = ( null !== $params[1] ) ? (string) $params[1] : null;
+
+		$params_index_key = null;
+		if ( isset( $params[2] ) ) {
+			if ( is_float( $params[2] ) || is_int( $params[2] ) ) {
+				$params_index_key = (int) $params[2];
+			} else {
+				$params_index_key = (string) $params[2];
+			}
+		}
+
+		$result_array = [];
+
+		foreach ( $params_input as $row ) {
+			$key       = null;
+			$value     = null;
+			$key_set   = false;
+			$value_set = false;
+
+			if ( null !== $params_index_key && array_key_exists( $params_index_key, $row ) ) {
+				$key_set = true;
+				$key     = (string) $row[ $params_index_key ];
+			}
+
+			if ( null === $params_column_key ) {
+				$value_set = true;
+				$value     = $row;
+			} elseif ( is_array( $row ) && array_key_exists( $params_column_key, $row ) ) {
+				$value_set = true;
+				$value     = $row[ $params_column_key ];
+			}
+
+			if ( $value_set ) {
+				if ( $key_set ) {
+					$result_array[ $key ] = $value;
+				} else {
+					$result_array[] = $value;
+				}
+			}
+		}
+
+		return $result_array;
+	}
+}
