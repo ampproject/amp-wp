abc0b697d1a0ee11a66417bf98193f7b36ce97b7
Jonny Harris <jon@spacedmonkey.co.uk>
2019-09-16 15:33:13 +0100

    Use state to copy / cut text based blocks. Also don't allow page blocks to be copied.

--- assets/src/stories-editor/blocks/amp-story-page/copy-paste-handler.js
+++ assets/src/stories-editor/blocks/amp-story-page/copy-paste-handler.js
@@ -13,12 +13,13 @@ import { withDispatch, useSelect, useDispatch } from '@wordpress/data';
 /**
  * Internal dependencies
  */
-import { ensureAllowedBlocksOnPaste } from '../../helpers';
+import { copyTextToClipBoard, ensureAllowedBlocksOnPaste } from '../../helpers';
 
 function CopyPasteHandler( { children, onCopy, onCut, clientId, isSelected } ) {
 	const {
 		isFirstPage,
 		canUserUseUnfilteredHTML,
+		getCopiedMarkupState,
 	} = useSelect(
 		( select ) => {
 			const {
@@ -26,14 +27,17 @@ function CopyPasteHandler( { children, onCopy, onCut, clientId, isSelected } ) {
 				getSettings,
 			} = select( 'core/block-editor' );
 			const { __experimentalCanUserUseUnfilteredHTML } = getSettings();
+			const { getCopiedMarkup } = select( 'amp/story' );
 			return {
 				isFirstPage: getBlockOrder().indexOf( clientId ) === 0,
 				canUserUseUnfilteredHTML: __experimentalCanUserUseUnfilteredHTML,
+				getCopiedMarkupState: getCopiedMarkup,
 			};
 		}, [ clientId ]
 	);
 
 	const { insertBlocks } = useDispatch( 'core/block-editor' );
+	const { clearCopiedMarkup } = useDispatch( 'amp/story' );
 
 	const onPaste = ( event ) => {
 		// Ignore if the Page is not the selected page.
@@ -45,20 +49,26 @@ function CopyPasteHandler( { children, onCopy, onCut, clientId, isSelected } ) {
 		let plainText = '';
 		let html = '';
 
-		// IE11 only supports `Text` as an argument for `getData` and will
-		// otherwise throw an invalid argument error, so we try the standard
-		// arguments first, then fallback to `Text` if they fail.
-		try {
-			plainText = clipboardData.getData( 'text/plain' );
-			html = clipboardData.getData( 'text/html' );
-		} catch ( error1 ) {
+		if ( getCopiedMarkupState().length ) {
+			plainText = getCopiedMarkupState();
+			html = getCopiedMarkupState();
+			clearCopiedMarkup();
+		} else {
+			// IE11 only supports `Text` as an argument for `getData` and will
+			// otherwise throw an invalid argument error, so we try the standard
+			// arguments first, then fallback to `Text` if they fail.
 			try {
-				html = clipboardData.getData( 'Text' );
-			} catch ( error2 ) {
-				// Some browsers like UC Browser paste plain text by default and
-				// don't support clipboardData at all, so allow default
-				// behaviour.
-				return;
+				plainText = clipboardData.getData( 'text/plain' );
+				html = clipboardData.getData( 'text/html' );
+			} catch ( error1 ) {
+				try {
+					html = clipboardData.getData( 'Text' );
+				} catch ( error2 ) {
+					// Some browsers like UC Browser paste plain text by default and
+					// don't support clipboardData at all, so allow default
+					// behaviour.
+					return;
+				}
 			}
 		}
 
@@ -100,6 +110,7 @@ export default withDispatch( ( dispatch, ownProps, { select } ) => {
 		hasMultiSelection,
 	} = select( 'core/block-editor' );
 	const { clearCopiedMarkup, setCopiedMarkup } = dispatch( 'amp/story' );
+	const { removeBlock } = dispatch( 'core/block-editor' );
 
 	/**
 	 * Creates cut/copy handler for ensuring that the store's copiedMarkup is in sync with what's actually in clipBoard.
@@ -123,16 +134,20 @@ export default withDispatch( ( dispatch, ownProps, { select } ) => {
 			clearCopiedMarkup();
 			return;
 		}
-		const serialized = serialize( getBlocksByClientId( selectedBlockClientIds ) );
-		setCopiedMarkup( serialized );
+		const copyBlocks = getBlocksByClientId( selectedBlockClientIds );
+		for ( const copyBlock of copyBlocks ) {
+			if ( copyBlock && copyBlock.name && 'amp/amp-story-page' === copyBlock.name ) {
+				return;
+			}
+		}
 
+		const serialized = serialize( copyBlocks );
+		setCopiedMarkup( serialized );
+		copyTextToClipBoard( serialized );
 		if ( isCut ) {
-			// TODO: Remove selected Blocks.
-			// The code below works most of the time, but sometimes (unable to tell when and why) another element on page is selected when the block is removed, and then the browser copies this other element in stead of the previously selected element (that has now been removed).
-			/* for ( const clientId of selectedBlockClientIds ) {
+			for ( const clientId of selectedBlockClientIds ) {
 				removeBlock( clientId );
-			} */
-			// wrapping the call in setTimeout fixes the case where another element is selected on cut, but throws an error in the cases, where the above code works fine.
+			}
 		}
 	};
 
