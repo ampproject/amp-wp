9fd43c3aa2379d4e1debd9ac7c38817a0bb123ef
Morten Barklund <morten@barklund.dk>
2019-09-23 17:20:38 +0200

    Better separation of concerns

--- assets/src/stories-editor/components/block-mover/block-draggable.js
+++ assets/src/stories-editor/components/block-mover/block-draggable.js
@@ -10,10 +10,8 @@ import PropTypes from 'prop-types';
 /**
  * WordPress dependencies
  */
-import {
-	withSelect,
-	dispatch,
-} from '@wordpress/data';
+import { withDispatch, withSelect } from '@wordpress/data';
+import { compose } from '@wordpress/compose';
 import { cloneBlock } from '@wordpress/blocks';
 
 /**
@@ -22,49 +20,12 @@ import { cloneBlock } from '@wordpress/blocks';
 import Draggable from './draggable';
 import { getPercentageFromPixels } from '../../helpers';
 
-const BlockDraggable = ( { children, clientId, blockName, rootClientId, blockElementId, block, index, getBlockOrder, onDragStart, onDragEnd } ) => {
+const BlockDraggable = ( { children, clientId, blockName, rootClientId, blockElementId, index, onDragStart, onDragEnd, onNeighborDrop, getNeighborPageId } ) => {
 	const transferData = {
 		type: 'block',
 		srcIndex: index,
 		srcRootClientId: rootClientId,
 		srcClientId: clientId,
-	};
-
-	const { setCurrentPage } = dispatch( 'amp/story' );
-	const { selectBlock, removeBlock, insertBlock, updateBlockAttributes } = dispatch( 'core/block-editor' );
-
-	const getNeighborPageId = ( offset ) => {
-		const pages = getBlockOrder();
-		const currentPageIndex = pages.findIndex( ( i ) => i === rootClientId );
-		const newPageIndex = currentPageIndex + offset;
-
-		// Do we even have a neighbor in that direction? (offset=0 is not a neighbor)
-		if ( offset === 0 || newPageIndex < 0 || newPageIndex >= pages.length ) {
-			return null;
-		}
-
-		return pages[ newPageIndex ];
-	};
-
-	const onNeighborDrop = ( offset, finalPosition ) => {
-		const newPageId = getNeighborPageId( offset );
-		if ( ! newPageId ) {
-			return;
-		}
-
-		// Remove block and add cloned block to new page.
-		removeBlock( clientId );
-		const clonedBlock = cloneBlock( block );
-		const newAttributes = {
-			positionTop: getPercentageFromPixels( 'y', finalPosition.top ),
-			positionLeft: getPercentageFromPixels( 'x', finalPosition.left ),
-		};
-		insertBlock( clonedBlock, null, newPageId );
-		updateBlockAttributes( clonedBlock.clientId, newAttributes );
-
-		// Switch to new page.
-		setCurrentPage( newPageId );
-		selectBlock( newPageId );
 	};
 
 	// This holds the currently highlighted element
@@ -124,17 +85,65 @@ BlockDraggable.propTypes = {
 	children: PropTypes.any.isRequired,
 	onDragStart: PropTypes.func,
 	onDragEnd: PropTypes.func,
+	onNeighborHover: PropTypes.func,
+	onNeighborDrop: PropTypes.func,
 	block: PropTypes.object,
 	getBlockOrder: PropTypes.func,
 };
 
-export default withSelect( ( select, { clientId } ) => {
-	const { getBlockIndex, getBlockRootClientId, getBlockOrder, getBlock } = select( 'core/block-editor' );
-	const rootClientId = getBlockRootClientId( clientId );
-	return {
-		index: getBlockIndex( clientId, rootClientId ),
-		rootClientId,
-		block: getBlock( clientId ),
-		getBlockOrder,
-	};
-} )( BlockDraggable );
+export default compose(
+	withSelect( ( select, { clientId } ) => {
+		const { getBlockIndex, getBlockRootClientId, getBlockOrder, getBlock } = select( 'core/block-editor' );
+		const rootClientId = getBlockRootClientId( clientId );
+		return {
+			index: getBlockIndex( clientId, rootClientId ),
+			rootClientId,
+			block: getBlock( clientId ),
+			getBlockOrder,
+		};
+	} ),
+	withDispatch( ( dispatch, { getBlockOrder, rootClientId, clientId, block } ) => {
+		const { setCurrentPage } = dispatch( 'amp/story' );
+		const { selectBlock, removeBlock, insertBlock, updateBlockAttributes } = dispatch( 'core/block-editor' );
+
+		const getNeighborPageId = ( offset ) => {
+			const pages = getBlockOrder();
+			const currentPageIndex = pages.findIndex( ( i ) => i === rootClientId );
+			const newPageIndex = currentPageIndex + offset;
+
+			// Do we even have a neighbor in that direction? (offset=0 is not a neighbor)
+			if ( offset === 0 || newPageIndex < 0 || newPageIndex >= pages.length ) {
+				return null;
+			}
+
+			return pages[ newPageIndex ];
+		};
+
+		const onNeighborDrop = ( offset, finalPosition ) => {
+			const newPageId = getNeighborPageId( offset );
+			if ( ! newPageId ) {
+				return;
+			}
+
+			// Remove block and add cloned block to new page.
+			removeBlock( clientId );
+			const clonedBlock = cloneBlock( block );
+			const newAttributes = {
+				positionTop: getPercentageFromPixels( 'y', finalPosition.top ),
+				positionLeft: getPercentageFromPixels( 'x', finalPosition.left ),
+			};
+			insertBlock( clonedBlock, null, newPageId );
+			updateBlockAttributes( clonedBlock.clientId, newAttributes );
+
+			// Switch to new page.
+			setCurrentPage( newPageId );
+			selectBlock( newPageId );
+		};
+
+		return {
+			onNeighborDrop,
+			getNeighborPageId,
+		};
+	} ),
+)( BlockDraggable );
+
