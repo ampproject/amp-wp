3928f60532889091d6001bd7c0e1b1dfbc25a350
Morten Barklund <morten@barklund.dk>
2019-09-24 09:21:54 +0200

    Adding documentation for functions

--- assets/src/stories-editor/components/block-mover/block-draggable.js
+++ assets/src/stories-editor/components/block-mover/block-draggable.js
@@ -18,6 +18,7 @@ import { cloneBlock } from '@wordpress/blocks';
  * Internal dependencies
  */
 import Draggable from './draggable';
+import { getCallToActionBlock } from '../../helpers';
 
 const BlockDraggable = ( { children, clientId, blockName, rootClientId, blockElementId, index, onDragStart, onDragEnd, onNeighborDrop, getNeighborPageId } ) => {
 	const transferData = {
@@ -30,6 +31,11 @@ const BlockDraggable = ( { children, clientId, blockName, rootClientId, blockEle
 	// This holds the currently highlighted element
 	const currentHoverElement = { pageId: null, current: null };
 
+	/**
+	 * Highlight neighboring page on hover. Neigboring page is given by offset.
+	 *
+	 * @param {number} offset  Integer specifying offset from current page - e.g. -2 on page 3 will return id of page 1. Page indices are zero-based.
+	 */
 	const onNeighborHover = ( offset ) => {
 		// Get neighboring page.
 		const newPageId = getNeighborPageId( offset );
@@ -104,23 +110,66 @@ export default compose(
 		const { setCurrentPage } = dispatch( 'amp/story' );
 		const { selectBlock, removeBlock, insertBlock, updateBlockAttributes } = dispatch( 'core/block-editor' );
 
+		const isCTABlock = 'amp/amp-story-cta' === blockName;
+
+		/**
+		 * Is the current element allowed on a given page?
+		 *
+		 * This is currently only used to filter out instances, where CTA's aren't allowed.
+		 *
+		 * @param {number} targetPageIndex  Zero-based index of new page
+		 * @param {string} targetPageId  Block id of new page
+		 * @return {boolean} Returns true if this element is allowed on the given page, false otherwise.
+		 */
+		const isElementAllowedOnPage = ( targetPageIndex, targetPageId ) => {
+			if ( isCTABlock ) {
+				// CTA's aren't allowed on the first page
+				if ( targetPageIndex === 0 ) {
+					return false;
+				}
+
+				// CTA's aren't allowed on pages, that already have a CTA
+				const ctaBlockOnTargetPage = getCallToActionBlock( targetPageId );
+				if ( ctaBlockOnTargetPage !== null ) {
+					return false;
+				}
+			}
+
+			return true;
+		};
+
+		/**
+		 * Get id of neighbor page that is `offset` away from the current page.
+		 *
+		 * If no page exists in that direction, if offset is zero or if element is not allowed on that page, null will be returned.
+		 *
+		 * @param {number} offset  Integer specifying offset from current page - e.g. -2 on page 3 will return id of page 1. Page indices are zero-based.
+		 * @return {string} Returns id of target page or null if element can't be dropped there for any reason.
+		 */
 		const getNeighborPageId = ( offset ) => {
 			const pages = getBlockOrder();
 			const currentPageIndex = pages.findIndex( ( i ) => i === rootClientId );
 			const newPageIndex = currentPageIndex + offset;
+			const isInsidePageCount = newPageIndex >= 0 && newPageIndex < pages.length;
+			const newPageId = pages[ newPageIndex ];
+			const isAllowedOnPage = isElementAllowedOnPage( newPageIndex, newPageId );
 
-			const isCTABlock = 'amp/amp-story-cta' === blockName;
-			const lowestPageNumberAllowed = isCTABlock ? 1 : 0;
-			const highestPageNumberAllowed = pages.length - 1;
-
-			// Do we even have a neighbor in that direction? (offset=0 is not a neighbor)
-			if ( offset === 0 || newPageIndex < lowestPageNumberAllowed || newPageIndex > highestPageNumberAllowed ) {
+			// Do we even have a legal neighbor in that direction? (offset=0 is not a neighbor)
+			if ( offset === 0 || ! isInsidePageCount || ! isAllowedOnPage ) {
 				return null;
 			}
 
-			return pages[ newPageIndex ];
+			return newPageId;
 		};
 
+		/**
+		 * Drop this element on the page `offset` away from the current page (if possible). If dropped, update the position of the element on the new page.
+		 *
+		 * Currently this function removes the old element and creates a clone on the new page.
+		 *
+		 * @param {number} offset  Integer specifying offset from current page - e.g. -2 on page 3 will return id of page 1. Page indices are zero-based.
+		 * @param {Object} newAttributes  Object with attributes to update on element on new page.
+		 */
 		const onNeighborDrop = ( offset, newAttributes ) => {
 			const newPageId = getNeighborPageId( offset );
 			if ( ! newPageId ) {
