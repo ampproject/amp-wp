685cfa13b40aff8516f82c7dda629574c899ccca
Weston Ruter <westonruter@google.com>
2019-09-24 10:12:39 -0700

    Harden logic for gathering allowed children for AMP Stories

--- includes/sanitizers/class-amp-story-sanitizer.php
+++ includes/sanitizers/class-amp-story-sanitizer.php
@@ -21,18 +21,14 @@ class AMP_Story_Sanitizer extends AMP_Base_Sanitizer {
 	public static $tag = 'amp-story-page';
 
 	/**
-	 * AMP Story tag spec.
+	 * Allowed children for given tags.
 	 *
 	 * @var array
 	 */
-	private $amp_story_tag_spec;
-
-	/**
-	 * AMP Story Page tag spec.
-	 *
-	 * @var array
-	 */
-	private $amp_story_page_tag_spec;
+	private $tag_allowed_children = [
+		'amp-story'      => [],
+		'amp-story-page' => [],
+	];
 
 	/**
 	 * Sanitize the AMP elements contained by <amp-story-page> element where necessary.
@@ -40,8 +36,23 @@ class AMP_Story_Sanitizer extends AMP_Base_Sanitizer {
 	 * @since 0.2
 	 */
 	public function sanitize() {
-		$this->amp_story_tag_spec      = AMP_Allowed_Tags_Generated::get_allowed_tag( 'amp-story' )[0];
-		$this->amp_story_page_tag_spec = AMP_Allowed_Tags_Generated::get_allowed_tag( 'amp-story-page' )[0];
+
+		// Gather the allowed children for amp-story and amp-story-page elements.
+		foreach ( array_keys( $this->tag_allowed_children ) as $tag_name ) {
+			$rule_specs = AMP_Allowed_Tags_Generated::get_allowed_tag( $tag_name );
+			if ( ! isset( $rule_specs ) ) {
+				continue;
+			}
+			foreach ( $rule_specs as $rule_spec ) {
+				if ( ! isset( $rule_spec['tag_spec']['child_tags']['child_tag_name_oneof'] ) ) {
+					continue;
+				}
+				$this->tag_allowed_children[ $tag_name ] = array_merge(
+					$this->tag_allowed_children[ $tag_name ],
+					$rule_spec['tag_spec']['child_tags']['child_tag_name_oneof']
+				);
+			}
+		}
 
 		$amp_story_element = $this->dom->getElementsByTagName( 'amp-story' )->item( 0 );
 		if ( $amp_story_element instanceof DOMElement ) {
@@ -64,8 +75,8 @@ class AMP_Story_Sanitizer extends AMP_Base_Sanitizer {
 				if ( 'amp-story-page' === $node->nodeName ) {
 					$page_number++;
 					$this->sanitize_story_page_element( $node, $page_number );
-				} elseif ( ! in_array( $node->nodeName, $this->amp_story_tag_spec['tag_spec']['child_tags']['child_tag_name_oneof'], true ) ) {
-					$this->remove_invalid_child( $node );
+				} else {
+					$this->maybe_remove_disallowed_child( $node );
 				}
 			}
 			$node = $next_node;
@@ -97,12 +108,35 @@ class AMP_Story_Sanitizer extends AMP_Base_Sanitizer {
 					if ( 1 === $page_number || $cta_layer_count > 1 ) {
 						$element->removeChild( $node );
 					}
-				} elseif ( ! in_array( $node->nodeName, $this->amp_story_page_tag_spec['tag_spec']['child_tags']['child_tag_name_oneof'], true ) ) {
-					$this->remove_invalid_child( $node );
+				} else {
+					$this->maybe_remove_disallowed_child( $node );
 				}
 			}
 			$node = $next_node;
 		}
 	}
 
+	/**
+	 * Remove an element if it is not allowed by the parent.
+	 *
+	 * @param DOMElement $node Child element.
+	 * @return bool Whether the child was removed.
+	 */
+	private function maybe_remove_disallowed_child( DOMElement $node ) {
+		if ( ! $node->parentNode ) {
+			return false;
+		}
+
+		$parent_node_name = $node->parentNode->nodeName;
+		if ( empty( $this->tag_allowed_children[ $parent_node_name ] ) ) {
+			return false;
+		}
+
+		if ( in_array( $node->nodeName, $this->tag_allowed_children[ $parent_node_name ], true ) ) {
+			return false;
+		}
+
+		$this->remove_invalid_child( $node );
+		return true;
+	}
 }
