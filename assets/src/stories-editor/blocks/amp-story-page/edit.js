85d7f33bf46f04c3a01cf2e259b9a6c833d54876
Pascal Birchler <pascalb@google.com>
2019-09-27 14:42:28 +0200

    Refactor PageEdit to use hooks

--- assets/src/stories-editor/blocks/amp-story-page/edit.js
+++ assets/src/stories-editor/blocks/amp-story-page/edit.js
@@ -10,12 +10,8 @@ import {
 	InnerBlocks,
 	InspectorControls,
 } from '@wordpress/block-editor';
-import { Component, createRef } from '@wordpress/element';
-import {
-	withSelect,
-	withDispatch,
-} from '@wordpress/data';
-import { compose } from '@wordpress/compose';
+import { createRef, useEffect } from '@wordpress/element';
+import { useSelect, useDispatch } from '@wordpress/data';
 
 /**
  * Internal dependencies
@@ -41,55 +37,113 @@ import PageSettings from './page-settings';
 import BackgroundMediaSettings from './background-media-settings';
 import AnimationSettings from './animation-settings';
 
-class PageEdit extends Component {
-	shouldComponentUpdate() {
-		this.ensureCorrectBlockOrder();
-		return true;
-	}
+const PageEdit = ( {
+	attributes,
+	clientId,
+	isSelected,
+	setAttributes,
+} ) => {
+	const {
+		anchor,
+		mediaId,
+		mediaType,
+		mediaUrl,
+		mediaAlt,
+		focalPoint = { x: 0.5, y: 0.5 },
+		overlayOpacity,
+		poster,
+		autoAdvanceAfter,
+		autoAdvanceAfterDuration,
+		backgroundColors,
+	} = attributes;
 
-	constructor( props ) {
-		super( props );
+	const { moveBlockToPosition } = useDispatch( 'core/block-editor' );
 
-		if ( ! props.attributes.anchor ) {
-			this.props.setAttributes( { anchor: getUniqueId() } );
+	const {
+		media,
+		videoFeaturedImage,
+		pagesOrder,
+		childrenOrder,
+		storySettingsAttributes,
+		autoAdvanceAfterOptions,
+		allowedVideoMimeTypes,
+	} = useSelect( ( select ) => {
+		const { getMedia } = select( 'core' );
+		const { getBlockOrder } = select( 'core/block-editor' );
+		const { getSettings } = select( 'amp/story' );
+
+		const mediaObject = mediaId ? getMedia( mediaId ) : undefined;
+
+		let videoThumbnail;
+
+		if ( VIDEO_BACKGROUND_TYPE === mediaType && media && media.featured_media && ! poster ) {
+			videoThumbnail = getMedia( media.featured_media );
 		}
 
-		if ( props.storySettingsAttributes ) {
-			Object.entries( props.storySettingsAttributes ).forEach( ( [ key, value ] ) => {
-				if ( ! props.attributes.hasOwnProperty( key ) ) {
-					this.props.setAttributes( { [ key ]: value } );
+		const { getEditedPostAttribute } = select( 'core/editor' );
+		const postMeta = getEditedPostAttribute( 'meta' ) || {};
+		const { storySettings } = getSettings();
+
+		return {
+			media: mediaObject,
+			videoFeaturedImage: videoThumbnail,
+			getBlockOrder,
+			pagesOrder: getBlockOrder(),
+			childrenOrder: getBlockOrder( clientId ),
+			storySettingsAttributes: metaToAttributeNames( postMeta ),
+			autoAdvanceAfterOptions: storySettings.autoAdvanceAfterOptions,
+			allowedVideoMimeTypes: getSettings().allowedVideoMimeTypes,
+		};
+	} );
+
+	const allowedBackgroundMediaTypes = [ IMAGE_BACKGROUND_TYPE, ...allowedVideoMimeTypes ];
+
+	const isFirstPage = pagesOrder.indexOf( clientId ) === 0;
+	const isCallToActionAllowed = ! isFirstPage && ! getCallToActionBlock( clientId ) && ! getPageAttachmentBlock( clientId );
+	const isPageAttachmentAllowed = ! getCallToActionBlock( clientId ) && ! getPageAttachmentBlock( clientId );
+
+	let allowedBlocks = ALLOWED_MOVABLE_BLOCKS;
+
+	if ( isCallToActionAllowed ) {
+		allowedBlocks = [
+			...allowedBlocks,
+			'amp/amp-story-cta',
+		];
+	}
+
+	if ( isPageAttachmentAllowed ) {
+		allowedBlocks = [
+			...allowedBlocks,
+			'amp/amp-story-page-attachment',
+		];
+	}
+
+	useEffect( () => {
+		if ( ! anchor ) {
+			setAttributes( { anchor: getUniqueId() } );
+		}
+	}, [ anchor ] );
+
+	useEffect( () => {
+		if ( storySettingsAttributes ) {
+			Object.entries( storySettingsAttributes ).forEach( ( [ key, value ] ) => {
+				if ( ! attributes.hasOwnProperty( key ) ) {
+					setAttributes( { [ key ]: value } );
 				}
 			} );
 		}
+	}, [ storySettingsAttributes ] );
 
-		this.state = {
-			extractingPoster: false,
-		};
+	const videoPlayer = createRef();
 
-		this.videoPlayer = createRef();
-	}
-
-	componentDidUpdate( prevProps ) {
-		const { attributes } = this.props;
-		const { mediaType, mediaUrl } = attributes;
-
-		if ( VIDEO_BACKGROUND_TYPE !== mediaType ) {
-			return;
+	useEffect( () => {
+		if ( videoPlayer.current ) {
+			videoPlayer.current.load();
 		}
+	}, [ mediaType, mediaUrl ] );
 
-		if ( prevProps.attributes.mediaUrl !== mediaUrl && this.videoPlayer.current ) {
-			this.videoPlayer.current.load();
-		}
-	}
-
-	ensureCorrectBlockOrder() {
-		const {
-			getBlockOrder,
-			moveBlockToPosition,
-			clientId,
-		} = this.props;
-		const order = getBlockOrder( clientId );
-		if ( 1 >= order.length ) {
+	useEffect( () => {
+		if ( childrenOrder.length <= 1 ) {
 			return;
 		}
 		const ctaBlock = getCallToActionBlock( clientId );
@@ -105,109 +159,83 @@ class PageEdit extends Component {
 
 		if ( blockToMove ) {
 			// If the either CTA or Attachment is not the last block, move it there.
-			if ( order[ order.length - 1 ] !== blockToMove.clientId ) {
-				moveBlockToPosition( blockToMove.clientId, clientId, clientId, order.length - 1 );
+			if ( childrenOrder[ childrenOrder.length - 1 ] !== blockToMove.clientId ) {
+				moveBlockToPosition( blockToMove.clientId, clientId, clientId, childrenOrder.length - 1 );
 			}
 		}
+	}, [ childrenOrder ] );
+
+	const style = {
+		backgroundImage: IMAGE_BACKGROUND_TYPE === mediaType && mediaUrl ? `url(${ mediaUrl })` : undefined,
+		backgroundPosition: IMAGE_BACKGROUND_TYPE === mediaType ? `${ focalPoint.x * 100 }% ${ focalPoint.y * 100 }%` : undefined,
+		backgroundRepeat: 'no-repeat',
+		backgroundSize: 'cover',
+	};
+
+	if ( VIDEO_BACKGROUND_TYPE === mediaType && poster ) {
+		style.backgroundImage = `url(${ poster })`;
 	}
 
-	render() {
-		const {
-			attributes,
-			autoAdvanceAfterOptions,
-			clientId,
-			isSelected,
-			media,
-			setAttributes,
-			allowedBlocks,
-			allowedBackgroundMediaTypes,
-			videoFeaturedImage,
-		} = this.props;
+	let overlayStyle = {
+		width: '100%',
+		height: '100%',
+		position: 'absolute',
+	};
 
-		const {
-			mediaId,
-			mediaType,
-			mediaUrl,
-			mediaAlt,
-			focalPoint = { x: 0.5, y: 0.5 },
-			overlayOpacity,
-			poster,
-			autoAdvanceAfter,
-			autoAdvanceAfterDuration,
-		} = attributes;
+	const bgColors = JSON.parse( backgroundColors );
 
-		const style = {
-			backgroundImage: IMAGE_BACKGROUND_TYPE === mediaType && mediaUrl ? `url(${ mediaUrl })` : undefined,
-			backgroundPosition: IMAGE_BACKGROUND_TYPE === mediaType ? `${ focalPoint.x * 100 }% ${ focalPoint.y * 100 }%` : undefined,
-			backgroundRepeat: 'no-repeat',
-			backgroundSize: 'cover',
-		};
+	overlayStyle = addBackgroundColorToOverlay( overlayStyle, bgColors );
+	overlayStyle.opacity = overlayOpacity / 100;
 
-		if ( VIDEO_BACKGROUND_TYPE === mediaType && poster ) {
-			style.backgroundImage = `url(${ poster })`;
-		}
-
-		let overlayStyle = {
-			width: '100%',
-			height: '100%',
-			position: 'absolute',
-		};
-
-		const backgroundColors = JSON.parse( attributes.backgroundColors );
-
-		overlayStyle = addBackgroundColorToOverlay( overlayStyle, backgroundColors );
-		overlayStyle.opacity = overlayOpacity / 100;
-
-		return (
-			<>
-				<InspectorControls>
-					<BackgroundColorSettings
-						backgroundColors={ JSON.parse( attributes.backgroundColors ) }
-						setAttributes={ setAttributes }
-						overlayOpacity={ overlayOpacity }
-					/>
-					<BackgroundMediaSettings
-						allowedBackgroundMediaTypes={ allowedBackgroundMediaTypes }
-						media={ media }
-						mediaId={ mediaId }
-						mediaType={ mediaType }
-						mediaAlt={ mediaAlt }
-						mediaUrl={ mediaUrl }
-						poster={ poster }
-						focalPoint={ focalPoint }
-						videoFeaturedImage={ videoFeaturedImage }
-						setAttributes={ setAttributes }
-					/>
-					<PageSettings
-						autoAdvanceAfter={ autoAdvanceAfter }
-						autoAdvanceAfterDuration={ autoAdvanceAfterDuration }
-						autoAdvanceAfterOptions={ autoAdvanceAfterOptions }
-						clientId={ clientId }
-						setAttributes={ setAttributes }
-					/>
-					<AnimationSettings clientId={ clientId } />
-				</InspectorControls>
-				<CopyPasteHandler clientId={ clientId } isSelected={ isSelected }>
-					<div
-						style={ style }
-					>
-						{ VIDEO_BACKGROUND_TYPE === mediaType && media && (
-							<div className="editor-amp-story-page-video-wrap">
-								<video autoPlay muted loop className="editor-amp-story-page-video" poster={ poster } ref={ this.videoPlayer }>
-									<source src={ mediaUrl } type={ media.mime_type } />
-								</video>
-							</div>
-						) }
-						{ backgroundColors.length > 0 && (
-							<div style={ overlayStyle } />
-						) }
-						<InnerBlocks allowedBlocks={ allowedBlocks } />
-					</div>
-				</CopyPasteHandler>
-			</>
-		);
-	}
-}
+	return (
+		<>
+			<InspectorControls>
+				<BackgroundColorSettings
+					backgroundColors={ bgColors }
+					setAttributes={ setAttributes }
+					overlayOpacity={ overlayOpacity }
+				/>
+				<BackgroundMediaSettings
+					allowedBackgroundMediaTypes={ allowedBackgroundMediaTypes }
+					media={ media }
+					mediaId={ mediaId }
+					mediaType={ mediaType }
+					mediaAlt={ mediaAlt }
+					mediaUrl={ mediaUrl }
+					poster={ poster }
+					focalPoint={ focalPoint }
+					videoFeaturedImage={ videoFeaturedImage }
+					setAttributes={ setAttributes }
+				/>
+				<PageSettings
+					autoAdvanceAfter={ autoAdvanceAfter }
+					autoAdvanceAfterDuration={ autoAdvanceAfterDuration }
+					autoAdvanceAfterOptions={ autoAdvanceAfterOptions }
+					clientId={ clientId }
+					setAttributes={ setAttributes }
+				/>
+				<AnimationSettings clientId={ clientId } />
+			</InspectorControls>
+			<CopyPasteHandler clientId={ clientId } isSelected={ isSelected }>
+				<div
+					style={ style }
+				>
+					{ VIDEO_BACKGROUND_TYPE === mediaType && media && (
+						<div className="editor-amp-story-page-video-wrap">
+							<video autoPlay muted loop className="editor-amp-story-page-video" poster={ poster } ref={ videoPlayer }>
+								<source src={ mediaUrl } type={ media.mime_type } />
+							</video>
+						</div>
+					) }
+					{ bgColors.length > 0 && (
+						<div style={ overlayStyle } />
+					) }
+					<InnerBlocks allowedBlocks={ allowedBlocks } />
+				</div>
+			</CopyPasteHandler>
+		</>
+	);
+};
 
 PageEdit.propTypes = {
 	clientId: PropTypes.string.isRequired,
@@ -227,78 +255,8 @@ PageEdit.propTypes = {
 		autoAdvanceAfterDuration: PropTypes.number,
 		mediaAlt: PropTypes.string,
 	} ).isRequired,
-	isSelected: PropTypes.bool.isRequired,
+	isSelected: PropTypes.bool,
 	setAttributes: PropTypes.func.isRequired,
-	media: PropTypes.object,
-	allowedBlocks: PropTypes.arrayOf( PropTypes.string ).isRequired,
-	getBlockOrder: PropTypes.func.isRequired,
-	moveBlockToPosition: PropTypes.func.isRequired,
-	videoFeaturedImage: PropTypes.shape( {
-		source_url: PropTypes.string,
-	} ),
-	storySettingsAttributes: PropTypes.shape( {
-		autoAdvanceAfter: PropTypes.string,
-		autoAdvanceAfterDuration: PropTypes.number,
-	} ),
-	autoAdvanceAfterOptions: PropTypes.array,
-	allowedBackgroundMediaTypes: PropTypes.arrayOf( PropTypes.string ).isRequired,
 };
 
-export default compose(
-	withDispatch( ( dispatch ) => {
-		const { moveBlockToPosition } = dispatch( 'core/block-editor' );
-		return {
-			moveBlockToPosition,
-		};
-	} ),
-	withSelect( ( select, { clientId, attributes } ) => {
-		const { getMedia } = select( 'core' );
-		const { getBlockOrder } = select( 'core/block-editor' );
-		const { getSettings } = select( 'amp/story' );
-
-		const isFirstPage = getBlockOrder().indexOf( clientId ) === 0;
-		const isCallToActionAllowed = ! isFirstPage && ! getCallToActionBlock( clientId ) && ! getPageAttachmentBlock( clientId );
-		const isPageAttachmentAllowed = ! getCallToActionBlock( clientId ) && ! getPageAttachmentBlock( clientId );
-
-		const { mediaType, mediaId, poster } = attributes;
-
-		const media = mediaId ? getMedia( mediaId ) : undefined;
-
-		let videoFeaturedImage;
-
-		if ( VIDEO_BACKGROUND_TYPE === mediaType && media && media.featured_media && ! poster ) {
-			videoFeaturedImage = getMedia( media.featured_media );
-		}
-
-		let allowedBlocks = ALLOWED_MOVABLE_BLOCKS;
-
-		if ( isCallToActionAllowed ) {
-			allowedBlocks = [
-				...allowedBlocks,
-				'amp/amp-story-cta',
-			];
-		}
-		if ( isPageAttachmentAllowed ) {
-			allowedBlocks = [
-				...allowedBlocks,
-				'amp/amp-story-page-attachment',
-			];
-		}
-
-		const { getEditedPostAttribute } = select( 'core/editor' );
-		const postMeta = getEditedPostAttribute( 'meta' ) || {};
-		const storySettingsAttributes = metaToAttributeNames( postMeta );
-		const { allowedVideoMimeTypes, storySettings } = getSettings();
-		const { autoAdvanceAfterOptions } = storySettings || {};
-
-		return {
-			media,
-			videoFeaturedImage,
-			allowedBlocks,
-			getBlockOrder,
-			storySettingsAttributes,
-			autoAdvanceAfterOptions,
-			allowedBackgroundMediaTypes: [ IMAGE_BACKGROUND_TYPE, ...allowedVideoMimeTypes ],
-		};
-	} ),
-)( PageEdit );
+export default PageEdit;
