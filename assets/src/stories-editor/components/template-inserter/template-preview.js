2e8a262b2d161a78f775fe058b63cc2daad51c94
Pascal Birchler <pascalb@google.com>
2019-09-27 15:40:01 +0200

    Refactor TemplatePreview to use hooks

--- assets/src/stories-editor/components/template-inserter/template-preview.js
+++ assets/src/stories-editor/components/template-inserter/template-preview.js
@@ -8,47 +8,37 @@ import PropTypes from 'prop-types';
  */
 import { withSafeTimeout } from '@wordpress/compose';
 import { Spinner } from '@wordpress/components';
-import { Component } from '@wordpress/element';
+import { useState, useEffect } from '@wordpress/element';
 
 /**
  * Internal dependencies
  */
 import { BlockPreview } from '../';
 
-class TemplatePreview extends Component {
-	constructor( ...args ) {
-		super( ...args );
+const TemplatePreview = ( { item } ) => {
+	const [ shouldLoad, setShouldLoad ] = useState( false );
 
-		this.state = {
-			shouldLoad: false,
-		};
-	}
-
-	componentDidMount() {
-		if ( ! this.state.shouldLoad ) {
+	useEffect( () => {
+		if ( ! shouldLoad ) {
 			// @todo Look into React Concurrent mode to replace this once it gets available.
 			// Set timeout to cause a small latency between loading the templates, otherwise they all try to load instantly and cause a lag.
 			this.props.setTimeout( () => {
-				this.setState( { shouldLoad: true } );
+				setShouldLoad( true );
 			}, 100 );
 		}
+	}, [ shouldLoad ] );
+
+	if ( ! shouldLoad ) {
+		return <Spinner />;
 	}
 
-	render() {
-		if ( ! this.state.shouldLoad ) {
-			return <Spinner />;
-		}
-
-		const { item } = this.props;
-
-		return (
-			<BlockPreview
-				name="core/block"
-				attributes={ { ref: item.id } }
-			/>
-		);
-	}
-}
+	return (
+		<BlockPreview
+			name="core/block"
+			attributes={ { ref: item.id } }
+		/>
+	);
+};
 
 TemplatePreview.propTypes = {
 	item: PropTypes.shape( {
