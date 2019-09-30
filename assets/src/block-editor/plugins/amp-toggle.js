4830a1b9e361c7586acc510d717f63cd048fdf2c
Pascal Birchler <pascalb@google.com>
2019-09-30 18:43:50 +0200

    Refactor AMPToggle to use hooks

--- assets/src/block-editor/plugins/amp-toggle.js
+++ assets/src/block-editor/plugins/amp-toggle.js
@@ -3,22 +3,16 @@
  * External dependencies
  */
 import { errorMessages } from 'amp-block-editor-data';
-import PropTypes from 'prop-types';
 
 /**
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
 import { FormToggle, Notice } from '@wordpress/components';
-import { RawHTML } from '@wordpress/element';
-import { withSelect, withDispatch } from '@wordpress/data';
+import { RawHTML, useCallback } from '@wordpress/element';
+import { useSelect, useDispatch } from '@wordpress/data';
 import { PluginPostStatusInfo } from '@wordpress/edit-post';
-import { compose, withInstanceId } from '@wordpress/compose';
-
-/**
- * Internal dependencies
- */
-import { isAMPEnabled } from '../helpers';
+import { withInstanceId } from '@wordpress/compose';
 
 /**
  * Adds an 'Enable AMP' toggle to the block editor 'Status & Visibility' section.
@@ -29,63 +23,66 @@ import { isAMPEnabled } from '../helpers';
  *
  * @return {Object} AMPToggle component.
  */
-function AMPToggle( { isEnabled, onChange } ) {
-	return (
-		<>
-			<PluginPostStatusInfo>
-				{ ! errorMessages.length && <label htmlFor="amp-enabled">{ __( 'Enable AMP', 'amp' ) }</label> }
-				{
-					! errorMessages.length &&
-					(
-						<FormToggle
-							checked={ isEnabled }
-							onChange={ () => onChange( ! isEnabled ) }
-							id="amp-enabled"
-						/>
-					)
-				}
-				{
-					Boolean( errorMessages.length ) &&
-					(
-						<Notice
-							status="warning"
-							isDismissible={ false }
-						>
-							{
-								errorMessages.map(
-									( message, index ) => <RawHTML key={ index }>{ message }</RawHTML>
-								)
-							}
-						</Notice>
-					)
-				}
-			</PluginPostStatusInfo>
-		</>
-	);
-}
+const AMPToggle = () => {
+	const isAMPEnabled = useSelect( ( select ) => {
+		const { getDefaultStatus, getPossibleStatuses } = select( 'amp/block-editor' );
+		const { getEditedPostAttribute } = select( 'core/editor' );
 
-AMPToggle.propTypes = {
-	isEnabled: PropTypes.bool.isRequired,
-	onChange: PropTypes.func.isRequired,
+		const type = getEditedPostAttribute( 'type' );
+
+		if ( 'amp_story' === type ) {
+			return true;
+		}
+
+		const meta = getEditedPostAttribute( 'meta' );
+
+		if ( meta && meta.amp_status && getPossibleStatuses().includes( meta.amp_status ) ) {
+			return 'enabled' === meta.amp_status;
+		}
+
+		return 'enabled' === getDefaultStatus();
+	} );
+
+	const { editPost } = useDispatch( 'core/editor' );
+
+	const toggleStatus = useCallback( () => {
+		const newStatus = isAMPEnabled ? 'disabled' : 'enabled';
+		editPost( { meta: { amp_status: newStatus } } );
+	}, [ isAMPEnabled, editPost ] );
+
+	return (
+		<PluginPostStatusInfo>
+			{ ! errorMessages.length && (
+				<>
+					<label htmlFor="amp-enabled">{ __( 'Enable AMP', 'amp' ) }</label>
+					<FormToggle
+						checked={ isAMPEnabled }
+						onChange={ toggleStatus }
+						id="amp-enabled"
+					/>
+				</>
+			) }
+			{
+				Boolean( errorMessages.length ) &&
+				(
+					<Notice
+						status="warning"
+						isDismissible={ false }
+					>
+						{
+							errorMessages.map(
+								( message, index ) => <RawHTML key={ index }>{ message }</RawHTML>
+							)
+						}
+					</Notice>
+				)
+			}
+		</PluginPostStatusInfo>
+	);
 };
 
 export const name = 'amp';
 
 export const icon = 'hidden';
 
-export const render = compose(
-	withSelect( () => {
-		return {
-			isEnabled: isAMPEnabled(),
-		};
-	} ),
-	withDispatch( ( dispatch ) => {
-		return {
-			onChange: ( isEnabled ) => {
-				const newStatus = isEnabled ? 'enabled' : 'disabled';
-				dispatch( 'core/editor' ).editPost( { meta: { amp_status: newStatus } } );
-			},
-		};
-	} ),
-	withInstanceId,
-)( AMPToggle );
+export const render = withInstanceId( AMPToggle );
