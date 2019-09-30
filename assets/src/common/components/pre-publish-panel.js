0ce9e7b7e4623ddcb7feb60713cd578f10c3a06a
Pascal Birchler <pascalb@google.com>
2019-09-30 19:04:52 +0200

    Refactor PrePublishPanel to use hooks

--- assets/src/common/components/pre-publish-panel.js
+++ assets/src/common/components/pre-publish-panel.js
@@ -8,7 +8,7 @@ import PropTypes from 'prop-types';
  */
 import { Notice } from '@wordpress/components';
 import { PluginPrePublishPanel } from '@wordpress/edit-post';
-import { withSelect } from '@wordpress/data';
+import { useSelect } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 
 /**
@@ -20,14 +20,23 @@ import { validateFeaturedImage } from '../helpers';
  * Conditionally adds a notice to the pre-publish panel for the featured image.
  *
  * @param {Object} props Component props.
- * @param {Object} props.featuredMedia Media object.
  * @param {Array} props.dimensions Required image dimensions.
  * @param {boolean} props.required Whether selecting a featured image is required.
  *
  * @return {Function} Either a plain pre-publish panel, or the panel with a featured image notice.
  */
-const PrePublishPanel = ( { featuredMedia, dimensions, required } ) => {
-	const errors = validateFeaturedImage( featuredMedia, dimensions, required );
+const PrePublishPanel = ( { dimensions, required } ) => {
+	const featuredImage = useSelect( ( select ) => {
+		const currentPost = select( 'core/editor' ).getCurrentPost();
+		const editedFeaturedMedia = select( 'core/editor' ).getEditedPostAttribute( 'featured_media' );
+		const featuredMedia = currentPost.featured_media || editedFeaturedMedia;
+
+		return {
+			featuredMedia: featuredMedia ? select( 'core' ).getMedia( featuredMedia ) : null,
+		};
+	} );
+
+	const errors = validateFeaturedImage( featuredImage, dimensions, required );
 
 	if ( ! errors ) {
 		return null;
@@ -57,7 +66,6 @@ const PrePublishPanel = ( { featuredMedia, dimensions, required } ) => {
 };
 
 PrePublishPanel.propTypes = {
-	featuredMedia: PropTypes.object,
 	dimensions: PropTypes.shape( {
 		width: PropTypes.number.isRequired,
 		height: PropTypes.number.isRequired,
@@ -65,12 +73,4 @@ PrePublishPanel.propTypes = {
 	required: PropTypes.bool,
 };
 
-export default withSelect( ( select ) => {
-	const currentPost = select( 'core/editor' ).getCurrentPost();
-	const editedFeaturedMedia = select( 'core/editor' ).getEditedPostAttribute( 'featured_media' );
-	const featuredMedia = currentPost.featured_media || editedFeaturedMedia;
-
-	return {
-		featuredMedia: featuredMedia ? select( 'core' ).getMedia( featuredMedia ) : null,
-	};
-} )( PrePublishPanel );
+export default PrePublishPanel;
