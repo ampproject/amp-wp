0a77de43b4d0e661a28ed9193ef46980bdac0ef8
Pascal Birchler <pascalb@google.com>
2019-10-04 19:10:14 +0200

    Refactor BlockPreviewLabel to use hooks

--- assets/src/stories-editor/components/block-preview-label.js
+++ assets/src/stories-editor/components/block-preview-label.js
@@ -8,11 +8,84 @@ import PropTypes from 'prop-types';
  */
 import { getBlockType } from '@wordpress/blocks';
 import { BlockIcon } from '@wordpress/block-editor';
-import { withSelect } from '@wordpress/data';
+import { useSelect } from '@wordpress/data';
 import { __experimentalGetSettings as getDateSettings, dateI18n } from '@wordpress/date';
 import { __ } from '@wordpress/i18n';
 
-const BlockPreviewLabel = ( { content, icon, displayIcon = true, alignIcon = 'left', accessibilityText = false } ) => {
+const BlockPreviewLabel = ( { block, label, displayIcon = true, alignIcon = 'left', accessibilityText = false } ) => {
+	const { attributes, name } = block;
+
+	const {
+		content,
+		icon,
+	} = useSelect( ( select ) => {
+		const { getEditedPostAttribute } = select( 'core/editor' );
+		const { getAuthors, getMedia } = select( 'core' );
+
+		const blockType = getBlockType( name );
+
+		label = blockType.title;
+		let blockContent = '';
+
+		switch ( name ) {
+			case 'core/image':
+				if ( attributes.url ) {
+					blockContent = attributes.url.slice( attributes.url.lastIndexOf( '/' ) ).slice( 1, 30 );
+
+					if ( blockContent.length > 0 ) {
+						label = blockContent;
+					}
+				}
+
+				if ( attributes.id ) {
+					const media = getMedia( attributes.id );
+
+					if ( media ) {
+						label = media.caption.raw || media.title.raw || label;
+					}
+				}
+
+				break;
+			case 'amp/amp-story-text':
+				if ( attributes.content.length > 0 ) {
+					blockContent = attributes.content
+						.replace( /<br>/g, ' ' )
+						.replace( /<[^<>]+>/g, '' )
+						.slice( 0, 30 );
+				}
+
+				label = blockContent.length > 0 ? blockContent : blockType.title;
+				break;
+
+			case 'amp/amp-story-post-author':
+				const author = getAuthors().find( ( { id } ) => id === getEditedPostAttribute( 'author' ) );
+
+				label = author ? author.name : __( 'Post Author', 'amp' );
+				break;
+
+			case 'amp/amp-story-post-date':
+				const postDate = getEditedPostAttribute( 'date' );
+				const dateSettings = getDateSettings();
+				const dateFormat = dateSettings.formats.date;
+				const date = postDate || new Date();
+
+				label = dateI18n( dateFormat, date );
+				break;
+
+			case 'amp/amp-story-post-title':
+				label = getEditedPostAttribute( 'title' ) || blockType.title;
+				break;
+
+			default:
+				break;
+		}
+
+		return {
+			content: label,
+			icon: blockType.icon,
+		};
+	}, [ name, attributes ] );
+
 	return (
 		<>
 			{ displayIcon && 'left' === alignIcon && <BlockIcon icon={ icon } /> }
@@ -29,7 +102,8 @@ const BlockPreviewLabel = ( { content, icon, displayIcon = true, alignIcon = 'le
 
 BlockPreviewLabel.propTypes = {
 	content: PropTypes.string.isRequired,
-	icon: PropTypes.object,
+	block: PropTypes.object,
+	label: PropTypes.string,
 	displayIcon: PropTypes.bool,
 	alignIcon: PropTypes.oneOf( [ 'left', 'right' ] ),
 	accessibilityText: PropTypes.oneOfType( [
@@ -38,77 +112,4 @@ BlockPreviewLabel.propTypes = {
 	] ),
 };
 
-export default withSelect( ( select, { block, label } ) => {
-	if ( ! block ) {
-		return {
-			content: label,
-			icon: null,
-		};
-	}
-
-	const { getEditedPostAttribute } = select( 'core/editor' );
-	const { getAuthors, getMedia } = select( 'core' );
-
-	const blockType = getBlockType( block.name );
-
-	label = blockType.title;
-	let content = '';
-
-	switch ( block.name ) {
-		case 'core/image':
-			if ( block.attributes.url ) {
-				content = block.attributes.url.slice( block.attributes.url.lastIndexOf( '/' ) ).slice( 1, 30 );
-
-				if ( content.length > 0 ) {
-					label = content;
-				}
-			}
-
-			if ( block.attributes.id ) {
-				const media = getMedia( block.attributes.id );
-
-				if ( media ) {
-					label = media.caption.raw || media.title.raw || label;
-				}
-			}
-
-			break;
-		case 'amp/amp-story-text':
-			if ( block.attributes.content.length > 0 ) {
-				content = block.attributes.content
-					.replace( /<br>/g, ' ' )
-					.replace( /<[^<>]+>/g, '' )
-					.slice( 0, 30 );
-			}
-
-			label = content.length > 0 ? content : blockType.title;
-			break;
-
-		case 'amp/amp-story-post-author':
-			const author = getAuthors().find( ( { id } ) => id === getEditedPostAttribute( 'author' ) );
-
-			label = author ? author.name : __( 'Post Author', 'amp' );
-			break;
-
-		case 'amp/amp-story-post-date':
-			const postDate = getEditedPostAttribute( 'date' );
-			const dateSettings = getDateSettings();
-			const dateFormat = dateSettings.formats.date;
-			const date = postDate || new Date();
-
-			label = dateI18n( dateFormat, date );
-			break;
-
-		case 'amp/amp-story-post-title':
-			label = getEditedPostAttribute( 'title' ) || blockType.title;
-			break;
-
-		default:
-			break;
-	}
-
-	return {
-		content: label,
-		icon: blockType.icon,
-	};
-} )( BlockPreviewLabel );
+export default BlockPreviewLabel;
