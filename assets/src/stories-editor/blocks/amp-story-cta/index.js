de56c044a200493665b616cc8899079390dc3ead
Miina Sikk <miina.sikk@gmail.com>
2019-10-09 13:52:25 +0200

    Fix selector.

--- assets/src/stories-editor/blocks/amp-story-cta/index.js
+++ assets/src/stories-editor/blocks/amp-story-cta/index.js
@@ -22,7 +22,7 @@ const schema = {
 	text: {
 		type: 'string',
 		source: 'html',
-		selector: 'a, .amp-cta-content',
+		selector: '.amp-cta-content',
 	},
 	// The rest of the color attributes are added by addAMPAttributes()
 	customTextColor: {
