81be395a5ab7c9ebc6b7aeb4b5be7a8b0fddd2d3
Ryan Kienstra <kienstraryan@gmail.com>
2019-10-14 18:40:55 -0500

    Add the remaining query vars to the submenu
    There is still much remaining,
    including adding the toggles and styling.

--- assets/src/admin/components/debug-options.js
+++ assets/src/admin/components/debug-options.js
@@ -3,26 +3,34 @@
  */
 import { __ } from '@wordpress/i18n';
 
+const debugOptions = {
+	'disable-post': __( 'Disable post processing', 'amp' ),
+	'disable-cache': __( 'Disable response cache', 'amp' ),
+	'prevent-redirect': __( 'Prevent redirect', 'amp' ),
+	'reject-all-errors': __( 'Reject all errors', 'amp' ),
+	'accept-excessive-css': __( 'Accept excessive CSS', 'amp' ),
+	'disable-amp': __( 'Disable AMP', 'amp' ),
+	'disable-tree-shaking': __( 'Disable tree shaking', 'amp' ),
+};
+
 /**
  * Adds a submenu of debug options to the admin bar's AMP submenu.
  *
  * @return {Function} The component.
  */
 const DebugOptions = () => {
-	return (
-		<>
-			<li id="wp-admin-bar-amp-disable-post" data-ampdevmode>
+	const listItems = [];
+	for ( const [ debugId, title ] of Object.entries( debugOptions ) ) {
+		listItems.push( (
+			<li id={ `wp-admin-bar-amp-${ debugId }` } data-ampdevmode>
 				<div className="ab-item">
-					{ __( 'Disable post processing', 'amp' ) }
+					{ title }
 				</div>
 			</li>
-			<li id="wp-admin-bar-amp-disable-cache" data-ampdevmode>
-				<div className="ab-item">
-					{ __( 'Disable response cache', 'amp' ) }
-				</div>
-			</li>
-		</>
-	);
+		) );
+	}
+
+	return listItems;
 };
 
 export default DebugOptions;
