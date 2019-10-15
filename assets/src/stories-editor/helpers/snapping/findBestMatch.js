a11a6cc6c0d596713efc15217936722652694abc
Morten Barklund <morten@barklund.dk>
2019-10-15 11:12:19 -0400

    Split snap targets in edge and center targets respectively

--- assets/src/stories-editor/helpers/snapping/findBestMatch.js
+++ assets/src/stories-editor/helpers/snapping/findBestMatch.js
@@ -1,23 +1,21 @@
 /**
  * Get the best match for a list of values each creating a match
  *
- * @param {Function.<number>} matcher Function finding closest match for a single value or null
- * @param {Array.<number>} values List of values to find a best match between.
+ * @param {Array.<Object>} targets List of values and matchers to find a best match between.
  * @return {number} The single best matching value or undefined.
  */
-const findBestMatch = ( matcher, ...values ) => values
-	.map( ( value ) => {
+const findBestMatch = ( ...targets ) => targets
+	.map( ( { value, matcher, ...rest } ) => {
 		const best = matcher( value );
 		if ( ! best ) {
 			return null;
 		}
-		return { value: best, distance: Math.abs( best - value ) };
+		return { value: best, distance: Math.abs( best - value ), ...rest };
 	} )
 	.filter( Boolean )
 	.reduce(
 		( best, candidate ) => candidate.distance < best.distance ? candidate : best,
 		{ distance: Number.MAX_VALUE, value: null },
-	)
-	.value;
+	);
 
 export default findBestMatch;
