0c93dfd5798b03fdd3eb43c0b37e7e2e3ca45dfe
Pierre Gordon <pierregordon@protonmail.com>
2019-10-18 20:00:41 -0500

    Add scaffold for beta tester plugin

---
+++ amp-beta-tester.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: AMP Beta Tester
+ * Description: Opt-in to receive non-stable release builds for the AMP plugin.
+ * Plugin URI: https://amp-wp.org
+ * Author: AMP Project Contributors
+ * Author URI: https://github.com/ampproject/amp-wp/graphs/contributors
+ * Version: 0.1
+ * Text Domain: amp-beta-tester
+ * Domain Path: /languages/
+ * License: GPLv2 or later
+ *
+ * @package AMP Beta Tester
+ */
