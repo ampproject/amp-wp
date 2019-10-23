d4f2963bfc1fa3011ceaf15bf993f3e9ad7452bf
Pierre Gordon <pierregordon@protonmail.com>
2019-10-22 19:25:49 -0500

    Add test for `get_amp_github_releases`

--- tests/php/test-amp-beta-tester.php
+++ tests/php/test-amp-beta-tester.php
@@ -7,6 +7,8 @@
  */
 class AMP_Beta_Tester_Test extends WP_UnitTestCase {
 
+	protected $github_api_response = '[{ "url": "https://api.github.com/repos/ampproject/amp-wp/releases/20819569", "assets_url": "https://api.github.com/repos/ampproject/amp-wp/releases/20819569/assets", "upload_url": "https://uploads.github.com/repos/ampproject/amp-wp/releases/20819569/assets{?name,label}", "html_url": "https://github.com/ampproject/amp-wp/releases/tag/1.4.0-beta1", "id": 20819569, "node_id": "MDc6UmVsZWFzZTIwODE5NTY5", "tag_name": "1.4.0-beta1", "target_commitish": "develop", "name": "1.4.0-beta1", "draft": false, "author": { "login": "westonruter", "id": 134745, "node_id": "MDQ6VXNlcjEzNDc0NQ==", "avatar_url": "https://avatars2.githubusercontent.com/u/134745?v=4", "gravatar_id": "", "url": "https://api.github.com/users/westonruter", "html_url": "https://github.com/westonruter", "followers_url": "https://api.github.com/users/westonruter/followers", "following_url": "https://api.github.com/users/westonruter/following{/other_user}", "gists_url": "https://api.github.com/users/westonruter/gists{/gist_id}", "starred_url": "https://api.github.com/users/westonruter/starred{/owner}{/repo}", "subscriptions_url": "https://api.github.com/users/westonruter/subscriptions", "organizations_url": "https://api.github.com/users/westonruter/orgs", "repos_url": "https://api.github.com/users/westonruter/repos", "events_url": "https://api.github.com/users/westonruter/events{/privacy}", "received_events_url": "https://api.github.com/users/westonruter/received_events", "type": "User", "site_admin": false }]';
+
 	/**
 	 * Runs the routine before setting up all tests.
 	 */
@@ -111,6 +113,29 @@ class AMP_Beta_Tester_Test extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Test get_amp_github_releases().
+	 *
+	 * @covers \AMP_Beta_Tester\get_amp_github_releases()
+	 */
+	public function test_get_amp_github_releases() {
+		// It returns `null` on a failed HTTP request.
+		add_filter( 'pre_http_request', [ $this, 'mock_http_request_error' ], 10, 3 );
+		$amp_releases = AMP_Beta_Tester\get_amp_github_releases();
+
+		$this->assertEquals( null, $amp_releases );
+
+		remove_filter( 'pre_http_request', [ $this, 'mock_http_request_error' ] );
+
+		// It returns a JSON array on a successful HTTP request.
+		add_filter( 'pre_http_request', [ $this, 'mock_github_api_http_request' ], 10, 3 );
+		$amp_releases = AMP_Beta_Tester\get_amp_github_releases();
+
+		$this->assertEquals( json_decode( $this->github_api_response ), $amp_releases );
+
+		remove_filter( 'pre_http_request', [ $this, 'mock_github_api_http_request' ] );
+	}
+
+	/**
 	 * Test is_pre_release().
 	 *
 	 * @covers \AMP_Beta_Tester\is_pre_release()
@@ -164,5 +189,46 @@ class AMP_Beta_Tester_Test extends WP_UnitTestCase {
 
 		$this->assertEquals( 'AMP v' . AMP__VERSION, $node_amp_beta_tester->title );
 		$this->assertEquals( admin_url( 'admin.php?page=amp-options' ), $node_amp_beta_tester->href );
+	}
+
+	/**
+	 * Mock a failed HTTP request to GitHub's API.
+	 *
+	 * @param mixed  $preempt Whether to preempt an HTTP request's return value. Default false.
+	 * @param mixed  $r       HTTP request arguments.
+	 * @param string $url     The request URL.
+	 * @return WP_Error Simulated HTTP error.
+	 */
+	public function mock_http_request_error( $preempt, $r, $url ) {
+		if ( false === strpos( $url, 'api.github.com' ) ) {
+			return $preempt;
+		}
+
+		return new WP_Error( 'failed_http_error' );
+	}
+
+	/**
+	 * Mock a successful HTTP request to GitHub's API.
+	 *
+	 * @param mixed  $preempt Whether to preempt an HTTP request's return value. Default false.
+	 * @param mixed  $r       HTTP request arguments.
+	 * @param string $url     The request URL.
+	 * @return array Response data.
+	 */
+	public function mock_github_api_http_request( $preempt, $r, $url ) {
+		if ( false === strpos( $url, 'api.github.com' ) ) {
+			return $preempt;
+		}
+
+		return [
+			'body'          => $this->github_api_response,
+			'headers'       => [],
+			'response'      => [
+				'code'    => 200,
+				'message' => 'ok',
+			],
+			'cookies'       => [],
+			'http_response' => null,
+		];
 	}
 }
