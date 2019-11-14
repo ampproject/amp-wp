59ff70cb41063e5680207c1e8bd2f38c7eada457
Weston Ruter <westonruter@google.com>
2019-11-14 12:06:21 -0800

    Build 1.4.1

--- assets/src/stories-editor/helpers/snapping/index.js
+++
@@ -1,4 +0,0 @@
-export { default as findClosestSnap } from './findClosestSnap';
-export { default as getHorizontalTargets } from './getHorizontalTargets';
-export { default as getVerticalTargets } from './getVerticalTargets';
-export { default as getBestSnapLines } from './getBestSnapLines';
