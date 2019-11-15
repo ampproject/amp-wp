e2951f5744363ccc350576f4636eab882b18465a
Alain Schlesser <alain.schlesser@gmail.com>
2019-11-15 16:10:35 +0100

    Add initial test for meta tag sanitization

---
+++ tests/php/test-class-amp-meta-sanitizer.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Tests for AMP_Meta_Sanitizer.
+ *
+ * @package AMP
+ */
+
+/**
+ * Tests for AMP_Meta_Sanitizer.
+ */
+class Test_AMP_Meta_Sanitizer extends WP_UnitTestCase {
+
+	/**
+	 * Provide data to the test_sanitize method.
+	 *
+	 * @return array[] Array of arrays with test data.
+	 */
+	public function get_data_for_sanitize() {
+		return [
+			// Turn HTML 4 charset into HTML 5 charset.
+			[
+				'<!DOCTYPE html><html><head><meta charset="UTF-8"></head><body></body></html>',
+				'<!DOCTYPE html><html><head><meta charset="UTF-8"></head><body></body></html>'
+			],
+			[
+				'<!DOCTYPE html><html><head><meta http-equiv="content-type" content="text/html"></head><body></body></html>',
+				'<!DOCTYPE html><html><head><meta charset="utf-8"></head><body></body></html>'
+			],
+			[
+				'<!DOCTYPE html><html><head><meta http-equiv="content-type" content="text/html; charset=UTF-8"></head><body></body></html>',
+				'<!DOCTYPE html><html><head><meta charset="utf-8"></head><body></body></html>'
+			],
+			[
+				'<!DOCTYPE html><html><head><meta http-equiv="content-type" content="text/html; charset=UTF-8" charset="UTF-8"></head><body></body></html>',
+				'<!DOCTYPE html><html><head><meta charset="utf-8"></head><body></body></html>'
+			],
+		];
+	}
+
+	/**
+	 * Tests the sanitize method.
+	 *
+	 * @dataProvider get_data_for_sanitize
+	 * @covers \AMP_Meta_Sanitizer::sanitize()
+	 *
+	 * @param string  $source_content   Source DOM content.
+	 * @param string  $expected_content Expected content after sanitization.
+	 */
+	public function test_sanitize( $source_content, $expected_content ) {
+		$dom       = AMP_DOM_Utils::get_dom( $source_content );
+		$sanitizer = new AMP_Meta_Sanitizer( $dom );
+		$sanitizer->sanitize();
+
+		$this->assertEqualMarkup( $expected_content, $dom->saveHTML() );
+	}
+
+	/**
+	 * Assert markup is equal.
+	 *
+	 * @param string $expected Expected markup.
+	 * @param string $actual   Actual markup.
+	 */
+	public function assertEqualMarkup( $expected, $actual ) {
+		$actual   = preg_replace( '/\s+/', ' ', $actual );
+		$expected = preg_replace( '/\s+/', ' ', $expected );
+		$actual   = preg_replace( '/(?<=>)\s+(?=<)/', '', trim( $actual ) );
+		$expected = preg_replace( '/(?<=>)\s+(?=<)/', '', trim( $expected ) );
+
+		$this->assertEquals(
+			array_filter( preg_split( '#(<[^>]+>|[^<>]+)#', $expected, -1, PREG_SPLIT_DELIM_CAPTURE ) ),
+			array_filter( preg_split( '#(<[^>]+>|[^<>]+)#', $actual, -1, PREG_SPLIT_DELIM_CAPTURE ) )
+		);
+	}
+}
