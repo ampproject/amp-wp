ca89bf96d4103bc6c208ad1bb9f86a46b6d59e91
Weston Ruter <westonruter@google.com>
2019-11-18 22:22:31 -0800

    Remove unused (and deprecated) functions amp_load_fasterimage_classes() and amp_get_fasterimage_client()

--- includes/deprecated.php
+++ includes/deprecated.php
@@ -4,27 +4,3 @@
  *
  * @package AMP
  */
-
-/**
- * Load classes for FasterImage.
- *
- * @deprecated This is obsolete now that there is an autoloader.
- */
-function amp_load_fasterimage_classes() {
-	_deprecated_function( __FUNCTION__, '0.6' );
-}
-
-/**
- * Get FasterImage client for user agent.
- *
- * @deprecated This function is no longer used in favor of just instantiating the class.
- *
- * @param string $user_agent User Agent.
- * @return \FasterImage\FasterImage Instance.
- */
-function amp_get_fasterimage_client( $user_agent ) {
-	_deprecated_function( __FUNCTION__, '1.0' );
-	$client = new FasterImage\FasterImage();
-	$client->setUserAgent( $user_agent );
-	return $client;
-}
