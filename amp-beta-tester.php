6ad0294ff04f3cfb0d9e0eaaf9071967fd6e87bd
Pierre Gordon <pierregordon@protonmail.com>
2019-12-06 00:22:04 -0500

    Require a minimum WP and PHP version

--- amp-beta-tester.php
+++ amp-beta-tester.php
@@ -1,14 +1,16 @@
 <?php
 /**
- * Plugin Name: AMP Beta Tester
- * Description: Opt-in to receive non-stable release builds for the AMP plugin.
- * Plugin URI: https://amp-wp.org
- * Author: AMP Project Contributors
- * Author URI: https://github.com/ampproject/amp-wp/graphs/contributors
- * Version: 1.5.0-alpha
- * Text Domain: amp
- * Domain Path: /languages/
- * License: GPLv2 or later
+ * Plugin Name:       AMP Beta Tester
+ * Description:       Opt-in to receive non-stable release builds for the AMP plugin.
+ * Plugin URI:        https://amp-wp.org
+ * Author:            AMP Project Contributors
+ * Author URI:        https://github.com/ampproject/amp-wp/graphs/contributors
+ * Version:           1.5.0-alpha
+ * Text Domain:       amp
+ * Domain Path:       /languages/
+ * License:           GPLv2 or later
+ * Requires at least: 4.9
+ * Requires PHP:      5.4
  *
  * @package AMP Beta Tester
  */
