dee0c503bdf4d3b5b0d97d94ffbe466d0e896919
Jonny Harris <jon@spacedmonkey.co.uk>
2019-12-06 11:13:57 +0000

    Add search icon. Fix issues with size of inserted elements. Styles fixes.

--- assets/src/edit-story/components/library/mediaLibrary.js
+++ assets/src/edit-story/components/library/mediaLibrary.js
@@ -7,7 +7,7 @@ import styled from 'styled-components';
 /**
  * WordPress dependencies
  */
-import { Spinner } from '@wordpress/components';
+import { Spinner, Dashicon } from '@wordpress/components';
 import { useEffect } from '@wordpress/element';
 
 /**
@@ -42,22 +42,23 @@ const Title = styled.h3`
 	font-size: 19px;
 	line-height: 20px;
 	line-height: 1.4em;
-	flex: 2 0 0;
+	flex: 3 0 0;
 `;
 
 const Button = styled.button`
-	 background: #242A3B;
+	 background: none;
 	 color: #ffffff;
 	 padding: 5px;
 	 font-weight: bold;
 	 flex: 1 0 0;
 	 text-align: center;
-	 border: 0px none;
+	 border: 1px solid #616877;;
+	 border-radius: 3px;
 `;
 
 const Header = styled.div`
 	display: flex;
-	margin: 0px 0px 10px;
+	margin: 0px 0px 25px;
 `;
 
 const Message = styled.div`
@@ -68,7 +69,7 @@ const Message = styled.div`
 const FilterButtons = styled.div`
 	border-bottom:  2px solid #606877;
 	padding: 18px 0px;
-	margin: 0px 0px 15px;
+	margin: 10px 0px 15px;
 `;
 
 const FilterButton = styled.button`
@@ -86,9 +87,30 @@ const FilterButton = styled.button`
 const Search = styled.input`
 	width: 100%;
 	background: #616877 !important;
-	color: rgba(255, 255, 255, 0.34)  !important;
-	padding: 2px 10px !important;
+	border-color: #616877 !important;
+	color: #DADADA !important;
+	padding: 2px 10px 2px 33px !important;
+	&::placeholder {
+	  color: #DADADA;
+	}
 `;
+
+const SUPPORTED_IMAGE_TYPES = [
+	'image/png',
+	'image/jpeg',
+	'image/jpg',
+	'image/gif',
+];
+
+const SUPPORTED_VIDEO_TYPES = [
+	'video/mp4',
+];
+
+const FILTERS = [
+	{ filter: '', name: 'All' },
+	{ filter: 'image', name: 'Images' },
+	{ filter: 'video', name: 'Video' },
+];
 
 function MediaLibrary( { onInsert } ) {
 	const {
@@ -116,15 +138,21 @@ function MediaLibrary( { onInsert } ) {
 		// When an image is selected, run a callback.
 		fileFrame.on( 'select', () => {
 			attachment = fileFrame.state().get( 'selection' ).first().toJSON();
-			const { url } = attachment;
-			onInsert( 'image', {
-				src: url,
-				width: 100,
-				height: 100,
-				x: 5,
-				y: 5,
-				rotationAngle: 0,
-			} );
+			const { url: src, mime, width: oWidth, height: oHeight } = attachment;
+			const width = 100;
+			const height = getRelativeHeight( oWidth, oHeight, width );
+			if ( SUPPORTED_IMAGE_TYPES.includes( mime ) ) {
+				onInsert( 'image', {
+					src,
+					width,
+					height,
+					x: 5,
+					y: 5,
+					rotationAngle: 0,
+				} );
+			} else if ( SUPPORTED_VIDEO_TYPES.includes( mime ) ) {
+				// TODO Implemenet Video element.
+			}
 		} );
 
 		fileFrame.on( 'close', () => {
@@ -146,17 +174,16 @@ function MediaLibrary( { onInsert } ) {
 		return n % 2 === 0;
 	};
 
-	const filters = [
-		{ filter: '', name: 'All' },
-		{ filter: 'image', name: 'Images' },
-		{ filter: 'video', name: 'Video' },
-	];
+	const getRelativeHeight = ( oWidth, oHeight, width ) => {
+		const racio = oWidth / width;
+		const height = Math.round( oHeight / racio );
+		return height;
+	};
 
 	const getMediaElement = ( mediaEl, width ) => {
 		const { src, oWidth, oHeight, mimeType } = mediaEl;
-		const racio = oWidth / width;
-		const height = Math.round( oHeight / racio );
-		if ( [ 'image/png', 'image/jpeg' ].includes( mimeType ) ) {
+		const height = getRelativeHeight( oWidth, oHeight, width );
+		if ( SUPPORTED_IMAGE_TYPES.includes( mimeType ) ) {
 			return ( <Image
 				key={ src }
 				src={ src }
@@ -165,23 +192,27 @@ function MediaLibrary( { onInsert } ) {
 				loading={ 'lazy' }
 				onClick={ () => onInsert( 'image', {
 					src,
-					width: 200,
-					height: 100,
+					width,
+					height,
 					x: 5,
 					y: 5,
 					rotationAngle: 0,
 				} ) }
 			/> );
-		} else if ( [ 'video/mp4' ].includes( mimeType ) ) {
+		} else if ( SUPPORTED_VIDEO_TYPES.includes( mimeType ) ) {
+			/* eslint-disable react/jsx-closing-tag-location */
 			return ( <Video
 				key={ src }
 				controls={ true }
 				width={ width }
 				height={ height }
-				onClick={ () => {} }
+				onClick={ () => {
+					// TODO Implement insert video.
+				} }
 			>
 				<source src={ src } type={ mimeType } />
 			</Video> );
+			/* eslint-enable react/jsx-closing-tag-location */
 		}
 		return null;
 	};
@@ -200,20 +231,31 @@ function MediaLibrary( { onInsert } ) {
 				</Button>
 			</Header>
 
-			<div>
+			<div style={ {
+				position: `relative`,
+			} }>
+				<Dashicon
+					icon="search"
+					style={ {
+						position: `absolute`,
+						top: `8px`,
+						left: `10px`,
+						fill: `#DADADA`,
+					} } />
 				<Search
 					type={ 'text' }
 					value={ searchTerm }
 					placeholder={ 'Search Media' }
 					onChange={ ( evt ) => {
-						setSearchTerm( evt.target.value ); setIsMediaLoading( false );
+						setSearchTerm( evt.target.value );
+						setIsMediaLoading( false );
 						setIsMediaLoaded( false );
 					} } />
 			</div>
 
 			<FilterButtons>
 				{
-					filters.map( ( { filter, name }, index ) => (
+					FILTERS.map( ( { filter, name }, index ) => (
 						<FilterButton
 							key={ index }
 							active={ filter === mediaType }
