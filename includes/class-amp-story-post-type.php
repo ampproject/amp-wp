83b53b6a133746a0a06400b19f4e9c413839e61a
spacedmonkey <jon@spacedmonkey.co.uk>
2019-12-13 12:39:02 +0000

    Format the PHP.

--- includes/class-amp-story-post-type.php
+++ includes/class-amp-story-post-type.php
@@ -105,40 +105,7 @@ class AMP_Story_Post_Type {
 				'rest_controller_class' => 'AMP_REST_Stories_Controller',
 			]
 		);
-
-		$meta_args = [
-			'type'           => 'array',
-			'object_subtype' => self::POST_TYPE_SLUG,
-			'single'         => true,
-			'show_in_rest'   => [
-				'schema' => [
-					'type'  => 'array',
-					'items' => [
-						'id'       => [
-							'type' => 'string',
-						],
-						'type'     => [
-							'type'    => 'string',
-							'default' => 'page',
-						],
-						'elements' => [
-							'type'    => 'array',
-							'default' => [],
-						],
-						'index'    => [
-							'type'    => 'integer',
-							'default' => 0,
-						],
-					],
-				],
-			],
-		];
-		// Hide the pages data from none logged in users.
-		if ( ! is_user_logged_in() ) {
-			$meta_args['show_in_rest'] = false;
-		}
-		register_meta( 'post', 'amp_pages', $meta_args );
-
+		
 		add_action( 'admin_enqueue_scripts', [ __CLASS__, 'admin_enqueue_scripts' ] );
 		add_filter( 'show_admin_bar', [ __CLASS__, 'show_admin_bar' ] );
 		add_filter( 'replace_editor', [ __CLASS__, 'replace_editor' ], 10, 2 );
--- includes/rest-api/class-wp-rest-stories-controller.php
+++ includes/rest-api/class-wp-rest-stories-controller.php
@@ -1,34 +1,60 @@
 <?php
 
+/**
+ * Class AMP_REST_Stories_Controller
+ */
 class AMP_REST_Stories_Controller extends WP_REST_Posts_Controller {
+	/**
+	 * Prepares a single story for create or update. Add post_content_filtered field to save/insert.
+	 *
+	 * @param WP_REST_Request $request Request object.
+	 *
+	 * @return stdClass|WP_Error Post object or WP_Error.
+	 */
 	protected function prepare_item_for_database( $request ) {
 		$prepared_story = parent::prepare_item_for_database( $request );
 
+		if ( is_wp_error( $prepared_story ) ) {
+			return $prepared_story;
+		}
+
 		if ( isset( $request['content_filtered'] ) ) {
-			if ( is_string( $request['content_filtered'] ) ) {
-				$prepared_story->post_content_filtered = $request['content_filtered'];
-			} elseif ( isset( $request['content_filtered']['raw'] ) ) {
+			$prepared_story->post_content_filtered = $request['content_filtered'];
+			if ( isset( $request['content_filtered']['raw'] ) ) {
 				$prepared_story->post_content_filtered = $request['content_filtered']['raw'];
 			}
+			$prepared_story->post_content_filtered = maybe_serialize( $prepared_story->post_content_filtered );
 		}
+
 
 		return $prepared_story;
 	}
 
+	/**
+	 * Prepares a single story output for response. Add post_content_filtered field to output.
+	 *
+	 *
+	 * @param WP_Post $post Post object.
+	 * @param WP_REST_Request $request Request object.
+	 *
+	 * @return WP_REST_Response Response object.
+	 */
 	public function prepare_item_for_response( $post, $request ) {
 		$response = parent::prepare_item_for_response( $post, $request );
 		$fields   = $this->get_fields_for_response( $request );
 		$data     = $response->get_data();
+		$schema   = $this->get_item_schema();
 
 		if ( in_array( 'content_filtered', $fields, true ) ) {
+			$post_content_filtered    = maybe_unserialize( $post->post_content_filtered );
 			$data['content_filtered'] = array(
-				'raw'      => $post->post_content_filtered,
+				'raw' => rest_sanitize_value_from_schema( $post_content_filtered, $schema['properties']['content_filtered']['properties']['raw'] ),
 			);
 		}
 
 		$context = ! empty( $request['context'] ) ? $request['context'] : 'view';
-		$data = $this->filter_response_by_context( $data, $context );
-		$links = $response->get_links();
+		$data    = $this->filter_response_by_context( $data, $context );
+		$links   = $response->get_links();
 		// Wrap the data in a response object.
 		$response = rest_ensure_response( $data );
 		foreach ( $links as $rel => $rel_links ) {
@@ -44,12 +70,18 @@ class AMP_REST_Stories_Controller extends WP_REST_Posts_Controller {
 		 *
 		 *
 		 * @param WP_REST_Response $response The response object.
-		 * @param WP_Post          $post     Post object.
-		 * @param WP_REST_Request  $request  Request object.
+		 * @param WP_Post $post Post object.
+		 * @param WP_REST_Request $request Request object.
 		 */
 		return apply_filters( "rest_prepare_{$this->post_type}", $response, $post, $request );
 	}
 
+	/**
+	 * Retrieves the attachment's schema, conforming to JSON Schema.
+	 *
+	 *
+	 * @return array Item schema as an array.
+	 */
 	public function get_item_schema() {
 		if ( $this->schema ) {
 			return $this->add_additional_fields_schema( $this->schema );
@@ -57,7 +89,7 @@ class AMP_REST_Stories_Controller extends WP_REST_Posts_Controller {
 		$schema = parent::get_item_schema();
 
 		$schema['properties']['content_filtered'] = array(
-			'description' => __( 'The post content filtered for the object.' ),
+			'description' => __( 'The post content filtered for the object.', 'amp' ),
 			'type'        => 'object',
 			'context'     => array( 'view', 'edit', 'embed' ),
 			'arg_options' => array(
@@ -65,15 +97,34 @@ class AMP_REST_Stories_Controller extends WP_REST_Posts_Controller {
 				'validate_callback' => null, // Note: validation implemented in self::prepare_item_for_database()
 			),
 			'properties'  => array(
-				'raw'      => array(
-					'description' => __( 'Content filtered for the object, as it exists in the database.' ),
-					'type'        => 'object',
+				'raw' => array(
+					'description' => __( 'Content filtered for the object, as it exists in the database.', 'amp' ),
+					'type'        => 'array',
+					'items'       => [
+						'id'       => [
+							'type' => 'string',
+						],
+						'type'     => [
+							'type'    => 'string',
+							'default' => 'page',
+						],
+						'elements' => [
+							'type'    => 'array',
+							'default' => [],
+						],
+						'index'    => [
+							'type'    => 'integer',
+							'default' => 0,
+						],
+					],
 					'context'     => array( 'edit' ),
+					'default'     => array(),
 				),
 			),
 		);
 
 		$this->schema = $schema;
+
 		return $this->add_additional_fields_schema( $this->schema );
 	}
 
