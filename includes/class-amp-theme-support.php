bb371b1c0115009d82289c49a72dd6009bababba
Mike Crantea <mike.crantea@xwp.co>
2019-12-15 01:50:57 +0200

    Add a filter to allow altering AMP output from themes

--- includes/class-amp-theme-support.php
+++ includes/class-amp-theme-support.php
@@ -2033,7 +2033,7 @@ class AMP_Theme_Support {
 	 */
 	public static function finish_output_buffering( $response ) {
 		self::$is_output_buffering = false;
-		return self::prepare_response( $response );
+		return apply_filters( 'amp_document_output', self::prepare_response( $response ) );
 	}
 
 	/**
