0c73fa33688708c0dfa8ed0de995cfb2211dc42c
Mike Crantea <mike.crantea@xwp.co>
2019-12-15 01:40:25 +0200

    AppShell polyfill: Update to latest version - 2.4.0

--- includes/amp-helper-functions.php
+++ includes/amp-helper-functions.php
@@ -462,7 +462,7 @@ function amp_register_default_scripts( $wp_scripts ) {
 		$handle,
 		sprintf(
 			'if ( ! Element.prototype.attachShadow ) { const script = document.createElement( "script" ); script.src = %s; script.async = true; document.head.appendChild( script ); }',
-			wp_json_encode( 'https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/1.3.3/webcomponents-sd-ce.js' )
+			wp_json_encode( 'https://cdnjs.cloudflare.com/ajax/libs/webcomponentsjs/2.4.0/webcomponents-bundle.js' )
 		),
 		'after'
 	);
