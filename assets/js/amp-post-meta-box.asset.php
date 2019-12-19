375993687574df1eca157aa697473bdd9b382c59
Weston Ruter <westonruter@google.com>
2019-12-19 11:08:17 -0800

    Build 1.4.2

---
+++ assets/js/amp-post-meta-box.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery', 'wp-polyfill'), 'version' => '3cfa391b8a85b9cd4a3e9ac404a4c14d');
---
+++ assets/js/wp-server-side-render.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-element', 'wp-url'), 'version' => 'a29c5e294c06df981c24d2f0f2d127d2');
