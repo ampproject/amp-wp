995467fa25197c99f6ba94ef6245e96f5fea62cc
spacedmonkey <jon@spacedmonkey.co.uk>
2020-01-09 23:27:38 +0000

    Tidy up logic for font weights.

--- assets/src/edit-story/panels/font.js
+++ assets/src/edit-story/panels/font.js
@@ -18,14 +18,16 @@ function FontPanel( { selectedElements, onSetProperties } ) {
 	const fontFamily = getCommonValue( selectedElements, 'fontFamily' );
 	const fontSize = getCommonValue( selectedElements, 'fontSize' );
 	const fontWeight = getCommonValue( selectedElements, 'fontWeight' );
+	const fontWeights = getCommonValue( selectedElements, 'fontWeights' );
 	const fontStyle = getCommonValue( selectedElements, 'fontStyle' );
 	const fontFallback = getCommonValue( selectedElements, 'fontFallback' );
 
 	const { state: { fonts }, actions: { getFontWeight, getFontFallback } } = useFont();
-	const [ state, setState ] = useState( { fontFamily, fontStyle, fontSize, fontWeight, fontFallback, fontWeights: [] } );
+	const [ state, setState ] = useState( { fontFamily, fontStyle, fontSize, fontWeight, fontFallback, fontWeights } );
 	useEffect( () => {
-		const fontWeights = getFontWeight( fontFamily );
-		setState( { fontFamily, fontStyle, fontSize, fontWeight, fontWeights } );
+		const currentFontWeights = getFontWeight( fontFamily );
+		const currentFontFallback = getFontFallback( fontFamily );
+		setState( { fontFamily, fontStyle, fontSize, fontWeight, fontWeights: currentFontWeights, fontFallback: currentFontFallback } );
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [ fontFamily, fontStyle, fontSize, fontWeight, getFontWeight ] );
 	const handleSubmit = ( evt ) => {
@@ -48,11 +50,11 @@ function FontPanel( { selectedElements, onSetProperties } ) {
 				options={ fonts }
 				value={ state.fontFamily }
 				onChange={ ( value ) => {
-					const fontWeights = getFontWeight( value );
+					const currentFontWeights = getFontWeight( value );
 					const currentFontFallback = getFontFallback( value );
-					const fontWeightsArr = fontWeights.map( ( { thisValue } ) => thisValue );
+					const fontWeightsArr = currentFontWeights.map( ( { thisValue } ) => thisValue );
 					const newFontWeight = ( fontWeightsArr && fontWeightsArr.includes( state.fontWeight ) ) ? state.fontWeight : 400;
-					setState( { ...state, fontWeights, fontFamily: value, fontWeight: parseInt( newFontWeight ), fontFallback: currentFontFallback } );
+					setState( { ...state, fontFamily: value, fontWeight: parseInt( newFontWeight ), fontWeights: currentFontWeights, fontFallback: currentFontFallback } );
 				} }
 			/>
 			<SelectMenu
