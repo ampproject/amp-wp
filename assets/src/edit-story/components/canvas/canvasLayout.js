7e1222e85752268fcebab51dd2af795874eb0870
Pascal Birchler <pascalb@google.com>
2020-01-14 16:04:24 +0100

    Rework page carousel

--- assets/src/edit-story/components/canvas/canvasLayout.js
+++ assets/src/edit-story/components/canvas/canvasLayout.js
@@ -6,10 +6,10 @@ import styled from 'styled-components';
 /**
  * Internal dependencies
  */
-import { CENTRAL_RIGHT_PADDING, PAGE_WIDTH, PAGE_HEIGHT } from '../../constants';
+import { PAGE_WIDTH, PAGE_HEIGHT } from '../../constants';
 import Page from './page';
 import Meta from './meta';
-import Carrousel from './carrousel';
+import Carousel from './carousel';
 import AddPage from './addpage';
 import SelectionCanvas from './selectionCanvas';
 
@@ -23,10 +23,11 @@ const Background = styled.div`
 
 	display: grid;
 	grid:
-    ".   meta       .        ." 1fr
-    ".   page       addpage  ." ${ PAGE_HEIGHT }px
-    ".   carrousel  .        ." 1fr
-    / 1fr ${ PAGE_WIDTH }px 1fr ${ CENTRAL_RIGHT_PADDING }px;
+    ".   meta              .       " 1fr
+    ".   page              addpage " ${ PAGE_HEIGHT }px
+    ".          .          .       " 1fr
+    "carousel   carousel   carousel" auto
+    / 1fr ${ PAGE_WIDTH }px 1fr;
 `;
 
 const Area = styled.div`
@@ -45,8 +46,8 @@ function CanvasLayout() {
 				<Area area="meta">
 					<Meta />
 				</Area>
-				<Area area="carrousel">
-					<Carrousel />
+				<Area area="carousel">
+					<Carousel />
 				</Area>
 				<Area area="addpage">
 					<AddPage />
---
+++ assets/src/edit-story/components/canvas/carousel/index.js
@@ -0,0 +1,171 @@
+/**
+ * External dependencies
+ */
+import styled from 'styled-components';
+
+/**
+ * WordPress dependencies
+ */
+import { useEffect, useRef, useState, createRef, useCallback } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import { useStory } from '../../../app';
+import LeftArrowIcon from './leftArrow.svg';
+import RightArrowIcon from './rightArrow.svg';
+import GridViewIcon from './gridView.svg';
+
+const PAGE_WIDTH = 72;
+const PAGE_HEIGHT = 128;
+
+const Wrapper = styled.div`
+	position: relative;
+	display: grid;
+	grid: "left-navigation carousel right-navigation" auto / 53px 1fr 53px;
+	height: 100%;
+	width: 100%;
+	color:  ${ ( { theme } ) => theme.colors.fg.v1 };
+`;
+
+const Area = styled.div`
+	grid-area: ${ ( { area } ) => area };
+	height: 100%;
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+`;
+
+const List = styled( Area )`
+	flex-direction: row;
+	align-items: flex-start;
+	justify-content: ${ ( { hasHorizontalOverflow } ) => hasHorizontalOverflow ? 'space-between' : 'center' };
+	height: 100%;
+	padding: 16px 0 24px;
+	overflow-x: ${ ( { hasHorizontalOverflow } ) => hasHorizontalOverflow ? 'scroll' : 'hidden' };
+`;
+
+const Page = styled.button`
+	padding: 0;
+	margin: 0 5px;
+	border: 3px solid ${ ( { isActive, theme } ) => isActive ? theme.colors.selection : theme.colors.bg.v1 };
+	height: ${ PAGE_HEIGHT }px;
+	width: ${ PAGE_WIDTH }px;
+	background-color: ${ ( { isActive, theme } ) => isActive ? theme.colors.fg.v1 : theme.colors.mg.v1 };
+	flex: none;
+`;
+
+const IconButton = styled.button`
+	padding: 0;
+	margin: 0;
+	border: none;
+	color: ${ ( { theme } ) => theme.colors.fg.v1 };
+	background: transparent;
+	svg {
+		width: 2em;
+		height: 2em;
+	}
+`;
+
+const NavigationButton = styled( IconButton )`
+	width: 53px;
+	height: 28px;
+`;
+
+const GridViewButton = styled( IconButton )`
+	width: 53px;
+	height: 28px;
+	position: absolute;
+	bottom: 24px;
+`;
+
+function Carousel() {
+	const { state: { pages, currentPageIndex, currentPageId }, actions: { setCurrentPage } } = useStory();
+	const [ hasHorizontalOverflow, setHasHorizontalOverflow ] = useState( false );
+	const listRef = useRef();
+
+	const pageRefs = pages.reduce( ( acc, page ) => {
+		acc[ page.id ] = createRef();
+		return acc;
+	}, {} );
+
+	useEffect( () => {
+		const { ResizeObserver } = window;
+
+		const observer = new ResizeObserver( ( entries ) => {
+			for ( const entry of entries ) {
+				const offsetWidth = entry.contentBoxSize ? entry.contentBoxSize.inlineSize : entry.contentRect.width;
+				setHasHorizontalOverflow( listRef.current.scrollWidth > offsetWidth );
+			}
+		} );
+
+		if ( listRef.current ) {
+			observer.observe( listRef.current );
+		}
+
+		return () => {
+			observer.disconnect();
+		};
+	} );
+
+	useEffect( () => {
+		if ( hasHorizontalOverflow ) {
+			const currentPageRef = pageRefs[ currentPageId ];
+
+			if ( currentPageRef.current ) {
+				// TODO: Don't actually scroll vertically.
+				currentPageRef.current.scrollIntoView( {
+					inline: 'center',
+					behavior: 'smooth',
+				} );
+			}
+		}
+	}, [ currentPageId, hasHorizontalOverflow, pageRefs ] );
+
+	const handleClickPage = ( page ) => () => setCurrentPage( { pageId: page.id } );
+
+	const scrollBy = useCallback( ( offset ) => {
+		if ( listRef.current ) {
+			listRef.current.scrollBy( {
+				left: offset,
+				behavior: 'smooth',
+			} );
+		}
+	}, [ listRef ] );
+
+	return (
+		<Wrapper>
+			<Area area="left-navigation">
+				{ hasHorizontalOverflow && (
+					<NavigationButton onClick={ () => scrollBy( -( 2 * PAGE_WIDTH ) ) }>
+						<LeftArrowIcon />
+					</NavigationButton>
+				) }
+			</Area>
+			<List area="carousel" ref={ listRef } hasHorizontalOverflow={ hasHorizontalOverflow }>
+				{ pages.map( ( page, index ) => (
+					<Page
+						key={ index }
+						onClick={ handleClickPage( page ) }
+						isActive={ index === currentPageIndex }
+						ref={ pageRefs[ page.id ] }
+					/>
+				) ) }
+			</List>
+			<Area area="right-navigation">
+				{ hasHorizontalOverflow && (
+					<NavigationButton onClick={ () => scrollBy( ( 2 * PAGE_WIDTH ) ) }>
+						<RightArrowIcon />
+					</NavigationButton>
+				) }
+				<GridViewButton disabled>
+					<GridViewIcon />
+				</GridViewButton>
+			</Area>
+		</Wrapper>
+	);
+}
+
+export default Carousel;
