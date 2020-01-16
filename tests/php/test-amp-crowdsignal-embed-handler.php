68686d4496a25ea45b67c2491cb01a683e1a8ef5
Weston Ruter <westonruter@google.com>
2020-01-16 15:08:27 -0800

    Fix regex in AMP_Crowdsignal_Embed_Handler_Test::adapt_iframe_title()

--- tests/php/test-amp-crowdsignal-embed-handler.php
+++ tests/php/test-amp-crowdsignal-embed-handler.php
@@ -117,7 +117,7 @@ class AMP_Crowdsignal_Embed_Handler_Test extends WP_UnitTestCase {
 	private function adapt_iframe_title( $html ) {
 		// Prior to 5.2, there was no 'title' attribute on an iframe.
 		if ( version_compare( get_bloginfo( 'version' ), '5.2', '<' ) ) {
-			$html = preg_replace( '/(<iframe.+)(\stitle=".+?")/', '${1}', $html );
+			$html = preg_replace( '/(<iframe.*)(\stitle=".+?")/', '${1}', $html );
 		}
 
 		return $html;
