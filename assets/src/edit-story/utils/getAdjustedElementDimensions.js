e1d63718f6461f271fc1573d3a276b2d9c7a1d54
Miina Sikk <miina.sikk@gmail.com>
2020-01-17 15:59:40 +0100

    Add logic for adjusting width when resizing height.

--- assets/src/edit-story/utils/getAdjustedElementDimensions.js
+++ assets/src/edit-story/utils/getAdjustedElementDimensions.js
@@ -1,5 +1,17 @@
 /**
+ * Internal dependencies
+ */
+import { PAGE_WIDTH } from '../constants';
+
+/**
  * Updates Text element's width and height if it's being resized from edges or there are font changes.
+ *
+ * @param {Object}  element Element to measure.
+ * @param {string}  content Text content.
+ * @param {number}  width   Current element width.
+ * @param {number}  height  Current element height.
+ * @param {boolean} fixedMeasure If one side is locked for changing automatically, can be 'width' or 'height'.
+ * @return {Object} Updated width and height.
  */
 function getAdjustedElementDimensions( { element, content, width, height, fixedMeasure } ) {
 	if ( ! element || ! content.length ) {
@@ -10,9 +22,31 @@ function getAdjustedElementDimensions( { element, content, width, height, fixedM
 			height = element.scrollHeight;
 		}
 	} else if ( 'height' === fixedMeasure ) {
-		// @todo This is not working fully as expected, it might also need adjusting width if the scrollHeight is over
 		if ( element.scrollWidth > width ) {
 			width = element.scrollWidth;
+		}
+		// Width isn't adjusted automatically, so we'll have to do it based on height.
+		const calcBuffer = 2;
+		if ( element.scrollHeight - height > calcBuffer ) {
+			let minWidth = width;
+			// Don't allow automatic resizing more than the page's width.
+			let maxWidth = PAGE_WIDTH;
+			while ( maxWidth - minWidth > 1 ) {
+				const mid = Math.floor( ( minWidth + maxWidth ) / 2 );
+				element.style.width = mid + 'px';
+				if ( element.scrollHeight - height > 2 ) {
+					maxWidth = mid;
+				} else {
+					minWidth = mid;
+				}
+			}
+			// If it still doesn't fit, restore the original width.
+			if ( element.scrollHeight - height > calcBuffer ) {
+				element.style.width = width + 'px';
+			} else {
+				// If it fits, return the updated width.
+				width = minWidth;
+			}
 		}
 	} else if ( element.scrollHeight > height || element.scrollWidth > width ) {
 		// If there's no fixed side, let's update both.
