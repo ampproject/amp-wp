b071ba52f7319823ee62f78e78c20d81c1d241e5
Morten Barklund <morten@barklund.dk>
2020-01-20 13:04:40 -0500

    Added relevant documentation to panel drag handle

--- assets/src/edit-story/panels/panel/handle.js
+++ assets/src/edit-story/panels/panel/handle.js
@@ -14,7 +14,7 @@ import {
 	useLayoutEffect,
 } from '@wordpress/element';
 
-const Handle = styled.button`
+const Handle = styled.button.attrs( { type: 'button' } )`
 	background: transparent;
 	border: 0;
 	padding: 0;
@@ -43,34 +43,47 @@ function DragHandle( { handleHeightChange } ) {
 	const handle = useRef();
 	const lastPosition = useRef();
 	const [ isDragging, setIsDragging ] = useState( false );
+
+	// On mouse move, check difference since last record vertical mouse position
+	// and invoke callback with this difference.
+	// Then record new vertical mouse position for next iteration.
 	const handleMouseMove = useCallback( ( evt ) => {
 		const delta = lastPosition.current - evt.pageY;
 		handleHeightChange( delta );
 		lastPosition.current = evt.pageY;
 	}, [ handleHeightChange ] );
 
+	// On mouse up, set dragging as false
+	// - will cause useLayoutEffect to unregister listeners.
 	const handleMouseUp = useCallback( () => setIsDragging( false ), [] );
 
+	// On mouse down, set dragging as true
+	// - will cause useLayoutEffect to register listeners.
+	// Also record the initial vertical mouse position on the page.
 	const handleMouseDown = useCallback( ( evt ) => {
 		lastPosition.current = evt.pageY;
 		setIsDragging( true );
 	}, [] );
 
+	// On initial render *and* every time `isDragging` changes value,
+	// register all relevant listeners. Note that all listeners registered
+	// will be correctly unregistered due to the cleanup function.
 	useLayoutEffect( () => {
 		const element = handle.current;
+		const doc = element.ownerDocument;
 		element.addEventListener( 'mousedown', handleMouseDown );
 
-		if ( isDragging && element.ownerDocument ) {
-			element.ownerDocument.addEventListener( 'mousemove', handleMouseMove );
-			element.ownerDocument.addEventListener( 'mouseup', handleMouseUp );
+		if ( isDragging && doc ) {
+			doc.addEventListener( 'mousemove', handleMouseMove );
+			doc.addEventListener( 'mouseup', handleMouseUp );
 		}
 
 		return () => {
 			if ( element ) {
 				element.removeEventListener( 'mousedown', handleMouseDown );
-				if ( isDragging && element.ownerDocument ) {
-					element.ownerDocument.removeEventListener( 'mousemove', handleMouseMove );
-					element.ownerDocument.removeEventListener( 'mouseup', handleMouseUp );
+				if ( isDragging && doc ) {
+					doc.removeEventListener( 'mousemove', handleMouseMove );
+					doc.removeEventListener( 'mouseup', handleMouseUp );
 				}
 			}
 		};
