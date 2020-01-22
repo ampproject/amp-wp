083006dc59a6a01f9cdc5db70eb5fcb244ac9d1e
Ryan Kienstra <kienstraryan@gmail.com>
2020-01-22 00:34:38 -0600

    Address error in Paired Browsing, using Weston's suggestion
    Prevent the warning from appearing,
    along with the console error:
    TypeError: Cannot read property 'domReady' of undefined.

--- tests/php/test-class-amp-theme-support.php
+++ tests/php/test-class-amp-theme-support.php
@@ -2472,6 +2472,89 @@ class Test_AMP_Theme_Support extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Test the enqueuing in setup_paired_browsing_client().
+	 *
+	 * @covers AMP_Theme_Support::setup_paired_browsing_client()
+	 */
+	public function test_setup_paired_browsing_client_enqueuing() {
+		$handle = 'amp-paired-browsing-client';
+
+		// The conditions aren't met, so this should not enqueue the script.
+		$_GET[ AMP_Theme_Support::PAIRED_BROWSING_QUERY_VAR ] = '1';
+		AMP_Theme_Support::setup_paired_browsing_client();
+		$this->assertFalse( wp_script_is( $handle ) );
+
+		// Only one condition is met, so this should still not enqueue the script.
+		unset( $_GET[ AMP_Theme_Support::PAIRED_BROWSING_QUERY_VAR ] ); // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+		AMP_Theme_Support::setup_paired_browsing_client();
+		$this->assertFalse( wp_script_is( $handle ) );
+
+		// Both of the conditions to enqueue are met.
+		add_filter( 'amp_dev_mode_enabled', '__return_true' );
+		AMP_Theme_Support::setup_paired_browsing_client();
+		$this->assertTrue( wp_script_is( $handle ) );
+	}
+
+	/**
+	 * Gets the test data for test_setup_paired_browsing_client_filter().
+	 *
+	 * @return array The test data.
+	 */
+	public function get_setup_paired_browsing_data() {
+		$original_script_tag      = '<script src="foo"></script>'; // phpcs:ignore WordPress.WP.EnqueuedResources.NonEnqueuedScript
+		$script_tag_with_dev_mode = '<script data-ampdevmode src="foo"></script>'; // phpcs:ignore WordPress.WP.EnqueuedResources.NonEnqueuedScript
+
+		return [
+			'no_parent_of_dependency'      => [
+				'',
+				$original_script_tag,
+				null,
+			],
+			'wrong_parent_of_dependency'   => [
+				'different-handle-completely',
+				$original_script_tag,
+				null,
+			],
+			'correct_parent_of_dependency' => [
+				'amp-paired-browsing-client',
+				$original_script_tag,
+				$script_tag_with_dev_mode,
+			],
+		];
+	}
+
+	/**
+	 * Test the filter in setup_paired_browsing_client().
+	 *
+	 * @dataProvider get_setup_paired_browsing_data
+	 * @covers AMP_Theme_Support::setup_paired_browsing_client()
+	 *
+	 * @param string $parent_of_dependency The script that has a dependency on the dependency handle.
+	 * @param string $original_script_tag  The <script> tag passed to the filter.
+	 * @param string $expected             The expected return value.
+	 */
+	public function test_setup_paired_browsing_client_filter( $parent_of_dependency, $original_script_tag, $expected ) {
+		if ( null === $expected ) {
+			$expected = $original_script_tag;
+		}
+
+		add_theme_support( AMP_Theme_Support::SLUG );
+		$this->go_to( get_permalink( self::factory()->post->create() ) );
+		add_filter( 'amp_dev_mode_enabled', '__return_true' );
+		$src               = 'https://example.com/script.js';
+		$dependency_handle = 'foo-handle';
+
+		wp_enqueue_script( $dependency_handle, $src, [], '0.1.0', true );
+		wp_enqueue_script( $parent_of_dependency, $src, [ $dependency_handle ], '0.1', true );
+		AMP_Theme_Support::setup_paired_browsing_client();
+
+		$this->assertEquals(
+			$expected,
+			apply_filters( 'script_loader_tag', $original_script_tag, $dependency_handle, '' )
+		);
+	}
+
+	/**
 	 * Test AMP_Theme_Support::whitelist_layout_in_wp_kses_allowed_html().
 	 *
 	 * @see AMP_Theme_Support::whitelist_layout_in_wp_kses_allowed_html()
