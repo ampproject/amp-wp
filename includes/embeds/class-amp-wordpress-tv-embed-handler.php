15eb7662e2bf8b15166c698881450d85c04929de
Ryan Kienstra <kienstraryan@gmail.com>
2020-01-22 11:37:14 -0600

    For WordPress.tv embed, Use an oembed filter instead of block filter (#4164)
    As Weston pointed out,
    it's still possible to have this embed in the classic editor.

--- includes/embeds/class-amp-wordpress-tv-embed-handler.php
+++ includes/embeds/class-amp-wordpress-tv-embed-handler.php
@@ -14,33 +14,39 @@
 class AMP_WordPress_TV_Embed_Handler extends AMP_Base_Embed_Handler {
 
 	/**
+	 * The URL pattern to determine if an embed URL is for this type, copied from WP_oEmbed.
+	 *
+	 * @see https://github.com/WordPress/wordpress-develop/blob/e13480/src/wp-includes/class-wp-oembed.php#L64
+	 */
+	const URL_PATTERN = '#https?://wordpress\.tv/.*#i';
+
+	/**
 	 * Register embed.
 	 */
 	public function register_embed() {
-		add_filter( 'render_block', [ $this, 'filter_rendered_block' ], 10, 2 );
+		add_filter( 'embed_oembed_html', [ $this, 'filter_oembed_html' ], 10, 2 );
 	}
 
 	/**
 	 * Unregister embed.
 	 */
 	public function unregister_embed() {
-		remove_filter( 'render_block', [ $this, 'filter_rendered_block' ], 10 );
+		remove_filter( 'embed_oembed_html', [ $this, 'filter_oembed_html' ], 10 );
 	}
 
 	/**
-	 * Filters the content of a single block to make it AMP valid.
+	 * Filters the oembed HTML to make it valid AMP.
 	 *
-	 * @param string $block_content The block content about to be appended.
-	 * @param array  $block         The full block, including name and attributes.
-	 * @return string Filtered block content.
+	 * @param mixed  $cache The cached rendered markup.
+	 * @param string $url   The embed URL.
+	 * @return string The filtered embed markup.
 	 */
-	public function filter_rendered_block( $block_content, $block ) {
-		if ( ! isset( $block['blockName'] ) || 'core-embed/wordpress-tv' !== $block['blockName'] ) {
-			return $block_content;
+	public function filter_oembed_html( $cache, $url ) {
+		if ( ! preg_match( self::URL_PATTERN, $url ) ) {
+			return $cache;
 		}
 
-		$modified_block_content = preg_replace( '#<script(?:\s.*?)?>.*?</script>#s', '', $block_content );
-
-		return null !== $modified_block_content ? $modified_block_content : $block_content;
+		$modified_block_content = preg_replace( '#<script(?:\s.*?)?>.*?</script>#s', '', $cache );
+		return null !== $modified_block_content ? $modified_block_content : $cache;
 	}
 }
