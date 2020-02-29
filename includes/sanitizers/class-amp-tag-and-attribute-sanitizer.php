ea26570a49ca6f3cbfc8a32c7e64aab3a4a91803
Ryan Kienstra <kienstraryan@gmail.com>
2020-02-28 18:27:19 -0600

    Add an error message when there is invalid JSON
    So far, these look to mainly
    be 'Invalid syntax'

--- includes/sanitizers/class-amp-tag-and-attribute-sanitizer.php
+++ includes/sanitizers/class-amp-tag-and-attribute-sanitizer.php
@@ -925,11 +925,14 @@ class AMP_Tag_And_Attribute_Sanitizer extends AMP_Base_Sanitizer {
 		}
 
 		// When the CDATA is expected to be JSON, ensure it's valid JSON.
-		if ( 'script' === $element->tagName &&
-			in_array( $element->getAttribute( 'type' ), [ 'application/json', 'application/ld+json' ], true ) &&
-			null === json_decode( $element->textContent )
-		) {
-			return [ 'code' => self::INVALID_JSON_CDATA ];
+		if ( 'script' === $element->tagName && in_array( $element->getAttribute( 'type' ), [ 'application/json', 'application/ld+json' ], true ) ) {
+			json_decode( $element->textContent );
+			if ( JSON_ERROR_NONE !== json_last_error() ) {
+				return [
+					'code'    => self::INVALID_JSON_CDATA,
+					'message' => json_last_error_msg(),
+				];
+			}
 		}
 
 		return true;
