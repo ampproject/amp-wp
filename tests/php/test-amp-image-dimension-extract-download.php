fd5a5516083a5ce19100d034e7d11d65e908a8d3
Pierre Gordon <pierregordon@protonmail.com>
2020-03-07 20:53:03 -0500

    Wait for server to start

--- tests/php/test-amp-image-dimension-extract-download.php
+++ tests/php/test-amp-image-dimension-extract-download.php
@@ -12,31 +12,90 @@
  */
 class AMP_Image_Dimension_Extract_Download_Test extends WP_UnitTestCase {
 
+	private static $pid;
+
 	const AMP_IMG_DIMENSION_TEST_INVALID_FILE = __DIR__ . '/assets/not-exists.png';
 
 	public static function setUpBeforeClass() {
 		parent::setUpBeforeClass();
-
-		/*
-		 * Start PHP built-in server to host images to test.
-		 * Not ideal to use remote URLs; mocking would be better for performance, but FasterImage doesn't provide means to do this.
-		 */
-		$host      = self::get_host();
-		$host_root = self::get_host_root();
-
-		exec( "php -S {$host} -t {$host_root} >/dev/null 2>/dev/null &" ); // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.system_calls_exec
+		self::start_server();
 	}
 
-	private static function get_host() {
+	public static function tearDownAfterClass() {
+		parent::tearDownAfterClass();
+		self::stop_server();
+	}
+
+	/**
+	 * Start PHP built-in server to host images to test.
+	 */
+	private static function start_server() {
+		// Not ideal to use remote URLs; mocking would be better for performance, but FasterImage doesn't provide means to do this.
+		$url = wp_parse_url( self::get_server_url() );
+
+		if ( ! isset( $url['host'], $url['port'] ) ) {
+			throw new \Error( 'A host and port needs to be set to start the PHP server' );
+		}
+
+		$host      = $url['host'];
+		$port      = $url['port'];
+		$host_root = self::get_server_root();
+
+		self::$pid = exec( sprintf( 'php -S %s -t %s >/dev/null 2>&1 & echo $!', escapeshellarg( "$host:$port" ), escapeshellarg( $host_root ) ) ); // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.system_calls_exec
+
+		// Wait up to 3 seconds for the server to start.
+		$started = false;
+		for ( $i = 0; $i < 3; $i++ ) {
+			$response = wp_remote_get( "http://$host:$port" );
+
+			if ( ! $response instanceof WP_Error ) {
+				$started = true;
+				break;
+			}
+
+			sleep( 1 );
+		}
+
+		if ( ! $started ) {
+			throw new \Error( 'Failed to start the PHP server' );
+		}
+	}
+
+	/**
+	 * Stop PHP server.
+	 */
+	private static function stop_server() {
+		if ( ! empty( self::$pid ) ) {
+			exec( 'kill ' . (int) self::$pid ); // phpcs:ignore WordPress.PHP.DiscouragedPHPFunctions.system_calls_exec
+		}
+	}
+
+	/**
+	 * Get host for PHP server.
+	 *
+	 * @return string
+	 */
+	private static function get_server_url() {
 		return getenv( 'AMP_TEST_HOST_URL' ) ?: '127.0.0.1:8891';
 	}
 
-	private static function get_host_root() {
+	/**
+	 * Get root path for PHP server.
+	 *
+	 * @return string
+	 */
+	private static function get_server_root() {
 		return getenv( 'AMP_TEST_HOST_ROOT' ) ?: __DIR__ . '/data/images';
 	}
 
+	/**
+	 * Get image URL for specified image file.
+	 *
+	 * @param string $file Image name.
+	 * @return string URL.
+	 */
 	private static function get_image_url( $file ) {
-		return self::get_host() . "/{$file}";
+		return self::get_server_url() . "/{$file}";
 	}
 
 	/**
