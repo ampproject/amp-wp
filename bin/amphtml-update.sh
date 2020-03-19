be03da4c1f27a557e51301c1f9e21afe75a29eef
Pierre Gordon <pierregordon@protonmail.com>
2020-03-19 00:36:03 -0400

    Revert "Generate PHP class with error codes from AMP specification"
    This reverts commit 4294c39a

--- bin/amphtml-update.sh
+++ bin/amphtml-update.sh
@@ -52,9 +52,7 @@ else
 fi
 
 # Run script.
-python "$BIN_PATH/amphtml-update.py" "$AMPHTML_LOCATION"
-mv "$BIN_PATH/class-amp-allowed-tags-generated.php" "$PROJECT_PATH/includes/sanitizers/class-amp-allowed-tags-generated.php"
-mv "$BIN_PATH/class-amp-error-codes-generated.php" "$PROJECT_PATH/includes/validation/class-amp-error-codes-generated.php"
+python "$BIN_PATH/amphtml-update.py" "$AMPHTML_LOCATION" > "$PROJECT_PATH/includes/sanitizers/class-amp-allowed-tags-generated.php"
 
 if [[ $CLEANUP == 1 ]]; then
 	rm -r "$AMPHTML_LOCATION"
