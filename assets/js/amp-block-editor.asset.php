2f1cf7a25e81039e4c7dff527e1db9750a95476e
Weston Ruter <westonruter@google.com>
2020-03-23 23:46:27 -0700

    Build 1.5.0-RC1

---
+++ assets/js/amp-block-editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => 'cc30d45bae94f6e732d15349b5344ed6');
