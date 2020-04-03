46b0f91c9bc812257ba8fa346e7025b3aa57acdf
Weston Ruter <westonruter@google.com>
2020-04-03 13:19:58 -0700

    Build 1.5.2

---
+++ assets/js/amp-block-editor.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'moment', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-dom-ready', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-plugins', 'wp-polyfill', 'wp-url'), 'version' => 'bbf906f2003754885823929ea8841cb8');
--- tests/e2e/utils/index.js
+++
@@ -1,4 +0,0 @@
-export { activatePlugin } from './activate-plugin';
-export { deactivatePlugin } from './deactivate-plugin';
-export { clickButton } from './click-button';
-export { uploadMedia } from './upload-media';
