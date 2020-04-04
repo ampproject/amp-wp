24fa804aeae35ec62996dd566cb7e28a0ff7cf84
Alain Schlesser <alain.schlesser@gmail.com>
2020-04-04 10:21:36 +0200

    Add CSS transient caching option to options manager validation

--- includes/options/class-amp-options-manager.php
+++ includes/options/class-amp-options-manager.php
@@ -5,6 +5,8 @@
  * @package AMP
  */
 
+use AmpProject\AmpWP\Option;
+
 /**
  * Class AMP_Options_Manager
  */
@@ -261,6 +263,12 @@ class AMP_Options_Manager {
 				}
 			}
 		}
+
+		if ( true === $new_options[ Option::DISABLE_CSS_TRANSIENT_CACHING ] ) {
+		    $options[ Option::DISABLE_CSS_TRANSIENT_CACHING ] = true;
+        } else {
+		    unset( $options[ Option::DISABLE_CSS_TRANSIENT_CACHING ] );
+        }
 
 		// Store the current version with the options so we know the format.
 		$options['version'] = AMP__VERSION;
