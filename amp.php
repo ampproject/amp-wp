6c42120eb9dc732a98eac48762ac05c2c54ae02f
Weston Ruter <westonruter@google.com>
2020-04-15 14:53:54 -0700

    Bump 1.5.4-alpha

--- amp.php
+++ amp.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://amp-wp.org
  * Author: AMP Project Contributors
  * Author URI: https://github.com/ampproject/amp-wp/graphs/contributors
- * Version: 1.5.3
+ * Version: 1.5.4-alpha
  * Text Domain: amp
  * Domain Path: /languages/
  * License: GPLv2 or later
@@ -17,7 +17,7 @@
 
 define( 'AMP__FILE__', __FILE__ );
 define( 'AMP__DIR__', dirname( __FILE__ ) );
-define( 'AMP__VERSION', '1.5.3' );
+define( 'AMP__VERSION', '1.5.4-alpha' );
 
 /**
  * Errors encountered while loading the plugin.
