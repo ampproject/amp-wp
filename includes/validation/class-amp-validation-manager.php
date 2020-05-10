a4eb3a6c28853315b6b04e18a7031591dc99aaed
Weston Ruter <westonruter@google.com>
2020-05-09 23:32:45 -0700

    Suppress plugin widgets

--- includes/validation/class-amp-validation-manager.php
+++ includes/validation/class-amp-validation-manager.php
@@ -1370,8 +1370,8 @@ class AMP_Validation_Manager {
 	/**
 	 * Wrap callbacks for registered widgets to keep track of queued assets and the source for anything printed for validation.
 	 *
-	 * @global array $wp_filter
 	 * @return void
+	 * @global array $wp_registered_widgets
 	 */
 	public static function wrap_widget_callbacks() {
 		global $wp_registered_widgets;
