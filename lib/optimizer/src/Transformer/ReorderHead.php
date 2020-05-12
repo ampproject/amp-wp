6c272288dc515c33721a26458703acbed908890d
Alain Schlesser <alain.schlesser@gmail.com>
2020-05-12 18:24:30 +0200

    Fix bug in Reorderhead with checking against empty nodelist

--- lib/optimizer/src/Transformer/ReorderHead.php
+++ lib/optimizer/src/Transformer/ReorderHead.php
@@ -10,6 +10,7 @@ use AmpProject\Optimizer\Transformer;
 use AmpProject\Tag;
 use DOMElement;
 use DOMNode;
+use DOMNodeList;
 
 /**
  * Transformer applying the head reordering transformations to the HTML input.
@@ -82,7 +83,7 @@ final class ReorderHead implements Transformer
     {
         $nodes = $document->head->childNodes;
 
-        if (! $nodes) {
+        if (! $nodes instanceof DOMNodeList || $nodes->length === 0) {
             return;
         }
 
