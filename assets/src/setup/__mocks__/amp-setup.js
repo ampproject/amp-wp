086b3bc7c08244c25c3f19b52fcabc6688bb0ce8
John Watkins <johnwatkins0@gmail.com>
2020-05-25 17:27:57 -0500

    Add mock property

--- assets/src/setup/__mocks__/amp-setup.js
+++ assets/src/setup/__mocks__/amp-setup.js
@@ -2,4 +2,5 @@ module.exports = {
 	APP_ROOT_ID: 'amp-setup',
 	EXIT_LINK: 'http://site.test/wp-admin/?page=amp-options',
 	OPTIONS_REST_ENDPOINT: 'http://site.test/wp-json/amp-wp/v1/options',
+	READER_THEMES_ENDPOINT: 'http://site.test/wp-json/amp-wp/v1/reader-themes',
 };
