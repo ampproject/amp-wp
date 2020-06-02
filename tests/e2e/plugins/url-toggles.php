96ef2b98fbe2f97d7b6115eb283f4df5adaf98e2
Alain Schlesser <alain.schlesser@gmail.com>
2020-06-02 10:41:36 +0200

    Fix PHPCS issues with URL Toggles plugin

--- tests/e2e/plugins/url-toggles.php
+++ tests/e2e/plugins/url-toggles.php
@@ -7,20 +7,23 @@
  * Author URI:  https://github.com/ampproject/amp-wp/graphs/contributors
  */
 
-if ( isset( $_GET['amp_optimizer'] ) ) {
+$amp_optimizer = filter_input( INPUT_GET, 'amp_optimizer', FILTER_VALIDATE_BOOLEAN );
+$amp_ssr       = filter_input( INPUT_GET, 'amp_ssr', FILTER_VALIDATE_BOOLEAN );
+
+if ( null !== $amp_optimizer ) {
 	add_filter(
 		'amp_enable_optimizer',
-		rest_sanitize_boolean( $_GET['amp_optimizer'] )
+		$amp_optimizer
 			? '__return_true'
 			: '__return_false',
 		100
 	);
 }
 
-if ( isset( $_GET['amp_ssr'] ) ) {
+if ( null !== $amp_ssr ) {
 	add_filter(
 		'amp_enable_ssr',
-		rest_sanitize_boolean( $_GET['amp_ssr'] )
+		$amp_ssr
 			? '__return_true'
 			: '__return_false',
 		100
