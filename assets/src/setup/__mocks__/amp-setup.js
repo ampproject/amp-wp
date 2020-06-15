81ba707933df78dc5ed82708c40f50eca7f6a86a
John Watkins <johnwatkins0@gmail.com>
2020-06-15 12:46:30 -0500

    Fix reader REST endpoint

--- assets/src/setup/__mocks__/amp-setup.js
+++ assets/src/setup/__mocks__/amp-setup.js
@@ -1,8 +1,8 @@
 module.exports = {
 	APP_ROOT_ID: 'amp-setup',
 	EXIT_LINK: 'http://site.test/wp-admin/?page=amp-options',
-	OPTIONS_REST_ENDPOINT: 'http://site.test/wp-json/amp-wp/v1/options',
-	READER_THEMES_REST_ENDPOINT: 'http://site.test/wp-json/amp-wp/v1/reader-themes',
+	OPTIONS_REST_ENDPOINT: 'http://site.test/wp-json/amp/v1/options',
+	READER_THEMES_REST_ENDPOINT: 'http://site.test/wp-json/amp/v1/reader-themes',
 	UPDATES_NONCE: '',
 	USER_OPTION_DEVELOPER_TOOLS: 'developer_tools',
 	USER_OPTIONS_KEY: 'amp_user_options',
