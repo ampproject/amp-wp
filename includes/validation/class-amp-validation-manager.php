81c043f04fc0e3f209a7fe5c2aa8740411b5cedb
Alain Schlesser <alain.schlesser@gmail.com>
2020-06-17 16:39:34 +0200

    Remove additional static access to plugin registry

--- includes/validation/class-amp-validation-manager.php
+++ includes/validation/class-amp-validation-manager.php
@@ -7,6 +7,7 @@
 
 use AmpProject\AmpWP\Icon;
 use AmpProject\AmpWP\PluginRegistry;
+use AmpProject\AmpWP\Services;
 use AmpProject\Dom\Document;
 
 /**
@@ -2332,8 +2333,10 @@ class AMP_Validation_Manager {
 			$invalid_plugins = isset( $errors[ AMP_Validation_Error_Taxonomy::SOURCES_INVALID_OUTPUT ]['plugin'] ) ? array_unique( $errors[ AMP_Validation_Error_Taxonomy::SOURCES_INVALID_OUTPUT ]['plugin'] ) : null;
 			if ( isset( $invalid_plugins ) ) {
 				$reported_plugins = [];
+				/** @var PluginRegistry $plugin_registry */
+				$plugin_registry = Services::get( 'plugin_registry' );
 				foreach ( $invalid_plugins as $plugin_slug ) {
-					$plugin_data        = PluginRegistry::get_plugin_from_slug( $plugin_slug );
+					$plugin_data        = $plugin_registry->get_plugin_from_slug( $plugin_slug );
 					$plugin_name        = is_array( $plugin_data ) ? $plugin_data['data']['Name'] : $plugin_slug;
 					$reported_plugins[] = $plugin_name;
 				}
