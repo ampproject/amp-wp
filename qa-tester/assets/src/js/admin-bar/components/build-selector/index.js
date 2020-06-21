76567fe8558cf462ae1e357382fef2d3cb5136dc
Pierre Gordon <pierregordon@protonmail.com>
2020-06-21 13:59:33 -0400

    Import `useCallback`

--- qa-tester/assets/src/js/admin-bar/components/build-selector/index.js
+++ qa-tester/assets/src/js/admin-bar/components/build-selector/index.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { useMemo, useState } from '@wordpress/element';
+import { useCallback, useMemo, useState } from '@wordpress/element';
 
 /**
  * External dependencies
