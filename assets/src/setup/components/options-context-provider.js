435dce7be854568976f2a104f308e545fce9d890
John Watkins <johnwatkins0@gmail.com>
2020-07-06 13:22:44 -0500

    Set mobile_redirect to true from `originalOptions` when value is unchanged in first run
    Co-authored-by: Pierre Gordon <16200219+pierlon@users.noreply.github.com>

--- assets/src/setup/components/options-context-provider.js
+++ assets/src/setup/components/options-context-provider.js
@@ -103,7 +103,7 @@ export function OptionsContextProvider( { children, optionsRestEndpoint } ) {
 			// If this is the first time running the wizard and mobile_redirect is not in updates, set mobile_redirect to true.
 			// We do this here instead of in the fetch effect to prevent the exit confirmation before the user has interacted.
 			if ( ! originalOptions.wizard_completed && ! ( 'mobile_redirect' in updatesToSave ) ) {
-				updatesToSave.mobile_redirect = true;
+				updatesToSave.mobile_redirect = originalOptions.mobile_redirect;
 			}
 
 			if ( ! originalOptions.wizard_completed ) {
