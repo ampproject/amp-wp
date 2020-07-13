59317b72cdc6903c43cb3f67581389525f5d9992
John Watkins <johnwatkins0@gmail.com>
2020-07-13 15:24:33 -0500

    Fix failing test

--- tests/php/src/Unit/OptionsMenuTest.php
+++ tests/php/src/Unit/OptionsMenuTest.php
@@ -85,7 +85,7 @@ class OptionsMenuTest extends WP_UnitTestCase {
 		$this->assertArrayHasKey( 'amp-options', $submenu );
 		$this->assertCount( 3, $submenu['amp-options'] );
 		$this->assertEquals( 'edit-tags.php?taxonomy=amp_validation_error&amp;post_type=amp_validated_url', $submenu['amp-options'][0][2] );
-		$this->assertEquals( 'amp-onboarding-wizard', $submenu['amp-options'][1][2] );
+		$this->assertEquals( 'amp-options', $submenu['amp-options'][1][2] );
 
 		// Test add_setting_field().
 		$this->assertArrayHasKey( 'amp-options', $wp_settings_fields );
