b9d100fc4fd07230c2b7251be5fd6001d0bfbc28
Weston Ruter <westonruter@google.com>
2020-07-13 22:37:27 -0700

    Remove naive theme mod copying theme mods from active theme to reader theme

--- src/ReaderThemeLoader.php
+++ src/ReaderThemeLoader.php
@@ -57,8 +57,6 @@ final class ReaderThemeLoader implements Service, Registerable {
 		// The following needs to run at plugins_loaded because that is when _wp_customize_include runs. Otherwise, the
 		// most logical action would be setup_theme.
 		add_action( 'plugins_loaded', [ $this, 'override_theme' ], 9 );
-
-		add_action( 'update_option_' . AMP_Options_Manager::OPTION_NAME, [ $this, 'populate_reader_theme_nav_menu_locations' ], 10, 2 );
 	}
 
 	/**
@@ -140,37 +138,6 @@ final class ReaderThemeLoader implements Service, Registerable {
 	}
 
 	/**
-	 * Populate reader theme nav menu locations upon activating Reader mode with a Reader theme.
-	 *
-	 * @todo We can't do this because the new theme is not active. We do not know what the nav menu locations are.
-	 *
-	 * @param array $old_options Old options.
-	 * @param array $new_options New options.
-	 */
-	public function populate_reader_theme_nav_menu_locations( $old_options, $new_options ) {
-		if (
-			! isset( $new_options[ Option::THEME_SUPPORT ], $new_options[ Option::READER_THEME ] )
-			||
-			AMP_Theme_Support::READER_MODE_SLUG !== $new_options[ Option::THEME_SUPPORT ]
-			||
-			ReaderThemes::DEFAULT_READER_THEME === $new_options[ Option::READER_THEME ]
-		) {
-			return;
-		}
-
-		$new_reader_theme_mods = get_option( 'theme_mods_' . $new_options[ Option::READER_THEME ] );
-		if ( ! empty( $new_reader_theme_mods ) ) {
-			return;
-		}
-
-		// @todo This will not work properly because (1) we need to only update nav menus at this point and (2) nav menu locations are not available!
-		update_option(
-			'theme_mods_' . $new_options[ Option::READER_THEME ],
-			get_option( 'theme_mods_' . get_stylesheet() )
-		);
-	}
-
-	/**
 	 * Switch theme if in Reader mode, a Reader theme was selected, and the AMP query var is present.
 	 *
 	 * Note that AMP_Theme_Support will redirect to the non-AMP version if AMP is not available for the query.
@@ -229,21 +196,6 @@ final class ReaderThemeLoader implements Service, Registerable {
 		$this->disable_widgets();
 		add_filter( 'customize_previewable_devices', [ $this, 'customize_previewable_devices' ] );
 		add_action( 'customize_register', [ $this, 'remove_customizer_themes_panel' ], 11 );
-
-		$active_theme_mods = get_option( 'theme_mods_' . $this->active_theme->get_stylesheet(), [] );
-
-		add_filter(
-			'option_theme_mods_' . $this->reader_theme->get_stylesheet(),
-			function( $reader_theme_mods ) use ( $active_theme_mods ) {
-				// @todo This is not correct. We need to call wp_map_nav_menu_locations() for nav_menu_locations, _but_ we need to not do this at runtime.
-				// @todo We should also facilitate updating it as changes are made in the active theme.
-				// @todo So we should only do the copy upon selecting the Reader theme, _if_ the theme_mods are empty. Otherwise, we require user action to apply via Customizer button.
-				return array_merge(
-					is_array( $reader_theme_mods ) ? $reader_theme_mods : [],
-					$active_theme_mods
-				);
-			}
-		);
 	}
 
 	/**
