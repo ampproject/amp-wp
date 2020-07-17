17daeecff9dfb07a4d0929ad229846881794162d
Weston Ruter <westonruter@google.com>
2020-07-16 18:10:35 -0700

    Use just PropTypes.node instead of PropTypes.oneOfType

--- assets/src/components/amp-setting-toggle/index.js
+++ assets/src/components/amp-setting-toggle/index.js
@@ -56,8 +56,5 @@ AMPSettingToggle.propTypes = {
 	disabled: PropTypes.bool,
 	onChange: PropTypes.func.isRequired,
 	text: PropTypes.string,
-	title: PropTypes.oneOfType( [
-		PropTypes.string,
-		PropTypes.node,
-	] ).isRequired,
+	title: PropTypes.node,
 };
