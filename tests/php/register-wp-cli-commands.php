cb77df99dac6def2b54bb0c9aa0f8c703517592c
Alain Schlesser <alain.schlesser@gmail.com>
2020-07-20 10:02:11 +0200

    Update Composer configuration

--- tests/php/register-wp-cli-commands.php
+++ tests/php/register-wp-cli-commands.php
@@ -2,12 +2,10 @@
 
 use AmpProject\AmpWP\Tests\Cli\ReferenceSiteCommandNamespace;
 use AmpProject\AmpWP\Tests\Cli\ReferenceSiteImportCommand;
-use AmpProject\AmpWP\Tests\Cli\SeedCommand;
 
 if ( ! defined( 'WP_CLI' ) || ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
 
 WP_CLI::add_command( 'amp reference-site', ReferenceSiteCommandNamespace::class );
-WP_CLI::add_command( 'amp reference-site seed', SeedCommand::class );
 WP_CLI::add_command( 'amp reference-site import', ReferenceSiteImportCommand::class );
