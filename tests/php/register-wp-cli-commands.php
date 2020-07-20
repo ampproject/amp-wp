4bbb0b4483078280377fd1926a11d23951b7ad26
Alain Schlesser <alain.schlesser@gmail.com>
2020-07-20 10:12:10 +0200

    Load reference site logic outside of active plugin

--- tests/php/register-wp-cli-commands.php
+++ tests/php/register-wp-cli-commands.php
@@ -3,6 +3,17 @@
 use AmpProject\AmpWP\Tests\Cli\ReferenceSiteCommandNamespace;
 use AmpProject\AmpWP\Tests\Cli\ReferenceSiteImportCommand;
 
+// Conditional bootstrapping to prepare outside of active plugin.
+if ( ! defined( 'AMP__DIR__' ) ) {
+	define( 'AMP__DIR__', realpath( dirname( dirname( __DIR__ ) ) ) );
+}
+if ( ! class_exists( 'AmpProject\AmpWP\Tests\Cli\ReferenceSiteCommandNamespace' ) ) {
+	$autoloader = AMP__DIR__ . '/vendor/autoload.php';
+	if ( file_exists( $autoloader ) ) {
+		include $autoloader;
+	}
+}
+
 if ( ! defined( 'WP_CLI' ) || ! class_exists( 'WP_CLI' ) ) {
 	return;
 }
