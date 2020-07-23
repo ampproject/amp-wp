78ddfedda0cce12a5c3230b07443e4b8ac0914be
Pierre Gordon <pierregordon@protonmail.com>
2020-07-23 17:23:23 -0400

    Modify comments link to go to non-AMP version of post when in legacy Reader mode

--- includes/templates/class-amp-post-template.php
+++ includes/templates/class-amp-post-template.php
@@ -5,6 +5,9 @@
  * @package AMP
  */
 
+use AmpProject\AmpWP\Option;
+use AmpProject\AmpWP\QueryVar;
+
 /**
  * Class AMP_Post_Template
  *
@@ -307,6 +310,17 @@ class AMP_Post_Template {
 		if ( ! $comments_open
 			&& ! $this->post->comment_count ) {
 			return;
+		}
+
+		if (
+			is_amp_endpoint()
+			&& amp_is_legacy()
+			&& true === AMP_Options_Manager::get_option( Option::MOBILE_REDIRECT )
+		) {
+			// Modify comments link to go to non-AMP version page when in legacy Reader mode.
+			add_filter( 'get_comments_link', static function ( $comments_link ) {
+				return add_query_arg( QueryVar::NOAMP, QueryVar::NOAMP_MOBILE, $comments_link );
+			} );
 		}
 
 		$comments_link_url  = get_comments_link( $this->post );
