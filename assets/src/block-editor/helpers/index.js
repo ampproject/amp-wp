dc7007135b298a00abe9cf233c9368ef703a6f58
Weston Ruter <westonruter@google.com>
2020-07-22 18:49:15 -0700

    Define deprecated array if not defined already

--- assets/src/block-editor/helpers/index.js
+++ assets/src/block-editor/helpers/index.js
@@ -209,6 +209,9 @@ export const removeDeprecatedAmpProps = ( settings ) => {
 			},
 		};
 
+		if ( ! settings.deprecated ) {
+			settings.deprecated = [];
+		}
 		settings.deprecated.unshift( deprecatedConfig );
 	}
 
