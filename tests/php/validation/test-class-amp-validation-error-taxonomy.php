216d6490c0e724fe18295fe4fae2f5c03aa59cdd
Pierre Gordon <pierregordon@protonmail.com>
2020-07-31 23:53:10 -0400

    Prevent scoping total error index count in admin menu

--- tests/php/validation/test-class-amp-validation-error-taxonomy.php
+++ tests/php/validation/test-class-amp-validation-error-taxonomy.php
@@ -565,7 +565,6 @@ class Test_AMP_Validation_Error_Taxonomy extends WP_UnitTestCase {
 		$this->assertEquals( 10, has_action( 'admin_notices', [ self::TESTED_CLASS, 'add_admin_notices' ] ) );
 		$this->assertEquals( PHP_INT_MAX, has_filter( AMP_Validation_Error_Taxonomy::TAXONOMY_SLUG . '_row_actions', [ self::TESTED_CLASS, 'filter_tag_row_actions' ] ) );
 		$this->assertEquals( 10, has_action( 'admin_menu', [ self::TESTED_CLASS, 'add_admin_menu_validation_error_item' ] ) );
-		$this->assertEquals( 10, has_filter( 'parse_term_query', [ self::TESTED_CLASS, 'parse_post_php_term_query' ] ) );
 		$this->assertEquals( 10, has_filter( 'manage_' . AMP_Validation_Error_Taxonomy::TAXONOMY_SLUG . '_custom_column', [ self::TESTED_CLASS, 'filter_manage_custom_columns' ] ) );
 		$this->assertEquals( 10, has_filter( 'manage_' . AMP_Validated_URL_Post_Type::POST_TYPE_SLUG . '_sortable_columns', [ self::TESTED_CLASS, 'add_single_post_sortable_columns' ] ) );
 		$this->assertEquals( 10, has_filter( 'posts_where', [ self::TESTED_CLASS, 'filter_posts_where_for_validation_error_status' ] ) );
@@ -1011,38 +1010,6 @@ class Test_AMP_Validation_Error_Taxonomy extends WP_UnitTestCase {
 		];
 		$amp_options      = $submenu[ AMP_Options_Manager::OPTION_NAME ];
 		$this->assertEquals( $expected_submenu, end( $amp_options ) );
-	}
-
-	/**
-	 * Test parse_post_php_term_query.
-	 *
-	 * @covers \AMP_Validation_Error_Taxonomy::parse_post_php_term_query()
-	 */
-	public function test_parse_post_php_term_query() {
-		$wp_term_query = new WP_Term_Query();
-
-		// If is_admin() is false, the conditional will be false and this won't add a query_var value.
-		set_current_screen( 'front' );
-		AMP_Validation_Error_Taxonomy::parse_post_php_term_query( $wp_term_query );
-		$this->assertEmpty( $wp_term_query->query_vars );
-
-		// This is now on the proper screen, but there is no post ID in $_GET['post'].
-		set_current_screen( 'post.php' );
-		$GLOBALS['pagenow'] = 'post.php';
-		AMP_Validation_Error_Taxonomy::parse_post_php_term_query( $wp_term_query );
-		$this->assertEmpty( $wp_term_query->query_vars );
-
-		// Though $_GET['post'] has a post ID, it's not for the amp_validated_url post type.
-		$post_id_wrong_type = self::factory()->post->create();
-		$_GET['post']       = $post_id_wrong_type;
-		AMP_Validation_Error_Taxonomy::parse_post_php_term_query( $wp_term_query );
-		$this->assertEmpty( $wp_term_query->query_vars );
-
-		// Now that $_GET['post'] has a post ID of the correct post type, it should be in the query var.
-		$post_id_correct_post_type = self::factory()->post->create( [ 'post_type' => AMP_Validated_URL_Post_Type::POST_TYPE_SLUG ] );
-		$_GET['post']              = $post_id_correct_post_type;
-		AMP_Validation_Error_Taxonomy::parse_post_php_term_query( $wp_term_query );
-		$this->assertEquals( $post_id_correct_post_type, $wp_term_query->query_vars['object_ids'] );
 	}
 
 	/**
