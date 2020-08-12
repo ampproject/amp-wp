34c6060bf0b05df70ab028a1d68daf55d151c1c3
Pierre Gordon <pierregordon@protonmail.com>
2020-08-12 04:29:18 -0400

    Default parsed URL host to localhost if in WP CLI context

--- includes/amp-helper-functions.php
+++ includes/amp-helper-functions.php
@@ -614,6 +614,10 @@ function amp_get_current_url() {
 
 	if ( ! is_array( $parsed_url ) ) {
 		$parsed_url = [];
+
+		if ( defined( 'WP_CLI' ) && WP_CLI ) {
+			$parsed_url['host'] = 'localhost';
+		}
 	}
 	if ( empty( $parsed_url['scheme'] ) ) {
 		$parsed_url['scheme'] = is_ssl() ? 'https' : 'http';
