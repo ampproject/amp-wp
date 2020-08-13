e0cb55343d779b0f6b3f2a63d38b9de0e751c602
Weston Ruter <westonruter@google.com>
2020-08-13 12:23:24 -0700

    Deprecate use of back-compat legacy templates from v0.3

--- back-compat/back-compat.php
+++ back-compat/back-compat.php
@@ -6,12 +6,19 @@
  */
 
 /**
- * Adds hooks to use legacy templates.
+ * Add hooks to use legacy AMP post templates from before v0.4.
  *
  * If you want to use the template that shipped with v0.3 and earlier, you can use this to force that.
  * Note that this may not stick around forever, so use caution and `function_exists`.
+ *
+ * Note that the old legacy post templates from AMP plugin v0.3 should no longer be used. Update to using
+ * the current AMP legacy post templates or better yet switch to using a full Reader theme.
+ *
+ * @deprecated Use Reader themes instead of old legacy AMP post templates.
+ * @since 0.4
  */
 function amp_backcompat_use_v03_templates() {
+	_deprecated_function( __FUNCTION__, '2.0' );
 	add_filter( 'amp_customizer_is_enabled', '__return_false' );
 	add_filter( 'amp_post_template_dir', '_amp_backcompat_use_v03_templates_callback', 0 ); // Early in case there are other overrides.
 }
@@ -19,7 +26,8 @@ function amp_backcompat_use_v03_templates() {
 /**
  * Callback for getting the legacy templates directory.
  *
- * @access private
+ * @internal
+ * @deprecated
  *
  * @param string $templates Template directory.
  * @return string Legacy template directory.
--- back-compat/templates-v0-3/single.php
+++ back-compat/templates-v0-3/single.php
@@ -17,11 +17,17 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1">
-	<?php do_action( 'amp_post_template_head', $this ); ?>
+	<?php
+	/** This action is documented in templates/html-start.php */
+	do_action( 'amp_post_template_head', $this );
+	?>
 
 	<style amp-custom>
 	<?php $this->load_parts( [ 'style' ] ); ?>
-	<?php do_action( 'amp_post_template_css', $this ); ?>
+	<?php
+	/** This action is documented in templates/html-start.php */
+	do_action( 'amp_post_template_css', $this );
+	?>
 	</style>
 </head>
 <body>
@@ -29,10 +35,23 @@
 <div class="amp-wp-content">
 	<h1 class="amp-wp-title"><?php echo wp_kses_data( $this->get( 'post_title' ) ); ?></h1>
 	<ul class="amp-wp-meta">
-		<?php $this->load_parts( apply_filters( 'amp_post_template_meta_parts', [ 'meta-author', 'meta-time', 'meta-taxonomy' ] ) ); ?>
+		<?php
+		/**
+		 * Filters the template parts to load in the meta area of the AMP legacy post template from before v0.4.
+		 *
+		 * @since 0.2
+		 * @deprecated
+		 *
+		 * @param string[] Templates to load.
+		 */
+		$this->load_parts( apply_filters( 'amp_post_template_meta_parts', [ 'meta-author', 'meta-time', 'meta-taxonomy' ] ) );
+		?>
 	</ul>
 	<?php echo $this->get( 'post_amp_content' ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 </div>
-<?php do_action( 'amp_post_template_footer', $this ); ?>
+<?php
+/** This action is documented in templates/html-start.php */
+do_action( 'amp_post_template_footer', $this );
+?>
 </body>
 </html>
