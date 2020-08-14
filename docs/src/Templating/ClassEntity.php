a02f5747ffd36d414c950008cbb0c0d1620e2715
Alain Schlesser <alain.schlesser@gmail.com>
2020-08-14 18:38:50 +0200

    Add Class entity

---
+++ docs/src/Templating/ClassEntity.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Class ClassEntity.
+ *
+ * @package AmpProject\AmpWP
+ */
+
+namespace AmpProject\AmpWP\Documentation\Templating;
+
+/**
+ * Entity object representing a class.
+ *
+ * @property string           $name
+ * @property string           $namespace
+ * @property int              $line
+ * @property int              $end_line
+ * @property bool             $final
+ * @property bool             $abstract
+ * @property string           $extends
+ * @property string[]         $implements
+ * @property PropertyEntity[] $properties
+ * @property MethodEntity[]   $methods
+ * @property DocBlockEntity   $doc
+ */
+final class ClassEntity {
+
+	use EntityConstruction;
+
+	/**
+	 * Get an associative array of known keys.
+	 *
+	 * @return string[]
+	 */
+	protected function get_known_keys() {
+		return [
+			'name',
+			'namespace',
+			'line',
+			'end_line',
+			'final',
+			'abstract',
+			'extends',
+			'implements',
+			'properties',
+			'methods',
+			'doc',
+		];
+	}
+
+	/**
+	 * Process a doc-block entry.
+	 *
+	 * @param array $value Associative array of the doc-block.
+	 * @return DocBlockEntity Doc-block entity object.
+	 */
+	private function process_doc( $value ) {
+		return new DocBlockEntity( $value );
+	}
+}
