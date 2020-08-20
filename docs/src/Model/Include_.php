3040869b9882dfc78d9e6278ace2f084b33e1f26
Alain Schlesser <alain.schlesser@gmail.com>
2020-08-20 12:46:35 +0200

    Add Include reference object

---
+++ docs/src/Model/Include_.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Class Include_.
+ *
+ * @package AmpProject\AmpWP
+ */
+
+namespace AmpProject\AmpWP\Documentation\Model;
+
+/**
+ * Documentation reference object representing an include or require statement.
+ *
+ * @property string $name
+ * @property int    $line
+ * @property string $type
+ */
+final class Include_ {
+
+	use LeafConstruction;
+
+	/**
+	 * Get an associative array of known keys.
+	 *
+	 * @return string[]
+	 */
+	protected function get_known_keys() {
+		return [
+			'name',
+			'line',
+			'type',
+		];
+	}
+}
