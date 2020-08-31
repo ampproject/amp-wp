717ce3616370ac509e72645031788b9cea29331a
John Watkins <johnwatkins0@gmail.com>
2020-08-31 17:28:11 -0500

    Refactor URL validation CLI script

---
+++ src/Validation/URLValidationProvider.php
@@ -0,0 +1,218 @@
+<?php
+/**
+ * Provides URL validation.
+ *
+ * @package AMP
+ * @since 2.1
+ */
+
+namespace AmpProject\AmpWP\Validation;
+
+use AMP_Validated_URL_Post_Type;
+use AMP_Validation_Error_Taxonomy;
+use AMP_Validation_Manager;
+use WP_Error;
+
+/**
+ * URLValidationProvider class.
+ *
+ * @since 2.1
+ */
+final class URLValidationProvider {
+
+	/**
+	 * Key for the transient signaling validation is locked.
+	 *
+	 * @var string
+	 */
+	const LOCK_TRANSIENT = 'amp_validation_locked';
+
+	/**
+	 * Flag to pass to get_url_validation to force revalidation.
+	 *
+	 * @param string
+	 */
+	const FLAG_FORCE_REVALIDATE = 'amp_force_revalidate';
+
+	/**
+	 * Flag to pass to get_url_validation to skip revalidation.
+	 *
+	 * @param string
+	 */
+	const FLAG_NO_REVALIDATE = 'amp_no_revalidate';
+
+	/**
+	 * The total number of validation errors, regardless of whether they were accepted.
+	 *
+	 * @var int
+	 */
+	public $total_errors = 0;
+
+	/**
+	 * The total number of unaccepted validation errors.
+	 *
+	 * If an error has been accepted in the /wp-admin validation UI,
+	 * it won't count toward this.
+	 *
+	 * @var int
+	 */
+	public $unaccepted_errors = 0;
+
+	/**
+	 * The number of URLs crawled, regardless of whether they have validation errors.
+	 *
+	 * @var int
+	 */
+	public $number_crawled = 0;
+
+	/**
+	 * The validation counts by type, like template or post type.
+	 *
+	 * @var array[] {
+	 *     Validity by type.
+	 *
+	 *     @type array $type {
+	 *         @type int $valid The number of valid URLs for this type.
+	 *         @type int $total The total number of URLs for this type, valid or invalid.
+	 *     }
+	 * }
+	 */
+	public $validity_by_type = [];
+
+	/**
+	 * Locks validation.
+	 */
+	private function lock() {
+		set_transient( self::LOCK_TRANSIENT, 'locked', $this->get_lock_timeout() );
+	}
+
+	/**
+	 * Unlocks validation.
+	 */
+	private function unlock() {
+		delete_transient( self::LOCK_TRANSIENT );
+	}
+
+	/**
+	 * Returns whether validation is currently locked.
+	 *
+	 * @return boolean
+	 */
+	public function is_locked() {
+		return get_transient( self::LOCK_TRANSIENT );
+	}
+
+	/**
+	 * Provides the length of time, in seconds, to lock validation when this runs.
+	 *
+	 * @return int
+	 */
+	private function get_lock_timeout() {
+		/**
+		 * Filters the length of time to lock URL validation when a process starts.
+		 *
+		 * @param int $timeout Time in seconds. Default 300 seconds.
+		 */
+		return apply_filters( 'amp_validation_lock_timeout', 5 * MINUTE_IN_SECONDS );
+	}
+
+	/**
+	 * Runs a callback with a lock set for the duration of the callback.
+	 *
+	 * @param callable $callback Callback to run with the lock set.
+	 * @return mixed  WP_Error if a lock is in place. Otherwise, the result of the callback or void if it doesn't return anything.
+	 */
+	public function with_lock( $callback ) {
+		if ( $this->is_locked() ) {
+			return new WP_Error(
+				'amp_url_validation_locked',
+				__( 'URL validation cannot start right now because another process is already validating URLs. Try again in a few minutes.', 'amp' )
+			);
+		}
+
+		$this->lock();
+		$result = call_user_func( $callback );
+		$this->unlock();
+
+		return $result;
+	}
+
+	/**
+	 * Validates a URL, stores the results, and increments the counts.
+	 *
+	 * @param string $url  The URL to validate.
+	 * @param string $type The type of template, post, or taxonomy.
+	 * @param string $flag Flag determining whether the URL should be revalidated.
+	 * @return array Associative array containing validity results along with error info and whether the URL was revalidated.
+	 */
+	public function get_url_validation( $url, $type, $flag = null ) {
+		$validity    = null;
+		$revalidated = true;
+		$error       = null;
+
+		if ( self::FLAG_FORCE_REVALIDATE !== $flag ) {
+			$url_post = AMP_Validated_URL_Post_Type::get_invalid_url_post( $url );
+
+			if ( $url_post && empty( AMP_Validated_URL_Post_Type::get_post_staleness( $url_post ) ) ) {
+				$validity    = AMP_Validated_URL_Post_Type::get_invalid_url_validation_errors( $url_post );
+				$revalidated = false;
+			}
+		}
+
+		if ( self::FLAG_NO_REVALIDATE !== $flag && ( is_null( $validity ) || self::FLAG_FORCE_REVALIDATE === $flag ) ) {
+			$validity = AMP_Validation_Manager::validate_url_and_store( $url );
+		}
+
+		if ( is_wp_error( $validity ) ) {
+			$validity = null;
+			$error    = $validity;
+		} elseif ( $validity && isset( $validity['results'] ) ) {
+			$this->update_state_from_validity( $validity, $type );
+		}
+
+		return compact( 'validity', 'revalidated', 'error' );
+	}
+
+	/**
+	 * Increments crawl counts from a validation result.
+	 *
+	 * @param array  $validity Validity results.
+	 * @param string $type The URL type.
+	 */
+	private function update_state_from_validity( $validity, $type ) {
+		$validation_errors      = wp_list_pluck( $validity['results'], 'error' );
+		$unaccepted_error_count = count(
+			array_filter(
+				$validation_errors,
+				static function( $error ) {
+					$validation_status = AMP_Validation_Error_Taxonomy::get_validation_error_sanitization( $error );
+					return (
+					AMP_Validation_Error_Taxonomy::VALIDATION_ERROR_ACK_ACCEPTED_STATUS !== $validation_status['term_status']
+					&&
+					AMP_Validation_Error_Taxonomy::VALIDATION_ERROR_NEW_ACCEPTED_STATUS !== $validation_status['term_status']
+					);
+				}
+			)
+		);
+
+		if ( count( $validation_errors ) > 0 ) {
+			$this->total_errors++;
+		}
+		if ( $unaccepted_error_count > 0 ) {
+			$this->unaccepted_errors++;
+		}
+
+		$this->number_crawled++;
+
+		if ( ! isset( $this->validity_by_type[ $type ] ) ) {
+			$this->validity_by_type[ $type ] = [
+				'valid' => 0,
+				'total' => 0,
+			];
+		}
+			$this->validity_by_type[ $type ]['total']++;
+		if ( 0 === $unaccepted_error_count ) {
+			$this->validity_by_type[ $type ]['valid']++;
+		}
+	}
+}
---
+++ tests/php/src/Validation/URLValidationProviderTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace AmpProject\AmpWP\Tests\Validation;
+
+use AmpProject\AmpWP\Tests\Helpers\AssertContainsCompatibility;
+use AmpProject\AmpWP\Tests\Helpers\PrivateAccess;
+use AmpProject\AmpWP\Tests\Helpers\ValidationRequestMocking;
+use AmpProject\AmpWP\Validation\URLValidationProvider;
+use WP_UnitTestCase;
+
+/** @coversDefaultClass URLValidationProvider */
+final class URLValidationProviderTest extends WP_UnitTestCase {
+	use PrivateAccess, AssertContainsCompatibility;
+
+	/**
+	 * Setup.
+	 *
+	 * @inheritdoc
+	 */
+	public function setUp() {
+		parent::setUp();
+		$this->validation_provider = new URLValidationProvider( 100 );
+		add_filter( 'pre_http_request', [ ValidationRequestMocking::class, 'get_validate_response' ] );
+	}
+
+	/**
+	 * Test get_url_validation.
+	 *
+	 * @covers ::get_url_validation()
+	 */
+	public function test_get_url_validation() {
+		$single_post_permalink = get_permalink( self::factory()->post->create() );
+		$this->call_private_method( $this->validation_provider, 'get_url_validation', [ $single_post_permalink, 'post' ] );
+		$this->assertTrue( in_array( $single_post_permalink, ValidationRequestMocking::get_validated_urls(), true ) );
+
+		$number_of_posts = 30;
+		$post_permalinks = [];
+
+		for ( $i = 0; $i < $number_of_posts; $i++ ) {
+			$permalink         = get_permalink( self::factory()->post->create() );
+			$post_permalinks[] = $permalink;
+			$this->call_private_method( $this->validation_provider, 'get_url_validation', [ $permalink, 'post' ] );
+		}
+
+		// All of the posts created should be present in the validated URLs.
+		$this->assertEmpty( array_diff( $post_permalinks, ValidationRequestMocking::get_validated_urls() ) );
+
+		$this->assertEquals( 31, $this->validation_provider->total_errors );
+		$this->assertEmpty( $this->validation_provider->unaccepted_errors );
+		$this->assertEquals( 31, $this->validation_provider->number_crawled );
+
+		$this->assertEquals(
+			[ 'post' ],
+			array_keys( $this->validation_provider->validity_by_type )
+		);
+	}
+
+	/**
+	 * Tests locking and unlocking.
+	 *
+	 * @covers ::lock
+	 * @covers ::unlock
+	 * @covers ::is_locked
+	 * @covers ::get_lock_timeout
+	 * @covers ::with_lock
+	 */
+	public function test_locking() {
+		$this->assertFalse( get_transient( URLValidationProvider::LOCK_TRANSIENT ) );
+
+		$expected_result = 'EXPECTED RESULT';
+		$result          = $this->validation_provider->with_lock(
+			function() use ( $expected_result ) {
+				$this->assertEquals( 'locked', get_transient( URLValidationProvider::LOCK_TRANSIENT ) );
+
+				// Expect an error when lock is already in place.
+				$this->assertWPError( $this->validation_provider->with_lock( '__return_empty_string' ) );
+
+				return $expected_result;
+			}
+		);
+
+		$this->assertEquals( $expected_result, $result );
+		$this->assertFalse( get_transient( URLValidationProvider::LOCK_TRANSIENT ) );
+
+		// Test with_lock with no return value.
+		$this->assertNull(
+			$this->validation_provider->with_lock(
+				function() {
+					$this->assertEquals( 'locked', get_transient( URLValidationProvider::LOCK_TRANSIENT ) );
+					return;
+				}
+			)
+		);
+		$this->assertFalse( get_transient( URLValidationProvider::LOCK_TRANSIENT ) );
+	}
+}
