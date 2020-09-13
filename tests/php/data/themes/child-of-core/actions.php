8370b2914672e5beb8969130a7db1aa5e10b10e5
Weston Ruter <westonruter@google.com>
2020-09-13 00:34:55 -0700

    Add test theme functions

---
+++ tests/php/data/themes/child-of-core/actions.php
@@ -0,0 +1,7 @@
+<?php
+
+if ( ! function_exists( 'child_of_core_after_setup_theme' ) ) {
+	function child_of_core_after_setup_theme() {
+		add_theme_support( 'amp' );
+	}
+}
---
+++ tests/php/data/themes/custom/functions.php
@@ -0,0 +1,8 @@
+<?php
+
+if ( ! function_exists( 'my_custom_after_setup_theme' ) ) {
+
+	function my_custom_after_setup_theme() {
+		add_theme_support( 'amp' );
+	}
+}
