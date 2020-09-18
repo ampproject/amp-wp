e1767d5047ef0b94ed3d44225e2700127fc14053
Weston Ruter <westonruter@google.com>
2020-09-18 13:09:06 -0700

    Build 2.0.3

---
+++ assets/js/amp-customize-controls-legacy.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-i18n', 'wp-polyfill'), 'version' => '43b397cefa0da8bf442afa2f95dd4df2');
---
+++ vendor/sabberworm/php-css-parser/lib/Sabberworm/CSS/Parsing/UnexpectedEOFException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Sabberworm\CSS\Parsing;
+
+/**
+* Thrown if the CSS parsers encounters end of file it did not expect
+* Extends UnexpectedTokenException in order to preserve backwards compatibility
+*/
+class UnexpectedEOFException extends UnexpectedTokenException {}
