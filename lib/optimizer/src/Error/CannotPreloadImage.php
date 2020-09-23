9b58169238e3a28b284e7ee9ee535987acafeefc
Alain Schlesser <alain.schlesser@gmail.com>
2020-09-23 13:47:14 +0200

    Make CannotPreloadImage work without element

--- lib/optimizer/src/Error/CannotPreloadImage.php
+++ lib/optimizer/src/Error/CannotPreloadImage.php
@@ -11,16 +11,22 @@ final class CannotPreloadImage implements Error
 
     const SRCSET_STRING = 'Not preloading the hero image because of the presence of a "srcset" attribute, which '
                           . 'can currently only be preloaded by Chromium-based browsers '
-                          . '(see https://web.dev/preload-responsive-images/): ';
+                          . '(see https://web.dev/preload-responsive-images/).';
 
     /**
      * Instantiate a CannotPreloadImage object for an image with a srcset attribute.
      *
-     * @param DOMElement $element Image element that has the srcset attribute.
+     * @param DOMElement|null $element Optional. Image element that has the srcset attribute, or null if no element.
      * @return self
      */
-    public static function fromImageWithSrcsetAttribute(DOMElement $element)
+    public static function fromImageWithSrcsetAttribute(DOMElement $element = null)
     {
-        return new self(self::SRCSET_STRING . new ElementDump($element));
+        $message = self::SRCSET_STRING;
+
+        if ($element !== null) {
+            $message .= "\n" . new ElementDump($element);
+        }
+
+        return new self($message);
     }
 }
