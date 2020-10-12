a6c1a8904219af1126d22657ede7201f6fb4b963
Alain Schlesser <alain.schlesser@gmail.com>
2020-10-12 14:59:28 +0200

    Fix typos in tests

--- lib/optimizer/tests/Transformer/PreloadHeroImageTest.php
+++ lib/optimizer/tests/Transformer/PreloadHeroImageTest.php
@@ -38,7 +38,7 @@ final class PreloadHeroImageTest extends TestCase
         };
 
         $output = static function ($body, $preloads = '') {
-            return TestMarkup::DOCTYPE . '<html ⚡ i-amphtml-layout=""><head>'
+            return TestMarkup::DOCTYPE . '<html ⚡><head>'
                    . TestMarkup::META_CHARSET . TestMarkup::META_VIEWPORT . $preloads . TestMarkup::SCRIPT_AMPRUNTIME
                    . TestMarkup::LINK_FAVICON . TestMarkup::LINK_CANONICAL . TestMarkup::STYLE_AMPBOILERPLATE . TestMarkup::NOSCRIPT_AMPBOILERPLATE
                    . '</head><body>'
@@ -54,7 +54,7 @@ final class PreloadHeroImageTest extends TestCase
                     . '<amp-img width="500" height="400" src="/img3.png"></amp-img>'
                 ),
                 $output(
-                    '<amp-img data-hero width="500" height="400" src="/img1.png" i-amphtml-ssr><img class="i-amphtml-fill-content i-amphtml-replaced-content" decoding="async" src="/hero1.png"></amp-img>'
+                    '<amp-img data-hero width="500" height="400" src="/img1.png" i-amphtml-ssr><img class="i-amphtml-fill-content i-amphtml-replaced-content" decoding="async" src="/img1.png"></amp-img>'
                     . '<amp-img width="500" height="400" src="/img2.png"></amp-img>'
                     . '<amp-img width="500" height="400" src="/img3.png"></amp-img>',
                     '<link rel=preload href="/img1.png" as="image" data-hero>'
@@ -69,7 +69,7 @@ final class PreloadHeroImageTest extends TestCase
                 ),
                 $output(
                     '<amp-img width="100" height="100" src="/img1.png"></amp-img>'
-                    . '<amp-img data-hero width="500" height="400" src="/img2.png" i-amphtml-ssr><img class="i-amphtml-fill-content i-amphtml-replaced-content" decoding="async" src="/hero1.png"></amp-img>'
+                    . '<amp-img data-hero width="500" height="400" src="/img2.png" i-amphtml-ssr><img class="i-amphtml-fill-content i-amphtml-replaced-content" decoding="async" src="/img2.png"></amp-img>'
                     . '<amp-img width="500" height="400" src="/img3.png"></amp-img>',
                     '<link rel=preload href="/img2.png" as="image" data-hero>'
                 ),
@@ -105,18 +105,32 @@ final class PreloadHeroImageTest extends TestCase
                     . '<amp-img width="500" height="400" src="https://example-com.cdn.ampproject.org/foo.png"></amp-img>'
                 ),
                 $output(
-                    '<amp-img width="500" height="400" src="https://example-com.cdn.ampproject.org/foo.png" srcset="test 100w test2 3dpr" i-amphtml-ssr data-hero>'
+                    '<amp-img width="500" height="400" src="https://example-com.cdn.ampproject.org/foo.png" srcset="test 100w test2 3dpr"></amp-img>'
+                    . '<amp-img data-hero width="500" height="400" i-amphtml-ssr src="https://example-com.cdn.ampproject.org/foo.png">'
                     . '<img class="i-amphtml-fill-content i-amphtml-replaced-content" decoding="async" src="https://example-com.cdn.ampproject.org/foo.png" srcset="test 100w test2 3dpr">'
                     . '</amp-img>'
-                    . '<amp-img width="500" height="400" src="https://example-com.cdn.ampproject.org/foo.png" srcset="test 100w test2 3dpr" i-amphtml-ssr data-hero></amp-img>'
                 ),
                 [
                     Error\CannotPreloadImage::fromImageWithSrcsetAttribute(
                         Document::fromHtmlFragment(
-                            '<amp-img width="500" height="400" src="https://example-com.cdn.ampproject.org/foo.png" srcset="test 100w test2 3dpr">'
+                            '<amp-img width="500" height="400" src="https://example-com.cdn.ampproject.org/foo.png" srcset="test 100w test2 3dpr"></amp-img>'
                         )->body->firstChild
                     ),
                 ],
+            ],
+
+            'fetches placeholders for animations' => [
+                $input(
+                    '<amp-anim data-hero width="500" height="400" src="/foo.gif">'
+                    . '<amp-img placeholder width="500" height="400" src="/foo.png"></amp-img>'
+                    . '</amp-anim>'
+                ),
+                $output(
+                    '<amp-anim data-hero width="500" height="400" src="/foo.gif">'
+                    . '<amp-img placeholder data-hero width="500" height="400" src="/foo.png" i-amphtml-ssr><img class="i-amphtml-fill-content i-amphtml-replaced-content" decoding="async" src="/foo.png"></amp-img>'
+                    . '</amp-anim>',
+                    '<link rel=preload href="/foo.png" as="image" data-hero>'
+                )
             ],
         ];
     }
