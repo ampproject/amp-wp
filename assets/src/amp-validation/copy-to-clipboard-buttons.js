61e571a473a8b229fcc61614bdbf47e07d72db76
John Watkins <johnwatkins0@gmail.com>
2020-10-21 09:54:18 -0500

    Use clipboard.js package for copying to clipboard

--- assets/src/amp-validation/copy-to-clipboard-buttons.js
+++ assets/src/amp-validation/copy-to-clipboard-buttons.js
@@ -1,25 +1,12 @@
 /**
+ * External dependencies
+ */
+import Clipboard from 'clipboard';
+
+/**
  * Internal dependencies
  */
 import { getURLValidationTableRows } from './get-url-validation-table-rows';
-
-/**
- * Copies any string to the clipboard.
- *
- * Note: This only works within a callback responding to a user action.
- *
- * @param {string} value Any string value.
- */
-function copyToClipboard( value ) {
-	const textareaElement = document.createElement( 'textarea' );
-	textareaElement.value = value;
-	textareaElement.style.position = 'absolute';
-	textareaElement.style.left = '-1000%';
-	document.body.appendChild( textareaElement );
-	textareaElement.select();
-	document.execCommand( 'copy' );
-	document.body.removeChild( textareaElement );
-}
 
 /**
  * Update the status field ("Kept"/"Removed") because it might have changed.
@@ -36,45 +23,36 @@ function updateJsonStatusField( json, button ) {
 }
 
 /**
- * Callback when a user clicks a button to copy error details to a clipboard.
- *
- * @param {Event} event Click event.
- */
-function handleCopyToClipboardClick( event ) {
-	// Handle a single error detail button.
-	if ( event.target.classList.contains( 'single-url-detail-copy' ) ) {
-		let json = JSON.parse( event.target.getAttribute( 'data-error-json' ) );
-
-		json = updateJsonStatusField( json, event.target );
-
-		copyToClipboard( JSON.stringify( json, null, '\t' ) );
-		return;
-	}
-
-	// Handle a click on the bulk action button.
-	if ( ! event.target.classList.contains( 'copy-all' ) ) {
-		return;
-	}
-
-	const value = getURLValidationTableRows( { checkedOnly: true } ).map( ( row ) => {
-		const copyButton = row.querySelector( '.single-url-detail-copy' );
-		if ( ! copyButton ) {
-			return null;
-		}
-
-		let json = JSON.parse( copyButton.getAttribute( 'data-error-json' ) );
-		json = updateJsonStatusField( json, copyButton );
-
-		return json;
-	} )
-		.filter( ( item ) => item );
-
-	copyToClipboard( JSON.stringify( value, null, '\t' ) );
-}
-
-/**
  * Sets up the "Copy to clipboard" buttons on the URL validation screen.
  */
 export function handleCopyToClipboardButtons() {
-	global.addEventListener( 'click', handleCopyToClipboardClick );
+	/* eslint-disable no-new */
+	new Clipboard( 'button.single-url-detail-copy', {
+		text: ( btn ) => {
+			let json = JSON.parse( btn.getAttribute( 'data-error-json' ) );
+			json = updateJsonStatusField( json, btn );
+
+			return JSON.stringify( json, null, '\t' );
+		},
+	} );
+
+	new Clipboard( 'button.copy-all', {
+	/* eslint-enable no-new */
+		text: () => {
+			const value = getURLValidationTableRows( { checkedOnly: true } ).map( ( row ) => {
+				const copyButton = row.querySelector( '.single-url-detail-copy' );
+				if ( ! copyButton ) {
+					return null;
+				}
+
+				let json = JSON.parse( copyButton.getAttribute( 'data-error-json' ) );
+				json = updateJsonStatusField( json, copyButton );
+
+				return json;
+			} )
+				.filter( ( item ) => item );
+
+			return JSON.stringify( value, null, '\t' );
+		},
+	} );
 }
