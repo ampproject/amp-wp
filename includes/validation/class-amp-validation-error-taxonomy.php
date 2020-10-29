c83dbb81901fdfb3d1edd6ba0fc626354e979ed7
John Watkins <johnwatkins0@gmail.com>
2020-10-29 14:15:23 -0500

    No need to pretty-print error details on the backend

--- includes/validation/class-amp-validation-error-taxonomy.php
+++ includes/validation/class-amp-validation-error-taxonomy.php
@@ -1613,7 +1613,7 @@ class AMP_Validation_Error_Taxonomy {
 		$json['removed']  = (bool) ( (int) $term->term_group & self::ACCEPTED_VALIDATION_ERROR_BIT_MASK );
 		$json['reviewed'] = (bool) ( (int) $term->term_group & self::ACKNOWLEDGED_VALIDATION_ERROR_BIT_MASK );
 
-		return wp_json_encode( $json, JSON_PRETTY_PRINT );
+		return wp_json_encode( $json );
 	}
 
 	/**
