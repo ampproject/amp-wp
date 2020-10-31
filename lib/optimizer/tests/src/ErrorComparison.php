3320b6f4c8693373ff4745aef8d0a390479c55e3
Alain Schlesser <alain.schlesser@gmail.com>
2020-10-31 14:51:29 +0100

    Rename expected_errors into expectedErrors

--- lib/optimizer/tests/src/ErrorComparison.php
+++ lib/optimizer/tests/src/ErrorComparison.php
@@ -17,24 +17,24 @@ trait ErrorComparison
     /**
      * Assert that two sets of errors are the same.
      *
-     * @param ErrorCollection|Error[] $expected_errors Set of expected errors.
-     * @param ErrorCollection|Error[] $actualErrors    Set of actual errors.
+     * @param ErrorCollection|Error[] $expectedErrors Set of expected errors.
+     * @param ErrorCollection|Error[] $actualErrors   Set of actual errors.
      */
-    protected function assertSameErrors($expected_errors, $actualErrors)
+    protected function assertSameErrors($expectedErrors, $actualErrors)
     {
-        $this->assertCount(count($expected_errors), $actualErrors, 'Unexpected number of errors');
+        $this->assertCount(count($expectedErrors), $actualErrors, 'Unexpected number of errors');
 
-        if ($expected_errors instanceof ErrorCollection) {
-            $expected_errors = iterator_to_array($expected_errors, false);
+        if ($expectedErrors instanceof ErrorCollection) {
+            $expectedErrors = iterator_to_array($expectedErrors, false);
         }
 
         if ($actualErrors instanceof ErrorCollection) {
             $actualErrors = iterator_to_array($actualErrors, false);
         }
 
-        $expectedCount = count($expected_errors);
+        $expectedCount = count($expectedErrors);
         for ($index = 0; $index < $expectedCount; $index++) {
-            $expectedError = $expected_errors[ $index];
+            $expectedError = $expectedErrors[$index];
             $actualError   = $actualErrors[$index];
             if (is_string($expectedError)) {
                 // If strings were passed, assume the error code is used.
