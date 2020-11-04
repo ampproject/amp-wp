8bcf6bda2979ee76f982b79b4fd99f734b7d2a24
Pierre Gordon <pierregordon@protonmail.com>
2020-11-03 19:24:57 -0500

    Hide AMP Noloading toggle if block does not have specified setting

--- assets/src/block-editor/helpers/index.js
+++ assets/src/block-editor/helpers/index.js
@@ -418,6 +418,10 @@ AmpLayoutControl.propTypes = {
 const AmpNoloadingToggle = ( props ) => {
 	const { attributes: { ampNoLoading }, setAttributes } = props;
 
+	if ( ! ampNoLoading ) {
+		return null;
+	}
+
 	const label = __( 'AMP Noloading', 'amp' );
 
 	return (
