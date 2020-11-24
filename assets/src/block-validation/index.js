9a056bf19146d238c7e21d6bd540035d539a343a
John Watkins <johnwatkins0@gmail.com>
2020-11-23 21:48:42 -0600

    Put AMPBlockValidation into index.js

--- assets/src/block-validation/index.js
+++ assets/src/block-validation/index.js
@@ -4,20 +4,58 @@
 import { registerPlugin } from '@wordpress/plugins';
 import { __ } from '@wordpress/i18n';
 import { addFilter } from '@wordpress/hooks';
+import { PluginSidebar, PluginSidebarMoreMenuItem } from '@wordpress/edit-post';
+import { useSelect } from '@wordpress/data';
 
 /**
  * Internal dependencies
  */
-import { INITIAL_STATE, createStore } from './store';
-import { MoreMenuIcon } from './icon';
-import { AMPBlockValidation } from './amp-block-validation';
+import { INITIAL_STATE, createStore, BLOCK_VALIDATION_STORE_KEY } from './store';
+import { MoreMenuIcon, ToolbarIcon } from './icon';
 import { filterBlocksEdit } from './toolbar-button';
-
-createStore( INITIAL_STATE );
+import { Sidebar } from './sidebar';
+import { InvalidBlockOutline } from './invalid-block-outline';
+import { BlockValidationStateUpdater } from './block-validation-state-updater';
 
 export const PLUGIN_NAME = 'amp-block-validation';
 export const SIDEBAR_NAME = 'amp-editor-sidebar';
 export const PLUGIN_TITLE = __( 'AMP for WordPress', 'amp' );
+
+createStore( INITIAL_STATE );
+
+/**
+ * Provides a dedicated sidebar for the plugin, with toggle buttons in the editor toolbar and more menu.
+ */
+function AMPBlockValidation() {
+	const { broken, errorCount } = useSelect( ( select ) => ( {
+		broken: select( BLOCK_VALIDATION_STORE_KEY ).getAMPBroken(),
+		errorCount: select( BLOCK_VALIDATION_STORE_KEY ).getUnreviewedValidationErrors()?.length || 0,
+	} ) );
+
+	return (
+		<>
+			<PluginSidebarMoreMenuItem
+				icon={ <MoreMenuIcon /> }
+				target={ SIDEBAR_NAME }
+			>
+				{ PLUGIN_TITLE }
+			</PluginSidebarMoreMenuItem>
+			<PluginSidebar
+				className={ `${ PLUGIN_NAME }-sidebar` }
+				icon={ (
+					<ToolbarIcon count={ errorCount } broken={ broken } />
+				) }
+				name={ SIDEBAR_NAME }
+				title={ PLUGIN_TITLE }
+			>
+
+				<Sidebar />
+				<InvalidBlockOutline />
+			</PluginSidebar>
+			<BlockValidationStateUpdater />
+		</>
+	);
+}
 
 registerPlugin(
 	PLUGIN_NAME,
