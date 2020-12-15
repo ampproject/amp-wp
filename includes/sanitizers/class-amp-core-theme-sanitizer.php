ed7d96a639707d8e2132f64e6475f43aefdb562e
Weston Ruter <westonruter@google.com>
2020-12-14 22:49:41 -0800

    Persist focus on mobile menu button when toggling

--- includes/sanitizers/class-amp-core-theme-sanitizer.php
+++ includes/sanitizers/class-amp-core-theme-sanitizer.php
@@ -2085,7 +2085,7 @@ class AMP_Core_Theme_Sanitizer extends AMP_Base_Sanitizer {
 							visibility: hidden;
 						}
 
-						#primary-mobile-menu[aria-expanded="true"] {
+						.primary-navigation-open #primary-mobile-menu {
 							visibility: visible;
 						}
 					}
