599796f095fb49d1c1fd792e263728a548ac733e
John Watkins <johnwatkins0@gmail.com>
2020-12-29 10:20:32 -0600

    Remove unneeded use statement
    Co-authored-by: Weston Ruter <westonruter@google.com>

--- src/AmpWpPlugin.php
+++ src/AmpWpPlugin.php
@@ -9,7 +9,6 @@ namespace AmpProject\AmpWP;
 
 use AmpProject\AmpWP\Admin;
 use AmpProject\AmpWP\BackgroundTask;
-use AmpProject\AmpWP\Infrastructure\Injector;
 use AmpProject\AmpWP\Infrastructure\ServiceBasedPlugin;
 use AmpProject\AmpWP\Instrumentation;
 use AmpProject\AmpWP\Validation\SavePostValidationEvent;
