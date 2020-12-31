950396c7a57d45dd4b86f0989c7c0ce95fd9cdd1
Pierre Gordon <16200219+pierlon@users.noreply.github.com>
2020-12-31 06:29:20 +0000

    [2.0] Remove E2E test utils that are now available upstream (#5755)
    * Update @wordpress/e2e-test-utils to v4.16.0
    
    * Remove E2E test utils that are now available upstream
    
    * Use new arg format for `deleteTheme()`
    
    * Set default timeout to 10s

--- tests/e2e/utils/index.js
+++ tests/e2e/utils/index.js
@@ -1,4 +1,2 @@
-export { activatePlugin } from './activate-plugin';
-export { deactivatePlugin } from './deactivate-plugin';
 export { clickButton } from './click-button';
 export { uploadMedia } from './upload-media';
