957425a5f080c07b61ff6f90723b22425d8e1e82
Weston Ruter <westonruter@google.com>
2021-01-12 22:34:02 -0800

    Add tests for PairedUrlStructures

---
+++ tests/php/src/PairedUrlStructure/PathSuffixUrlStructureTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AmpProject\AmpWP\Tests;
+
+use AmpProject\AmpWP\PairedUrlStructure\PathSuffixUrlStructure;
+
+/** @coversDefaultClass \AmpProject\AmpWP\PairedUrlStructure\PathSuffixUrlStructure */
+class PathSuffixUrlStructureTest extends DependencyInjectedTestCase {
+
+	/** @var PathSuffixUrlStructure */
+	private $instance;
+
+	public function setUp() {
+		parent::setUp();
+		$this->set_permalink_structure( '/%year%/%monthnum%/%day%/%postname%/' ); // Needed for user_trailingslashit().
+		$this->instance = $this->injector->make( PathSuffixUrlStructure::class );
+	}
+
+	/** @covers ::add_endpoint() */
+	public function test_add_endpoint() {
+		$slug = amp_get_slug();
+		$this->assertEquals(
+			home_url( "/foo/$slug/" ),
+			$this->instance->add_endpoint( home_url( '/foo/' ) )
+		);
+	}
+
+	/** @covers ::has_endpoint() */
+	public function test_has_endpoint() {
+		$slug = amp_get_slug();
+		$this->assertFalse( $this->instance->has_endpoint( home_url( '/foo/' ) ) );
+		$this->assertTrue( $this->instance->has_endpoint( home_url( "/foo/$slug/" ) ) );
+	}
+
+	/** @covers ::remove_endpoint() */
+	public function test_remove_endpoint() {
+		$slug = amp_get_slug();
+		$this->assertEquals(
+			home_url( '/foo/' ),
+			$this->instance->remove_endpoint( home_url( "/foo/$slug/" ) )
+		);
+	}
+}
