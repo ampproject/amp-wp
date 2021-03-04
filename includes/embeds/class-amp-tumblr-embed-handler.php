77c736779e5d698839a7ec47d4a709fc21c70733
Weston Ruter <westonruter@google.com>
2021-03-03 23:36:36 -0800

    Set placeholder attribute when we are sure we will match

--- includes/embeds/class-amp-tumblr-embed-handler.php
+++ includes/embeds/class-amp-tumblr-embed-handler.php
@@ -66,7 +66,6 @@ class AMP_Tumblr_Embed_Handler extends AMP_Base_Embed_Handler {
 		}
 
 		foreach ( $placeholders as $placeholder ) {
-			$placeholder->setAttribute( Attribute::PLACEHOLDER, '' );
 			$div = $placeholder->parentNode;
 			if ( ! $div instanceof DOMElement ) {
 				continue; // @codeCoverageIgnore
@@ -98,6 +97,7 @@ class AMP_Tumblr_Embed_Handler extends AMP_Base_Embed_Handler {
 			);
 			$overflow_element->textContent = __( 'See more', 'amp' );
 			$amp_element->appendChild( $overflow_element );
+			$placeholder->setAttribute( Attribute::PLACEHOLDER, '' );
 			$amp_element->appendChild( $placeholder );
 
 			$this->maybe_remove_script_sibling(
