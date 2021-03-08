792859fcffb96beacbf02beb1c98f83f51f1da8c
Pierre Gordon <pierregordon@protonmail.com>
2021-03-08 07:30:50 -0500

    Stub `WP_CLI` class and related methods used throughout plugin

--- tests/php/static-analysis-stubs/wp-cli.php
+++ tests/php/static-analysis-stubs/wp-cli.php
@@ -2,6 +2,200 @@
 
 // Generated via https://github.com/szepeviktor/phpstan-wordpress
 
+namespace {
+	/**
+	 * Various utilities for WP-CLI commands.
+	 */
+	class WP_CLI {
+		/**
+		 * Register a command to WP-CLI.
+		 *
+		 * WP-CLI supports using any callable class, function, or closure as a
+		 * command. `WP_CLI::add_command()` is used for both internal and
+		 * third-party command registration.
+		 *
+		 * Command arguments are parsed from PHPDoc by default, but also can be
+		 * supplied as an optional third argument during registration.
+		 *
+		 * ```
+		 * # Register a custom 'foo' command to output a supplied positional param.
+		 * #
+		 * # $ wp foo bar --append=qux
+		 * # Success: bar qux
+		 *
+		 * /**
+		 *  * My awesome closure command
+		 *  *
+		 *  * <message>
+		 *  * : An awesome message to display
+		 *  *
+		 *  * --append=<message>
+		 *  * : An awesome message to append to the original message.
+		 *  *
+		 *  * @when before_wp_load
+		 *  *\/
+		 * $foo = function( $args, $assoc_args ) {
+		 *     WP_CLI::success( $args[0] . ' ' . $assoc_args['append'] );
+		 * };
+		 * WP_CLI::add_command( 'foo', $foo );
+		 * ```
+		 *
+		 * @access public
+		 * @category Registration
+		 *
+		 * @param string   $name Name for the command (e.g. "post list" or "site empty").
+		 * @param callable $callable Command implementation as a class, function or closure.
+		 * @param array    $args {
+		 *    Optional. An associative array with additional registration parameters.
+		 *
+		 *    @type callable $before_invoke Callback to execute before invoking the command.
+		 *    @type callable $after_invoke  Callback to execute after invoking the command.
+		 *    @type string   $shortdesc     Short description (80 char or less) for the command.
+		 *    @type string   $longdesc      Description of arbitrary length for examples, etc.
+		 *    @type string   $synopsis      The synopsis for the command (string or array).
+		 *    @type string   $when          Execute callback on a named WP-CLI hook (e.g. before_wp_load).
+		 *    @type bool     $is_deferred   Whether the command addition had already been deferred.
+		 * }
+		 * @return bool True on success, false if deferred, hard error if registration failed.
+		 */
+		public static function add_command($name, $callable, $args = array())
+		{
+		}
+		/**
+		 * Display error message prefixed with "Error: " and exit script.
+		 *
+		 * Error message is written to STDERR. Defaults to halting script execution
+		 * with return code 1.
+		 *
+		 * Use `WP_CLI::warning()` instead when script execution should be permitted
+		 * to continue.
+		 *
+		 * ```
+		 * # `wp cache flush` considers flush failure to be a fatal error.
+		 * if ( false === wp_cache_flush() ) {
+		 *     WP_CLI::error( 'The object cache could not be flushed.' );
+		 * }
+		 * ```
+		 *
+		 * @access public
+		 * @category Output
+		 *
+		 * @param string|WP_Error  $message Message to write to STDERR.
+		 * @param boolean|integer  $exit    True defaults to exit(1).
+		 * @return null
+		 */
+		public static function error($message, $exit = \true)
+		{
+		}
+		/**
+		 * Ask for confirmation before running a destructive operation.
+		 *
+		 * If 'y' is provided to the question, the script execution continues. If
+		 * 'n' or any other response is provided to the question, script exits.
+		 *
+		 * ```
+		 * # `wp db drop` asks for confirmation before dropping the database.
+		 *
+		 * WP_CLI::confirm( "Are you sure you want to drop the database?", $assoc_args );
+		 * ```
+		 *
+		 * @access public
+		 * @category Input
+		 *
+		 * @param string $question Question to display before the prompt.
+		 * @param array $assoc_args Skips prompt if 'yes' is provided.
+		 */
+		public static function confirm($question, $assoc_args = array())
+		{
+		}
+		/**
+		 * Display warning message prefixed with "Warning: ".
+		 *
+		 * Warning message is written to STDERR.
+		 *
+		 * Use instead of `WP_CLI::debug()` when script execution should be permitted
+		 * to continue.
+		 *
+		 * ```
+		 * # `wp plugin activate` skips activation when plugin is network active.
+		 * $status = $this->get_status( $plugin->file );
+		 * // Network-active is the highest level of activation status
+		 * if ( 'active-network' === $status ) {
+		 *   WP_CLI::warning( "Plugin '{$plugin->name}' is already network active." );
+		 *   continue;
+		 * }
+		 * ```
+		 *
+		 * @access public
+		 * @category Output
+		 *
+		 * @param string $message Message to write to STDERR.
+		 * @return null
+		 */
+		public static function warning($message)
+		{
+		}
+		/**
+		 * Display success message prefixed with "Success: ".
+		 *
+		 * Success message is written to STDOUT.
+		 *
+		 * Typically recommended to inform user of successful script conclusion.
+		 *
+		 * ```
+		 * # wp rewrite flush expects 'rewrite_rules' option to be set after flush.
+		 * flush_rewrite_rules( \WP_CLI\Utils\get_flag_value( $assoc_args, 'hard' ) );
+		 * if ( ! get_option( 'rewrite_rules' ) ) {
+		 *     WP_CLI::warning( "Rewrite rules are empty." );
+		 * } else {
+		 *     WP_CLI::success( 'Rewrite rules flushed.' );
+		 * }
+		 * ```
+		 *
+		 * @access public
+		 * @category Output
+		 *
+		 * @param string $message Message to write to STDOUT.
+		 * @return null
+		 */
+		public static function success($message)
+		{
+		}
+		/**
+		 * Display informational message without prefix, and ignore `--quiet`.
+		 *
+		 * Message is written to STDOUT. `WP_CLI::log()` is typically recommended;
+		 * `WP_CLI::line()` is included for historical compat.
+		 *
+		 * @access public
+		 * @category Output
+		 *
+		 * @param string $message Message to display to the end user.
+		 * @return null
+		 */
+		public static function line($message = '')
+		{
+		}
+		/**
+		 * Display informational message without prefix.
+		 *
+		 * Message is written to STDOUT, or discarded when `--quiet` flag is supplied.
+		 *
+		 * ```
+		 * # `wp cli update` lets user know of each step in the update process.
+		 * WP_CLI::log( sprintf( 'Downloading from %s...', $download_url ) );
+		 * ```
+		 *
+		 * @access public
+		 * @category Output
+		 *
+		 * @param string $message Message to write to STDOUT.
+		 */
+		public static function log($message)
+		{
+		}
+	}
+}
 namespace WP_CLI\Utils {
     /**
      * Render a collection of items as an ASCII table, JSON, CSV, YAML, list of ids, or count.
