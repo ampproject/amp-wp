8d19c25baeb4b338a2ba949923f268e97e7d886b
Pierre Gordon <pierregordon@protonmail.com>
2021-04-05 15:47:58 -0400

    Retrieve editor color palette and generate style rules

--- includes/amp-post-template-functions.php
+++ includes/amp-post-template-functions.php
@@ -23,6 +23,7 @@ function amp_post_template_init_hooks() {
 	add_action( 'amp_post_template_head', 'wp_generator' );
 	add_action( 'amp_post_template_head', 'amp_post_template_add_block_styles' );
 	add_action( 'amp_post_template_head', 'amp_post_template_add_default_styles' );
+	add_action( 'amp_post_template_css', 'amp_post_template_add_editor_color_styles' );
 	add_action( 'amp_post_template_css', 'amp_post_template_add_styles', 99 );
 	add_action( 'amp_post_template_footer', 'amp_post_template_add_analytics_data' );
 
@@ -89,6 +90,64 @@ function amp_post_template_add_block_styles() {
 
 	// Note that this will also print the admin-bar styles since WP_Admin_Bar::initialize() has been called.
 	wp_styles()->do_items();
+}
+
+/**
+ * Print editor color styles.
+ *
+ * @internal
+ * @since 2.1
+ */
+function amp_post_template_add_editor_color_styles() {
+	$color_palette = current( (array) get_theme_support( 'editor-color-palette' ) );
+
+	if ( false === $color_palette ) {
+		return;
+	}
+
+	foreach ( $color_palette as $color_option ) {
+		$text_color = 127 > amp_get_relative_luminance_from_hex( $color_option['color'] ) ? '#fff' : '#000';
+
+		printf(
+			':root .has-%1$s-color { color: %2$s }
+				:root .has-%1$s-background-color { color: %3$s; background-color: %2$s }',
+			$color_option['slug'], // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			$color_option['color'], // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			$text_color // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		);
+	}
+}
+
+/**
+ * Get relative luminance from color hex value.
+ *
+ * Copied from `\Twenty_Twenty_One_Custom_Colors::get_relative_luminance_from_hex()`.
+ *
+ * @internal
+ * @see https://github.com/WordPress/wordpress-develop/blob/acbbbd18b32b5429264622141a6d058b64f3a5ad/src/wp-content/themes/twentytwentyone/classes/class-twenty-twenty-one-custom-colors.php#L138-L156
+ * @since 2.1
+ *
+ * @param $hex string Color hex value.
+ * @return int Relative luminance value.
+ */
+function amp_get_relative_luminance_from_hex( $hex ) {
+
+	// Remove the "#" symbol from the beginning of the color.
+	$hex = ltrim( $hex, '#' );
+
+	// Make sure there are 6 digits for the below calculations.
+	if ( 3 === strlen( $hex ) ) {
+		$hex = $hex[0] . $hex[0] . $hex[1] . $hex[1] . $hex[2] . $hex[2];
+	}
+
+	// Get red, green, blue.
+	$red   = hexdec( substr( $hex, 0, 2 ) );
+	$green = hexdec( substr( $hex, 2, 2 ) );
+	$blue  = hexdec( substr( $hex, 4, 2 ) );
+
+	// Calculate the luminance.
+	$lum = ( 0.2126 * $red ) + ( 0.7152 * $green ) + ( 0.0722 * $blue );
+	return (int) round( $lum );
 }
 
 /**
