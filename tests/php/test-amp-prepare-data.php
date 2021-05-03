3b314771898a7a5dc42c3a5aafcda5e1b0eeb914
Paul Clark <paul.clark@rtcamp.com>
2021-05-03 16:01:05 -0500

    Test AMP_Prepare_Data::parse_args()

---
+++ tests/php/test-amp-prepare-data.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Test AMP_Prepare_Data.
+ *
+ * @package AMP
+ */
+
+/**
+ * Test AMP_Prepare_Data.
+ *
+ * @covers AMP_Prepare_Data
+ */
+class AMP_Prepare_Data_Test extends WP_UnitTestCase {
+
+	/**
+	 * Test __construct method.
+	 *
+	 * @covers \AMP_Prepare_Data::__construct()
+	 */
+	public function test_construct() {
+		$prepare_data = new \AMP_Prepare_Data( [] );
+		$this->assertTrue(
+			is_a(
+				$prepare_data,
+				'AMP_Prepare_Data'
+			)
+		);
+	}
+
+	/**
+	 * Test parse_args method.
+	 *
+	 * @covers \AMP_Prepare_Data::parse_args()
+	 */
+	public function test_parse_args() {
+		$args = [
+			'urls'     => [ 'https://google.com/' ],
+			'post_ids' => [ 123, 456 ],
+			'term_ids' => [ 789 ],
+		];
+
+		$prepare_data = new \AMP_Prepare_Data( $args );
+
+		$this->assertSame(
+			$args['urls'],
+			$prepare_data->urls
+		);
+		$this->assertSame(
+			$args['term_ids'],
+			$prepare_data->args['term_ids']
+		);
+		$this->assertSame(
+			$args['post_ids'],
+			$prepare_data->args['post_ids']
+		);
+
+		// If valid term IDs, permalinks should be added to URLs.
+		$term = wp_insert_term( 'test', 'category' );
+
+		$expected = [
+			'urls' => [
+				\AMP_Prepare_Data::normalize_url_for_storage(
+					get_term_link( $term['term_id'] )
+				),
+			],
+			'term_ids' => [
+				$term['term_id'],
+			],
+		];
+
+		$prepare_data = new \AMP_Prepare_Data( $expected );
+
+		$this->assertSame(
+			$expected['urls'],
+			$prepare_data->urls
+		);
+
+		// If valid post IDs, permalinks should be added to URLs.
+		$p = wp_insert_post(
+			[
+				'post_title'  => 'test',
+				'post_status' => 'publish',
+			]
+		);
+
+		$expected = [
+			'urls' => [
+				\AMP_Prepare_Data::normalize_url_for_storage(
+					get_permalink( $p->ID )
+				),
+			],
+			'post_ids' => [
+				$p->ID,
+			],
+		];
+
+		$prepare_data = new \AMP_Prepare_Data( $expected );
+
+		$this->assertSame(
+			$expected['urls'],
+			$prepare_data->urls
+		);
+	}
+}
