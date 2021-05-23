41ac5fe79744962c77329b3b1b22ff773e7eb808
Weston Ruter <westonruter@google.com>
2021-05-23 14:41:03 -0700

    Reduce length of specificity-hacked CSS selectors

--- includes/sanitizers/class-amp-style-sanitizer.php
+++ includes/sanitizers/class-amp-style-sanitizer.php
@@ -2917,16 +2917,10 @@ class AMP_Style_Sanitizer extends AMP_Base_Sanitizer {
 					if ( $old_selector->getSpecificity() % 10 > 0 ) {
 						$specificity_multiplier++;
 					}
-					$selector_mod = str_repeat( ':not(#_)', $specificity_multiplier ); // Here "_" is just a short single-char ID.
 
-					$new_selector = $old_selector->getSelector();
+					$new_selector  = $old_selector->getSelector();
+					$new_selector .= ':not(' . str_repeat( '#_', $specificity_multiplier ) . ')'; // Here "_" is just a short single-char ID.
 
-					// Amend the selector mod to the first element in selector if it is already the root; otherwise add new root ancestor.
-					if ( preg_match( '/^\s*(html|:root)\b/i', $new_selector, $matches ) ) {
-						$new_selector = substr( $new_selector, 0, strlen( $matches[0] ) ) . $selector_mod . substr( $new_selector, strlen( $matches[0] ) );
-					} else {
-						$new_selector = sprintf( ':root%s %s', $selector_mod, $new_selector );
-					}
 					return new Selector( $new_selector );
 				},
 				$ruleset->getSelectors()
@@ -2975,9 +2969,9 @@ class AMP_Style_Sanitizer extends AMP_Base_Sanitizer {
 			return;
 		}
 
-		$class = 'amp-wp-' . substr( md5( $value ), 0, 7 );
-		$root  = ':root' . str_repeat( ':not(#_)', self::INLINE_SPECIFICITY_MULTIPLIER );
-		$rule  = sprintf( '%s .%s { %s }', $root, $class, $value );
+		$class       = 'amp' . substr( md5( $value ), 0, 7 );
+		$specificity = ':not(' . str_repeat( '#_', self::INLINE_SPECIFICITY_MULTIPLIER ) . ')';
+		$rule        = sprintf( '.%s%s{%s}', $class, $specificity, $value );
 
 		$this->set_current_node( $element ); // And sources when needing to be located.
 
