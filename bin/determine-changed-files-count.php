67eb41ffe1442b5bd76c83a6ad44194e9cdedb0b
Pierre Gordon <pierregordon@protonmail.com>
2021-06-13 04:09:08 -0400

    Fix lint errors

--- bin/determine-changed-files-count.php
+++ bin/determine-changed-files-count.php
@@ -9,10 +9,13 @@
  * php -f determine-changed-files-count.php "foo\/bar\nbar*" "foo/bar/baz\nquux"
  *
  * Would output: 1
+ *
+ * @codeCoverageIgnore
+ * @package AMP
  */
 
-$ignore_pattern = str_replace( "\n", '|', rtrim($argv[1] ) );
-$changed_files   = explode( "\n", rtrim( $argv[2] ) );
+$ignore_pattern = str_replace( "\n", '|', rtrim( $argv[1] ) );
+$changed_files  = explode( "\n", rtrim( $argv[2] ) );
 
 $filtered_files = preg_grep( "/^${ignore_pattern}$/m", $changed_files, PREG_GREP_INVERT );
 
