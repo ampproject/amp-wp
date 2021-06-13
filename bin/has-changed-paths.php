78235b6144b844bf3b596286b65ec5a776b8331f
Pierre Gordon <pierregordon@protonmail.com>
2021-06-13 03:37:55 -0400

    Debug

--- bin/has-changed-paths.php
+++ bin/has-changed-paths.php
@@ -3,6 +3,8 @@
 $pattern = str_replace( "\n", '|', $argv[1] );
 $changed_files = explode( "\n", $argv[2] );
 
+var_dump( $pattern, $changed_files );
+
 $filtered_files = preg_grep("/^${pattern}$/m", $changed_files, PREG_GREP_INVERT );
 
 echo $filtered_files ? count( $filtered_files ) : 0;
