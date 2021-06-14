79639c19be2b9c3ba5f04f277c50878e595f6ef2
Pierre Gordon <pierregordon@protonmail.com>
2021-06-14 00:37:08 -0400

    Fix lint error

--- bin/determine-modified-files-count.php
+++ bin/determine-modified-files-count.php
@@ -15,7 +15,7 @@
  */
 
 $file_pattern    = sprintf( '/^%s$/m', $argv[1] );
-$modified_files   = explode( "\n", trim($argv[2] ) );
+$modified_files  = explode( "\n", trim( $argv[2] ) );
 $preg_grep_flags = isset( $argv[3] ) && trim( $argv[3] ) === '--invert' ? PREG_GREP_INVERT : 0;
 
 $filtered_files = preg_grep( $file_pattern, $modified_files, $preg_grep_flags );
