bed5c82f2dbee2c80eae7a5c6bd0fbbf4ec87efa
Weston Ruter <westonruter@google.com>
2021-06-19 23:55:48 -0700

    Add data-hero-candidate attr to .amp-wp-site-icon

--- templates/header-bar.php
+++ templates/header-bar.php
@@ -24,7 +24,7 @@
 		<a href="<?php echo esc_url( $this->get( 'home_url' ) ); ?>">
 			<?php $site_icon_url = $this->get( 'site_icon_url' ); ?>
 			<?php if ( $site_icon_url ) : ?>
-				<amp-img src="<?php echo esc_url( $site_icon_url ); ?>" width="32" height="32" class="amp-wp-site-icon"></amp-img>
+				<amp-img src="<?php echo esc_url( $site_icon_url ); ?>" width="32" height="32" class="amp-wp-site-icon" data-hero-candidate></amp-img>
 			<?php endif; ?>
 			<span class="amp-site-title">
 				<?php echo esc_html( wptexturize( $this->get( 'blog_name' ) ) ); ?>
