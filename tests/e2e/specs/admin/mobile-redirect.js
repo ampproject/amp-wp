a1dfcd097ffd4e7e7580bc81ee7531a7f4954137
Piotr Delawski <piotr.delawski@gmail.com>
2021-07-30 19:04:55 +0300

    Open "Other" panel before searching for mobile redirect toggle

--- tests/e2e/specs/admin/mobile-redirect.js
+++ tests/e2e/specs/admin/mobile-redirect.js
@@ -8,6 +8,7 @@ const { visitAdminPage } = require( '@wordpress/e2e-test-utils/build/visit-admin
  */
 const { completeWizard, cleanUpSettings } = require( '../../utils/onboarding-wizard-utils' );
 
+const panelToggleSelector = '#other-settings .components-panel__body-toggle';
 const toggleSelector = '.mobile-redirection .amp-setting-toggle input[type="checkbox"]';
 
 describe( 'Mobile redirect settings', () => {
@@ -19,6 +20,7 @@ describe( 'Mobile redirect settings', () => {
 		await completeWizard( { mode: 'reader', mobileRedirect: true } );
 		await visitAdminPage( 'admin.php', 'page=amp-options' );
 
+		await expect( page ).toClick( panelToggleSelector );
 		await page.waitForSelector( toggleSelector );
 		await expect( page ).toMatchElement( `${ toggleSelector }:checked` );
 	} );
@@ -27,6 +29,7 @@ describe( 'Mobile redirect settings', () => {
 		await completeWizard( { mode: 'reader', mobileRedirect: false } );
 		await visitAdminPage( 'admin.php', 'page=amp-options' );
 
+		await expect( page ).toClick( panelToggleSelector );
 		await page.waitForSelector( toggleSelector );
 		await expect( page ).not.toMatchElement( `${ toggleSelector }:checked` );
 	} );
