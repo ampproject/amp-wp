cb4ae4935c77aa6f52b711093955cc99bd9679a0
Dhaval Parekh <dmparekh007@gmail.com>
2021-08-03 18:30:45 +0530

    Update unit test cases

--- tests/php/src/Admin/SiteHealthTest.php
+++ tests/php/src/Admin/SiteHealthTest.php
@@ -110,6 +110,7 @@ class SiteHealthTest extends TestCase {
 	 * Test persistent_object_cache.
 	 *
 	 * @covers ::persistent_object_cache()
+	 * @covers ::check_available_cache()
 	 */
 	public function test_persistent_object_cache() {
 		$data = [
