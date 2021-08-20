a1f9b31e80bdee2b9f22ffcd30d4bb2035595d1b
Piotr Delawski <piotr.delawski@gmail.com>
2021-08-20 14:56:02 +0200

    Improve dev tools toggle description

--- assets/src/settings-page/developer-tools.js
+++ assets/src/settings-page/developer-tools.js
@@ -16,7 +16,10 @@ export function DeveloperTools() {
 		<section className="developer-tools">
 			<DevToolsToggle />
 			<p>
-				{ __( 'Only enabled for your user account. This is not a sitewide setting.', 'amp' ) }
+				{ __( 'Enable AMP developer tools to surface validation errors when editing posts and viewing the site.', 'amp' ) }
+			</p>
+			<p>
+				{ __( 'Only enabled for your user account. This is not a sitewide setting. This presumes you have some experience coding with HTML, CSS, JS, and PHP.', 'amp' ) }
 			</p>
 		</section>
 	);
