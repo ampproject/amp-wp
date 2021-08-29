6245411b85f3a43b60a725a44fe0fa5342d74e5e
Weston Ruter <westonruter@google.com>
2021-08-28 22:34:53 -0700

    Update comment

--- includes/class-amp-theme-support.php
+++ includes/class-amp-theme-support.php
@@ -900,7 +900,7 @@ class AMP_Theme_Support {
 
 		add_filter( 'wp_kses_allowed_html', [ __CLASS__, 'include_layout_in_wp_kses_allowed_html' ], 10 );
 		add_filter( 'get_header_image_tag', [ __CLASS__, 'amend_header_image_with_video_header' ], PHP_INT_MAX );
-		remove_action( 'comment_form', 'wp_comment_form_unfiltered_html_nonce' ); // @todo The AMP_Comments_Sanitizer should add data-ampdevmode since user would be logged-in.
+		remove_action( 'comment_form', 'wp_comment_form_unfiltered_html_nonce' ); // @todo The AMP_Comments_Sanitizer should add data-ampdevmode since user would be logged-in, or else the this script should be allowed whenever comment-reply is also allowed.
 		add_action(
 			'wp_print_footer_scripts',
 			static function() {
@@ -1089,7 +1089,6 @@ class AMP_Theme_Support {
 			}
 		}
 
-		// @todo See <https://github.com/ampproject/amp-wp/issues/2489>.
 		$state_id     = self::get_comment_form_state_id( get_the_ID() );
 		$text_binding = sprintf(
 			'%s.replyToName ? %s : %s',
