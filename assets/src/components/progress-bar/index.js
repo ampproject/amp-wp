79af03db5933f037dbf83c79ef465e1da7b5c39d
Piotr Delawski <piotr.delawski@gmail.com>
2021-09-15 16:20:44 +0200

    Add `ProgressBar` component

---
+++ assets/src/components/progress-bar/index.js
@@ -0,0 +1,41 @@
+/**
+ * External dependencies
+ */
+import PropTypes from 'prop-types';
+
+/**
+ * Internal dependencies
+ */
+import './style.scss';
+
+/**
+ * Progress bar component.
+ *
+ * @param {number} value The value of the progress bar indicator (between 0 and 100).
+ */
+export function ProgressBar( { value } ) {
+	const style = {
+		transform: `translateX(${ Math.max( value, 3 ) - 100 }%)`,
+	};
+
+	return (
+		<div
+			className="progress-bar"
+			role="progressbar"
+			aria-valuenow={ value }
+			aria-valuemin="0"
+			aria-valuemax="100"
+		>
+			<div className="progress-bar__track">
+				<div
+					className="progress-bar__indicator"
+					style={ style }
+				/>
+			</div>
+		</div>
+	);
+}
+
+ProgressBar.propTypes = {
+	value: PropTypes.number.isRequired,
+};
