718b950f0824fa2b077a357b434ce5c5c3f2d16d
Pierre Gordon <pierregordon@protonmail.com>
2021-09-15 21:00:43 -0400

    Suppress LGTM alert

--- assets/src/customizer/amp-customize-controls.js
+++ assets/src/customizer/amp-customize-controls.js
@@ -94,7 +94,7 @@ window.ampCustomizeControls = ( function( api, $ ) {
 			panelDescription.append( originalParagraph );
 		}
 
-		const ampDescription = $( '<p>' + component.data.l10n.rootPanelDescription + '</p>' ); // Contents have been sanitized with wp_kses_post().
+		const ampDescription = $( '<p>' + component.data.l10n.rootPanelDescription + '</p>' ); // Contents have been sanitized with wp_kses_post(). lgtm[js/html-constructed-from-input]
 		panelDescription.append( ampDescription );
 	};
 
