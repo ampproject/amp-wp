4bd7a32bcab582663ece3c2cbcaefefc1e51a1e7
Piotr Delawski <piotr.delawski@gmail.com>
2021-09-16 11:38:36 +0200

    Move `SiteScanContextProvider` to common components folder

--- assets/src/onboarding-wizard/index.js
+++ assets/src/onboarding-wizard/index.js
@@ -33,11 +33,11 @@ import { ReaderThemesContextProvider } from '../components/reader-themes-context
 import { ErrorBoundary } from '../components/error-boundary';
 import { ErrorContextProvider } from '../components/error-context-provider';
 import { ErrorScreen } from '../components/error-screen';
+import { SiteScanContextProvider } from '../components/site-scan-context-provider';
 import { UserContextProvider } from '../components/user-context-provider';
 import { PAGES } from './pages';
 import { SetupWizard } from './setup-wizard';
 import { NavigationContextProvider } from './components/navigation-context-provider';
-import { SiteScanContextProvider } from './components/site-scan-context-provider';
 import { TemplateModeOverrideContextProvider } from './components/template-mode-override-context-provider';
 
 const { ajaxurl: wpAjaxUrl } = global;
