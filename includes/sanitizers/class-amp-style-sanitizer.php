f99c56baac4fb1cb41d75eb3de4acf2baf16943a
Weston Ruter <westonruter@google.com>
2021-09-21 22:25:41 -0700

    Bump parsed stylesheet cache group

--- includes/sanitizers/class-amp-style-sanitizer.php
+++ includes/sanitizers/class-amp-style-sanitizer.php
@@ -1588,7 +1588,7 @@ class AMP_Style_Sanitizer extends AMP_Base_Sanitizer {
 		$parsed         = null;
 		$cache_key      = null;
 		$cached         = true;
-		$cache_group    = 'amp-parsed-stylesheet-v36'; // This should be bumped whenever the PHP-CSS-Parser is updated or parsed format is updated.
+		$cache_group    = 'amp-parsed-stylesheet-v37'; // This should be bumped whenever the PHP-CSS-Parser is updated or parsed format is updated.
 		$use_transients = $this->should_use_transient_caching();
 
 		// @todo If ValidationExemption::is_px_verified_for_node( $this->current_node ) then keep !important.
