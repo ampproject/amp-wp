9bc44c000a665d6b5d1cb8c2ecf70b5b8e2acf67
Piotr Delawski <piotr.delawski@gmail.com>
2021-09-23 14:50:16 +0200

    Update look and feel of scan complete Onboarding Wizard step

--- assets/src/onboarding-wizard/pages/site-scan/complete.js
+++ assets/src/onboarding-wizard/pages/site-scan/complete.js
@@ -2,30 +2,86 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { useContext } from '@wordpress/element';
+import { VisuallyHidden } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
 import { Selectable } from '../../../components/selectable';
+import { SiteScan } from '../../../components/site-scan-context-provider';
 import { IconLandscapeHillsCogs } from '../../../components/svg/landscape-hills-cogs';
+import { IconWebsitePaintBrush } from '../../../components/svg/website-paint-brush';
+import { IconLaptopPlug } from '../../../components/svg/laptop-plug';
 
 /**
  * Screen with site scan summary.
  */
 export function SiteScanComplete() {
+	const {
+		pluginIssues,
+		themeIssues,
+	} = useContext( SiteScan );
+
 	return (
 		<div className="site-scan">
-			<Selectable>
+			<Selectable className="site-scan__section">
 				<div className="site-scan__header">
 					<IconLandscapeHillsCogs />
-					<p className="site-scan__heading">
+					<h2 className="site-scan__heading">
 						{ __( 'Scan complete', 'amp' ) }
-					</p>
+					</h2>
 				</div>
 				<p>
 					{ __( 'Site scan found issues on your site. Proceed to the next step to follow recommendations for choosing a template mode.', 'amp' ) }
 				</p>
 			</Selectable>
+			{ themeIssues.length > 0 && (
+				<Selectable className="site-scan__section site-scan__section--compact">
+					<div className="site-scan__header">
+						<IconWebsitePaintBrush />
+						<p
+							className="site-scan__heading"
+							data-badge-content={ themeIssues.length }
+						>
+							{ __( 'Themes with AMP incompatibilty', 'amp' ) }
+							<VisuallyHidden as="span">
+								{ `(${ themeIssues.length })` }
+							</VisuallyHidden>
+						</p>
+					</div>
+					<div className="site-scan__content">
+						{ themeIssues.map( ( issue ) => (
+							<div key={ issue }>
+								{ issue }
+							</div>
+						) ) }
+					</div>
+				</Selectable>
+			) }
+			{ pluginIssues.length > 0 && (
+				<Selectable className="site-scan__section site-scan__section--compact">
+					<div className="site-scan__header">
+						<IconLaptopPlug />
+						<p
+							className="site-scan__heading"
+							data-badge-content={ pluginIssues.length }
+						>
+							{ __( 'Plugins with AMP incompatibility', 'amp' ) }
+							<VisuallyHidden as="span">
+								{ `(${ pluginIssues.length })` }
+							</VisuallyHidden>
+						</p>
+					</div>
+					<div className="site-scan__content">
+						{ pluginIssues.map( ( issue ) => (
+							<div key={ issue }>
+								{ issue }
+							</div>
+						) ) }
+					</div>
+				</Selectable>
+			) }
 		</div>
 	);
 }
