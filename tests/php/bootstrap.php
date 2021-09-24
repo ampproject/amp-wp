19996ef9495ef3da4189d4a565940e8265b043ad
Pierre Gordon <pierregordon@protonmail.com>
2021-09-24 17:12:33 -0400

    define `TESTS_PLUGIN_DIR` first

--- tests/php/bootstrap.php
+++ tests/php/bootstrap.php
@@ -5,10 +5,9 @@
 
 use Yoast\WPTestUtils\WPIntegration;
 
-require_once TESTS_PLUGIN_DIR . '/vendor/yoast/wp-test-utils/src/WPIntegration/bootstrap-functions.php';
-
 define( 'TESTS_PLUGIN_DIR', dirname( dirname( __DIR__ ) ) );
 
+require_once TESTS_PLUGIN_DIR . '/vendor/yoast/wp-test-utils/src/WPIntegration/bootstrap-functions.php';
 $_tests_dir = Yoast\WPTestUtils\WPIntegration\get_path_to_wp_test_dir();
 require_once $_tests_dir . 'includes/functions.php';
 
