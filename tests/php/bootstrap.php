ecac5f77008b1f3c83fd88ac2ca18511e7e3f1ca
Pierre Gordon <pierregordon@protonmail.com>
2021-09-24 17:07:30 -0400

    Require bootstrap file

--- tests/php/bootstrap.php
+++ tests/php/bootstrap.php
@@ -5,9 +5,11 @@
 
 use Yoast\WPTestUtils\WPIntegration;
 
+require_once TESTS_PLUGIN_DIR . '/vendor/yoast/wp-test-utils/src/WPIntegration/bootstrap-functions.php';
+
 define( 'TESTS_PLUGIN_DIR', dirname( dirname( __DIR__ ) ) );
 
-$_tests_dir = WPIntegration\get_path_to_wp_test_dir();
+$_tests_dir = Yoast\WPTestUtils\WPIntegration\get_path_to_wp_test_dir();
 require_once $_tests_dir . 'includes/functions.php';
 
 // Force plugins defined in a constant (supplied by phpunit.xml) to be active at runtime.
