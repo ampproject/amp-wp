5f56cc140c01957b80ebfa00dae08e716bd5b28d
Dhaval Parekh <dmparekh007@gmail.com>
2021-10-13 19:40:00 +0530

    Revert "Fix unit test case"
    This reverts commit e7f51ea22c2ce39f6d61fccd374e1bb336093655.

--- src/BackgroundTask/RecurringBackgroundTask.php
+++ src/BackgroundTask/RecurringBackgroundTask.php
@@ -7,8 +7,6 @@
 
 namespace AmpProject\AmpWP\BackgroundTask;
 
-use function wp_get_scheduled_event;
-
 /**
  * Abstract base class for using cron to execute a background task that runs on a schedule.
  *
