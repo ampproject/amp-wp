bd39a6e67806c52dbe96fe1b1953f5888ed41886
Piotr Delawski <piotr.delawski@gmail.com>
2021-10-27 14:17:40 +0200

    Rename test to match tested file and remove impossible test case

--- assets/src/common/helpers/test/get-plugin-slug-from-file.js
+++ assets/src/common/helpers/test/get-plugin-slug-from-file.js
@@ -4,10 +4,6 @@
 import { getPluginSlugFromFile } from '../get-plugin-slug-from-file';
 
 describe( 'getPluginSlugFromFile', () => {
-	it( 'should return an empty string if no path is provided', () => {
-		expect( getPluginSlugFromFile() ).toBe( '' );
-	} );
-
 	it( 'should return correct plugin slug', () => {
 		expect( getPluginSlugFromFile( 'foo' ) ).toBe( 'foo' );
 		expect( getPluginSlugFromFile( 'foo.php' ) ).toBe( 'foo' );
