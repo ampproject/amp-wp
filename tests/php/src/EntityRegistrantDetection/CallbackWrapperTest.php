5fc07322cdc047e42500e0a8c28a7bd23a511136
Dhaval Parekh <dmparekh007@gmail.com>
2021-11-23 19:08:06 +0530

    Fix unit test cases

--- tests/php/src/EntityRegistrantDetection/CallbackWrapperTest.php
+++ tests/php/src/EntityRegistrantDetection/CallbackWrapperTest.php
@@ -12,6 +12,7 @@ use AmpProject\AmpWP\EntityRegistrantDetection\CallbackWrapper;
 use AmpProject\AmpWP\EntityRegistrantDetection\EntityRegistrantDetectionManager;
 use AmpProject\AmpWP\Tests\DependencyInjectedTestCase;
 use AmpProject\AmpWP\Tests\Helpers\PrivateAccess;
+use WP_Block_Type_Registry;
 
 /**
  * @coversDefaultClass \AmpProject\AmpWP\EntityRegistrantDetection\CallbackWrapper
@@ -19,6 +20,34 @@ use AmpProject\AmpWP\Tests\Helpers\PrivateAccess;
 class CallbackWrapperTest extends DependencyInjectedTestCase {
 
 	use PrivateAccess;
+
+	/**
+	 * Post type slug.
+	 *
+	 * @var string
+	 */
+	const POST_TYPE = 'amp_test_post_type';
+
+	/**
+	 * Taxonomy slug.
+	 *
+	 * @var string
+	 */
+	const TAXONOMY = 'amp_test_taxonomy';
+
+	/**
+	 * Shortcode name.
+	 *
+	 * @var string
+	 */
+	const SHORTCODE = 'amp_test_shortcode';
+
+	/**
+	 * Block type.
+	 *
+	 * @var string
+	 */
+	const BLOCK_TYPE = 'amp/test-block';
 
 	/**
 	 * Instance of CallbackWrapper
@@ -81,14 +110,14 @@ class CallbackWrapperTest extends DependencyInjectedTestCase {
 	 */
 	public static function register_entities() {
 
-		register_post_type( 'amp_test_post_type', [] );
+		register_post_type( self::POST_TYPE, [] );
 
-		register_taxonomy( 'amp_test_taxonomy', [] );
+		register_taxonomy( self::TAXONOMY, [] );
 
-		add_shortcode( 'amp_test_shortcode', '__return_empty_string' );
+		add_shortcode( self::SHORTCODE, '__return_empty_string' );
 
 		register_block_type(
-			'amp/test-block',
+			self::BLOCK_TYPE,
 			[
 				'render_callback' => '__return_empty_string',
 			]
@@ -99,13 +128,15 @@ class CallbackWrapperTest extends DependencyInjectedTestCase {
 	 * Unregister entities.
 	 */
 	public static function unregister_entities() {
-		unregister_post_type( 'amp_test_post_type' );
+		unregister_post_type( self::POST_TYPE );
 
-		unregister_taxonomy( 'amp_test_taxonomy' );
+		unregister_taxonomy( self::TAXONOMY );
 
-		remove_shortcode( 'amp_test_shortcode' );
+		remove_shortcode( self::SHORTCODE );
 
-		unregister_block_type( 'amp/test-block' );
+		if ( WP_Block_Type_Registry::get_instance()->is_registered( self::BLOCK_TYPE ) ) {
+			unregister_block_type( self::BLOCK_TYPE );
+		}
 	}
 
 	/**
@@ -123,10 +154,10 @@ class CallbackWrapperTest extends DependencyInjectedTestCase {
 		$registered_entities = $this->get_private_property( $this->instance, 'registered_entities' );
 		$source              = $this->get_private_property( $this->instance, 'source' );
 
-		$this->assertContains( 'amp_test_post_type', $registered_entities['post_type'] );
-		$this->assertContains( 'amp_test_taxonomy', $registered_entities['taxonomy'] );
-		$this->assertContains( 'amp_test_shortcode', $registered_entities['shortcode'] );
-		$this->assertContains( 'amp/test-block', $registered_entities['block'] );
+		$this->assertContains( self::POST_TYPE, $registered_entities['post_type'] );
+		$this->assertContains( self::TAXONOMY, $registered_entities['taxonomy'] );
+		$this->assertContains( self::SHORTCODE, $registered_entities['shortcode'] );
+		$this->assertContains( self::BLOCK_TYPE, $registered_entities['block'] );
 
 		$this->assertArrayHasKey( 'function', $source );
 		$this->assertArrayHasKey( 'hook', $source );
