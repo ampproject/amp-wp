2f491fcd444d7b90d89490a56b88f2ef46041d68
Bartosz Gadomski <kontakt@bartoszgadomski.pl>
2021-11-25 10:00:27 +0100

    Initial POC code

--- includes/sanitizers/class-amp-navigation-block-sanitizer.php
+++ includes/sanitizers/class-amp-navigation-block-sanitizer.php
@@ -5,6 +5,10 @@
  * @package AMP
  */
 
+use AmpProject\Attribute;
+use AmpProject\Extension;
+use AmpProject\Layout;
+
 /**
  * Class AMP_Navigation_Block_Sanitizer
  *
@@ -37,29 +41,102 @@ class AMP_Navigation_Block_Sanitizer extends AMP_Base_Sanitizer {
 	 *
 	 * Steps:
 	 * - "fake" the "open" state by adding `is-menu-open has-modal-open` classes to `div.wp-block-navigation__responsive-container`
-	 * - add `on="tap:menuX.open"` to `button.wp-block-navigation__responsive-container-open` element
-	 * - add `on="tap:menu1.close"` to `button.wp-block-navigation__responsive-container-close` element
-	 * - wrap `div.wp-block-navigation__responsive-container` with `<amp-lightbox id="menuX" layout="nodisplay">...</amp-lightbox>`
+	 * - add `on="tap:navigationX.open"` to `button.wp-block-navigation__responsive-container-open` element
+	 * - add `on="tap:navigationX.close"` to `button.wp-block-navigation__responsive-container-close` element
+	 * - wrap `div.wp-block-navigation__responsive-container` with `<amp-lightbox id="navigationX" layout="nodisplay">...</amp-lightbox>`
 	 * - remove `data-micromodal-trigger` and `data-micromodal-close` attributes
 	 * - remove `aria-expanded` and `aria-modal` attributes
-	 * - duplicate `div.wp-block-navigation__responsive-container` (original one, without extra classes) outside the `amp-lightbox` wrapper
+	 * - duplicate `div.wp-block-navigation__responsive-container` (original one, without extra classes) outside the `amp-lightbox` wrapper and unwrap it from modal-related wrappers
 	 * - dequeue the `wp-block-navigation-view` script (see AMP_Navigation_Block_Sanitizer::add_buffering_hooks method)
 	 *
 	 * @see https://github.com/ampproject/amp-wp/issues/6319#issuecomment-978246093
 	 * @since 2.2
 	 */
 	public function sanitize() {
-		$class_query = sprintf( 'contains( concat( " ", normalize-space( @class ), " " ), " %s " )', self::$class );
-		$expr        = sprintf(
-			'//%1$s[ %2$s ]',
-			self::$tag,
-			$class_query
-		);
-		$nodes       = $this->dom->xpath->query( $expr );
+		$nodes = $this->dom->xpath->query( $this->generate_class_query( self::$class, self::$tag ) );
 
-		foreach ( $nodes as $node ) {
+		foreach ( $nodes as $index => $node ) {
 			/** @var DOMElement $node */
+
+			$container_node = $this->dom->xpath->query( $this->generate_class_query( 'wp-block-navigation__responsive-container' ), $node )->item( 0 );
+			if ( ! $container_node instanceof DOMElement ) {
+				continue;
+			}
+
+			$container_node->removeAttribute( Attribute::ID );
+
+			$cloned_container_node = $container_node->cloneNode( true );
+			$cloned_container_node->setAttribute( Attribute::CLASS_, trim( $cloned_container_node->getAttribute( Attribute::CLASS_ ) ) . ' is-menu-open has-modal-open' );
+
+			$element_id        = sprintf( 'navigation%d', $index + 1 );
+			$amp_lightbox_node = AMP_DOM_Utils::create_node(
+				$this->dom,
+				Extension::LIGHTBOX,
+				[
+					Attribute::ID     => $element_id,
+					Attribute::LAYOUT => Layout::NODISPLAY,
+				]
+			);
+
+			$amp_lightbox_node->appendChild( $cloned_container_node );
+			$node->appendChild( $amp_lightbox_node );
+
+			// Unwrap original container content out of "wp-block-navigation__responsive-close" and "wp-block-navigation__responsive-dialog" wrappers.
+			$content_node = $this->dom->xpath->query( $this->generate_class_query( 'wp-block-navigation__responsive-container-content' ), $container_node )->item( 0 );
+			$close_node   = $this->dom->xpath->query( $this->generate_class_query( 'wp-block-navigation__responsive-close' ), $container_node )->item( 0 );
+
+			if ( $content_node instanceof DOMElement && $close_node instanceof DOMElement ) {
+				$content_node->removeAttribute( Attribute::ID );
+
+				$container_node->appendChild( $content_node );
+				$container_node->removeChild( $close_node );
+			}
+
+			// Extend "open" and "close" buttons.
+			$open_button_node  = $this->dom->xpath->query( $this->generate_class_query( 'wp-block-navigation__responsive-container-open', 'button' ), $node )->item( 0 );
+			$close_button_node = $this->dom->xpath->query( $this->generate_class_query( 'wp-block-navigation__responsive-container-close', 'button' ), $node )->item( 0 );
+
+			if ( $open_button_node instanceof DOMElement && $close_button_node instanceof DOMElement ) {
+				$open_button_node->setAttribute( 'on', sprintf( 'tap:%s.open', $element_id ) );
+				$close_button_node->setAttribute( 'on', sprintf( 'tap:%s.close', $element_id ) );
+			}
+
+			// Remove unwanted attributes.
+			$unwanted_attributes = [
+				'aria-expanded',
+				'aria-modal',
+				'data-micromodal-trigger',
+				'data-micromodal-close',
+			];
+
+			foreach ( $unwanted_attributes as $unwanted_attribute ) {
+				$items = $this->dom->xpath->query( sprintf( '//*[@%s]', $unwanted_attribute ), $node );
+				foreach ( $items as $item ) {
+					if ( ! $item instanceof DOMElement ) {
+						continue;
+					}
+					$item->removeAttribute( $unwanted_attribute );
+				}
+			}
 		}
+	}
+
+	/**
+	 * Generate class query
+	 *
+	 * @since 2.2
+	 *
+	 * @param string $class_name Class name to use in query.
+	 * @param string $tag        Tag to use in query.
+	 *
+	 * @return string Class query.
+	 */
+	private function generate_class_query( $class_name, $tag = 'div' ) {
+		return sprintf(
+			'//%1$s[ contains( concat( " ", normalize-space( @class ), " " ), " %2$s " ) ]',
+			$tag,
+			$class_name
+		);
 	}
 
 	/**
