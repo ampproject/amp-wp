1d0776fe9fc80e54f4f9f2522b76dde59943a8dd
Weston Ruter <westonruter@google.com>
2021-12-03 07:52:51 -0800

    Improve translation strings
    Co-authored-by: Piotr Delawski <delawski@users.noreply.github.com>

--- assets/src/settings-page/sandboxing.js
+++ assets/src/settings-page/sandboxing.js
@@ -46,7 +46,7 @@ export function Sandboxing( { focusedSection } ) {
 			initialOpen={ 'sandboxing' === focusedSection }
 		>
 			<p>
-				{ __( 'Try out a more flexible AMP by generating pages that use AMP components without requiring AMP validity! By selecting a sandboxing level, you are indicating the minimum degree of sanitization. For example, if you selected the loose leve but have a page without any POST form and no custom scripts, it will still be served as valid AMP—the same as if you had selected the strict level.', 'amp' ) }
+				{ __( 'Try out a more flexible AMP by generating pages that use AMP components without requiring AMP validity! By selecting a sandboxing level, you are indicating the minimum degree of sanitization. For example, if you selected the loose level but have a page without any POST form and no custom scripts, it will still be served as valid AMP—the same as if you had selected the strict level.', 'amp' ) }
 			</p>
 
 			<CheckboxControl
@@ -72,10 +72,10 @@ export function Sandboxing( { focusedSection } ) {
 							} }
 						/>
 						<label htmlFor="sandboxing-level-1">
-							<strong>
-								{ __( 'Loose:', 'amp' ) }
-							</strong>
-							{ ' ' + __( 'Do not remove any AMP-invalid markup by default, including custom scripts. CSS processing is disabled.', 'amp' ) }
+							{ createInterpolateElement(
+								__( '<b>Loose:</b> Do not remove any AMP-invalid markup by default, including custom scripts. CSS processing is disabled.', 'amp' ),
+								{ b: <strong /> },
+							) }
 						</label>
 					</li>
 					<li>
@@ -88,10 +88,10 @@ export function Sandboxing( { focusedSection } ) {
 							} }
 						/>
 						<label htmlFor="sandboxing-level-2">
-							<strong>
-								{ __( 'Moderate:', 'amp' ) }
-							</strong>
-							{ ' ' + __( 'Remove anything invalid AMP except for POST forms, excessive CSS, and other PX-verified markup.', 'amp' ) }
+							{ createInterpolateElement(
+								__( '<b>Moderate:</b> Remove anything invalid AMP except for POST forms, excessive CSS, and other PX-verified markup.', 'amp' ),
+								{ b: <strong /> },
+							) }
 						</label>
 					</li>
 					<li>
@@ -104,10 +104,10 @@ export function Sandboxing( { focusedSection } ) {
 							} }
 						/>
 						<label htmlFor="sandboxing-level-3">
-							<strong>
-								{ __( 'Strict:', 'amp' ) }
-							</strong>
-							{ ' ' + __( 'Require valid AMP, removing all markup that causes validation errors (except for excessive CSS).', 'amp' ) }
+							{ createInterpolateElement(
+								__( '<b>Strict:</b> Require valid AMP, removing all markup that causes validation errors (except for excessive CSS).', 'amp' ),
+								{ b: <strong /> },
+							) }
 						</label>
 					</li>
 				</ol>
