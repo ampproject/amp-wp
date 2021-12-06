91033f4601a64a87e9d4bf65a3b6bc69260af918
Weston Ruter <westonruter@google.com>
2021-12-05 21:55:35 -0800

    Introduce garbage collection for validation data

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -62,6 +62,7 @@ namespace PHPSTORM_META {
 			'url_validation_cron'                => \AmpProject\AmpWP\Validation\URLValidationCron::class,
 			'url_validation_rest_controller'     => \AmpProject\AmpWP\Validation\URLValidationRESTController::class,
 			'validated_url_stylesheet_gc'        => \AmpProject\AmpWP\BackgroundTask\ValidatedUrlStylesheetDataGarbageCollection::class,
+			'validation_data_gc'                 => \AmpProject\AmpWP\BackgroundTask\ValidationDataGarbageCollection::class,
 			'validation.scannable_url_provider'  => \AmpProject\AmpWP\Validation\ScannableURLProvider::class,
 			'validation.url_validation_provider' => \AmpProject\AmpWP\Validation\URLValidationProvider::class,
 		] )
---
+++ src/BackgroundTask/ValidationDataGarbageCollection.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Class ValidationDataGarbageCollection.
+ *
+ * @package AmpProject\AmpWP
+ */
+
+namespace AmpProject\AmpWP\BackgroundTask;
+
+use AMP_Validated_URL_Post_Type;
+use AMP_Validation_Error_Taxonomy;
+
+/**
+ * Garbage collect validation data.
+ *
+ * This scheduled event removes validation data (amp_validated_url posts and amp_validation_error terms) which no longer
+ * have any need to be retained. The batch size of 100 follows the lead of `_wp_batch_update_comment_type()` in
+ * WordPress 5.5. Deleting data from posts older than 1 week follows the lead of `wp_delete_auto_drafts()`.
+ *
+ * @since 2.2
+ * @see _wp_batch_update_comment_type()
+ * @see wp_delete_auto_drafts()
+ *
+ * @link https://github.com/ampproject/amp-wp/issues/4779
+ * @package AmpProject\AmpWP
+ * @internal
+ */
+final class ValidationDataGarbageCollection extends RecurringBackgroundTask {
+
+	/**
+	 * Name of the event to schedule.
+	 *
+	 * @var string
+	 */
+	const EVENT_NAME = 'amp_validation_data_gc';
+
+	/**
+	 * Get the interval to use for the event.
+	 *
+	 * @return string An existing interval name.
+	 */
+	protected function get_interval() {
+		return self::DEFAULT_INTERVAL_DAILY;
+	}
+
+	/**
+	 * Get the event name.
+	 *
+	 * This is the "slug" of the event, not the display name.
+	 *
+	 * Note: the event name should be prefixed to prevent naming collisions.
+	 *
+	 * @return string Name of the event.
+	 */
+	protected function get_event_name() {
+		return self::EVENT_NAME;
+	}
+
+	/**
+	 * Process a single cron tick.
+	 *
+	 * @param mixed[] ...$args Unused callback arguments.
+	 * @return void
+	 */
+	public function process( ...$args ) { // phpcs:ignore VariableAnalysis.CodeAnalysis.VariableAnalysis.UnusedVariable
+		AMP_Validated_URL_Post_Type::garbage_collect_validated_urls( 100, '1 week ago' );
+
+		// Finally, delete validation errors which may now be empty.
+		AMP_Validation_Error_Taxonomy::delete_empty_terms();
+
+	}
+}
