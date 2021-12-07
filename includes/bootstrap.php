83106dac625908db869dca1d992fa895a78529bf
Weston Ruter <westonruter@google.com>
2021-12-07 09:32:13 -0800

    Revert "Add back-compat aliases for Attribute/Tag interfaces from amp-toolbox-php<0.9.0"
    This reverts commit b305db3d856a2fe08e3283cdbf2c990be467508e.

--- includes/bootstrap.php
+++ includes/bootstrap.php
@@ -21,17 +21,3 @@ foreach ( $files_to_include as $file_to_include => $function_to_check ) {
 		include $file_to_include;
 	}
 }
-
-// Add back-compat aliases for amp-toolbox-php interfaces which were renamed in v0.9.0.
-spl_autoload_register(
-	function ( $class_name ) {
-		switch ( $class_name ) {
-			case 'AmpProject\Attribute':
-				class_alias( \AmpProject\Html\Attribute::class, $class_name );
-				break;
-			case 'AmpProject\Tag':
-				class_alias( \AmpProject\Html\Tag::class, $class_name );
-				break;
-		}
-	}
-);
