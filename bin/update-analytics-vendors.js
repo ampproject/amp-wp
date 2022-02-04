9d6d30797da1930d6fbf49103491f9921851d53b
Rutvik Savsani <savsanirutviklivetolearn@gmail.com>
2022-02-04 14:51:08 +0530

    Add CLI command to generate analytics vendor file in JSON format.

---
+++ bin/update-analytics-vendors.js
@@ -0,0 +1,143 @@
+/**
+ * External dependencies
+ */
+const { once } = require( 'events' );
+const fs = require( 'fs' );
+const readline = require( 'readline' );
+const stream = require( 'stream' );
+const axios = require( 'axios' );
+
+/**
+ * File path of the analytics vendors list.
+ */
+const ANALYTICS_VENDORS_FILE = 'data/analytics-vendors-list.json';
+
+class UpdateAnalyticsVendors {
+	/**
+	 * Construct method.
+	 */
+	constructor() {
+		this.vendors = {};
+		this.content = '';
+		this.init();
+	}
+
+	/**
+	 * Initialize the script.
+	 */
+	async init() {
+		await this.fetchData();
+		await this.extractVendors();
+		this.saveData();
+	}
+
+	/**
+	 * Fetch analytics vendors.
+	 *
+	 * @return {Promise<void>}
+	 */
+	async fetchData() {
+		// Remote URL for analytics vendors.
+		const url =
+			'https://api.github.com/repos/ampproject/amphtml/contents/extensions/amp-analytics/analytics-vendors-list.md';
+		const response = await axios.get( url );
+
+		// Getting encoded content of the file.
+		const encodedContent = response.data.content;
+
+		// Convert base64 to utf8.
+		const decodedContent = Buffer.from( encodedContent, 'base64' );
+		const bufferStream = new stream.PassThrough();
+		bufferStream.end( decodedContent );
+
+		// Create a readline interface to read the file line by line.
+		const fileContent = readline.createInterface( {
+			input: bufferStream,
+			crlfDelay: Infinity,
+		} );
+
+		this.content = fileContent;
+	}
+
+	/**
+	 * Extract vendors from recieved data.
+	 *
+	 * @return {Promise<void>}
+	 */
+	async extractVendors() {
+		let commentFlag = false;
+		let vendorSlug = '';
+		let vendorTitle = '';
+
+		try {
+			this.content.on( 'line', ( line ) => {
+				// Check if the line is in a comment.
+				if ( line.trim() === '<!--' ) {
+					commentFlag = true;
+				}
+
+				if ( line.trim() === '-->' ) {
+					commentFlag = false;
+				}
+
+				// Check if line contains vendor title.
+				if ( line.indexOf( '###' ) === 0 && ! commentFlag ) {
+					vendorTitle = line.replace( '###', '' ).trim();
+				}
+
+				// Check if line contains vendor slug.
+				if ( line.indexOf( 'Type attribute value:' ) === 0 && ! commentFlag ) {
+					vendorSlug = line.replace( 'Type attribute value:', '' ).trim();
+				}
+
+				// Populate vendors object.
+				if ( vendorSlug && vendorTitle ) {
+					const vendorSlugs = vendorSlug.replace( /`/g, '' ).trim().split( ',' );
+
+					// Loop through multiple vendor slugs with same titles and append extra information to title.
+					vendorSlugs.forEach( ( slug ) => {
+						if ( vendorSlugs.indexOf( slug ) === 0 ) {
+							this.vendors[ vendorSlugs[ 0 ] ] = vendorTitle.replace( /(<([^>]+)>)/gi, '' ).trim();
+							return;
+						}
+
+						slug = slug.trim();
+						/**
+						 * Get extra information from vendor slug if the title is same.
+						 * remove common prefixes from the vendor slug and convert to sentence case.
+						 */
+						const vendorInfo = slug.replace( /.*_/g, '' ) // Remove common prefixes.
+							.replace( /([A-Z]+)/g, ' $1' ) // Add space before each capital letter.
+							.toLowerCase();	// Convert to lowercase.
+
+						// Strip HTML tags from title if any and append extra information.
+						this.vendors[ slug ] = vendorTitle.replace( /(<([^>]+)>)/gi, '' ).concat( ' (', vendorInfo, ')' );
+					} );
+
+					vendorSlug = '';
+					vendorTitle = '';
+				}
+			} );
+
+			await once( this.content, 'close' );
+		} catch ( error ) {
+			throw error;
+		}
+	}
+
+	/**
+	 * Save data to JSON file.
+	 */
+	saveData() {
+		if ( this.vendors ) {
+			// Save vendors to JSON file.
+			fs.writeFileSync(
+				ANALYTICS_VENDORS_FILE,
+				JSON.stringify( this.vendors, null, 4 ),
+			);
+		}
+	}
+}
+
+// eslint-disable-next-line no-new
+new UpdateAnalyticsVendors();
