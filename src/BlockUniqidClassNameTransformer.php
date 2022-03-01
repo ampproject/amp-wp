fcb72448ba6f0df6e13ff5780addd755e1c7b860
Piotr Delawski <piotr.delawski@gmail.com>
2022-03-01 15:12:24 +0100

    Use `wp_unique_id`-based block class names instead of `uniqid`
    This transforms hash-based block class names into a more predictable class names based on `wp_unique_id()`. This should allow for re-enabling automatic parsed CSS transient caching.

---
+++ src/BlockUniqidClassNameTransformer.php
@@ -0,0 +1,188 @@
+<?php
+/**
+ * Class BlockUniqidClassNameTransformer.
+ *
+ * @package AmpProject\AmpWP
+ */
+
+namespace AmpProject\AmpWP;
+
+use AmpProject\AmpWP\Infrastructure\Conditional;
+use AmpProject\AmpWP\Infrastructure\Delayed;
+use AmpProject\AmpWP\Infrastructure\Registerable;
+use AmpProject\AmpWP\Infrastructure\Service;
+
+/**
+ * Transform uniqid-based IDs into cacheable IDs based on wp_unique_id.
+ *
+ * Random strings based on `uniqid()` should not be used to generate CSS class
+ * names and used in inline styles so that parsed CSS transient caching is not
+ * automatically disabled.
+ *
+ * Instead, `wp_unique_id()` should be used so that the IDs in the class names
+ * are more predictable and the CSS transient caching works as expected.
+ *
+ * @link https://github.com/ampproject/amp-wp/pull/6925
+ *
+ * @package AmpProject\AmpWP
+ * @since 2.2.2
+ * @internal
+ */
+final class BlockUniqidClassNameTransformer implements Conditional, Service, Registerable, Delayed {
+
+	/**
+	 * Prefixes of class names that should be transformed.
+	 *
+	 * @var string[]
+	 */
+	const CLASS_NAME_PREFIXES = [
+		'wp-container-',
+		'wp-duotone-',
+	];
+
+	/**
+	 * The mapping between uniqid- and wp_unique_id-based class names.
+	 *
+	 * @var array
+	 */
+	private $class_name_mapping = [];
+
+	/**
+	 * Get the action to use for registering the service.
+	 *
+	 * @return string Registration action to use.
+	 */
+	public static function get_registration_action() {
+		return 'wp';
+	}
+
+	/**
+	 * Check whether the Gutenberg plugin is present.
+	 *
+	 * @return bool
+	 */
+	public static function has_gutenberg_plugin() {
+		return defined( 'GUTENBERG_VERSION' );
+	}
+
+	/**
+	 * Check whether the conditional object is currently needed.
+	 *
+	 * @return bool Whether the conditional object is needed.
+	 */
+	public static function is_needed() {
+		// At least WP 5.0.3 is needed since `wp_unique_id` has been introduced there.
+		if ( version_compare( get_bloginfo( 'version' ), '5.0.3', '<' ) ) {
+			return false;
+		}
+
+		return (
+			(
+				self::has_gutenberg_plugin()
+				||
+				version_compare( get_bloginfo( 'version' ), '5.9', '>=' )
+			)
+			&&
+			amp_is_request()
+		);
+	}
+
+	/**
+	 * Return a name of an action hook used to register block inline styles.
+	 *
+	 * For block based themes, inline block styles are loaded in head.
+	 * For classic themes, styles are loaded in the body because the `wp_head`
+	 * action (and `wp_enqueue_scripts`) happens before the `render_block`.
+	 *
+	 * @return string Action hook name.
+	 */
+	private static function get_block_inline_style_registration_hook_name() {
+		if ( function_exists( 'wp_is_block_theme' ) && wp_is_block_theme() ) {
+			return 'wp_enqueue_scripts';
+		}
+
+		return 'wp_footer';
+	}
+
+	/**
+	 * Register the service with the system.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		add_filter(
+			'render_block',
+			[ $this, 'transform_class_names_in_block_content' ],
+			PHP_INT_MAX,
+			1
+		);
+		add_filter(
+			self::get_block_inline_style_registration_hook_name(),
+			[ $this, 'transform_class_names_in_inline_styles' ],
+			defined( 'PHP_INT_MIN' ) ? PHP_INT_MIN : ~PHP_INT_MAX // phpcs:ignore PHPCompatibility.Constants.NewConstants.php_int_minFound
+		);
+	}
+
+	/**
+	 * Get regular expression pattern for matching uniqid-based class name.
+	 *
+	 * Note that `uniqid()` returns a string consisting of 13 hex characters.
+	 *
+	 * @return string Regular expression pattern.
+	 */
+	private static function get_class_name_regexp_pattern() {
+		$combined_block_prefixes = implode( '|', self::CLASS_NAME_PREFIXES );
+
+		return "/($combined_block_prefixes)[a-f0-9]{13}/";
+	}
+
+	/**
+	 * Transform uniqid-based class names in block content.
+	 *
+	 * @param string $block_content Block content.
+	 *
+	 * @return string
+	 */
+	public function transform_class_names_in_block_content( $block_content ) {
+		preg_match( self::get_class_name_regexp_pattern(), $block_content, $matches );
+
+		if ( empty( $matches ) ) {
+			return $block_content;
+		}
+
+		list( $class_name, $class_name_prefix ) = $matches;
+
+		$new_class_name                          = wp_unique_id( $class_name_prefix );
+		$this->class_name_mapping[ $class_name ] = $new_class_name;
+
+		$new_block_content = str_replace( $class_name, $new_class_name, $block_content );
+
+		return $new_block_content;
+	}
+
+	/**
+	 * Transform uniqid-based class names in inline styles.
+	 *
+	 * @return void
+	 */
+	public function transform_class_names_in_inline_styles() {
+		global $wp_styles;
+
+		$mapped_handles = array_intersect(
+			$wp_styles->queue,
+			array_keys( $this->class_name_mapping )
+		);
+
+		foreach ( $mapped_handles as $handle ) {
+			if ( empty( $wp_styles->registered[ $handle ]->extra['after'] ) ) {
+				continue;
+			}
+
+			$new_class_name = $this->class_name_mapping[ $handle ];
+
+			foreach ( $wp_styles->registered[ $handle ]->extra['after'] as &$inline_styles ) {
+				$inline_styles = str_replace( $handle, $new_class_name, $inline_styles );
+			}
+		}
+	}
+}
---
+++ tests/php/src/BlockUniqidClassNameTransformerTest.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace AmpProject\AmpWP\Tests;
+
+use AMP_Options_Manager;
+use AMP_Theme_Support;
+use AmpProject\AmpWP\BlockUniqidClassNameTransformer;
+use AmpProject\AmpWP\Infrastructure\Conditional;
+use AmpProject\AmpWP\Infrastructure\Delayed;
+use AmpProject\AmpWP\Infrastructure\Registerable;
+use AmpProject\AmpWP\Infrastructure\Service;
+use AmpProject\AmpWP\Option;
+use AmpProject\AmpWP\Tests\Helpers\MarkupComparison;
+
+/** @coversDefaultClass \AmpProject\AmpWP\BlockUniqidClassNameTransformer */
+final class BlockUniqidClassNameTransformerTest extends TestCase {
+
+	use MarkupComparison;
+
+	/** @var BlockUniqidClassNameTransformer */
+	private $instance;
+
+	/**
+	 * Setup.
+	 *
+	 * @inheritdoc
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		if ( version_compare( get_bloginfo( 'version' ), '5.0.3', '<' ) ) {
+			$this->markTestSkipped( 'Block uniqid class name transformer requires WordPress 5.0.3.' );
+		}
+
+		$this->instance = new BlockUniqidClassNameTransformer();
+	}
+
+	public function test_it_can_be_initialized() {
+		$this->assertInstanceOf( Conditional::class, $this->instance );
+		$this->assertInstanceOf( Delayed::class, $this->instance );
+		$this->assertInstanceOf( Registerable::class, $this->instance );
+		$this->assertInstanceOf( Service::class, $this->instance );
+	}
+
+	/** @covers ::get_registration_action() */
+	public function test_get_registration_action() {
+		$this->assertEquals( 'wp', BlockUniqidClassNameTransformer::get_registration_action() );
+	}
+
+	/**
+	 * @covers ::is_needed()
+	 * @covers ::has_gutenberg_plugin()
+	 */
+	public function test_is_needed() {
+		if (
+			! defined( 'GUTENBERG_VERSION' )
+			&&
+			version_compare( get_bloginfo( 'version' ), '5.9', '<' )
+		) {
+			$this->assertFalse( BlockUniqidClassNameTransformer::is_needed() );
+		} else {
+			AMP_Options_Manager::update_option( Option::THEME_SUPPORT, AMP_Theme_Support::READER_MODE_SLUG );
+			$post_id = self::factory()->post->create();
+
+			$this->go_to( get_permalink( $post_id ) );
+			$this->assertFalse( amp_is_request() );
+			$this->assertFalse( BlockUniqidClassNameTransformer::is_needed() );
+
+			$this->go_to( amp_get_permalink( $post_id ) );
+			$this->assertTrue( amp_is_request() );
+			$this->assertTrue( BlockUniqidClassNameTransformer::is_needed() );
+		}
+	}
+
+	/**
+	 * @covers ::register()
+	 * @covers ::get_block_inline_style_registration_hook_name()
+	 */
+	public function test_register() {
+		$this->instance->register();
+		$this->assertEquals( PHP_INT_MAX, has_filter( 'render_block', [ $this->instance, 'transform_class_names_in_block_content' ] ) );
+
+		$expected_hook_name = function_exists( 'wp_is_block_theme' ) && wp_is_block_theme() ? 'wp_enqueue_scripts' : 'wp_footer';
+		$this->assertEquals(
+			defined( 'PHP_INT_MIN' ) ? PHP_INT_MIN : ~PHP_INT_MAX, // phpcs:ignore PHPCompatibility.Constants.NewConstants.php_int_minFound
+			has_filter( $expected_hook_name, [ $this->instance, 'transform_class_names_in_inline_styles' ] )
+		);
+	}
+
+	/** @return array */
+	public function get_block_data() {
+		return [
+			'transform_duotone_class_name'           => [
+				'block_content'          => '<div class="wp-duotone-621e12fb51e3a wp-block-cover"></div>',
+				'expected_block_content' => '<div class="wp-duotone-1 wp-block-cover"></div>',
+				'style_handle'           => 'wp-duotone-621e12fb51e3a',
+				'style_content'          => ".wp-duotone-621e12fb51e3a > .wp-block-cover__image-background, .wp-duotone-621e12fb51e3a > .wp-block-cover__video-background{filter:url(\'#wp-duotone-621e12fb51e3a\') !important;}",
+				'expected_style_content' => ".wp-duotone-1 > .wp-block-cover__image-background, .wp-duotone-1 > .wp-block-cover__video-background{filter:url(\'#wp-duotone-1\') !important;}",
+			],
+			'transform_container_class_name'         => [
+				'block_content'          => '<div class="wp-container-621e133aaf0e2 wp-block-group is-style-default has-black-background-color has-background" style="padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"></div>',
+				'expected_block_content' => '<div class="wp-container-2 wp-block-group is-style-default has-black-background-color has-background" style="padding-top:20px;padding-right:20px;padding-bottom:20px;padding-left:20px"></div>',
+				'style_handle'           => null,
+				'style_content'          => null,
+				'expected_style_content' => null,
+			],
+			'ignore_class_names_without_hash'        => [
+				'block_content'          => '<div class="wp-duotone-test wp-block-cover"></div>',
+				'expected_block_content' => '<div class="wp-duotone-test wp-block-cover"></div>',
+				'style_handle'           => null,
+				'style_content'          => null,
+				'expected_style_content' => null,
+			],
+			'ignore_already_transformed_class_names' => [
+				'block_content'          => '<div class="wp-duotone-1 wp-block-cover"></div>',
+				'expected_block_content' => '<div class="wp-duotone-1 wp-block-cover"></div>',
+				'style_handle'           => null,
+				'style_content'          => null,
+				'expected_style_content' => null,
+			],
+		];
+	}
+
+	/**
+	 * @covers ::transform_class_names_in_block_content()
+	 * @covers ::transform_class_names_in_inline_styles()
+	 * @covers ::get_class_name_regexp_pattern()
+	 *
+	 * @dataProvider get_block_data
+	 *
+	 * @param string      $block_content
+	 * @param string      $expected_block_content
+	 * @param string|null $style_handle
+	 * @param string|null $style_content
+	 * @param string|null $expected_style_content
+	 */
+	public function test_transform_class_names( $block_content, $expected_block_content, $style_handle, $style_content, $expected_style_content ) {
+		$transformed_block_content = $this->instance->transform_class_names_in_block_content( $block_content );
+		$this->assertEqualMarkup( $expected_block_content, $transformed_block_content );
+
+		if ( ! empty( $style_handle ) && ! empty( $style_content ) && ! empty( $expected_style_content ) ) {
+			global $wp_styles;
+
+			wp_register_style( $style_handle, '', [], '1' );
+			wp_add_inline_style( $style_handle, $style_content );
+			wp_enqueue_style( $style_handle );
+
+			$this->assertTrue( wp_style_is( $style_handle ) );
+
+			$this->instance->transform_class_names_in_inline_styles();
+
+			$this->assertEquals(
+				$expected_style_content,
+				$wp_styles->registered[ $style_handle ]->extra['after'][0]
+			);
+		}
+	}
+}
