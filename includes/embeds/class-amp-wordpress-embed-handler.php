73b43050ecbc4ed20ab0f06de114429f385db5fb
Weston Ruter <westonruter@google.com>
2022-03-02 17:13:26 -0800

    Update namespace for Attribute and Tag interfaces

--- includes/embeds/class-amp-wordpress-embed-handler.php
+++ includes/embeds/class-amp-wordpress-embed-handler.php
@@ -5,12 +5,12 @@
  * @package AMP
  */
 
-use AmpProject\Attribute;
 use AmpProject\Extension;
 use AmpProject\Dom\Document;
 use AmpProject\Dom\Element;
+use AmpProject\Html\Attribute;
+use AmpProject\Html\Tag;
 use AmpProject\Layout;
-use AmpProject\Tag;
 
 /**
  * Class AMP_WordPress_Embed_Handler
