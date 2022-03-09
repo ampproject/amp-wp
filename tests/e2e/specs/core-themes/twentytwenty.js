9d94d2d42f83c9c144485b53fb5e5545f66305cd
Piotr Delawski <piotr.delawski@gmail.com>
2022-03-09 12:10:03 +0100

    Test Twenty Twenty search modal on mobile breakpoint

--- tests/e2e/specs/core-themes/twentytwenty.js
+++ tests/e2e/specs/core-themes/twentytwenty.js
@@ -18,11 +18,7 @@ describe( 'Twenty Twenty theme on AMP', () => {
 		await setTemplateMode( 'standard' );
 	} );
 
-	describe( 'main navigation on mobile', () => {
-		beforeAll( async () => {
-			await assignMenuToLocation( 'mobile' );
-		} );
-
+	describe( 'for mobile breakpoint', () => {
 		beforeEach( async () => {
 			await setBrowserViewport( MOBILE_BROWSER_VIEWPORT_SIZE );
 			await page.goto( createURL( '/' ) );
@@ -33,59 +29,59 @@ describe( 'Twenty Twenty theme on AMP', () => {
 			await setBrowserViewport( DEFAULT_BROWSER_VIEWPORT_SIZE );
 		} );
 
-		it( 'should be initially hidden', async () => {
-			await expect( page ).toMatchElement( '.mobile-nav-toggle[aria-expanded=false]' );
-			await expect( page ).toMatchElement( '.menu-modal', { visible: false } );
+		describe( 'main navigation', () => {
+			beforeAll( async () => {
+				await assignMenuToLocation( 'mobile' );
+			} );
+
+			it( 'should be initially hidden', async () => {
+				await expect( page ).toMatchElement( '.mobile-nav-toggle[aria-expanded=false]' );
+				await expect( page ).toMatchElement( '.menu-modal', { visible: false } );
+			} );
+
+			it( 'should be togglable', async () => {
+				await expect( page ).toClick( '.mobile-nav-toggle' );
+				await expect( page ).toMatchElement( '.mobile-nav-toggle[aria-expanded=true]' );
+				await expect( page ).toMatchElement( '.menu-modal', { visible: true } );
+
+				await expect( page ).toClick( '.mobile-nav-toggle' );
+				await expect( page ).toMatchElement( '.mobile-nav-toggle[aria-expanded=false]' );
+				await expect( page ).toMatchElement( '.menu-modal', { visible: false } );
+			} );
+
+			it( 'should have a togglable submenu', async () => {
+				await expect( page ).toClick( '.mobile-nav-toggle' );
+
+				const menuItemWithSubmenu = await page.$( '.menu-modal .menu-item-has-children' );
+
+				expect( menuItemWithSubmenu ).not.toBeNull();
+
+				await expect( menuItemWithSubmenu ).toMatchElement( '.sub-menu-toggle[aria-expanded=false]' );
+				await expect( menuItemWithSubmenu ).toMatchElement( '.sub-menu', { visible: false } );
+
+				await expect( menuItemWithSubmenu ).toClick( '.sub-menu-toggle' );
+				await expect( menuItemWithSubmenu ).toMatchElement( '.sub-menu-toggle[aria-expanded=true]' );
+				await expect( menuItemWithSubmenu ).toMatchElement( '.sub-menu', { visible: true } );
+
+				await expect( menuItemWithSubmenu ).toClick( '.sub-menu-toggle' );
+				await expect( menuItemWithSubmenu ).toMatchElement( '.sub-menu-toggle[aria-expanded=false]' );
+				await expect( menuItemWithSubmenu ).toMatchElement( '.sub-menu', { visible: false } );
+			} );
 		} );
 
-		it( 'should be togglable', async () => {
-			await expect( page ).toClick( '.mobile-nav-toggle' );
-			await expect( page ).toMatchElement( '.mobile-nav-toggle[aria-expanded=true]' );
-			await expect( page ).toMatchElement( '.menu-modal', { visible: true } );
+		describe( 'search modal', () => {
+			it( 'should be togglable', async () => {
+				await expect( page ).toMatchElement( '.mobile-search-toggle[aria-expanded=false]' );
+				await expect( page ).toMatchElement( '.search-modal', { visible: false } );
 
-			await expect( page ).toClick( '.mobile-nav-toggle' );
-			await expect( page ).toMatchElement( '.mobile-nav-toggle[aria-expanded=false]' );
-			await expect( page ).toMatchElement( '.menu-modal', { visible: false } );
-		} );
+				await expect( page ).toClick( '.mobile-search-toggle' );
+				await expect( page ).toMatchElement( '.search-toggle[aria-expanded=true]' );
+				await expect( page ).toMatchElement( '.search-modal', { visible: true } );
 
-		it( 'should have a togglable submenu', async () => {
-			await expect( page ).toClick( '.mobile-nav-toggle' );
-
-			const menuItemWithSubmenu = await page.$( '.menu-modal .menu-item-has-children' );
-
-			expect( menuItemWithSubmenu ).not.toBeNull();
-
-			await expect( menuItemWithSubmenu ).toMatchElement( '.sub-menu-toggle[aria-expanded=false]' );
-			await expect( menuItemWithSubmenu ).toMatchElement( '.sub-menu', { visible: false } );
-
-			await expect( menuItemWithSubmenu ).toClick( '.sub-menu-toggle' );
-			await expect( menuItemWithSubmenu ).toMatchElement( '.sub-menu-toggle[aria-expanded=true]' );
-			await expect( menuItemWithSubmenu ).toMatchElement( '.sub-menu', { visible: true } );
-
-			await expect( menuItemWithSubmenu ).toClick( '.sub-menu-toggle' );
-			await expect( menuItemWithSubmenu ).toMatchElement( '.sub-menu-toggle[aria-expanded=false]' );
-			await expect( menuItemWithSubmenu ).toMatchElement( '.sub-menu', { visible: false } );
-		} );
-	} );
-
-	describe( 'search modal on desktop', () => {
-		beforeEach( async () => {
-			await setBrowserViewport( DEFAULT_BROWSER_VIEWPORT_SIZE );
-			await page.goto( createURL( '/' ) );
-			await page.waitForSelector( '#site-header' );
-		} );
-
-		it( 'should be togglable', async () => {
-			await expect( page ).toMatchElement( '.desktop-search-toggle[aria-expanded=false]' );
-			await expect( page ).toMatchElement( '.search-modal', { visible: false } );
-
-			await expect( page ).toClick( '.desktop-search-toggle' );
-			await expect( page ).toMatchElement( '.search-toggle[aria-expanded=true]' );
-			await expect( page ).toMatchElement( '.search-modal', { visible: true } );
-
-			await expect( page ).toMatchElement( '.search-modal .close-search-toggle[aria-expanded=true]' );
-			await expect( page ).toClick( '.search-modal .close-search-toggle' );
-			await expect( page ).toMatchElement( '.search-modal', { visible: false } );
+				await expect( page ).toMatchElement( '.search-modal .close-search-toggle[aria-expanded=true]' );
+				await expect( page ).toClick( '.search-modal .close-search-toggle' );
+				await expect( page ).toMatchElement( '.search-modal', { visible: false } );
+			} );
 		} );
 	} );
 } );
