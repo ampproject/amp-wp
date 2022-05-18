ed38a452937a101a574c8f3366cdd13bebde03f7
Weston Ruter <westonruter@google.com>
2022-05-18 14:03:31 -0700

    Expand on description for native HTML image tag toggle

--- assets/src/settings-page/toggle-use-native-img-tag.js
+++ assets/src/settings-page/toggle-use-native-img-tag.js
@@ -35,7 +35,7 @@ export function ToggleUseNativeImgTag() {
 			<p>
 				{
 					createInterpolateElement(
-						__( 'By enabling this, The AMP plugin will use native <ImgTag /> instead of <AMPImgTag />.', 'amp' ),
+						__( 'The native <ImgTag /> HTML can now be used instead of the AMP-specific <AMPImgTag /> tag. AMP no longer requires the latter because lazy-loading is now a feature of the Web platform. Using native images can further improve page performance, although not all AMP components have been updated to work with them (see <a>example</a>). If you have CSS that is specifically targeting <AMPImgTag /> tags, you will need to update them to target <ImgTag /> instead (which can be done by default since such tags are automatically rewritten in CSS selectors when conversions are done).', 'amp' ),
 						{
 							ImgTag: (
 								<code>
@@ -46,6 +46,10 @@ export function ToggleUseNativeImgTag() {
 								<code>
 									{ '<amp-img>' }
 								</code>
+							),
+							a: (
+								// eslint-disable-next-line jsx-a11y/anchor-has-content -- Anchor has content defined in the translated string.
+								<a href="https://github.com/ampproject/amphtml/pull/38028" target="_blank" rel="noreferrer noopener" />
 							),
 						},
 					)
