c8a5ed4a863ba7ca924c2d27cf2dda1d3f029155
thelovekesh <lovekesh.kumar@rtcamp.com>
2022-10-10 10:54:21 +0530

    Add GA4 scripts sanitizer

---
+++ includes/sanitizers/class-amp-gtag-script-sanitizer.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * GA4 Script Sanitizer.
+ *
+ * - This sanitizer will facilitate using GA4 while waiting on an AMP implementation.
+ * - This sanitizer will be only used in Moderate or Loose sandboxing level
+ *
+ * @since 2.3.1
+ * @package AMP
+ */
+
+use AmpProject\AmpWP\ValidationExemption;
+
+/**
+ * Class AMP_GTag_Script_Sanitizer
+ *
+ * @since 2.3.1
+ * @internal
+ */
+class AMP_GTag_Script_Sanitizer extends AMP_Base_Sanitizer {
+	/**
+	 * Option key for enabling sandboxing.
+	 *
+	 * @var string
+	 */
+	const OPTION_ENABLED = 'sandboxing_enabled';
+
+	/**
+	 * Option key for sandboxing level.
+	 *
+	 * @todo Move this to the Options interface once no longer experimental.
+	 * @var string
+	 */
+	const OPTION_LEVEL = 'sandboxing_level';
+
+	/**
+	 * Sanitize the AMP response for GA4 scripts.
+	 *
+	 * @since 2.3.1
+	 */
+	public function sanitize() {
+		if ( ! AMP_Options_Manager::get_option( self::OPTION_ENABLED ) ) {
+			return;
+		}
+
+		$sandboxing_level = AMP_Options_Manager::get_option( self::OPTION_LEVEL );
+
+		if ( 1 !== $sandboxing_level && 2 !== $sandboxing_level ) {
+			return;
+		}
+
+		/**
+		 * GTag Script looks like this:
+		 *
+		 * <script async src="https://www.googletagmanager.com/gtag/js?id=xxxxxx"></script>
+		 * <script>
+		 *   window.dataLayer = window.dataLayer || [];
+		 *   function gtag(){dataLayer.push(arguments);}
+		 *   gtag('js', new Date());
+		 *
+		 *   gtag('config', 'xxxxxx');
+		 * </script>
+		 */
+		$scripts = $this->dom->xpath->query( '//script[ contains( @src, "https://www.googletagmanager.com/gtag/js" ) or contains( text(), "gtag(\'config\'" ) ]' );
+
+		if ( $scripts instanceof DOMNodeList ) {
+			foreach ( $scripts as $script ) {
+				ValidationExemption::mark_node_as_px_verified( $script );
+			}
+		}
+	}
+}
