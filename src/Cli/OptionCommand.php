3eca69da50d833224befa9706b228e7633fb223f
thelovekesh <lovekesh.kumar@rtcamp.com>
2022-12-20 13:49:04 +0530

    Update get, list and update commands to use REST API

--- src/Cli/OptionCommand.php
+++ src/Cli/OptionCommand.php
@@ -24,6 +24,32 @@ use AmpProject\AmpWP\Infrastructure\CliCommand;
 final class OptionCommand implements Service, CliCommand {
 
 	/**
+	 * Options endpoint.
+	 *
+	 * @var string
+	 */
+	const OPTIONS_ENDPOINT = '/amp/v1/options';
+
+	/**
+	 * Allowed options to be managed via the CLI.
+	 *
+	 * @var string[]
+	 */
+	const ALLOWED_OPTIONS = [
+		Option::READER_THEME,
+		Option::THEME_SUPPORT,
+		Option::MOBILE_REDIRECT,
+		Option::SUPPRESSED_PLUGINS,
+	];
+
+	/**
+	 * Reader themes key.
+	 *
+	 * @var string
+	 */
+	const READER_THEMES = 'reader_themes';
+
+	/**
 	 * ReaderThemes instance.
 	 *
 	 * @var ReaderThemes
@@ -64,7 +90,27 @@ final class OptionCommand implements Service, CliCommand {
 	 * @param array $assoc_args Associative array of associative arguments.
 	 */
 	public function get( $args, $assoc_args ) {
-		// TODO: Implement get command using REST API.
+		list( $option_name ) = $args;
+
+		$this->check_user();
+
+		$options = $this->get_options();
+
+		if ( ! isset( $options[ $option_name ] ) ) {
+			/* translators: %s: option name */
+			WP_CLI::error( sprintf( __( 'Option %s does not exist.', 'amp' ), $option_name ), false );
+			WP_CLI::line( WP_CLI::colorize( '%y' . __( 'Try using `wp amp option list` to see all available options.', 'amp' ) . '%n' ) );
+			WP_CLI::halt( 1 );
+		}
+
+		WP_CLI\Utils\format_items(
+			'table',
+			[
+				'option_name'  => $option_name,
+				'option_value' => $options[ $option_name ],
+			],
+			[ 'option_name', 'option_value' ]
+		);
 	}
 
 	/**
@@ -76,7 +122,31 @@ final class OptionCommand implements Service, CliCommand {
 	 * @alias set
 	 */
 	public function update( $args, $assoc_args ) {
-		// TODO: Implement update command using REST API.
+		list( $option_name, $option_value ) = $args;
+
+		$help_message = __( 'Try using `wp amp option list` to see all available options.', 'amp' );
+
+		// Check if option is allowed to be managed via CLI.
+		if ( ! in_array( $option_name, self::ALLOWED_OPTIONS, true ) ) {
+			/* translators: %s: option name */
+			WP_CLI::error( sprintf( __( 'Option %s is not allowed to be managed via CLI.', 'amp' ), $option_name ), false );
+			WP_CLI::line( WP_CLI::colorize( '%y' . $help_message . '%n' ) );
+			WP_CLI::halt( 1 );
+		}
+
+		$this->check_user();
+
+		$options = $this->get_options();
+
+		if ( ! isset( $options[ $option_name ] ) ) {
+			/* translators: %s: option name */
+			WP_CLI::error( sprintf( __( 'Option %s does not exist.', 'amp' ), $option_name ), false );
+			WP_CLI::line( WP_CLI::colorize( '%y' . $help_message . '%n' ) );
+			WP_CLI::halt( 1 );
+		}
+
+		// Update the option.
+		$this->update_option( $option_name, $option_value );
 	}
 
 	/**
@@ -85,7 +155,93 @@ final class OptionCommand implements Service, CliCommand {
 	 * @subcommand list
 	 */
 	public function list_() {
-		// TODO: Implement list command using REST API.
+		$this->check_user();
+
+		$options = $this->get_options();
+
+		// Add reader themes to the options.
+		$options[ self::READER_THEMES ] = wp_list_pluck( $this->reader_themes->get_themes(), 'slug' );
+
+		WP_CLI::line( WP_CLI::colorize( '%y' . __( 'Available options:', 'amp' ) . '%n' ) );
+		WP_CLI\Utils\format_items(
+			'table',
+			array_map(
+				static function ( $option_name, $option_value ) {
+					return compact( 'option_name', 'option_value' );
+				},
+				array_keys( $options ),
+				$options
+			),
+			[ 'option_name', 'option_value' ]
+		);
+
+		WP_CLI::line( '' ); // Add a line break for readability.
+		WP_CLI::line( WP_CLI::colorize( '%y' . __( 'Allowed options to be managed via CLI:', 'amp' ) . '%n' ) );
+		WP_CLI\Utils\format_items(
+			'table',
+			array_map(
+				static function ( $option_name ) {
+					return compact( 'option_name' );
+				},
+				self::ALLOWED_OPTIONS
+			),
+			[ 'option_name' ]
+		);
+	}
+
+	/**
+	 * Get the options.
+	 *
+	 * @return array Options.
+	 */
+	private function get_options() {
+		$response = $this->do_request( 'GET', self::OPTIONS_ENDPOINT, [] );
+
+		if ( $response->as_error() ) {
+			/* translators: %s: option name */
+			WP_CLI::error( sprintf( __( 'Could not get options: %s', 'amp' ), $response->as_error()->get_error_message() ) );
+		}
+
+		return $response->get_data();
+	}
+
+	/**
+	 * Update an option.
+	 *
+	 * @param string $option_name  Option name.
+	 * @param string $option_value Option value.
+	 */
+	private function update_option( $option_name, $option_value ) {
+		$response = $this->do_request(
+			'POST',
+			self::OPTIONS_ENDPOINT,
+			[
+				$option_name => $option_value,
+			]
+		);
+
+		if ( $response->as_error() ) {
+			/* translators: %s: option name */
+			WP_CLI::error( sprintf( __( 'Could not update option: %s', 'amp' ), $response->as_error()->get_error_message() ) );
+		}
+
+			/* translators: %s: option name */
+		WP_CLI::success( sprintf( __( 'Option %s updated.', 'amp' ), $option_name ) );
+	}
+
+	/**
+	 * Check if the user is set up to use the REST API.
+	 *
+	 * @return bool Whether the user is set up to use the REST API.
+	 */
+	private function check_user() {
+		if ( ! current_user_can( 'manage_options' ) ) {
+			WP_CLI::error( __( 'Sorry, you are not allowed to manage options for this site.', 'amp' ), false );
+			WP_CLI::line( WP_CLI::colorize( '%y' . __( 'Try using --user=<id|login|email> to set the user context or set it in wp-cli.yml.', 'amp' ) . '%n' ) );
+			WP_CLI::halt( 1 );
+		}
+
+		return true;
 	}
 
 	/**
