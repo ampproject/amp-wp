73acef1051dddf8bec75f25fddf4e7c67b99b35c
thelovekesh <lovekesh.kumar@rtcamp.com>
2023-09-02 01:52:54 +0530

    Disable `jest/unbound-method` since this is not a typescript codebase

--- .eslintrc.js
+++ .eslintrc.js
@@ -115,6 +115,9 @@ module.exports = {
 				'jest/prefer-inline-snapshots': 'off',
 				'jest/prefer-snapshot-hint': 'off',
 				'jest/no-untyped-mock-factory': 'off',
+				// Disabling because we are not using typescript.
+				// @see <https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/unbound-method.md>
+				'jest/unbound-method': 'off',
 			},
 		},
 		{
@@ -134,6 +137,7 @@ module.exports = {
 				'jest/no-hooks': 'off',
 				'jest/prefer-expect-assertions': 'off',
 				'jest/prefer-inline-snapshots': 'off',
+				'jest/unbound-method': 'off',
 			},
 		},
 		{
