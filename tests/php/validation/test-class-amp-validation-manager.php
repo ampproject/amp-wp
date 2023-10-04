4f1c89313791c1f75567f01c5ab1fe4f993e2599
thelovekesh <lovekesh.kumar@rtcamp.com>
2023-10-04 18:06:29 +0530

    Fix version compare with alpha version

--- tests/php/validation/test-class-amp-validation-manager.php
+++ tests/php/validation/test-class-amp-validation-manager.php
@@ -1907,7 +1907,7 @@ class Test_AMP_Validation_Manager extends DependencyInjectedTestCase {
 
 		// This will be called after `do_shortcode` in WP 6.4 and later.
 		// @see <https://core.trac.wordpress.org/ticket/58853>.
-		if ( version_compare( get_bloginfo( 'version' ), '6.3', '<=' ) ) {
+		if ( version_compare( get_bloginfo( 'version' ), '6.4-alpha', '<' ) ) {
 			$sources[] = [
 				'type'     => 'core',
 				'name'     => 'wp-includes',
