7e64d34386e6bdf00e9b680371e02fec59b55ee7
thelovekesh <lovekesh.kumar@rtcamp.com>
2024-06-07 00:21:14 +0530

    Remove redundant svg attribute

--- assets/src/icons/index.js
+++ assets/src/icons/index.js
@@ -116,7 +116,6 @@ export const AMPFilledIcon = ({
 		viewBox={viewBox}
 		version="1.1"
 		xmlns="http://www.w3.org/2000/svg"
-		xmlns:xlink="http://www.w3.org/1999/xlink"
 	>
 		<G id="AMP-Icon" fill="#82878c">
 			<Path
