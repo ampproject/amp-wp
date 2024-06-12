9a0049cf2d13e25fd706c4682ec464ca850dea2c
thelovekesh <lovekesh.kumar@rtcamp.com>
2024-06-12 17:53:55 +0530

    Update axe disabled rules

--- tests/e2e/config/bootstrap.js
+++ tests/e2e/config/bootstrap.js
@@ -202,6 +202,8 @@ async function runAxeTestsForBlockEditor() {
 		disabledRules: [
 			'aria-allowed-role',
 			'aria-valid-attr-value',
+			'aria-required-children',
+			'aria-required-parent',
 			'button-name',
 			'color-contrast',
 			'dlitem',
@@ -210,10 +212,6 @@ async function runAxeTestsForBlockEditor() {
 			'link-name',
 			'listitem',
 			'region',
-			// Disabled due to this rule being erroneously recorded as a violation after
-			// downgrading package-lock.json to v1 (see https://github.com/ampproject/amp-wp/pull/6618).
-			// This can be reverted once node v16 becomes LTS.
-			'nested-interactive',
 		],
 		exclude: [
 			// Ignores elements created by metaboxes.
