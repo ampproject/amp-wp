92d8d3b664e087c385eb1621e9e78e5052af00bd
Pascal Birchler <pascalb@google.com>
2025-09-15 11:57:21 +0200

    Try using PHPUnit 10

--- tests/php/src/TestCase.php
+++ tests/php/src/TestCase.php
@@ -71,4 +71,12 @@ abstract class TestCase extends PolyfilledTestCase {
 			$this->assertContains( $expected_value, $actual_superset );
 		}
 	}
+
+	/**
+	 * Temporary workaround to allow the tests to run on PHPUnit 10.
+	 *
+	 * @link https://core.trac.wordpress.org/ticket/59486
+	 */
+	public function expectDeprecated(): void {
+	}
 }
