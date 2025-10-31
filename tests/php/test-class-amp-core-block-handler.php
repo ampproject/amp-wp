dce8436db3f074de8ef1648b82e99a7c5afeecb0
Pascal Birchler <pascalb@google.com>
2025-10-31 12:18:52 +0100

    Tests: Adjust category block assertion

--- tests/php/test-class-amp-core-block-handler.php
+++ tests/php/test-class-amp-core-block-handler.php
@@ -99,7 +99,10 @@ class Test_AMP_Core_Block_Handler extends TestCase {
 		$handler->unregister_embed();
 		$rendered = do_blocks( $categories_block );
 		$this->assertStringContainsString( '<select', $rendered );
-		$this->assertStringContainsString( 'onchange', $rendered );
+
+		// Markup changed in WP 6.9, see https://github.com/WordPress/gutenberg/commit/29292491520762d4f7e23d280948f255c1404ce3.
+		// $this->assertStringContainsString( 'onchange', $rendered );
+
 		$this->assertStringNotContainsString( 'on="change', $rendered );
 		if ( WP_Block_Type_Registry::get_instance()->is_registered( 'core/archives' ) ) {
 			$rendered = do_blocks( $archives_block );
